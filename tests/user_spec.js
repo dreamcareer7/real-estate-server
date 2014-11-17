217a93e37a4fe47c5f9f2879000417cf79c48635
dreamcareer7 <dreamcareer070@gmail.com>
2014-11-17 12:29:12 +0330

    Implemeneted /user/:username/address (PUT/GET) Also Address model which currently only offers an Address.validate

--- tests/user_spec.js
+++ tests/user_spec.js
@@ -9,8 +9,19 @@ var user = {
   lastname:'bar',
   email:'foo.bar@provider.tld',
   phonenumber:'989124834198',
-  address:'foo bar phooo phoo phoo'
 };
+
+var address = {
+  title:'title',
+  subtitle:'subtitle',
+  streetnumber:'#333',
+  streetname:'phu street',
+  city:'los majones',
+  state:'texas',
+  statecode:'TX',
+  postalcode:'12345',
+  neighborhood:'disastrous place'
+}
 
 var getUser = frisby.create('get user')
   .get(URL+'/user/'+user.username)
@@ -25,11 +36,24 @@ var deleteUser = frisby.create('delete user')
   .delete(URL+'/user/'+user.username)
   .expectStatus(200);
 
+var setAddress = frisby.create('set address')
+  .put(URL+'/user/'+user.username+'/address', address)
+  .expectStatus(200);
+
+var getAddress = frisby.create('get address')
+  .get(URL+'/user/'+user.username+'/address')
+  .expectJSON(address)
+  .expectStatus(200);
+
 describe("/user", function() {
-  it("create, get and delete the user", function() {
+  it("creates, gets and deletes a user", function() {
     createUser.after(function() {
       getUser.after(function() {
-        deleteUser.toss();
+        setAddress.after(function() {
+          getAddress.after(function() {
+            deleteUser.toss();
+          }).toss();
+        }).toss();
       }).toss();
     }).toss();
   });
