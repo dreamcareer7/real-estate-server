f7c7141532257889b5819cede8deca9cef4cc3f9
dreamcareer7 <dreamcareer070@gmail.com>
2014-11-18 12:17:39 +0330

    API has been changed: use user ID for now

--- tests/user_spec.js
+++ tests/user_spec.js
@@ -23,42 +23,46 @@ var address = {
   neighborhood:'disastrous place'
 }
 
-var getUser = frisby.create('get user')
-  .get(URL+'/user/'+user.username)
-  .expectJSON(user)
-  .expectStatus(200);
-
 var createUser = frisby.create('create user')
   .post(URL+'/user', user)
-  .expectStatus(201);
+  .expectStatus(201)
+  .afterJSON(function(json) {
+    user.id = json.id;
 
-var updatedUser = JSON.parse(JSON.stringify(user));
-updatedUser.firstname = 'updated first name';
-var updateUser = frisby.create('update user')
-  .put(URL+'/user/'+user.username, updatedUser)
-  .expectStatus(200);
+    getUser = frisby.create('get user')
+      .get(URL+'/user/'+user.id)
+      .expectJSON(user)
+      .expectStatus(200);
 
-var getUpdatedUser = frisby.create('get updated user')
-  .get(URL+'/user/'+user.username)
-  .expectJSON(updatedUser)
-  .expectStatus(200);
+    updatedUser = JSON.parse(JSON.stringify(user));
+    updatedUser.firstname = 'updated first name';
+    updateUser = frisby.create('update user')
+      .put(URL+'/user/'+user.id, updatedUser)
+      .expectStatus(200);
 
-var deleteUser = frisby.create('delete user')
-  .delete(URL+'/user/'+user.username)
-  .expectStatus(200);
+    getUpdatedUser = frisby.create('get updated user')
+      .get(URL+'/user/'+user.id)
+      .expectJSON(updatedUser)
+      .expectStatus(200);
 
-var setAddress = frisby.create('set address')
-  .put(URL+'/user/'+user.username+'/address', address)
-  .expectStatus(200);
+    deleteUser = frisby.create('delete user')
+      .delete(URL+'/user/'+user.id)
+      .expectStatus(200);
 
-var deleteAddress = frisby.create('delete address')
-  .delete(URL+'/user/'+user.username+'/address')
-  .expectStatus(200);
+    setAddress = frisby.create('set address')
+      .put(URL+'/user/'+user.id+'/address', address)
+      .expectStatus(200);
 
-var getAddress = frisby.create('get address')
-  .get(URL+'/user/'+user.username+'/address')
-  .expectJSON(address)
-  .expectStatus(200);
+    deleteAddress = frisby.create('delete address')
+      .delete(URL+'/user/'+user.id+'/address')
+      .expectStatus(200);
+
+    getAddress = frisby.create('get address')
+      .get(URL+'/user/'+user.id+'/address')
+      .expectJSON(address)
+      .expectStatus(200);
+  });
+
 
 describe("/user", function() {
   it("creates, gets and deletes a user", function() {
