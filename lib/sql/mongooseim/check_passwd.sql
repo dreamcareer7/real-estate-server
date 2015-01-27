5a5abdf0551097940e856fbdd209c92af4c3b82c
dreamcareer7 <dreamcareer070@gmail.com>
2015-01-27 23:40:41 +0330

    we must consider access tokens only when validating mongooseim requests on internal endpoints to authentication.

--- lib/sql/mongooseim/check_passwd.sql
+++ lib/sql/mongooseim/check_passwd.sql
@@ -2,4 +2,5 @@ SELECT *
 FROM tokens
 WHERE user_id = $1
   AND token = $2
+  AND type = 'access'
   AND expire_date > NOW()
--- lib/sql/mongooseim/get_passwd.sql
+++ lib/sql/mongooseim/get_passwd.sql
@@ -1,5 +1,6 @@
 SELECT token
 FROM tokens
 WHERE user_id = $1
+  AND type = 'access'
 ORDER BY expire_date DESC
 LIMIT 1
