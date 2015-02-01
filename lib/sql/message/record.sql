6a4430f0bc5588c939239743a67877f41c3502ac
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-02 01:28:59 +0330

    message and message_room endpoints initial implementation. fixed couple of glitches in message_room endpoints

---
+++ lib/sql/message/record.sql
@@ -0,0 +1,19 @@
+INSERT INTO messages(
+    message_room,
+    message_type,
+    comment,
+    image_url,
+    document_url,
+    video_url,
+    object,
+    author
+)
+VALUES($1,
+       $2,
+       $3,
+       $4,
+       $5,
+       $6,
+       $7,
+       $8)
+RETURNING *, 'message' AS type
