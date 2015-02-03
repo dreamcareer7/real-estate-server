40f76f8a86f89ee57959cc71df1e86fdcdcfd21f
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-04 00:10:34 +0330

    providing message_count on message_room collection.

--- lib/sql/message_room/get.sql
+++ lib/sql/message_room/get.sql
@@ -14,6 +14,7 @@ SELECT 'message_room' AS type,
        (SELECT COUNT(*) FROM messages WHERE message_room = message_rooms.id AND image_url <> NULL)::INT AS image_count,
        (SELECT COUNT(*) FROM messages WHERE message_room = message_rooms.id AND document_url <> NULL)::INT AS document_count,
        (SELECT COUNT(*) FROM messages WHERE message_room = message_rooms.id AND video_url <> NULL)::INT AS video_count,
+       (SELECT COUNT(*) FROM messages WHERE message_room = message_rooms.id)::INT AS message_count,
        EXTRACT(EPOCH FROM created_at)::INT AS created_at,
        EXTRACT(EPOCH FROM updated_at)::INT AS updated_at
 FROM message_rooms
