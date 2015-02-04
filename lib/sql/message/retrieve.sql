3fb09478185b2b2ba0c126b388ec6694bfd5047b
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-04 20:17:28 +0330

    fixed GET /message_rooms/:id/messages response.

--- lib/sql/message/retrieve.sql
+++ lib/sql/message/retrieve.sql
@@ -1,7 +1,9 @@
 SELECT
     (COUNT(*) OVER())::INT AS full_count,
+    'message' AS type,
     *,
-    'message' AS type
+    EXTRACT(EPOCH FROM created_at)::INT AS created_at,
+    EXTRACT(EPOCH FROM updated_at)::INT AS updated_at
 FROM messages
 WHERE message_room = $1
 AND created_at > to_timestamp($2)
