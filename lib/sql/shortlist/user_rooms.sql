f23edce7419776b1626055952252f221c21ca86d
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-09 02:14:25 +0330

    added filtering by message_room type on GET /shortlists/:id/users/:id/message_room through message_room_type in request body.

--- lib/sql/shortlist/user_rooms.sql
+++ lib/sql/shortlist/user_rooms.sql
@@ -3,4 +3,10 @@ FROM message_rooms_users
 INNER JOIN message_rooms
     ON message_rooms_users.message_room = message_rooms.id
 WHERE "user" = $1
-    AND message_rooms.shortlist = $2;
+    AND message_rooms.shortlist = $2
+    AND CASE $3 WHEN 'Shortlist' THEN message_rooms.message_room_type = 'Shortlist'
+                WHEN 'OneToOneMessaging' THEN message_rooms.message_room_type = 'OneToOneMessaging'
+                WHEN 'Comment' THEN message_rooms.message_room_type = 'Comment'
+                WHEN 'GroupMessaging' THEN message_rooms.message_room_type = 'GroupMessaging'
+                ELSE TRUE
+        END
