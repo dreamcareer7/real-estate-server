284c6b116d3fdae5448059766f092f660cb16c99
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-12 16:39:39 +0330

    IOS-322 #close #comment Count endpoints for feed/favorites is now fixed at GET /shortlists/:id/users/:id/recs/feed/count, GET /shortlists/:id/users/:id/recs/favorites/count

---
+++ lib/sql/recommendation/feed_count.sql
@@ -0,0 +1,7 @@
+SELECT
+    (COUNT(*))::INT AS count,
+    'count' AS type
+FROM recommendations
+WHERE recommendations.referring_user = $1
+  AND recommendations.referred_shortlist = $2
+  AND recommendations.status = 'Unacknowledged'
