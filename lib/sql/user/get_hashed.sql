dce7fc1fd8582107727382824e329b8e2da25fac
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-12 19:47:21 +0330

    IOS-323 #close #comment endpoint for patching user data set at PATCH /users/:id. CAUTION: It accepts the whole user object with all the following parameters present first_name, last_name, email, phone_number, password. We currently have the idential endpoint set at PUT /users/:id. This endpoint should be fixed to accept any combination of parameters if this is more convenient.

---
+++ lib/sql/user/get_hashed.sql
@@ -0,0 +1,3 @@
+SELECT password
+FROM users
+WHERE id = $1
--- lib/sql/user/update.sql
+++ lib/sql/user/update.sql
@@ -2,5 +2,6 @@ UPDATE users
 SET first_name = $1,
     last_name = $2,
     email = $3,
-    phone_number = $4
-WHERE id = $5
+    phone_number = $4,
+    password = $5
+WHERE id = $6
