a7c143e7faba88db64b2af29e3886ff2667aa450
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-14 03:29:21 +0330

    IOS-329 #close #comment Pagination on message collection done. parameters: {ascending: boolean, from: timestamp, limit: int, offset: int}

--- lib/sql/message/retrieve.sql
+++ lib/sql/message/retrieve.sql
@@ -3,6 +3,10 @@ SELECT
     id
 FROM messages
 WHERE message_room = $1
-AND created_at > to_timestamp($2)
-LIMIT $3
-OFFSET $4
+AND CASE WHEN $3 THEN created_at > to_timestamp($2)
+                 ELSE created_at < to_timestamp($2)
+    END
+ORDER BY CASE WHEN $3 THEN created_at END,
+         CASE WHEN NOT $3 THEN created_at END DESC
+LIMIT $4
+OFFSET $5
