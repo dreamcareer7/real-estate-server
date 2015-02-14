05218ad25880696ee1d47d9d6ac6db200e5367c6
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-14 19:44:09 +0330

    fixed forward pagination on messages endpoint.

--- lib/sql/message/retrieve.sql
+++ lib/sql/message/retrieve.sql
@@ -3,10 +3,8 @@ SELECT
     id
 FROM messages
 WHERE message_room = $1
-AND CASE WHEN $3 THEN created_at > to_timestamp($2)
-                 ELSE created_at < to_timestamp($2)
+AND CASE WHEN $3 THEN created_at >= to_timestamp($2)
+                 ELSE created_at < NOW()
     END
 ORDER BY CASE WHEN $3 THEN created_at END,
          CASE WHEN NOT $3 THEN created_at END DESC
-LIMIT $4
-OFFSET $5
