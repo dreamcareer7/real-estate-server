b0190bc3f5dd4a5f1c0ada2dd62a20a964063931
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-27 20:27:41 +0330

    fixed favorites sql.

--- lib/sql/recommendation/favorites.sql
+++ lib/sql/recommendation/favorites.sql
@@ -17,19 +17,19 @@ WHERE recommendations.referring_user = $1
   AND recommendations.referred_shortlist = $2
   AND favs.favorited = TRUE
 AND CASE
-    WHEN $3 = 'Since_C' THEN created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
-    WHEN $3 = 'Max_C' THEN created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
-    WHEN $3 = 'Since_U' THEN updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
-    WHEN $3 = 'Max_U' THEN updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
+    WHEN $3 = 'Since_C' THEN recommendations.created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
+    WHEN $3 = 'Max_C' THEN recommendations.created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
+    WHEN $3 = 'Since_U' THEN recommendations.updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
+    WHEN $3 = 'Max_U' THEN recommendations.updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 SECOND'
     ELSE TRUE
     END
 ORDER BY
     CASE $3
-        WHEN 'Since_C' THEN created_at
-        WHEN 'Since_U' THEN updated_at
+        WHEN 'Since_C' THEN recommendations.created_at
+        WHEN 'Since_U' THEN recommendations.updated_at
     END,
     CASE $3
-        WHEN 'Max_C' THEN created_at
-        WHEN 'Max_U' THEN updated_at
+        WHEN 'Max_C' THEN recommendations.created_at
+        WHEN 'Max_U' THEN recommendations.updated_at
     END DESC
 LIMIT $5;
