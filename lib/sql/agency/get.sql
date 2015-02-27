a1db82346f8720b3dd2251c920f4f4b004fba112
dreamcareer7 <dreamcareer070@gmail.com>
2015-02-27 21:52:37 +0330

    fixed address created_at and updated_at conversion to double on Agency model.

--- lib/sql/agency/get.sql
+++ lib/sql/agency/get.sql
@@ -4,6 +4,8 @@ SELECT 'agency' AS TYPE,
   (SELECT ROW_TO_JSON(_)
    FROM
      (SELECT addresses.*,
+             EXTRACT(EPOCH FROM addresses.created_at) AS created_at,
+             EXTRACT(EPOCH FROM addresses.updated_at) AS updated_at,
              'address' AS TYPE) AS _) AS address
 FROM agencies
 LEFT JOIN addresses ON agencies.address_id = addresses.id
