f178032f3feb4f60e067ac4d7ad6ce161db5898a
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-07 02:24:11 +0430

    add missing recommendations fix upon user joining a shortlist.

--- lib/sql/shortlist/add_missing.sql
+++ lib/sql/shortlist/add_missing.sql
@@ -5,13 +5,16 @@ INSERT INTO recommendations(source,
                             object,
                             message_room,
                             recommendation_type,
+                            matrix_unique_id,
                             referring_user)
     SELECT source,
            source_url,
            referring_savedsearch,
            referred_shortlist,
-           object, message_room,
+           object,
+           message_room,
            recommendation_type,
+           matrix_unique_id,
            $1 AS referring_user
     FROM recommendations
     WHERE referred_shortlist = $2
@@ -21,4 +24,5 @@ INSERT INTO recommendations(source,
              referred_shortlist,
              object,
              message_room,
+             matrix_unique_id,
              recommendation_type;
