8c798ff53393ff5bd345f4009d73ca93566048ff
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-13 05:58:42 +0430

    fixing Alert's property_subtypes array elements having double-quotes around them. (this is caused by the way postgresql likes to keep it's internal enumerables.) -r2

--- lib/sql/alert/get.sql
+++ lib/sql/alert/get.sql
@@ -3,6 +3,7 @@ SELECT
     *,
     ST_AsGeoJSON(location) AS location,
     EXTRACT(EPOCH FROM created_at) AS created_at,
-    EXTRACT(EPOCH FROM updated_at) AS updated_at
+    EXTRACT(EPOCH FROM updated_at) AS updated_at,
+    property_subtypes::text[] AS property_subtypes
 FROM alerts
 WHERE id = $1
