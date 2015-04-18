286765ead8b54a099b3408634cf6477223335338
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-19 02:48:08 +0430

    Fixed an issue with sql_matching query on Alert.matchingListings().

--- lib/sql/alert/matching.sql
+++ lib/sql/alert/matching.sql
@@ -3,8 +3,8 @@ FROM listings
 INNER JOIN properties
 ON listings.property_id = properties.id
 WHERE
-    listing.price >= $1 AND
-    listing.price <= $2 AND
+    listings.price >= $1 AND
+    listings.price <= $2 AND
     properties.square_meters >= $3 AND
     properties.square_meters <= $4 AND
     properties.bedroom_count >= $5 AND
