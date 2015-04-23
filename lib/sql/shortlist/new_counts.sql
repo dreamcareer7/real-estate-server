a0c0a4ace274d4db9f05eb9b2ef118064ed98d69
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-23 20:16:27 +0430

    Added new_comments_count to Shortlists collection.

---
+++ lib/sql/shortlist/new_counts.sql
@@ -0,0 +1,9 @@
+SELECT (SELECT COUNT(*)
+FROM messages_ack
+WHERE message_room_id IN
+    (SELECT id
+     FROM message_rooms
+     WHERE shortlist = $1
+    )
+    AND user_id = $2
+    AND read = false) AS new_comments_count;
