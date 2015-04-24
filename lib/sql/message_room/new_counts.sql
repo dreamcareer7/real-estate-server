cce9d81d7974b928131d81df755c3ae5f5852f6b
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-25 03:30:28 +0430

    Changing all count properties to singular.

--- lib/sql/message_room/new_counts.sql
+++ lib/sql/message_room/new_counts.sql
@@ -1,8 +1,8 @@
 SELECT
-    SUM(CASE WHEN video_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_videos_count,
-    SUM(CASE WHEN document_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_documents_count,
-    SUM(CASE WHEN image_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_images_count,
-    SUM(CASE WHEN TRUE THEN 1 ELSE 0 END)::INT as new_comments_count
+    SUM(CASE WHEN video_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_video_count,
+    SUM(CASE WHEN document_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_document_count,
+    SUM(CASE WHEN image_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_image_count,
+    SUM(CASE WHEN TRUE THEN 1 ELSE 0 END)::INT as new_comment_count
 FROM messages
 INNER JOIN messages_ack
 ON messages.id = messages_ack.message_id AND
