009f8b895bfc1155af4a85e58672bf652b57afb8
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-26 00:13:48 +0430

    Filtering Recommendation collection based on Recommendations that have MessageRooms with at least one message in them. Endpoint fixed at /shortlists/:id/users/:id/recs/actives

---
+++ lib/sql/recommendation/actives.sql
@@ -0,0 +1,6 @@
+SELECT id,
+       (COUNT(*) OVER())::INT AS total
+FROM recommendations
+WHERE recommendations.referring_user = $1
+  AND recommendations.referred_shortlist = $2
+AND (SELECT COUNT(*) FROM messages WHERE messages.message_room = recommendations.message_room) > 0
