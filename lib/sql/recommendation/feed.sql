3b72f86aba7a5921428f7fcaf17f9cf559d968a0
dreamcareer7 <dreamcareer070@gmail.com>
2015-04-25 20:00:02 +0430

    Filtering Recommendation collection based on Recommendations that have MessageRooms with at least one message in them. Using ?filter=Active on rec url.

--- lib/sql/recommendation/feed.sql
+++ lib/sql/recommendation/feed.sql
@@ -11,6 +11,10 @@ AND CASE
     WHEN $3 = 'Max_U' THEN updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 MICROSECOND'
     ELSE TRUE
     END
+AND CASE
+    WHEN $6 = 'Active' THEN (SELECT COUNT(*) FROM messages WHERE messages.message_room = recommendations.message_room) > 0
+    ELSE TRUE
+    END
 ORDER BY
     CASE $3
         WHEN 'Since_C' THEN created_at
