deded17903dce4891ec0baeedf7995a97558b6db
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-18 15:22:45 +0430

    Updating Recommendations when posting a message to a MessageRoom of type comment. -r3

--- lib/sql/recommendation/actives.sql
+++ lib/sql/recommendation/actives.sql
@@ -1,6 +1,7 @@
 SELECT id,
        (COUNT(*) OVER())::INT AS total
 FROM recommendations
-WHERE recommendations.referring_user = $1
-  AND recommendations.referred_shortlist = $2
-AND (SELECT COUNT(*) FROM messages WHERE messages.message_room = recommendations.message_room) > 0
+WHERE recommendations.referring_user = $1 AND
+      recommendations.referred_shortlist = $2 AND
+      (SELECT COUNT(*) FROM messages WHERE messages.message_room = recommendations.message_room) > 0
+ORDER BY updated_at DESC
