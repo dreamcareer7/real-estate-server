7024ff1808c887211fe6feece9d1be17e9de810d
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-23 02:26:08 +0430

    Fixing active rooms and recommendations being removed as orphaned recommendations.

--- lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
+++ lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
@@ -1,4 +1,4 @@
-DELETE FROM recommendations
+SELECT DISTINCT (message_room) FROM recommendations
 WHERE COALESCE(ARRAY_LENGTH(referring_alerts, 1), 0) = 0 AND
       referred_shortlist = $1 AND
       (SELECT COUNT(*) FROM messages where messages.message_room = recommendations.message_room) = 0
