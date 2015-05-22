d91516cd10e5c37c8a4b008b34b1a68e4d9173d7
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-23 03:10:05 +0430

    Fixed an issue with add_alerts_to_recs sql syntax.

--- lib/sql/recommendation/add_alert_to_recs.sql
+++ lib/sql/recommendation/add_alert_to_recs.sql
@@ -1,5 +1,5 @@
 UPDATE recommendations
 SET referring_alerts = ARRAY_APPEND(referring_alerts, $3)
 WHERE referred_shortlist = $1 AND
-      object = $2
+      object = $2 AND
       NOT ($3 = ANY(referring_alerts));
