aafd23e12d45c066320f2f53c04d1036892c37e4
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-23 03:00:51 +0430

    Fixed an issue with Alert.matchingListing().

--- lib/sql/alert/matching.sql
+++ lib/sql/alert/matching.sql
@@ -6,17 +6,12 @@ INNER JOIN addresses
 ON properties.address_id = addresses.id
 WHERE
     listings.status = 'Active' AND
-    listings.price >= $2 AND
-    listings.price <= $3 AND
-    properties.square_meters >= $4 AND
-    properties.square_meters <= $5 AND
-    properties.bedroom_count >= $6 AND
-    ((properties.half_bathroom_count + properties.full_bathroom_count) >= $7) AND
-    properties.property_type = $8 AND
-    properties.property_subtype = ANY ($9::property_subtype[]) AND
-    ST_Distance(ST_SetSRID(ST_MakePoint($10, $11), 4326), addresses.location, TRUE) < $12 AND
-    listings.id NOT IN (
-      SELECT DISTINCT(recommendations.object)
-      FROM recommendations
-      WHERE recommendations.referred_shortlist = $1
-    )
+    listings.price >= $1 AND
+    listings.price <= $2 AND
+    properties.square_meters >= $3 AND
+    properties.square_meters <= $4 AND
+    properties.bedroom_count >= $5 AND
+    ((properties.half_bathroom_count + properties.full_bathroom_count) >= $6) AND
+    properties.property_type = $7 AND
+    properties.property_subtype = ANY ($8::property_subtype[]) AND
+    ST_Distance(ST_SetSRID(ST_MakePoint($9, $10), 4326), addresses.location, TRUE) < $11
