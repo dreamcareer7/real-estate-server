d0c9c29a83d305b5fefd0f23f09af1a68479d3a1
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-27 03:22:53 +0430

    Fixed remove_orphaned_recs_message_rooms sql.

--- lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
+++ lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT (message_room) FROM recommendations
 WHERE COALESCE(ARRAY_LENGTH(referring_alerts, 1), 0) = 0 AND
       referred_shortlist = $1 AND
-      (SELECT COUNT(*) FROM messages where messages.message_room = recommendations.message_room) = 0
+      (SELECT COUNT(*) FROM messages where messages.message_room = recommendations.message_room) = 0 AND
+      (SELECT BOOL_OR(CASE WHEN status = 'Pinned' THEN TRUE ELSE FALSE END) FROM recommendations r WHERE t.referred_shortlist = $1 AND t.object = r.object) = FALSE
