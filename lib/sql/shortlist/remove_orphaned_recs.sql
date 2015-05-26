692acdfae103ee3b60d9b77151cd2f7bcf3707e1
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-27 01:46:27 +0430

    Fixed a typo on remove_orphaned_recs sql file.

--- lib/sql/shortlist/remove_orphaned_recs.sql
+++ lib/sql/shortlist/remove_orphaned_recs.sql
@@ -2,4 +2,4 @@ DELETE FROM recommendations t
 WHERE COALESCE(ARRAY_LENGTH(referring_alerts, 1), 0) = 0 AND
       referred_shortlist = $1 AND
       (SELECT COUNT(*) FROM messages where messages.message_room = t.message_room) = 0 AND
-      (SELECT BOOL_OR(CASE WHEN status = 'Pinned' THEN TRUE ELSE FALSE END) FROM recommendations r where t.referred_shortlist = $1 AND t.object = r.object) = FALSE
+      (SELECT BOOL_OR(CASE WHEN status = 'Pinned' THEN TRUE ELSE FALSE END) FROM recommendations r WHERE t.referred_shortlist = $1 AND t.object = r.object) = FALSE
