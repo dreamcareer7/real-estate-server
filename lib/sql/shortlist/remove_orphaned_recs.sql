a02a82f46cbabb496d3f6440ba4e8fdc7def0366
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-27 00:59:31 +0430

    Fixed a Hotsheet delete flow: A Pinned recommendation must not get deleted when referring hotsheet is removed

--- lib/sql/shortlist/remove_orphaned_recs.sql
+++ lib/sql/shortlist/remove_orphaned_recs.sql
@@ -1,4 +1,5 @@
-DELETE FROM recommendations
+DELETE FROM recommendations t
 WHERE COALESCE(ARRAY_LENGTH(referring_alerts, 1), 0) = 0 AND
       referred_shortlist = $1 AND
-      (SELECT COUNT(*) FROM messages where messages.message_room = recommendations.message_room) = 0
+      (SELECT COUNT(*) FROM messages where messages.message_room = t.message_room) = 0 AND
+      (SELECT BOOL_OR(CASE WHEN status = 'Pinned' THEN TRUE ELSE FALSE END) FROM recommendations r where t.referred_shortlist = $1 AND t.object = r.object) = FALSE
