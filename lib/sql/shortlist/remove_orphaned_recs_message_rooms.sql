58ad53cf48b879d21daa6fa89ddb2a282f607111
dreamcareer7 <dreamcareer070@gmail.com>
2015-05-27 03:27:23 +0430

    Fixed remove_orphaned_recs_message_rooms sql. -r1

--- lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
+++ lib/sql/shortlist/remove_orphaned_recs_message_rooms.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT (message_room) FROM recommendations
+SELECT DISTINCT (message_room) FROM recommendations t
 WHERE COALESCE(ARRAY_LENGTH(referring_alerts, 1), 0) = 0 AND
       referred_shortlist = $1 AND
       (SELECT COUNT(*) FROM messages where messages.message_room = recommendations.message_room) = 0 AND
