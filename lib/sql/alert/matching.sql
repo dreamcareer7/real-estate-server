db2f72a41ad5949d9ac189008bfdc6471e8420e4
dreamcareer7 <dreamcareer070@gmail.com>
2015-06-05 20:29:55 +0430

    Considering null locations.

--- lib/sql/alert/matching.sql
+++ lib/sql/alert/matching.sql
@@ -14,4 +14,5 @@ WHERE
     ((properties.half_bathroom_count + properties.full_bathroom_count) >= $6) AND
     properties.property_type = $7 AND
     properties.property_subtype = ANY ($8::property_subtype[]) AND
-    ST_Within(ST_SetSRID(ST_MakePoint($9, $10), 4326), ST_SetSRID(ST_GeomFromText($11), 4326)) = TRUE
+    ST_Within(ST_SetSRID(ST_MakePoint($9, $10), 4326), ST_SetSRID(ST_GeomFromText($11), 4326)) = TRUE AND
+    addresses.location IS NOT NULL
