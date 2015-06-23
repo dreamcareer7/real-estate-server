f09106bafed7e20421310acc9692903887c33257
dreamcareer7 <dreamcareer070@gmail.com>
2015-06-23 11:13:27 +0430

    Added geo_source to Address model. Geocodings now set markers for each address. We now know the source of lat/long and partial information.

--- lib/sql/address/update_latlong.sql
+++ lib/sql/address/update_latlong.sql
@@ -1,4 +1,7 @@
 UPDATE addresses
 SET location = ST_SetSRID(ST_MakePoint($1, $2), 4326),
-    updated_at = NOW()
+    updated_at = NOW(),
+    geo_source = $4,
+    geocoded = TRUE,
+    partial_match = FALSE
 WHERE id = $3
--- lib/sql/address/update_latlong_partial.sql
+++ lib/sql/address/update_latlong_partial.sql
@@ -1,4 +1,7 @@
 UPDATE addresses
 SET location_partial = ST_SetSRID(ST_MakePoint($1, $2), 4326),
-    updated_at = NOW()
+    updated_at = NOW(),
+    geo_source = $4,
+    geocoded = TRUE,
+    partial_match = TRUE
 WHERE id = $3
