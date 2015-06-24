54565c80e9314d060486de039d4147e0d272c4c4
dreamcareer7 <dreamcareer070@gmail.com>
2015-06-24 10:02:00 +0430

    Adding FormattedAddress from sources to models.

--- lib/sql/address/update_latlong.sql
+++ lib/sql/address/update_latlong.sql
@@ -3,5 +3,6 @@ SET location = ST_SetSRID(ST_MakePoint($1, $2), 4326),
     updated_at = NOW(),
     geo_source = $4,
     geocoded = TRUE,
-    partial_match = FALSE
+    partial_match = FALSE,
+    geo_source_formatted_address = $5
 WHERE id = $3
--- lib/sql/address/update_latlong_partial.sql
+++ lib/sql/address/update_latlong_partial.sql
@@ -3,5 +3,6 @@ SET location_partial = ST_SetSRID(ST_MakePoint($1, $2), 4326),
     updated_at = NOW(),
     geo_source = $4,
     geocoded = TRUE,
-    partial_match = TRUE
+    partial_match = TRUE,
+    geo_source_formatted_address = $5
 WHERE id = $3
