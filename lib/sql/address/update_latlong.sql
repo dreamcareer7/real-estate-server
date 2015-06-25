18fccc587585e3623d6a862f9182b149b67a5391
dreamcareer7 <dreamcareer070@gmail.com>
2015-06-25 15:02:55 +0430

    New address model changes to keep geocoding information.

--- lib/sql/address/update_latlong.sql
+++ lib/sql/address/update_latlong.sql
@@ -1,8 +1,10 @@
 UPDATE addresses
 SET location = ST_SetSRID(ST_MakePoint($1, $2), 4326),
     updated_at = NOW(),
-    geo_source = $4,
+    geo_source = $3,
     geocoded = TRUE,
-    partial_match = FALSE,
-    geo_source_formatted_address = $5
-WHERE id = $3
+    partial_match = $4,
+    geo_source_formatted_address = $5,
+    geo_confidence = $6,
+    approximate = $7
+WHERE id = $8
