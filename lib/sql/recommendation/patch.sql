3138aa09f2c7a92c33e4eea044b362970d2ee8ec
dreamcareer7 <dreamcareer070@gmail.com>
2015-07-02 02:44:30 +0430

    Setting to read on Recommendation.patch().

--- lib/sql/recommendation/patch.sql
+++ lib/sql/recommendation/patch.sql
@@ -1,4 +1,5 @@
 UPDATE recommendations
 SET favorited = $2,
-    updated_at = CASE WHEN $2 = TRUE THEN NOW() ELSE updated_at END
+    updated_at = CASE WHEN $2 = TRUE THEN NOW() ELSE updated_at END,
+    read = TRUE
 WHERE id = $1
