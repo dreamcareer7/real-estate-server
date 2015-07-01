9fae201a038f293f6a4e33dfa12c76002224cddd
dreamcareer7 <dreamcareer070@gmail.com>
2015-07-01 20:27:12 +0430

    Updated geocoding policy and rules.

--- lib/sql/address/update_latlong_bing.sql
+++ lib/sql/address/update_latlong_bing.sql
@@ -1,6 +1,7 @@
 UPDATE addresses
 SET location_bing = ST_SetSRID(ST_MakePoint($1, $2), 4326),
     geocoded_bing = TRUE,
+    corrupted_bing = FALSE,
     geo_source_formatted_address_bing = $3,
     geo_confidence_bing = $4,
     updated_at = NOW()
--- lib/sql/address/update_latlong_google.sql
+++ lib/sql/address/update_latlong_google.sql
@@ -1,6 +1,7 @@
 UPDATE addresses
 SET location_google = ST_SetSRID(ST_MakePoint($1, $2), 4326),
     geocoded_google = TRUE,
+    corrupted_google = FALSE,
     partial_match_google = $3,
     geo_source_formatted_address_google = $4,
     geo_confidence_google = $5,
