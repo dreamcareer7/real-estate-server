6d790fe7bd6659a61ff74266eb99c3133cab46e3
dreamcareer7 <dreamcareer070@gmail.com>
2015-07-03 21:10:24 +0430

    Changed message count behavior: we now have different message types (Text, Video, Photo and Document). -r2

--- lib/sql/message_room/new_counts.sql
+++ lib/sql/message_room/new_counts.sql
@@ -2,7 +2,7 @@ SELECT
     SUM(CASE WHEN video_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_video_count,
     SUM(CASE WHEN document_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_document_count,
     SUM(CASE WHEN image_url IS NOT NULL THEN 1 ELSE 0 END)::INT AS new_image_count,
-    SUM(CASE WHEN TRUE THEN 1 ELSE 0 END)::INT as new_comment_count
+    SUM(CASE WHEN (image_url IS NULL AND video_url IS NULL AND document_url IS NULL) THEN 1 ELSE 0 END)::INT as new_comment_count
 FROM messages
 INNER JOIN messages_ack
 ON messages.id = messages_ack.message_id AND
