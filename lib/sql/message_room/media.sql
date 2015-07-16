c65810048e8de11b64465032683a2a33fa84783b
dreamcareer7 <dreamcareer070@gmail.com>
2015-07-16 19:06:16 +0430

    Fixed media_gallery endpoint behavior to include all types of media including photos, videos and documents.

--- lib/sql/message_room/media.sql
+++ lib/sql/message_room/media.sql
@@ -1,41 +1,33 @@
 SELECT 'url' AS type,
        id,
-       CASE WHEN $2 = 'Photo' THEN image_url
-            WHEN $2 = 'Video' THEN video_url
-            WHEN $2 = 'Document' THEN document_url
-            ELSE image_url
+       CASE WHEN image_url IS NOT NULL THEN image_url
+            WHEN video_url IS NOT NULL THEN video_url
+            WHEN document_url IS NOT NULL THEN document_url
        END AS url,
-       CASE WHEN $2 = 'Photo' THEN 'photo'
-            WHEN $2 = 'Video' THEN 'video'
-            WHEN $2 = 'Document' THEN 'document'
-            ELSE 'photo'
+       CASE WHEN image_url IS NOT NULL THEN 'image'
+            WHEN video_url IS NOT NULL THEN 'video'
+            WHEN document_url IS NOT NULL THEN 'document'
        END AS document_type
 FROM messages
 WHERE message_room = $1
 AND CASE
-    WHEN $3 = 'Since_C' THEN created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 MICROSECOND'
-    WHEN $3 = 'Max_C' THEN created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 MICROSECOND'
-    WHEN $3 = 'Since_U' THEN updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 MICROSECOND'
-    WHEN $3 = 'Max_U' THEN updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $4 * INTERVAL '1 MICROSECOND'
-    WHEN $3 = 'Init_C' THEN created_at < NOW()
-    WHEN $3 = 'Init_U' THEN updated_at < NOW()
+    WHEN $2 = 'Since_C' THEN created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Max_C' THEN created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Since_U' THEN updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Max_U' THEN updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Init_C' THEN created_at < NOW()
+    WHEN $2 = 'Init_U' THEN updated_at < NOW()
     ELSE TRUE
     END
-AND CASE
-    WHEN $2 = 'Photo' THEN image_url IS NOT NULL
-    WHEN $2 = 'Video' THEN video_url IS NOT NULL
-    WHEN $2 = 'Document' THEN document_url IS NOT NULL
-    ELSE image_url IS NOT NULL
-    END
 ORDER BY
-    CASE $3
+    CASE $2
         WHEN 'Since_C' THEN created_at
         WHEN 'Since_U' THEN updated_at
     END,
-    CASE $3
+    CASE $2
         WHEN 'Max_C' THEN created_at
         WHEN 'Max_U' THEN updated_at
         WHEN 'Init_C' THEN created_at
         WHEN 'Init_U' THEN updated_at
     END DESC
-LIMIT $5;
+LIMIT $4;
