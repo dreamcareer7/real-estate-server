8375087e060e728c7e2800aae53211bc44b19964
dreamcareer7 <dreamcareer070@gmail.com>
2015-08-01 20:09:38 +0430

    Applied all model changes to notification objects.

--- lib/sql/notification/insert.sql
+++ lib/sql/notification/insert.sql
@@ -11,6 +11,8 @@ INSERT INTO notifications(
             shortlist,
             auxiliary_subject,
             subject_class,
-            auxiliary_subject_class
+            auxiliary_subject_class,
+            extra_subject_class,
+            extra_object_class
         )
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id;
