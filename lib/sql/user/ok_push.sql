f543bbc9fcc6bbf109c9ccd973863922e33e2a3d
dreamcareer7 <dreamcareer070@gmail.com>
2015-08-18 04:06:29 +0430

    Added notification time restriction.

---
+++ lib/sql/user/ok_push.sql
@@ -0,0 +1,4 @@
+SELECT ((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time > '08:00:00'::time) AND
+       ((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time < '23:59:59'::time) AS ok
+FROM users
+WHERE id = $1;
---
+++ lib/sql/user/patch_timezone.sql
@@ -0,0 +1,3 @@
+UPDATE users
+SET timezone = $2
+WHERE id = $1
