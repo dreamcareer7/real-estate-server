a209b9ab73725a3ee6da2d710e7e25d8284bbf5a
dreamcareer7 <dreamcareer070@gmail.com>
2015-09-09 05:08:46 +0430

    Added user_type to User model.

--- lib/sql/user/insert.sql
+++ lib/sql/user/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (type, first_name, last_name, password, email, phone_number, agency_id)
+INSERT INTO users (first_name, last_name, password, email, phone_number, agency_id, user_code)
 VALUES ($1,
         $2,
         $3,
-        $4,
-        LOWER($5),
+        LOWER($4),
+        $5,
         $6,
         $7) RETURNING id
