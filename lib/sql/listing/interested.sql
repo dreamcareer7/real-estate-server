d677bdc71fc74c47f0e4f923ea3867e1b8d61040
dreamcareer7 <dreamcareer070@gmail.com>
2015-09-10 20:19:40 +0430

    We generate a notification for a listing if and only if someone within that shortlist has favorited or toured that listing.

--- lib/sql/listing/interested.sql
+++ lib/sql/listing/interested.sql
@@ -1,3 +1,5 @@
 SELECT DISTINCT(referred_shortlist) AS id
 FROM recommendations
-WHERE object = $1
+WHERE object = $1 AND
+      (favorited IS TRUE OR
+       added_tour IS TRUE)
--- lib/sql/user/ok_push_shortlist.sql
+++ lib/sql/user/ok_push_shortlist.sql
@@ -1,4 +1,6 @@
-SELECT push_enabled AS ok
+SELECT (shortlists_users.push_enabled AND (NOT shortlists.archived)) AS ok
 FROM shortlists_users
-WHERE "user" = $1 AND
-      shortlist = $2
+INNER JOIN shortlists
+    ON shortlists_users.shortlist = shortlists.id
+WHERE shortlist_users."user" = $1 AND
+      shortlist_users.shortlist = $2
