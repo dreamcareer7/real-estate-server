ee4f0a912586da2e6986c693543c2abea308b0f3
dreamcareer7 <dreamcareer070@gmail.com>
2015-09-11 22:42:16 +0430

    Fixed OK push on shortlist for user sql.

--- lib/sql/user/ok_push_shortlist.sql
+++ lib/sql/user/ok_push_shortlist.sql
@@ -2,5 +2,5 @@ SELECT (shortlists_users.push_enabled AND (NOT shortlists.archived)) AS ok
 FROM shortlists_users
 INNER JOIN shortlists
     ON shortlists_users.shortlist = shortlists.id
-WHERE shortlist_users."user" = $1 AND
-      shortlist_users.shortlist = $2
+WHERE shortlists_users."user" = $1 AND
+      shortlists_users.shortlist = $2
