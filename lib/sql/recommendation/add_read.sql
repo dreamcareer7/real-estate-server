d07a9d6a155858887b37d831989533ca02643c49
dreamcareer7 <dreamcareer070@gmail.com>
2015-10-22 04:12:22 +0330

    Added notifications on Favorite/TourRequest.

--- lib/sql/recommendation/add_read.sql
+++ lib/sql/recommendation/add_read.sql
@@ -3,7 +3,7 @@ INSERT INTO recommendations_eav(
                                 recommendation,
                                 action
                                )
-SELECT v.* FROM (VALUES($1::uuid, $2::uuid, 'Read'::recommendation_action)) AS v(u, r, a)
+SELECT v.* FROM (VALUES($1::uuid, $2::uuid, 'Read'::recommendation_eav_action)) AS v(u, r, a)
 LEFT JOIN recommendations_eav t ON
     t."user" = v.u AND
     t.recommendation = v.r AND
