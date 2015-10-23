3ac1dd6828db54ddfd26470f5c49c51faf4ae275
dreamcareer7 <dreamcareer070@gmail.com>
2015-10-24 01:19:41 +0330

    Fixed lat/long on location object. Added type = location.

--- lib/sql/listing/get_compacts.sql
+++ lib/sql/listing/get_compacts.sql
@@ -2,14 +2,15 @@ SELECT 'compact_listing' AS TYPE,
        EXTRACT(EPOCH FROM listings.created_at) AS created_at,
        EXTRACT(EPOCH FROM listings.updated_at) AS updated_at,
        EXTRACT(EPOCH FROM listings.deleted_at) AS deleted_at,
-       listings.price as price,
-       listings.status as status,
-       listings.mls_number as mls_number,
+       listings.price AS price,
+       listings.status AS status,
+       listings.mls_number AS mls_number,
        json_build_object(
-          'latitude',  ST_X(addresses.location),
-          'longitude', ST_Y(addresses.location)
-      ) as location
+          'latitude', ST_Y(addresses.location),
+          'longitude', ST_X(addresses.location),
+          'type', 'location'
+      ) AS location
 FROM listings
 JOIN properties ON listings.property_id = properties.id
-JOIN addresses  ON properties.address_id = addresses.id
+JOIN addresses ON properties.address_id = addresses.id
 WHERE listings.id = ANY($1::uuid[])
