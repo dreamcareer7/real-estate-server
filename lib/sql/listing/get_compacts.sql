77643a4d492992707074cec5ce09bc5d09c90dfe
dreamcareer7 <dreamcareer070@gmail.com>
2015-10-24 00:43:53 +0330

    Fixed get_compacts SQL.

--- lib/sql/listing/get_compacts.sql
+++ lib/sql/listing/get_compacts.sql
@@ -2,11 +2,11 @@ SELECT 'compact_listing' AS TYPE,
        EXTRACT(EPOCH FROM listings.created_at) AS created_at,
        EXTRACT(EPOCH FROM listings.updated_at) AS updated_at,
        EXTRACT(EPOCH FROM listings.deleted_at) AS deleted_at,
-       listings.price as price,
-       listings.status as status,
-       listings.mls_number as mls_number,
-       ST_AsGeoJSON(addresses.location) as location
+       listings.price AS price,
+       listings.status AS status,
+       listings.mls_number AS mls_number,
+       ST_AsGeoJSON(addresses.location) AS location
 FROM listings
-JOIN properties ON listings.property_id = properties.id
-JOIN addresses  ON properties.address_id = addresses.id
+INNER JOIN properties ON listings.property_id = properties.id
+INNER JOIN addresses ON properties.address_id = addresses.id
 WHERE listings.id = ANY($1::uuid[])
