4f61c6274c51a5d79dda6cc6f142d330c6765996
dreamcareer7 <dreamcareer070@gmail.com>
2015-10-25 20:53:44 +0100

    Totally removed the concept of an agency

--- lib/sql/user/insert.sql
+++ lib/sql/user/insert.sql
@@ -1,8 +1,7 @@
-INSERT INTO users (first_name, last_name, password, email, phone_number, agency_id, user_type)
+INSERT INTO users (first_name, last_name, password, email, phone_number, user_type)
 VALUES ($1,
         $2,
         $3,
         LOWER($4),
         $5,
-        $6,
-        $7) RETURNING id
+        $6) RETURNING id
