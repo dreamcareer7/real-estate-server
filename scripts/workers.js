1cf58f7d4dd61d629901f51c684cf2147fb09e03
dreamcareer7 <dreamcareer070@gmail.com>
2015-10-29 06:43:24 +0330

    Fixed an issue with workers script.

--- scripts/workers.js
+++ scripts/workers.js
@@ -6,16 +6,16 @@ var config = require('../lib/config.js');
 var queue  = require('../lib/utils/queue.js');
 
 queue.process('airship_transport_send', config.airship.parallel, function(job, done) {
-  console.log('<-'.green + ' Processed an Airship push notification');
+  console.log('<- Processed an Airship push notification'.green);
   Notification.send(job.data.user_id, job.data.room_id, job.data.airship_notification, done);
 });
 
 queue.process('email', config.email.parallel, function(job, done) {
-  console.log('<-'.cyan + ' Processed a SES email');
+  console.log('<- Processed a SES email'.cyan);
   SES.callSES(job.data, done);
 });
 
 queue.process('sms', config.twilio.parallel, function(job, done) {
-  console.log('<-'.magenta + ' Processed a Twilio SMS');
-  twilio.callTwilio(job.data, done);
+  console.log('<- Processed a Twilio SMS'.magenta);
+  Twilio.callTwilio(job.data, done);
 });
