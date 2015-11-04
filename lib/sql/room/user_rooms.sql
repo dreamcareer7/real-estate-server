9c9087294f073520e180e20dd70998f439d0b7b9
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-04 04:09:00 +0330

    Fixed sorting on Rooms collection. Now we should be having our rooms in that collection based on latest activity if requested by sorting_value=Update

--- lib/sql/room/user_rooms.sql
+++ lib/sql/room/user_rooms.sql
@@ -1,27 +1,39 @@
-SELECT room,
-       (COUNT(*) OVER())::INT AS total
-FROM rooms_users
-INNER JOIN rooms
-    ON rooms_users.room = rooms.id
-WHERE "user" = $1
-    AND rooms.deleted_at IS NULL
-    AND CASE WHEN $2 = 'Since_C' THEN rooms.created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-             WHEN $2 = 'Max_C' THEN rooms.created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-             WHEN $2 = 'Since_U' THEN rooms.updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-             WHEN $2 = 'Max_U' THEN rooms.updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-             WHEN $2 = 'Init_C' THEN rooms.created_at < NOW()
-             WHEN $2 = 'Init_U' THEN rooms.updated_at < NOW()
-             ELSE TRUE
-    END
+WITH user_rooms AS (
+    SELECT rooms_users.room AS id,
+           (COUNT(*) OVER())::INT AS total,
+           MAX(messages.created_at) AS updated_at,
+           MAX(rooms.created_at) AS created_at
+           FROM rooms_users
+           INNER JOIN rooms
+               ON rooms_users.room = rooms.id
+           INNER JOIN messages
+               ON rooms_users.room = messages.room
+           WHERE "user" = $1 AND
+               rooms.deleted_at IS NULL
+           GROUP BY messages.room,
+                    rooms_users.room
+    )
+SELECT id,
+       total
+FROM user_rooms
+WHERE
+   CASE WHEN $2 = 'Since_C' THEN user_rooms.created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+        WHEN $2 = 'Max_C' THEN user_rooms.created_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+        WHEN $2 = 'Since_U' THEN user_rooms.updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+        WHEN $2 = 'Max_U' THEN user_rooms.updated_at < TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+        WHEN $2 = 'Init_C' THEN user_rooms.created_at < NOW()
+        WHEN $2 = 'Init_U' THEN user_rooms.updated_at < NOW()
+        ELSE TRUE
+        END
 ORDER BY
     CASE $2
-        WHEN 'Since_C' THEN rooms.created_at
-        WHEN 'Since_U' THEN rooms.updated_at
+        WHEN 'Since_C' THEN user_rooms.created_at
+        WHEN 'Since_U' THEN user_rooms.updated_at
     END,
     CASE $2
-        WHEN 'Max_C' THEN rooms.created_at
-        WHEN 'Max_U' THEN rooms.updated_at
-        WHEN 'Init_C' THEN rooms.created_at
-        WHEN 'Init_U' THEN rooms.updated_at
+        WHEN 'Max_C' THEN user_rooms.created_at
+        WHEN 'Max_U' THEN user_rooms.updated_at
+        WHEN 'Init_C' THEN user_rooms.created_at
+        WHEN 'Init_U' THEN user_rooms.updated_at
     END DESC
 LIMIT $4;
