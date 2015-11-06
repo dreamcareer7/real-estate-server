b339930279555fe47d24599df1b5a5ce17be9669
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-06 21:56:00 +0330

    Return results on user search endpoint based on relevancy. Also fixes the issue of the calling user themselves appearing on search results.

--- lib/sql/user/string_search.sql
+++ lib/sql/user/string_search.sql
@@ -1,8 +1,24 @@
-SELECT id,
-       (COUNT(*) OVER())::INT AS total
-FROM users
+WITH related_users AS (
+    WITH user_rooms AS (
+        SELECT rooms_users.room
+        FROM rooms_users
+        WHERE rooms_users."user" = $1
+    )
+    SELECT DISTINCT(rooms_users."user") AS id
+    FROM rooms_users
+    INNER JOIN user_rooms ON
+        user_rooms.room = rooms_users.room
+    INNER JOIN users ON
+        rooms_users."user" = users.id
+    WHERE rooms_users."user" <> $1
+)
+SELECT related_users.id,
+(COUNT(*) OVER())::INT AS total
+FROM related_users
+INNER JOIN users
+ON related_users.id = users.id
 WHERE
-    (first_name ~* $1 OR
-    last_name ~* $1) AND
-    deleted_at IS NULL
-ORDER BY first_name
+    (users.first_name ~* $2 OR
+     users.last_name ~* $2) AND
+     users.deleted_at IS NULL
+ORDER BY users.first_name
