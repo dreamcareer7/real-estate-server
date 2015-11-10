6c40da50212d1d90cb66dee557fe4cef1b636bf0
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-11 00:18:17 +0100

    proper newrelic

--- newrelic.js
+++ newrelic.js
@@ -1,10 +1,2 @@
-var merge = require('merge');
-
-var base = require('./lib/configs/newrelic.js');
-
-var env = process.env.NODE_ENV || 'development';
-var environmental = require('./lib/configs/' + env + '.js').newrelic;
-
-var config = merge.recursive(base, environmental);
-
-exports.config = config;
+var config = require('./lib/config.js');
+module.exports = config.newrelic;
