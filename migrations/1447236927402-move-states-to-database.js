411630c040c2a61ef855088a65280453938b28a5
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-12 04:49:03 +0330

    Modified migration file for creating migrations table to include IF NOT EXISTS

--- migrations/1447236927402-move-states-to-database.js
+++ migrations/1447236927402-move-states-to-database.js
@@ -2,7 +2,7 @@
 
 var db = require('../lib/utils/db');
 
-var createDb   = 'CREATE TABLE migrations ( created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), state jsonb );';
+var createDb   = 'CREATE TABLE IF NOT EXISTS migrations ( created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), state jsonb );';
 var insert = 'INSERT INTO migrations (state) VALUES (\'{"pos":14}\');';
 var dropDb = 'DROP TABLE migrations';
 
