bb771ae37df5244c647e0d98b12b231eb05b4b3b
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-13 16:34:47 +0330

    notification suite added

--- newrelic.js
+++ newrelic.js
@@ -1,2 +1,24 @@
-var config = require('./lib/config.js');
-exports.config = config.newrelic;
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+    /**
+     * Array of application names.
+     */
+    app_name: ['rechat-ehsan'],
+    /**
+     * Your New Relic license key.
+     */
+    license_key: '61c65408021cc994f5cafbe86d181a91b4e98b73',
+    logging: {
+        /**
+         * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+         * issues with the agent, 'info' and higher will impose the least overhead on
+         * production applications.
+         */
+        level: 'info'
+    }
+}
--- tests/suites/alert.js
+++ tests/suites/alert.js
@@ -4,26 +4,109 @@ var criteria = require('./data/alert_criteria.js');
 registerSuite('room', ['create']);
 
 var create = (cb) => {
-  return frisby.create('create alert')
+    return frisby.create('create alert')
         .post('/rooms/' + results.room.create.data.id + '/alerts', criteria)
         .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
         .expectStatus(200)
         .expectJSON({
-          code: 'OK',
-          data: criteria
+            code: 'OK',
+            data: criteria
         });
 }
 
 var virtual = (cb) => {
-  var criteria = require('./data/valert_criteria.js')
-  return frisby.create('virtual alert')
-         .post('/valerts', criteria)
-         .expectStatus(200)
-         .after(cb);
+    var criteria = require('./data/valert_criteria.js')
+    return frisby.create('virtual alert')
+        .post('/valerts', criteria)
+        .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
+        .expectStatus(200)
+        .expectJSON({
+            code: 'OK',
+            data: [
+                {
+                    type: 'compact_listing'
+                }
+            ]
+        })
+}
+
+
+var bulkAlertShare = (cb) => {
+    return frisby.create('bulk alert share')
+        .post('/alerts', {alert: results.alert.create.data})
+        .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
+        .expectStatus(200)
+        .expectJSON({
+            code: 'OK'
+        })
+        .expectJSONTypes({
+            code: String,
+            data: Array,
+            info: Object
+        });
+}
+
+var patchAlert = (cb) => {
+    return frisby.create('patch alert')
+        .put('/rooms/' + results.alert.create.data.room + '/alerts/' + results.alert.create.data.id)
+        .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
+        .expectStatus(200)
+        .expectJSON({
+            code: 'OK'
+        })
+        .expectJSONTypes({
+            data: Object
+        });
+}
+
+var getRoomAlerts = (cb) => {
+    return frisby.create('get room alerts')
+        .get('/rooms/' + results.alert.create.data.room + '/alerts')
+        .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
+        .expectStatus(200)
+        .expectJSON({
+            code: 'OK'
+        })
+        .expectJSONTypes({
+            data: Object,
+            info: Object
+        });
+}
+
+var getAlerts = (cb) => {
+    return frisby.create('get room alerts')
+        .get('/alerts')
+        .after(cb)
+        .expectHeaderContains('Content-Type', 'json')
+        .expectStatus(200)
+        .expectJSON({
+            code: 'OK'
+        })
+        .expectJSONTypes({
+            data: Object,
+            info: Object
+        });
+}
+
+var deleteAlert = (cb) => {
+    return frisby.create('delete alert')
+        .delete('/rooms/' + results.alert.create.data.room + '/alerts/' + results.alert.create.data.id)
+        .after(cb)
+        .expectStatus(204)
 }
 
 
 module.exports = {
-  create,
-  virtual
+    create,
+    virtual,
+    bulkAlertShare,
+    patchAlert,
+    getRoomAlerts,
+    getAlerts,
+    deleteAlert
 }
