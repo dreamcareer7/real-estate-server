eae4721b6ee0df71b0b847d73a1c1b3ac17439cc
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-13 20:18:17 +0330

    fixed self invitation issue

--- tests/suites/invitation.js
+++ tests/suites/invitation.js
@@ -8,7 +8,7 @@ var create = (cb) => {
     .post('/invitations/', {
       invitations: [{
         invited_user: results.user.create.data.id,
-        inviting_user: results.user.create.data.id,
+        inviting_user: results.authorize.token.data.id,
         email: results.user.create.data.email,
         phone_number: results.user.create.data.phone_number,
         invitee_name: results.user.create.data.first_name,
