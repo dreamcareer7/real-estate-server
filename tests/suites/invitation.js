5b6c46311cd3f1cdb62d00990287b3cb204e4455
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-13 20:07:36 +0330

    tabsize changed

--- tests/suites/invitation.js
+++ tests/suites/invitation.js
@@ -4,32 +4,31 @@ registerSuite('user', ['create'])
 
 
 var create = (cb) => {
-    return frisby.create('create invitation')
-        .post('/invitations/', {
-            invitations: [{
-                invited_user: results.user.create.data.id,
-                inviting_user: results.user.create.data.id,
-                email: results.user.create.data.email,
-                phone_number: results.user.create.data.phone_number,
-                invitee_name: results.user.create.data.first_name,
-                room: results.room.create.data.id,
-                url: ""
-            }]
-        })
-        .after(cb)
-        .expectHeaderContains('Content-Type', 'json')
-        .expectStatus(200)
-        .expectJSON({
-            code: 'OK',
-            data: [
-                {
-                    type: 'invitation'
-                }
-            ]
-        })
-        .expectJSONLength('data', 1);
+  return frisby.create('create invitation')
+    .post('/invitations/', {
+      invitations: [{
+        invited_user: results.user.create.data.id,
+        inviting_user: results.user.create.data.id,
+        email: results.user.create.data.email,
+        phone_number: results.user.create.data.phone_number,
+        invitee_name: results.user.create.data.first_name,
+        room: results.room.create.data.id,
+        url: ""
+      }]
+    })
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [
+        {
+          type: 'invitation'
+        }
+      ]
+    })
+    .expectJSONLength('data', 1);
 }
 
 module.exports = {
-    create
+  create
 }
--- tests/suites/notification.js
+++ tests/suites/notification.js
@@ -4,96 +4,92 @@ registerSuite('invitation', ['create']);
 
 
 var getUsersNotification = (cb) => {
-    return frisby.create('get all notifications for a user')
-        .get('/notifications')
-        .after(cb)
-        .expectHeaderContains('Content-Type', 'json')
-        .expectStatus(200)
-        .expectJSON({
-            code: 'OK',
-            data: [
-                {
-                    type: 'notification'
-                }
-            ]
-        })
+  return frisby.create('get all notifications for a user')
+    .get('/notifications')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [
+        {
+          type: 'notification'
+        }
+      ]
+    })
 }
 
 
 var get = (cb) => {
-    return frisby.create('get a notification by id')
-        .get('/notifications/' + results.notification.getUsersNotification.data[0].id)
-        .after(cb)
-        .expectHeaderContains('Content-Type', 'json')
-        .expectStatus(200)
-        .expectJSON({
-            code: 'OK',
-            data: {
-                type: 'notification'
-            }
+  return frisby.create('get a notification by id')
+    .get('/notifications/' + results.notification.getUsersNotification.data[0].id)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        type: 'notification'
+      }
 
-        })
+    })
 }
 
 
 var acknowledgeNotification = (cb) => {
-    return frisby.create('acknowledge notification')
-        .delete('/notifications/' + results.notification.getUsersNotification.data[0].id)
-        .after(cb)
-        .expectStatus(204)
+  return frisby.create('acknowledge notification')
+    .delete('/notifications/' + results.notification.getUsersNotification.data[0].id)
+    .after(cb)
+    .expectStatus(204)
 }
 
 
 var push = (cb) => {
-    var token = require('./data/token.js')
-    var token_model = JSON.parse(JSON.stringify(token));
-    return frisby.create('register push')
-        .post('/notifications/tokens', token_model)
-        .after(cb)
-        .expectHeaderContains('Content-Type', 'json')
-        .expectStatus(201)
-        .expectJSON({
-            code: 'OK',
-            data: {
-                type: 'user'
-            }
+  var token = require('./data/token.js')
+  var token_model = JSON.parse(JSON.stringify(token));
+  return frisby.create('register push')
+    .post('/notifications/tokens', token_model)
+    .after(cb)
+    .expectStatus(201)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        type: 'user'
+      }
 
-        })
-        .expectJSONLength(2);
+    })
+    .expectJSONLength(2);
 }
 
 
 var cancelPush = (cb) => {
-    return frisby.create('cancel push')
-        .delete('/notifications/tokens/' + results.notification.push.data.id)
-        .after(cb)
-        .expectHeaderContains('Content-Type', 'json')
-        .expectStatus(200)
-        .expectJSON({
-            code: 'OK',
-            data: {
-                type: 'user'
-            }
+  return frisby.create('cancel push')
+    .delete('/notifications/tokens/' + results.notification.push.data.id)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        type: 'user'
+      }
 
-        })
-        .expectJSONLength(2);
+    })
+    .expectJSONLength(2);
 }
 
 var patchNotificationSettings = (cb) => {
-    return frisby.create('update notification settings')
-        .patch('/room/' + results.room.create.data.id + '/notifications', {
-            notification: "true"
-        })
-        .after(cb)
-        .expectStatus(204)
+  return frisby.create('update notification settings')
+    .patch('/room/' + results.room.create.data.id + '/notifications', {
+      notification: "true"
+    })
+    .after(cb)
+    .expectStatus(204)
 }
 
 
 module.exports = {
-    getUsersNotification,
-    acknowledgeNotification,
-    get: get,
-    push,
-    cancelPush,
-    patchNotificationSettings
+  getUsersNotification,
+  acknowledgeNotification,
+  get: get,
+  push,
+  cancelPush,
+  patchNotificationSettings
 }
