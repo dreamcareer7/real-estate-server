7a9378654be2081deeaf46c04a7f0a0d3af92eb3
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-16 02:01:39 +0330

    Added basic contact model updates.

--- lib/sql/contact/add.sql
+++ lib/sql/contact/add.sql
@@ -3,7 +3,14 @@ INSERT INTO contacts("user",
                      first_name,
                      last_name,
                      phone_number,
-                     email)
-VALUES ($1, $2, $3, $4, $5, $6)
+                     email,
+                     cover_image_url,
+                     profile_image_url,
+                     invitation_url,
+                     company,
+                     birthday,
+                     address
+                    )
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
 ON CONFLICT DO NOTHING
 RETURNING id
---
+++ migrations/1447626055840-adding-address-to-contacts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var db = require('../lib/utils/db');
+
+var sql_up   = 'DO $$ BEGIN ALTER TABLE contacts ADD COLUMN address text; EXCEPTION WHEN duplicate_column THEN END; $$;';
+var sql_down = 'ALTER TABLE contacts DROP COLUMN IF EXISTS address;';
+
+var runSql = (sql) => {
+  return (next) => {
+    db.conn( (err, client) => {
+      if(err)
+        return next(err);
+
+      return client.query(sql, next);
+    });
+  };
+};
+
+exports.up = runSql(sql_up);
+exports.down = runSql(sql_down);
