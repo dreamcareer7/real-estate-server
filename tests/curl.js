5560a308a60cdea759cb9b34c0145319ebcaaa22
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-15 12:20:38 +0100

    so we show results in proper order with higher concurrencies

--- tests/curl.js
+++ tests/curl.js
@@ -3,13 +3,12 @@ var ascurl = require('request-as-curl');
 var enableResponse;
 
 function logger(req, res, next) {
-  console.log( ('--------- '+req.headers['x-suite']+': '+req.method+' '+req.path+' ---------').yellow );
-  console.log(ascurl(req, req.body).green);
-
-
   var end = res.end;
 
   res.end = function(data, encoding, callback) {
+    console.log( ('--------- '+req.headers['x-suite']+': '+req.method+' '+req.path+' ---------').yellow );
+    console.log(ascurl(req, req.body).green);
+
     if (enableResponse && data)
       console.log(data.toString().red)
     end.call(res, data, encoding, callback);
