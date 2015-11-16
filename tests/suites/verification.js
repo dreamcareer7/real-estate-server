66ee263ac9713b47721884f4e6b1592c8f34c2f9
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-16 23:17:55 +0330

    changed response model

--- tests/suites/verification.js
+++ tests/suites/verification.js
@@ -5,14 +5,32 @@ var create = (cb) => {
   return frisby.create('create verification')
     .post('/verifications')
     .after(cb)
-    .expectStatus(201);
+    .expectStatus(201)
+    .expectJSON({
+      code: 'OK',
+      data: [
+        {
+          type: 'invitation'
+        }
+      ]
+    })
+    .expectJSONLength('data', 1);
 }
 
 var verify = (cb) => {
   return frisby.create('verify code')
-    .get('/verifications/' + results.verification.create.code + '/users/' + results.verification.create.user_id)
+    .get('/verifications/' + results.verification.create.data.code + '/users/' + results.verification.create.data.user_id)
     .after(cb)
-    .expectStatus(200);
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [
+        {
+          status: 'verified'
+        }
+      ]
+    })
+    .expectJSONLength('data', 1);
 }
 
 module.exports = {
