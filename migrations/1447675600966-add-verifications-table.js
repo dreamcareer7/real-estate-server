043954a243f3e1fa3729f18918d9f19e4baa9f4a
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-16 15:53:30 +0330

    added verification table to migration

--- migrations/1447675600966-add-verifications-table.js
+++ migrations/1447675600966-add-verifications-table.js
@@ -2,13 +2,31 @@
 
 var db = require('../lib/utils/db');
 
-var sql_up   = 'SOME SQL TO DO';
-var sql_down = 'SOME SQL TO UNDO';
+var sql_up = 'CREATE TABLE public.verification_codes\
+(\
+  id uuid DEFAULT uuid_generate_v1(),\
+  code character(5),\
+  user_id uuid,\
+  CONSTRAINT verificationCode_user_id_fkey FOREIGN KEY (user_id)\
+REFERENCES public.users (id) MATCH SIMPLE\
+ON UPDATE NO ACTION ON DELETE NO ACTION\
+)\
+WITH (\
+  OIDS=FALSE\
+);\
+ALTER TABLE public.tokens\
+OWNER TO postgres;\
+CREATE UNIQUE INDEX verificationCode_user_id_idx\
+ON public.verification_codes\
+USING btree\
+(user_id);';
+
+var sql_down = 'DROP TABLE public.verification_codes';
 
 var runSql = (sql) => {
   return (next) => {
-    db.conn( (err, client) => {
-      if(err)
+    db.conn((err, client) => {
+      if (err)
         return next(err);
 
       return client.query(sql, next);
