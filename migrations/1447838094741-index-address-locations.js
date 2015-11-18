a3a2d5764d9776b7e0ad794c0b036b1409111389
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-18 10:16:18 +0100

    Index address locations

---
+++ migrations/1447838094741-index-address-locations.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var db = require('../lib/utils/db');
+
+var sql_up   = 'CREATE INDEX addresses_location_gix ON addresses USING gist(location);';
+var sql_down = 'DROP INDEX addresses_location_gix;';
+
+var runSql = (sql) => {
+  return (next) => {
+    db.conn( (err, client) => {
+      if(err)
+        return next(err);
+
+      return client.query(sql, next);
+    });
+  };
+};
+
+exports.up = runSql(sql_up);
+exports.down = runSql(sql_down);
