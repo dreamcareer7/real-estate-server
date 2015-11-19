c49a52d7ab4d3852890e3812516cdbdfee6f3cb9
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-19 21:23:05 +0330

    Added patch for profile_image_url and cover_image_url on PATCH /contacts/:id/profile_image_url and /contacts/:id/cover_image_url

---
+++ lib/sql/contact/patch.sql
@@ -0,0 +1,4 @@
+UPDATE contacts
+SET profile_image_url = CASE WHEN $2 = 'Profile' THEN $3 ELSE profile_image_url END,
+    cover_image_url = CASE WHEN $2 = 'Cover' THEN $3 ELSE cover_image_url END
+WHERE id = $1
