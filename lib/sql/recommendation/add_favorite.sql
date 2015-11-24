d24b0ae86d9541c7d6b28bfd76fa1b1e9fe35e8f
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-24 23:59:39 +0330

    Revising ack, add_favorite, add_read and add_tour with ON CONFLICT feature.

--- lib/sql/recommendation/add_favorite.sql
+++ lib/sql/recommendation/add_favorite.sql
@@ -3,9 +3,5 @@ INSERT INTO recommendations_eav(
                                 recommendation,
                                 action
                                )
-SELECT v.* FROM (VALUES($1::uuid, $2::uuid, 'Favorited'::recommendation_eav_action)) AS v(u, r, a)
-LEFT JOIN recommendations_eav t ON
-    t."user" = v.u AND
-    t.recommendation = v.r AND
-    t.action = v.a
-WHERE t.recommendation IS NULL
+VALUES($1, $2, 'Favorited'::recommendation_eav_action)
+ON CONFLICT DO NOTHING;
