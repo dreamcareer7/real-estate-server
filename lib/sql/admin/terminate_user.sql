b2454874829bfc08b972225dbc64f3fa6e4037cc
dreamcareer7 <dreamcareer070@gmail.com>
2015-11-24 19:20:44 +0330

    Added user delete facility for Dan. -r1

--- lib/sql/admin/terminate_user.sql
+++ lib/sql/admin/terminate_user.sql
@@ -8,8 +8,14 @@ delete_notification_tokens AS (
 delete_notification_acks AS (
     DELETE FROM notifications_acks WHERE "user" = $1
 ),
-delete_invitation_records AS (
+delete_inviting_records AS (
     DELETE FROM invitation_records WHERE invited_user = $1
+),
+delete_invited_records AS (
+    DELETE FROM invitation_records WHERE inviting_user = $1
+),
+delete_rec_eav AS (
+    DELETE FROM recommendations_eav WHERE "user" = $1
 )
 
 DELETE FROM users WHERE id = $1
