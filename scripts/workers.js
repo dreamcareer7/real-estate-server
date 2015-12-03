f53432ea50f64027e9a92ee104d18eea9d7c48fd
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-03 20:32:22 +0330

    Fixed an issue on notifications causing circular dependency. Wrapping notifications the right way.

--- scripts/workers.js
+++ scripts/workers.js
@@ -5,17 +5,17 @@ require('colors');
 var config = require('../lib/config.js');
 var queue  = require('../lib/utils/queue.js');
 
-queue.process('airship_transport_send', config.airship.parallel, function(job, done) {
-  console.log('<- Processed an Airship push notification'.green);
-  Notification.send(job.data.user_id, job.data.room_id, job.data.airship_notification, done);
+queue.process('airship_transport_send_device', config.airship.parallel, function(job, done) {
+  console.log('-> Processed an Airship push notification'.green);
+  Notification.sendToDevice(job.data.notification, job.data.token, done);
 });
 
 queue.process('email', config.email.parallel, function(job, done) {
-  console.log('<- Processed a SES email'.cyan);
+  console.log('-> Processed a SES email'.cyan);
   SES.callSES(job.data, done);
 });
 
 queue.process('sms', config.twilio.parallel, function(job, done) {
-  console.log('<- Processed a Twilio SMS'.magenta);
+  console.log('-> Processed a Twilio SMS'.magenta);
   Twilio.callTwilio(job.data, done);
 });
