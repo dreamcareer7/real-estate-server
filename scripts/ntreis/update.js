b42beb7a8b3a31fdee73b381a4a4b93528c44717
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-05 02:04:14 +0100

    Report to DataDogs properly

--- scripts/ntreis/update.js
+++ scripts/ntreis/update.js
@@ -8,18 +8,14 @@ var program = require('commander');
 var util = require('util');
 var request = require('request');
 var config = require('../../lib/config.js');
+var metrics = require('datadog-metrics');
 
-var StatsD = require('node-dogstatsd').StatsD;
+var metrics_enabled = !!config.datadogs.api_key;
 
-var enable_stats = config.dogstatsd && config.dogstatsd.host && config.dogstatsd.port;
-
-if(enable_stats)
-  var stats = new StatsD(config.dogstatsd.host, config.dogstatsd.port);
-
-console.log('Stats', enable_stats, config.dogstatsd, stats);
-
-// if(enable_stats)
-//   stats.socket.on('err', e => console.log(e));
+if(metrics_enabled)
+  metrics.init({
+    apiKey:config.datadogs.api_key
+  });
 
 program.version(config.ntreis.version)
 .option('-e, --enable-recs', 'Enable recommending listings to matching alerts')
@@ -145,13 +141,8 @@ var considerExit = () => {
 var initialCompleted = false;
 
 function processResponse(err) {
-  if(enable_stats) {
-    stats.gauge('ntreis.total_items', counts.total);
-
-    console.log(stats.socket);
-    if(stats.socet)
-      stats.socket.on('error', err => console.log(err));
-  }
+  if(metrics_enabled)
+    metrics.gauge('ntreis.total_items', counts.total);
 
   console.log('Total Running Time:', (getElapsed()/1000) + 's');
   var text;
