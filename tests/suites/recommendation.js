0c7476f1c4c3e5777fe6ee9ea58ffb48fd09edff
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-06 15:54:49 +0330

    improved coverage of recommendation suite and bugfix

--- tests/suites/recommendation.js
+++ tests/suites/recommendation.js
@@ -13,7 +13,8 @@ var feed = (cb) => {
         {
           type: 'recommendation'
         }
-      ]
+      ],
+      info: {}
     });
 }
 
@@ -24,31 +25,11 @@ var getFavorites = (cb) => {
     .expectStatus(200)
     .expectJSON({
       code: 'OK',
-      data: []
-    })
-    .expectJSONLength('data', 0);
-}
-
-var getTours = (cb) => {
-  return frisby.create('get tours')
-    .get('/rooms/' + results.room.create.data.id + '/recs/tours')
-    .after(cb)
-    .expectStatus(200)
-    .expectJSON({
-      code: 'OK',
-      data: []
-    })
-    .expectJSONLength('data', 0);
-}
-
-var getActives = (cb) => {
-  return frisby.create('get actives')
-    .get('/rooms/' + results.room.create.data.id + '/recs/actives')
-    .after(cb)
-    .expectStatus(200)
-    .expectJSON({
-      code: 'OK',
-      data: []
+      data: [],
+      info:{
+        count:0,
+        total:0
+      }
     })
     .expectJSONLength('data', 0);
 }
@@ -56,27 +37,6 @@ var getActives = (cb) => {
 var markAsFavorite = (cb) => {
   return frisby.create('favorite a rec')
     .patch('/rooms/' + results.room.create.data.id + '/recs/' + results.recommendation.feed.data[1].id + '/favorite', {
-      favorite: true
-    })
-    .after(cb)
-    .expectStatus(200)
-}
-
-var markAsFavoriteWorked = (cb) => {
-  return frisby.create('make sure favorite was successful')
-    .get('/rooms/' + results.room.create.data.id + '/recs/favorites')
-    .after(cb)
-    .expectStatus(200)
-    .expectJSON({
-      code: 'OK',
-      data: [results.recommendation.feed.data[1]]
-    })
-    .expectJSONLength('data', 1);
-}
-
-var markAsTour = (cb) => {
-  return frisby.create('tour a rec')
-    .patch('/rooms/' + results.room.create.data.id + '/recs/' + results.recommendation.feed.data[1].id + '/tour', {
       favorite: true
     })
     .after(cb)
@@ -89,6 +49,85 @@ var markAsTour = (cb) => {
     });
 }
 
+var markAsFavoriteWorked = (cb) => {
+  return frisby.create('make sure favorite was successful')
+    .get('/rooms/' + results.room.create.data.id + '/recs/favorites')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [results.recommendation.feed.data[1]],
+      info:{
+        count:1,
+        total:1
+      }
+    })
+    .expectJSONLength('data', 1);
+}
+
+var getTours = (cb) => {
+  return frisby.create('get tours')
+    .get('/rooms/' + results.room.create.data.id + '/recs/tours')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [],
+      info:{
+        count:0,
+        total:0
+      }
+    })
+    .expectJSONLength('data', 0);
+}
+
+var markAsTour = (cb) => {
+  return frisby.create('tour a rec')
+    .patch('/rooms/' + results.room.create.data.id + '/recs/' + results.recommendation.feed.data[1].id + '/tour', {
+      tour: true
+    })
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        type: 'recommendation'
+      }
+    });
+}
+
+var getActives = (cb) => {
+  return frisby.create('get actives')
+    .get('/rooms/' + results.room.create.data.id + '/recs/actives')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [],
+      info:{
+        count:0,
+        total:0
+      }
+    })
+    .expectJSONLength('data', 0);
+}
+
+var markAsTourWorked = (cb) => {
+  return frisby.create('get tours')
+    .get('/rooms/' + results.room.create.data.id + '/recs/tours')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [],
+      info:{
+        count:1,
+        total:1
+      }
+    })
+    .expectJSONLength('data', 1);
+}
+
 var seen = (cb) => {
   return frisby.create('get seen')
     .get('/rooms/' + results.room.create.data.id + '/recs/seen')
@@ -96,7 +135,11 @@ var seen = (cb) => {
     .expectStatus(200)
     .expectJSON({
       code: 'OK',
-      data: []
+      data: [],
+      info:{
+        count:0,
+        total:0
+      }
     })
     .expectJSONLength('data', 0);
 }
@@ -151,6 +194,19 @@ var recommendManually = (cb) => {
     });
 }
 
+var recommendManuallyWorked = (cb) => {
+  return frisby.create('make sure recommendManually was successful')
+    .get('/rooms/' + results.room.create.data.id + '/recs/' + results.recommendation.feed.data[1].id)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        type: 'recommendation'
+      }
+    });
+}
+
 var bulkRecommendManually = (cb) => {
   return frisby.create('bulk recommend manually')
     .post('/recs', {
@@ -164,19 +220,6 @@ var bulkRecommendManually = (cb) => {
     });
 }
 
-var getRecommendation = (cb) => {
-  return frisby.create('get a recommendation')
-    .get('/rooms/' + results.room.create.data.id + '/recs/' + results.recommendation.feed.data[1].id)
-    .after(cb)
-    .expectStatus(200)
-    .expectJSON({
-      code: 'OK',
-      data: {
-        type: 'recommendation'
-      }
-    });
-}
-
 module.exports = {
   feed,
   getFavorites,
@@ -185,10 +228,11 @@ module.exports = {
   markAsFavorite,
   markAsFavoriteWorked,
   markAsTour,
+  markAsTourWorked,
   seen,
   markAsSeen,
   markAsSeenWorked,
   recommendManually,
-  bulkRecommendManually,
-  getRecommendation
+  recommendManuallyWorked,
+  bulkRecommendManually
 }
