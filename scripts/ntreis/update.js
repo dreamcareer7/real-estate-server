9c708a7d4202b08e54cd2e54f6d5bbed71435352
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-08 08:32:32 +0100

    show it in notice

--- scripts/ntreis/update.js
+++ scripts/ntreis/update.js
@@ -31,6 +31,7 @@ program.version(config.ntreis.version)
 
 (function notice() {
   console.log('NTREIS connector'.cyan, config.ntreis.version.cyan);
+  console.log('NTREIS url'.cyan, config.ntreis.login_url.cyan);
   console.log('Instant Recommendation:'.yellow, (program.enableRecs) ? 'yes'.green : 'no'.red);
   console.log('Photo Fetching:'.yellow, (program.enablePhotoFetch) ? 'yes'.green : 'no'.red);
   console.log('Show CloudFront Links:'.yellow, (program.enableCfLinks) ? 'yes'.green : 'no'.red);
