c0a6e15fda188095a2cbf8bb089f8a3b96ccc722
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-11 01:29:29 +0100

    Fixed syntax error

--- migrations/1449791716714-new_rets_structure.js
+++ migrations/1449791716714-new_rets_structure.js
@@ -5,13 +5,13 @@ var async = require('async');
 var db = require('../lib/utils/db');
 
 var sqls = [
-  'ALTER TABLE raw_listings RENAME TO mls_data'
-  'ALTER TABLE mls_data RENAME listing  TO value'
-  'ALTER TABLE mls_data ADD class varchar'
-  'ALTER TABLE mls_data ADD resource varchar'
-  'ALTER TABLE mls_data ADD matrix_unique_id integer'
-  'ALTER TABLE mls_data ADD CONSTRAINT matrix_unique_id UNIQUE (matrix_unique_id)'
-  'ALTER TABLE ntreis_jobs ADD class varchar'
+  'ALTER TABLE raw_listings RENAME TO mls_data',
+  'ALTER TABLE mls_data RENAME listing  TO value',
+  'ALTER TABLE mls_data ADD class varchar',
+  'ALTER TABLE mls_data ADD resource varchar',
+  'ALTER TABLE mls_data ADD matrix_unique_id integer',
+  'ALTER TABLE mls_data ADD CONSTRAINT matrix_unique_id UNIQUE (matrix_unique_id)',
+  'ALTER TABLE ntreis_jobs ADD class varchar',
   'ALTER TABLE ntreis_jobs ADD resource varchar'
 ];
 
