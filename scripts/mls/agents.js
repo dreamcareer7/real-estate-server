a768f94722b3ada578db5d250bf9ea23e88f6517
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-12 18:43:04 +0100

    Basic agent processing

--- scripts/mls/agents.js
+++ scripts/mls/agents.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
+var async   = require('async');
 var Client = require('./rets_client.js');
 
 var program = require('./program.js')
 var options = program.parse(process.argv);
 
+
 options.resource = 'Agent';
 options.class = 'Agent';
+options.processor = processData;
 
 Client.work(options, report);
 
-function report() {
+function processData(cb, results) {
+  async.mapLimit(results.mls, 100, insertAgent, cb);
+}
+
+function insertAgent(photo, cb) {
+  Client.increment('processed_agent');
+  Agent.create({
+    matrix_unique_id:parseInt(photo.matrix_unique_id),
+    listing_mui:parseInt(photo.Table_MUI),
+    description:photo.Description,
+    order:parseInt(photo.Order)
+  }, cb);
+}
+
+function report(err) {
+  console.log('Exiting', err);
   process.exit();
 }
