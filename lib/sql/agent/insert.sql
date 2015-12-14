5164d775fa462ad02cf338372219af4da965639c
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-14 15:30:01 +0100

    On conflict, update

--- lib/sql/agent/insert.sql
+++ lib/sql/agent/insert.sql
@@ -21,3 +21,20 @@ VALUES
 (
   $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16
 )
+ON CONFLICT (matrix_unique_id) DO UPDATE SET
+  email = $1,
+  mlsid = $2,
+  fax = $3,
+  full_name = $4,
+  first_name = $5,
+  last_name = $6,
+  middle_name = $7,
+  phone_number = $8,
+  nar_number = $9,
+  office_mui = $10,
+  status = $11,
+  office_mlsid = $12,
+  work_phone = $13,
+  generational_name = $14,
+  matrix_modified_dt = $16
+  WHERE agents.matrix_unique_id = $15
