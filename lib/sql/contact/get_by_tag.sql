a9a000e0b7a1016db26e09be1c9ed32cd1f78e16
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-18 08:28:12 +0330

    excluded deleted contacts from response in getByTag

--- lib/sql/contact/get_by_tag.sql
+++ lib/sql/contact/get_by_tag.sql
@@ -1,4 +1,6 @@
 SELECT contacts.id FROM contacts
 INNER JOIN tags
 ON contacts.id = tags.entity
-WHERE tags.type = $1 AND tags.tag =ANY($2::text[])
+WHERE contacts.deleted_at IS NULL AND
+  tags.type = $1 AND
+  tags.tag =ANY($2::text[])
