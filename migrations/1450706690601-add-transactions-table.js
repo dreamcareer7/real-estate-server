b790e246d21e2a9e4dc1e9eb6520a7f6c5d0c43e
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-23 18:07:45 +0330

    Barebone transaction implementation. -r2

--- migrations/1450706690601-add-transactions-table.js
+++ migrations/1450706690601-add-transactions-table.js
@@ -2,17 +2,13 @@
 
 var db = require('../lib/utils/db');
 
-var sql_up   = 'CREATE TABLE IF NOT EXISTS transactions(id uuid DEFAULT uuid_generate_v1(), room uuid NOT NULL REFERENCES rooms(id), title text, \
+var sql_up   = 'CREATE TABLE IF NOT EXISTS transactions(id uuid DEFAULT uuid_generate_v1(), "user" uuid NOT NULL REFERENCES users(id), title text, \
 recommendation uuid REFERENCES recommendations(id), listing uuid REFERENCES listings(id), listing_data jsonb, \
-transaction_type transaction_type NOT NULL, buyer uuid references contacts(id), seller uuid references contacts(id), \
-buyer_agent uuid REFERENCES contacts(id), seller_agent uuid REFERENCES contacts(id), transaction_status listing_status NOT NULL, \
+transaction_type transaction_type NOT NULL, transaction_status listing_status NOT NULL, \
 contract_price float, original_price float, sale_commission_rate float, buyer_sale_commission_split_share float, \
 seller_sale_commission_split_share float, buyer_sale_commission_split float, seller_sale_commission_split float, \
 broker_commission float, referral float, sale_commission_total float, earnest_money_amount float, earnest_money_held_by float, \
-escrow_number text, co_buyer_agent uuid REFERENCES contacts(id), co_seller_agent uuid REFERENCES contacts(id), \
-lawyer uuid REFERENCES contacts(id), lender uuid REFERENCES contacts(id), broker uuid REFERENCES contacts(id), \
-team_lead uuid REFERENCES contacts(id), appraiser uuid REFERENCES contacts(id), inspector uuid REFERENCES contacts(id), \
-created_at timestamptz DEFAULT NOW(), updated_at timestamptz DEFAULT NOW(), deleted_at timestamptz);';
+escrow_number text, created_at timestamptz DEFAULT NOW(), updated_at timestamptz DEFAULT NOW(), deleted_at timestamptz);';
 var sql_down = 'DROP TABLE IF EXISTS transactions;';
 
 var runSql = (sql) => {
