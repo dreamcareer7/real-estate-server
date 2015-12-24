c078cd146c31f6cbc7fadece99461df14eb58d8a
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-24 02:08:14 +0100

    This script helps with manually fetching a listing by its mui

---
+++ scripts/mls/get_listing.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var async   = require('async');
+var util    = require('util');
+var Client = require('./rets_client.js');
+
+var program = require('./program.js')
+  .option('--mui <n>', 'Get listing by its matrix unique id');
+
+var options = program.parse(process.argv);
+
+options.resource = 'Property';
+options.class = 'Listing';
+options.dontSave = true;
+options.query = '(Matrix_Unique_ID='+options.mui+')';
+Client.work(options, report);
+
+function report(cb, results) {
+  console.log(util.inspect(results.mls[0]))
+  process.exit();
+}
