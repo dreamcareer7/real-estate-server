cd5a0f1d137120735d38d767e3e97b5bfd054310
dreamcareer7 <dreamcareer070@gmail.com>
2015-12-31 02:27:23 +0330

    Possible fix for S3 fuckup.

--- lib/sql/room/get.sql
+++ lib/sql/room/get.sql
@@ -3,7 +3,17 @@ SELECT 'room' AS TYPE,
        EXTRACT(EPOCH FROM created_at) AS created_at,
        EXTRACT(EPOCH FROM updated_at) AS updated_at,
        EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
-       (SELECT id FROM messages WHERE room = $1 ORDER BY created_at DESC LIMIT 1) AS latest_message
+       (SELECT id FROM messages WHERE room = $1 ORDER BY created_at DESC LIMIT 1) AS latest_message,
+       (
+         WITH t AS (
+                     SELECT array_agg("user"::text) u,
+                            array_agg(push_enabled::text) p
+                     FROM rooms_users
+                     WHERE room = $1
+                   )
+         SELECT json_object(t.u, t.p)
+         FROM t
+       ) AS notification_settings
 FROM rooms
 WHERE id = $1
 LIMIT 1
