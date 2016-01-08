674477d055fbe241500f8dc2a2154a7a1335962e
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-08 15:31:49 +0100

    created at for agents

---
+++ migrations/1452263321920-created_at-for-agents.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var db = require('../lib/utils/db');
+
+var sql_up   = 'DO $$ BEGIN ALTER TABLE agents ADD COLUMN created_at TIMESTAMPTZ DEFAULT NOW(); EXCEPTION WHEN duplicate_column THEN END; $$;';
+var sql_down = 'ALTER TABLE IF EXISTS agents DROP COLUMN IF EXISTS created_at';
+
+var runSql = (sql) => {
+  return (next) => {
+    db.conn( (err, client) => {
+      if(err)
+        return next(err);
+
+      return client.query(sql, next);
+    });
+  };
+};
+
+exports.up = runSql(sql_up);
+exports.down = runSql(sql_down);
