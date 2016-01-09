c92ab6f7f3b8afc2e421c47a60dce4a5c648b2eb
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-09 14:52:23 +0330

    Search now returns more relevant results.

--- lib/sql/listing/string_search.sql
+++ lib/sql/listing/string_search.sql
@@ -4,18 +4,23 @@ INNER JOIN addresses ON
     listings.matrix_unique_id = addresses.matrix_unique_id
 WHERE
     (
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.title ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.subtitle ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.street_number ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.street_name ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.city ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.state ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.state_code ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.street_suffix ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.country::text ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.country_code::text ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.street_dir_prefix ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE addresses.street_dir_suffix ~* ANY($1) END) OR
-     (CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NULL THEN TRUE ELSE listings.mls_number ~* ANY($1) END)
+     CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NOT NULL THEN
+     (
+      (
+       addresses.title || ' ' ||
+       addresses.subtitle || ' ' ||
+       addresses.street_number || ' ' ||
+       addresses.street_name || ' ' ||
+       addresses.city || ' ' ||
+       addresses.state || ' ' ||
+       addresses.state_code || ' ' ||
+       addresses.street_suffix || ' ' ||
+       addresses.country::text || ' ' ||
+       addresses.country_code::text || ' ' ||
+       addresses.street_dir_prefix || ' ' ||
+       addresses.street_dir_suffix || ' ' ||
+       listings.mls_number
+      ) ~* ALL($1)
+     ) ELSE TRUE END
     )
 LIMIT 10
