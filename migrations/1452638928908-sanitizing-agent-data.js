9d69442f608da1d7568e2f955e5926c00d711bb3
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-13 02:25:25 +0330

    Sanitizing agent data

---
+++ migrations/1452638928908-sanitizing-agent-data.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var async = require('async');
+var db = require('../lib/utils/db');
+
+var up = [
+  'ALTER TABLE agents ALTER COLUMN email DROP NOT NULL;',
+  'UPDATE agents SET email = CASE WHEN LENGTH(email) = 0 THEN NULL ELSE email END, \
+fax = CASE WHEN LENGTH(fax) = 0 THEN NULL ELSE fax END, \
+full_name = CASE WHEN LENGTH(full_name) = 0 THEN NULL ELSE full_name END, \
+first_name = CASE WHEN LENGTH(first_name) = 0 THEN NULL ELSE first_name END, \
+last_name = CASE WHEN LENGTH(last_name) = 0 THEN NULL ELSE last_name END, \
+middle_name = CASE WHEN LENGTH(middle_name) = 0 THEN NULL ELSE middle_name END, \
+phone_number = CASE WHEN LENGTH(phone_number) = 0 THEN NULL ELSE phone_number END, \
+nar_number = CASE WHEN LENGTH(nar_number) = 0 THEN NULL ELSE nar_number END, \
+work_phone = CASE WHEN LENGTH(work_phone) = 0 THEN NULL ELSE work_phone END, \
+generational_name = CASE WHEN LENGTH(generational_name) = 0 THEN NULL ELSE generational_name END;'
+];
+
+var down = [
+  'ALTER TABLE agents ALTER COLUMN email SET NOT NULL;'
+];
+
+var runAll = (sqls, next) => {
+  db.conn( (err, client) => {
+    if(err)
+      return next(err);
+
+    async.eachSeries(sqls, client.query.bind(client), next);
+  });
+};
+
+var run = (queries) => {
+  return (next) => {
+    runAll(queries, next);
+  };
+};
+
+exports.up = run(up);
+exports.down = run(down);
