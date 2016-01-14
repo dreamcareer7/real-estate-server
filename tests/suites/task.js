1081a6bd6e00b100de4379739cf822386a50bd5c
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-14 15:12:49 +0330

    added task suite

---
+++ tests/suites/task.js
@@ -0,0 +1,79 @@
+var uuid = require('node-uuid');
+
+registerSuite('contact', ['create']);
+
+var create = (cb) => {
+  return frisby.create('create new task')
+    .post('/tasks', {
+      user: results.contact.create.data[0].contact_user.id,
+      title: 'NewTask'
+    })
+    .after(cb)
+    .expectStatus(200)
+}
+
+var assign = (cb) => {
+  return frisby.create('assign contact to a task')
+    .post('/tasks/' + results.task.create.data.id + '/contacts', {
+      contacts:[results.contact.create.data[0].id]
+    })
+    .after(cb)
+    .expectStatus(200)
+}
+
+var assign400 = (cb) => {
+  return frisby.create('assign contact to a task')
+    .post('/tasks/' + results.task.create.data.id + '/contacts')
+    .after(cb)
+    .expectStatus(400)
+}
+
+var getTask = (cb) => {
+  return frisby.create('get task by id')
+    .get('/tasks/' + results.task.create.data.id)
+    .after(cb)
+    .expectStatus(200)
+}
+
+var getUserTasks = (cb) => {
+  return frisby.create('get user\'s task')
+    .get('/tasks')
+    .after(cb)
+    .expectStatus(200)
+}
+
+var patchTask = (cb) => {
+  return frisby.create('update a task')
+    .put('/tasks/' + results.task.create.data.id,{
+    user: results.contact.create.data[0].contact_user.id,
+      title: 'UpdatedTask'
+  })
+    .after(cb)
+    .expectStatus(200)
+}
+
+var withdraw = (cb) => {
+  return frisby.create('withdraw contact from a task')
+    .delete('/tasks/' + results.task.create.data.id + '/contacts/' + results.contact.create.data[0].id)
+    .after(cb)
+    .expectStatus(200)
+}
+
+var deleteTask = (cb) => {
+  return frisby.create('delete a task')
+    .delete('/tasks/' + results.task.create.data.id)
+    .after(cb)
+    .expectStatus(204)
+}
+
+
+module.exports = {
+  create,
+  assign,
+  assign400,
+  getTask,
+  getUserTasks,
+  patchTask,
+  withdraw,
+  deleteTask
+}
