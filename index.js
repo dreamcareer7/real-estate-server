f8f9966acade7f7d5b4be192f7433ab9b2c06d2b
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-16 01:31:50 +0100

    Allow auth

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = require('./lib/bootstrap.js')();
 // For dev only
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
