a1929f8e42fe6c819dd0e888a5d9acf025289432
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-16 19:14:46 +0330

    Seamless communication post on Message model + controllers.

--- scripts/workers.js
+++ scripts/workers.js
@@ -6,16 +6,16 @@ var config = require('../lib/config.js');
 var queue  = require('../lib/utils/queue.js');
 
 queue.process('airship_transport_send_device', config.airship.parallel, function(job, done) {
-  console.log('-> Processed an Airship push notification'.green);
+  console.log('-> Processed a push notification'.green);
   Notification.sendToDevice(job.data.notification, job.data.token, done);
 });
 
 queue.process('email', config.email.parallel, function(job, done) {
-  console.log('-> Processed a SES email'.cyan);
-  SES.callSES(job.data, done);
+  console.log('-> Processed an email'.cyan);
+  Mailgun.callMailgun(job.data, done);
 });
 
 queue.process('sms', config.twilio.parallel, function(job, done) {
-  console.log('-> Processed a Twilio SMS'.magenta);
+  console.log('-> Processed an SMS'.magenta);
   Twilio.callTwilio(job.data, done);
 });
