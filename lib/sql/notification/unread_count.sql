cb06a9f004a79e3d55bc5847d91f433148aa5ea9
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-22 00:51:16 +0330

    Finally, found the issue with Notification collection response and summary endpoint response.

--- lib/sql/notification/unread_count.sql
+++ lib/sql/notification/unread_count.sql
@@ -12,12 +12,14 @@ WITH rn AS (
   FROM notifications
   FULL JOIN notifications_acks
     ON notifications.id = notifications_acks.notification
-  WHERE notifications.room = ANY (
-    SELECT room
-    FROM rooms_users
-    WHERE "user" = $1
-  ) AND notifications_acks.id IS NULL
-    AND notifications.room IS NOT NULL
+  INNER JOIN rooms
+    ON notifications.room = rooms.id
+  WHERE notifications.room = ANY (SELECT room FROM rooms_users WHERE "user" = $1) AND
+    notifications_acks.id IS NULL AND
+    notifications.room IS NOT NULL AND
+    rooms.deleted_at IS NULL AND
+    (COALESCE(notifications.exclude <> $1, TRUE) OR
+    COALESCE(notifications.specific = $1, FALSE))
   GROUP BY notifications.room
 ) SELECT 'notification_summary' AS type,
          0 AS task_notification_count,
