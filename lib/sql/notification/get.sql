05500f247448a9a486243f1e7a9cab1674d9d02e
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-26 19:54:16 +0330

    Fixed notification_type to new format -r1

--- lib/sql/notification/get.sql
+++ lib/sql/notification/get.sql
@@ -1,7 +1,7 @@
 SELECT
     'notification' AS type,
     *,
-    notification.subject_class::text || notification.action::text || notification.object_class::text AS notification_type,
+    notifications.subject_class::text || notifications.action::text || notifications.object_class::text AS notification_type,
     EXTRACT(EPOCH FROM created_at) AS created_at,
     EXTRACT(EPOCH FROM updated_at) AS updated_at,
     EXTRACT(EPOCH FROM deleted_at) AS deleted_at
