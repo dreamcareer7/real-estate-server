cf460b3a4e63f69d149ed90048f92fcd758a1257
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-30 23:42:48 +0100

    Farewell XMPP.

--- lib/sql/xmpp/check_passwd.sql
+++
@@ -1,6 +0,0 @@
-SELECT *
-FROM tokens
-WHERE user_id = $1
-  AND token = $2
-  AND type = 'access'
-  AND expire_date > NOW()
--- lib/sql/xmpp/get_passwd.sql
+++
@@ -1,6 +0,0 @@
-SELECT token
-FROM tokens
-WHERE user_id = $1
-  AND type = 'access'
-ORDER BY expire_date DESC
-LIMIT 1
