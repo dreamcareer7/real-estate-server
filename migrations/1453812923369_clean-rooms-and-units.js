204b2d48b0a8a78e3e957a4a529b2fa53073482b
dreamcareer7 <dreamcareer070@gmail.com>
2016-01-31 01:01:06 +0100

    Rooms and Units are finally fully functional

---
+++ migrations/1453812923369_clean-rooms-and-units.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var async = require('async');
+var db = require('../lib/utils/db');
+
+var up = [
+  'ALTER TABLE property_rooms DROP COLUMN listing',
+  'ALTER TABLE property_rooms RENAME COLUMN type TO room_type',
+  'ALTER TABLE units DROP COLUMN listing',
+  'ALTER TABLE units RENAME COLUMN square_meters TO square_feet'
+  'ALTER TABLE units RENAME TO property_units'
+];
+
+var down = [
+  'ALTER TABLE property_units RENAME TO units',
+  'ALTER TABLE property_rooms RENAME COLUMN room_type TO type',
+  'ALTER TABLE property_rooms ADD COLUMN listing uuid',
+  'ALTER TABLE units ADD COLUMN listing uuid',
+  'ALTER TABLE units RENAME COLUMN square_feet TO square_meters'
+];
+
+var runAll = (sqls, next) => {
+  db.conn( (err, client) => {
+    if(err)
+      return next(err);
+
+    async.eachSeries(sqls, client.query.bind(client), next);
+  });
+};
+
+var run = (queries) => {
+  return (next) => {
+    runAll(queries, next);
+  };
+};
+
+exports.up = run(up);
+exports.down = run(down);
--- scripts/mls/units.js
+++ scripts/mls/units.js
@@ -7,19 +7,16 @@ var util = require('util');
 var config = require('../../lib/config.js');
 require('../../lib/models/index.js')();
 
-Error.autoReport = false;
-
 var program = require('./program.js')
-  .option('-d, --download-concurency <n>', 'Download (From RETS) concurrency');
 var options = program.parse(process.argv);
-
-options.process = true;
 
 options.resource = 'PropertySubTable';
 options.class = 'Unit';
-
-if (options.process)
-  options.processor = processData;
+options.job = 'units';
+options.fields = {
+  id:'matrix_unique_id'
+}
+options.processor = processData;
 
 Client.work(options, report);
 
@@ -29,78 +26,34 @@ function processData(cb, results) {
 
 function map(mls_unit) {
   return {
-    dining_length: parseInt(mls_unit.DiningAreaLength),
-    dining_width: parseInt(mls_unit.DiningAreaWidth),
-    kitchen_length: parseInt(mls_unit.KitchenLength),
-    kitchen_width: parseInt(mls_unit.KitchenWidth),
-    lease: parseInt(mls_unit.Lease),
-    listing_mui: parseInt(mls_unit.Listing_MUI),
-    listing: mls_unit.listing,
-    living_length: parseInt(mls_unit.LivingAreaLength),
-    living_width: parseInt(mls_unit.LivingAreaWidth),
-    master_length: parseInt(mls_unit.MasterBedLength),
-    master_width: parseInt(mls_unit.MasterBedWidth),
-    matrix_unique_id: parseInt(mls_unit.matrix_unique_id),
+    dining_length: parseInt(mls_unit.DiningAreaLength) || null,
+    dining_width: parseInt(mls_unit.DiningAreaWidth) || null,
+    kitchen_length: parseInt(mls_unit.KitchenLength) || null,
+    kitchen_width: parseInt(mls_unit.KitchenWidth) || null,
+    lease: parseInt(mls_unit.Lease) || null,
+    listing_mui: parseInt(mls_unit.Listing_MUI) || null,
+    living_length: parseInt(mls_unit.LivingAreaLength) || null,
+    living_width: parseInt(mls_unit.LivingAreaWidth) || null,
+    master_length: parseInt(mls_unit.MasterBedLength) || null,
+    master_width: parseInt(mls_unit.MasterBedWidth) || null,
+    matrix_unique_id: parseInt(mls_unit.matrix_unique_id) ,
     matrix_modified_dt: mls_unit.MatrixModifiedDT,
-    full_bath: parseInt(mls_unit.NumberOfBathsFull),
-    half_bath: parseInt(mls_unit.NumberOfBathsHalf),
-    beds: parseInt(mls_unit.NumberOfBeds),
-    units: parseInt(mls_unit.NumberOfUnits),
-    square_meters: parseInt(mls_unit.SqFt)
+    full_bath: parseInt(mls_unit.NumberOfBathsFull) || null,
+    half_bath: parseInt(mls_unit.NumberOfBathsHalf) || null,
+    beds: parseInt(mls_unit.NumberOfBeds) || null,
+    units: parseInt(mls_unit.NumberOfUnits) || null,
+    square_feet: parseInt(mls_unit.SqFt) || null
   }
 }
 
 var upsert = function (unit, cb) {
-  Metric.increment('mls.processed_units');
-  Listing.getByMUI(unit.Listing_MUI, function (err, listing) {
-    if (listing)
-      unit.listing = listing.id;
-
-    Unit.getByMUI(unit.matrix_unique_id, function (err, id) {
-      if (err && err.code !== 'ResourceNotFound')
-        return cb(err);
-
-      if (err && err.code === 'ResourceNotFound') {
-        Metric.increment('mls.new_units');
-
-        Unit.create(map(unit), cb);
-        return;
-      }
-
-      Metric.increment('mls.updated_units');
-      Unit.update(id, map(unit), cb);
-    });
-  });
-
+  Metric.increment('mls.processed_unit');
+  PropertyUnit.create(map(unit), cb);
 }
 
-var firstId, lastId = null;
+function report(err) {
+  if(err)
+    console.log(err);
 
-Client.on('data fetched', (data) => {
-  Client.rets.logout(); // We're done for the moment. Release the connection.
-
-  firstId = data[0].Matrix_Unique_ID;
-  lastId = data[data.length - 1].Matrix_Unique_ID;
-});
-
-function report() {
-  Metric.flush();
-
-  var text = [
-    'Execution time: %d seconds',
-    'Total items: %d',
-    'First item: %s',
-    'Last item: %s',
-    'Units: %s new, %s updated',
-    '----------------------------------'
-  ].join('\n');
-
-  text = util.format(text,
-    slack.elapsed() / 1000,
-    Metric.get('mls.processed_units'),
-    firstId,
-    lastId,
-    Metric.get('mls.new_units'), Metric.get('mls.updated_units')
-  );
-  console.log(text);
+  process.exit();
 }
