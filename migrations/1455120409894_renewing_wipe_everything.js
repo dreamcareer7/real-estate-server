5854ae6e5d8a8964e045e106155c23ffe353673a
dreamcareer7 <dreamcareer070@gmail.com>
2016-02-10 19:53:04 +0330

    Updated wipe_everything() function

---
+++ migrations/1455120409894_renewing_wipe_everything.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var async = require('async');
+var db = require('../lib/utils/db');
+
+var up = [
+'CREATE OR REPLACE FUNCTION wipe_everything() RETURNS VOID AS $$\
+BEGIN\
+    TRUNCATE TABLE messages_acks CASCADE;\
+    TRUNCATE TABLE messages CASCADE;\
+    TRUNCATE TABLE notifications_acks CASCADE;\
+    TRUNCATE TABLE notifications CASCADE;\
+    TRUNCATE TABLE recommendations_eav CASCADE;\
+    TRUNCATE TABLE recommendations CASCADE;\
+    TRUNCATE TABLE rooms_users CASCADE;\
+    TRUNCATE TABLE rooms CASCADE;\
+    TRUNCATE TABLE invitation_records CASCADE;\
+    TRUNCATE TABLE alerts CASCADE;\
+    TRUNCATE TABLE contacts CASCADE;\
+    TRUNCATE TABLE notification_tokens CASCADE;\
+    TRUNCATE TABLE password_recovery_records CASCADE;\
+    TRUNCATE TABLE email_verifications CASCADE;\
+    TRUNCATE TABLE phone_verifications CASCADE;\
+    TRUNCATE TABLE attachments_eav CASCADE;\
+    TRUNCATE TABLE attachments CASCADE;\
+    TRUNCATE TABLE important_dates CASCADE;\
+    TRUNCATE TABLE notes CASCADE;\
+    TRUNCATE TABLE task_contacts CASCADE;\
+    TRUNCATE TABLE tasks CASCADE;\
+    TRUNCATE TABLE transaction_contact_roles CASCADE;\
+    TRUNCATE TABLE transaction_contacts CASCADE;\
+    TRUNCATE TABLE transactions CASCADE;\
+END;\
+$$ LANGUAGE plpgsql;'
+];
+
+var down = [
+  'UNDO SOMETHING',
+  'UNDO SOMETHING ELSE',
+  'UNDO EVEN MORE'
+];
+
+var runAll = (sqls, next) => {
+  db.conn( (err, client) => {
+    if(err)
+      return next(err);
+
+    async.eachSeries(sqls, client.query.bind(client), next);
+  });
+};
+
+var run = (queries) => {
+  return (next) => {
+    runAll(queries, next);
+  };
+};
+
+exports.up = run(up);
+exports.down = run(down);
