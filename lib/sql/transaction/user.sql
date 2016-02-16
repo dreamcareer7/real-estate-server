ae9c72ddfb49fdda59ab4f9c7528d645bcfcf958
dreamcareer7 <dreamcareer070@gmail.com>
2016-02-16 18:38:25 +0100

    Also show me transactions that are shared with me

--- lib/sql/transaction/user.sql
+++ lib/sql/transaction/user.sql
@@ -1,4 +1,9 @@
 SELECT id
 FROM transactions
-WHERE "user" = $1 AND
-      deleted_at IS NULL
+WHERE (
+  "user" = $1 OR
+  id IN(
+    SELECT transaction FROM transaction_contacts WHERE contact = $1
+    )
+  )
+  AND deleted_at IS NULL
