5d39e48ed081da20963ad3d4839520a50bceee43
dreamcareer7 <dreamcareer070@gmail.com>
2016-02-18 00:34:56 +0330

    Fixing an issue on Contact dup detection.

---
+++ lib/sql/contact/is_dup.sql
@@ -0,0 +1,5 @@
+SELECT COALESCE(BOOL_OR(contacts.email = LOWER($3::text)), FALSE) OR
+       COALESCE(BOOL_OR(contacts.phone_number = $4::text), FALSE) OR
+       COALESCE(BOOL_OR(contacts.contact_user = $2::uuid), FALSE) AS is_dup
+FROM contacts
+WHERE contacts."user" = $1
