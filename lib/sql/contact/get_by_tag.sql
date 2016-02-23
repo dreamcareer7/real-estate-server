ddc1ec6851654bce9e9a4dab40dced2b9615c4a2
dreamcareer7 <dreamcareer070@gmail.com>
2016-02-23 20:30:04 +0330

    Fixed GET /contacts?tags= behaviour causing other people's contacts showing up

--- lib/sql/contact/get_by_tag.sql
+++ lib/sql/contact/get_by_tag.sql
@@ -1,6 +1,8 @@
-SELECT contacts.id FROM contacts
+SELECT contacts.id
+FROM contacts
 INNER JOIN tags
 ON contacts.id = tags.entity
 WHERE contacts.deleted_at IS NULL AND
-  tags.type = $1 AND
-  tags.tag =ANY($2::text[])
+      CASE WHEN $3::uuid IS NULL THEN FALSE ELSE contacts."user" = $3 END AND
+      tags.type = $1 AND
+      tags.tag =ANY($2::text[])
