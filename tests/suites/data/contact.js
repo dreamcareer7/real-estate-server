8183643814ed2c90a73096a52e4772100ad0ed77
dreamcareer7 <dreamcareer070@gmail.com>
2016-03-17 17:47:58 +0330

    Migrations to rechat.com

--- tests/suites/data/contact.js
+++ tests/suites/data/contact.js
@@ -1,8 +1,8 @@
 module.exports = {
-  profile_image_url: 'http://rechat.co/profile_image_url/profile_image_url.png',
-  invitation_url: 'http://rechat.co/join',
+  profile_image_url: 'http://rechat.com/profile_image_url/profile_image_url.png',
+  invitation_url: 'http://rechat.com/join',
   company: 'rechat',
   birthday: 1000000,
-  cover_image_url: 'http://rechat.co/cover_image_url/cover_image_url.png',
+  cover_image_url: 'http://rechat.com/cover_image_url/cover_image_url.png',
   tags: [ 'New' ]
 };
