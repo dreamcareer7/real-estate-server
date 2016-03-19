afe3ab994af7fb4c331c129123f7b4be3dcb7602
dreamcareer7 <dreamcareer070@gmail.com>
2016-03-19 10:50:20 +0100

    Performance optimizations.Hopefully.

--- lib/sql/alert/matching.sql
+++ lib/sql/alert/matching.sql
@@ -8,13 +8,15 @@ WITH listing_muis AS
 address_ids AS
 (
   SELECT addresses.id FROM addresses
+  JOIN listing_muis ON addresses.matrix_unique_id = listing_muis.matrix_unique_id
   WHERE
     addresses.location IS NOT NULL AND
-    ST_Within(addresses.location, ST_SetSRID(ST_GeomFromText($14), 4326)) AND
-    matrix_unique_id IN( SELECT matrix_unique_id FROM listing_muis )
+    ST_Within(addresses.location, ST_SetSRID(ST_GeomFromText($14), 4326))
 ), property_ids AS
 (
-  SELECT id FROM properties WHERE
+  SELECT properties.id FROM properties
+  JOIN address_ids ON properties.address_id = address_ids.id
+  WHERE
     square_meters >= $3 AND
     square_meters <= $4 AND
     bedroom_count >= $5 AND
@@ -25,16 +27,16 @@ address_ids AS
     COALESCE(year_built <= $10, TRUE) = TRUE AND
     COALESCE(pool_yn = $11, TRUE) = TRUE AND
     COALESCE(lot_square_meters >= $12, TRUE) = TRUE AND
-    COALESCE(lot_square_meters <= $13, TRUE) = TRUE AND
-    address_id IN (SELECT id FROM address_ids)
+    COALESCE(lot_square_meters <= $13, TRUE) = TRUE
 )
 
 SELECT
   listings.id AS id,
   (COUNT(*) OVER())::INT AS total
-FROM listings WHERE
-  listings.property_id IN (SELECT * FROM property_ids)
-  AND (
+FROM listings
+  JOIN property_ids ON listings.property_id = property_ids.id
+WHERE
+  (
     ($15::boolean IS NULL OR $15::boolean = false) OR (
       SELECT count(*) > 0 FROM open_houses WHERE
       listing_mui = listings.matrix_unique_id AND start_time > NOW()
