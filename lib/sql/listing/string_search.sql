e4083b248f8b50d1cf51a9efcfd0934b1b9c7938
dreamcareer7 <dreamcareer070@gmail.com>
2016-03-22 18:12:04 +0100

    Implemented full text search for listing search

--- lib/sql/listing/string_search.sql
+++ lib/sql/listing/string_search.sql
@@ -1,32 +1,11 @@
-SELECT listings.id
-FROM listings
-INNER JOIN addresses ON
-    listings.matrix_unique_id = addresses.matrix_unique_id
+SELECT id
+FROM listings_filters
 WHERE
-    (
-     CASE WHEN ARRAY_LENGTH($1::text[], 1) IS NOT NULL THEN
-     (
-      (
-       addresses.title || ' ' ||
-       addresses.subtitle || ' ' ||
-       addresses.street_number || ' ' ||
-       addresses.street_name || ' ' ||
-       addresses.city || ' ' ||
-       addresses.state || ' ' ||
-       addresses.state_code || ' ' ||
-       addresses.street_suffix || ' ' ||
-       addresses.country::text || ' ' ||
-       addresses.country_code::text || ' ' ||
-       addresses.street_dir_prefix || ' ' ||
-       addresses.street_dir_suffix || ' ' ||
-       listings.mls_number
-      ) ~* ALL($1)
-     ) ELSE TRUE END
-    )
+    to_tsvector('english', address) @@ plainto_tsquery('english', $1)
     AND (
       CASE
         WHEN $2::listing_status[] IS NULL THEN TRUE
-        ELSE listings.status = ANY($2::listing_status[])
+        ELSE status = ANY($2::listing_status[])
       END
     )
 LIMIT 10
