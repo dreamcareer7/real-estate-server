6d5bb483e3850723e322c853c3c84d648d138854
dreamcareer7 <dreamcareer070@gmail.com>
2016-03-25 11:41:34 +0100

    Delete all verifications when user is done

--- lib/sql/verification/email_verify.sql
+++ lib/sql/verification/email_verify.sql
@@ -1,15 +1,15 @@
-WITH verified AS (
+WITH verification AS (
+  SELECT *
+  FROM email_verifications
+  WHERE
+    code  = $1 AND
+    email = $2 AND
+    ((NOW() - created_at) < '1 day'::interval)
+  LIMIT 1
+)
+,verified AS (
     UPDATE users
     SET email_confirmed = TRUE
-    WHERE email = $2 AND
-    EXISTS (
-        SELECT id
-        FROM email_verifications
-        WHERE code  = $1 AND
-              email = $2 AND
-              ((NOW() - created_at) < '1 day'::interval)
-    )
+    WHERE email = (SELECT email FROM verification)
 )
-DELETE FROM email_verifications
-WHERE code = $1 AND
-      email = $2
+DELETE FROM email_verifications WHERE email = (SELECT email FROM verification)
