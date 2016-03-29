40d5443142cef3cf002bfb6c60caaef60b6a4465
dreamcareer7 <dreamcareer070@gmail.com>
2016-03-29 19:23:56 +0200

    another try

--- lib/sql/agent/office_mls.sql
+++ lib/sql/agent/office_mls.sql
@@ -3,6 +3,8 @@ SELECT
 FROM agents
 WHERE
   office_mlsid IN(
-    SELECT mls_id FROM offices WHERE office_mls_id = $1
+    SELECT mls_id FROM offices WHERE office_mls_id = (
+      SELECT office_mls_id FROM offices WHERE mls_id = $1
+    )
   )
 ORDER BY first_name
