81d2552e24e27e557638835b369035ce643eff23
dreamcareer7 <dreamcareer070@gmail.com>
2016-05-06 18:01:40 +0430

    added calendar routines

--- lib/sql/google_token/insert.sql
+++ lib/sql/google_token/insert.sql
@@ -1,10 +1,12 @@
   INSERT INTO google_tokens("user",
     access_token,
     refresh_token,
-    expiry_date)
+    expiry_date,
+    calendar_id)
 VALUES ($1,
         $2,
         $3,
-        $4)
+        $4,
+        $5)
 ON CONFLICT ("user") DO NOTHING
 RETURNING id
