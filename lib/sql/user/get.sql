c9e6b80200990f6371cd1fa15d52d208c4368247
dreamcareer7 <dreamcareer070@gmail.com>
2016-07-06 14:19:37 +0430

    Added calendar_id to user model

--- lib/sql/user/get.sql
+++ lib/sql/user/get.sql
@@ -1,5 +1,5 @@
 SELECT 'user' AS type,
-       users.*,
+       users.*, google_tokens.calendar_id,
        to_char(NOW() AT TIME ZONE users.timezone, 'FMHH:MI AM - FMDay Mon DD, YYYY') AS current_time,
        (((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time > '08:00:00'::time) AND
        ((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time < '23:59:59'::time)) AS push_allowed,
@@ -7,6 +7,8 @@ SELECT 'user' AS type,
        EXTRACT(EPOCH FROM users.updated_at) AS updated_at,
        EXTRACT(EPOCH FROM users.deleted_at) AS deleted_at
 FROM users
-WHERE id = $1 AND
-      deleted_at IS NULL
+LEFT OUTER JOIN google_tokens
+ON users.id = google_tokens.user
+WHERE users.id = $1 AND
+      users.deleted_at IS NULL
 LIMIT 1
