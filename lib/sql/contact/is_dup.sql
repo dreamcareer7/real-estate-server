fa04ea55db26951073d794caac9860e472124c5b
dreamcareer7 <dreamcareer070@gmail.com>
2016-07-15 22:22:36 +0430

    Removed Room archiving on sure with phone number of a non-existing user.

--- lib/sql/contact/is_dup.sql
+++ lib/sql/contact/is_dup.sql
@@ -1,4 +1,4 @@
-SELECT COALESCE(BOOL_OR(contacts.email = LOWER($3::text)), FALSE) OR
+SELECT COALESCE(BOOL_OR(LOWER(contacts.email) = LOWER($3::text)), FALSE) OR
        COALESCE(BOOL_OR(contacts.phone_number = $4::text), FALSE) OR
        COALESCE(BOOL_OR(contacts.contact_user = $2::uuid), FALSE) AS is_dup
 FROM contacts
