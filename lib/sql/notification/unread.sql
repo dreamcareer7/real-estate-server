b87291b3b04411d47452995458daa997c5ab6da7
dreamcareer7 <dreamcareer070@gmail.com>
2016-07-20 14:17:55 +0200

    Fix communication channels. Save them all in notifications_deliveries Make sure we dont send multiple notifications Remove unused workers

--- lib/sql/notification/unread.sql
+++ lib/sql/notification/unread.sql
@@ -1,3 +1,9 @@
+-- Online users get the notifications immediately on Websocket. Therefore, we dont send them push notifications immediately.
+-- Results of this query are notifications that are *NOT* acknowledged by the user yet,
+-- and we have not *NOT* pushed them yet, because they are already online.
+-- 2 minutes is the window we give them to ack their notifications while they are online
+-- Also if 30 minutes is passed and we still have not pushed them (for whatever issue), give up, its too late.
+
 SELECT
   notifications_users."user",
   ARRAY_AGG(notifications_users.notification) as notifications
@@ -9,7 +15,5 @@ WHERE
   notifications_deliveries.notification IS NULL AND
   notifications_users.acked_at IS NULL
   AND notifications_users.created_at < (NOW() - '2 minute'::interval)
-  AND notifications_users.user IN (
-    SELECT DISTINCT "user" FROM notification_tokens
-  )
+  AND notifications_users.created_at > (NOW() - '30 minute'::interval)
 GROUP BY notifications_users."user"
--- scripts/workers.js
+++ scripts/workers.js
@@ -9,36 +9,6 @@ queue.process('seamless_communication', config.email.parallel, (job, done) => {
   Message.processSeamless(job, done);
 });
 
-queue.process('alert_share_email', config.email.parallel, (job, done) => {
-  console.log('-> Processed an Alert share email'.black.greenBG);
-  Alert.processEmail(job, done);
-});
-
-queue.process('alert_share_sms', config.email.parallel, (job, done) => {
-  console.log('-> Processed an Alert share SMS'.black.blueBG);
-  Alert.processSMS(job, done);
-});
-
-queue.process('cma_share_email', config.email.parallel, (job, done) => {
-  console.log('-> Processed a CMA share email'.black.yellowBG);
-  CMA.processEmail(job, done);
-});
-
-queue.process('cma_share_sms', config.email.parallel, (job, done) => {
-  console.log('-> Processed a CMA share SMS'.black.magentaBG);
-  CMA.processSMS(job, done);
-});
-
-queue.process('listing_share_email', config.email.parallel, (job, done) => {
-  console.log('-> Processed a Listing share email'.black.yellowBG.dim);
-  Listing.processEmail(job, done);
-});
-
-queue.process('listing_share_sms', config.email.parallel, (job, done) => {
-  console.log('-> Processed a Listing share SMS'.black.magentaBG.dim);
-  Listing.processSMS(job, done);
-});
-
 queue.process('airship_transport_send_device', config.airship.parallel, (job, done) => {
   console.log('-> Processed a push notification'.green);
   Notification.sendToDevice(job.data.notification, job.data.token, job.data.user_id, done);
