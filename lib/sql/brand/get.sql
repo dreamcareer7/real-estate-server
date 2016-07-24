73f2ac5cd8398764a4de13922075042c4f0aa857
dreamcareer7 <dreamcareer070@gmail.com>
2016-07-24 23:11:37 +0200

    brand.office_mls_id

--- lib/sql/brand/get.sql
+++ lib/sql/brand/get.sql
@@ -1,7 +1,10 @@
 SELECT *,
        'brand' AS TYPE,
        EXTRACT(EPOCH FROM created_at) AS created_at,
-       EXTRACT(EPOCH FROM updated_at) AS updated_at
+       EXTRACT(EPOCH FROM updated_at) AS updated_at,
+
+      -- If offices.brand is set, then this brand will have the office_mls_id of that office
+       (SELECT mls_id FROM offices WHERE brand = $1 LIMIT 1) as office_mls_id
 FROM brands
 WHERE id = $1
 LIMIT 1
