f1d4b40b4d80ecf94180973531ca0671c957aebe
dreamcareer7 <dreamcareer070@gmail.com>
2016-08-01 19:59:11 +0200

    Better listing area search

--- lib/sql/listing/area/mls_areas.mv.sql
+++ lib/sql/listing/area/mls_areas.mv.sql
@@ -1,12 +1,19 @@
 CREATE MATERIALIZED VIEW mls_areas AS
+SELECT
+  DISTINCT ON(mls_area_major)
+  (regexp_matches(mls_area_major, '(.+)\s\((\d+)\)$'))[1]::text AS title,
+  (regexp_matches(mls_area_major, '(.+)\s\((\d+)\)$'))[2]::int  AS number,
+  0::int as parent
+FROM listings
+
+UNION
+
 SElECT
   DISTINCT ON (mls_area_major, mls_area_minor)
-  (regexp_matches(mls_area_major, '(.+)\s\((\d+)\)$'))[1]::text AS major_title,
-  (regexp_matches(mls_area_major, '(.+)\s\((\d+)\)$'))[2]::int AS major_number,
-
-  (regexp_matches(mls_area_minor, '(.+)\s\((\d+)\)$'))[1]::text AS minor_title,
-  (regexp_matches(mls_area_minor, '(.+)\s\((\d+)\)$'))[2]::int AS minor_number
+  (regexp_matches(mls_area_minor, '(.+)\s\((\d+)\)$'))[1]::text AS title,
+  (regexp_matches(mls_area_minor, '(.+)\s\((\d+)\)$'))[2]::int  AS number,
+  (regexp_matches(mls_area_major, '(.+)\s\((\d+)\)$'))[2]::int  AS parent
 FROM listings;
 
-CREATE INDEX mls_areas_major_title_gin ON mls_areas USING gin (major_title gin_trgm_ops);
-CREATE INDEX mls_areas_minor_title_gin ON mls_areas USING gin (minor_title gin_trgm_ops);
+CREATE INDEX mls_areas_title_gin ON mls_areas USING gin (title gin_trgm_ops);
+CREATE INDEX mls_areas_parent    ON mls_areas (parent);
--- lib/sql/listing/area/search.sql
+++ lib/sql/listing/area/search.sql
@@ -1,25 +1,17 @@
-WITH majors AS (
-  SELECT
-    DISTINCT ON(major_number)
-    major_title::text as title,
-    major_number::int as number,
-    0::int as parent_number,
-    'mls_area_major'::text as type
-  FROM mls_areas
-    WHERE major_title ILIKE '%' || $1 || '%'
-),
-
-minors AS (
-  SELECT
-    DISTINCT ON(minor_number)
-    minor_title::text as title,
-    minor_number::int as number,
-    major_number::int as parent_number,
-    'mls_area_minor'::text as type
-  FROM mls_areas
-    WHERE minor_title ILIKE '%' || $1 || '%'
+SELECT
+  title,
+  number,
+  parent
+FROM mls_areas
+WHERE
+(
+  ($2::text IS NULL) OR (
+    parent = ANY($2::int[])
+  )
 )
-
-SELECT * FROM majors
-UNION
-SELECT * FROM minors;
+AND
+(
+  ($1::text IS NULL) OR (
+    title ILIKE '%' || $1 || '%'
+  )
+)
