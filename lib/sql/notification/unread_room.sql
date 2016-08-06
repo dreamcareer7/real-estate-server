aedef37dd2348ce300d12d0f9e44c949f3f179fe
dreamcareer7 <dreamcareer070@gmail.com>
2016-08-06 14:41:14 +0200

    Initial commit which consists a whole rewrite of seamless communications fixes #428 fixes #369 fixes #407 fixes #375 #210

---
+++ lib/sql/notification/unread_room.sql
@@ -0,0 +1,8 @@
+SELECT notifications.id FROM notifications_users
+JOIN notifications            ON notifications_users.notification = notifications.id
+FULL JOIN notifications_deliveries ON notifications_users.notification = notifications_deliveries.notification
+WHERE
+  notifications_users.user = $1
+  AND notifications.room = $2
+  AND notifications_deliveries.id IS NULL
+  AND notifications_users.acked_at IS NULL
