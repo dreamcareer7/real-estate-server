57bc8cf8fa3b89b899038be8ab563feb551f178f
dreamcareer7 <dreamcareer070@gmail.com>
2016-08-21 16:28:00 +0200

    User.stringSearch and Room.stringSearch with similarity. Fixes to other issues with POST /rooms.

---
+++ lib/sql/room/string_search.sql
@@ -0,0 +1,26 @@
+WITH r AS (
+  SELECT rooms_users.room AS id,
+         (
+           COALESCE(SIMILARITY(rooms.title, $2), 0) +
+           COALESCE(SIMILARITY(STRING_AGG(users.first_name, ' '), $2), 0) +
+           COALESCE(SIMILARITY(STRING_AGG(users.last_name, ' '), $2), 0) +
+           COALESCE(SIMILARITY(STRING_AGG(users.email, ' '), $2), 0) +
+           COALESCE(SIMILARITY(STRING_AGG(users.phone_number, ' '), $2), 0)
+         ) / 5.0 AS sim,
+         ARRAY_AGG(rooms_users."user") AS users
+  FROM rooms_users
+  INNER JOIN users
+    ON rooms_users."user" = users.id
+  INNER JOIN rooms
+    ON rooms_users."room" = rooms.id
+  WHERE rooms.deleted_at IS NULL
+  GROUP BY rooms_users.room,
+           rooms.title
+  ORDER BY sim DESC
+)
+SELECT id,
+       (COUNT(*) OVER())::INT AS total
+FROM r
+WHERE $1 = ANY (r.users) AND
+      sim >= $4
+LIMIT $3
--- lib/sql/user/string_search.sql
+++ lib/sql/user/string_search.sql
@@ -1,28 +1,19 @@
-WITH related_users AS (
-    WITH user_rooms AS (
-        SELECT rooms_users.room
-        FROM rooms_users
-        WHERE rooms_users."user" = $1
-    )
-    SELECT DISTINCT(rooms_users."user") AS id
-    FROM rooms_users
-    INNER JOIN user_rooms ON
-        user_rooms.room = rooms_users.room
-    INNER JOIN users ON
-        rooms_users."user" = users.id
-    WHERE rooms_users."user" <> $1
-)
-SELECT related_users.id,
-(COUNT(*) OVER())::INT AS total
-FROM related_users
-INNER JOIN users
-ON related_users.id = users.id
-WHERE
-    (
-     (CASE WHEN ARRAY_LENGTH($2::text[], 1) IS NULL THEN TRUE ELSE users.first_name ~* ANY($2) END) OR
-     (CASE WHEN ARRAY_LENGTH($2::text[], 1) IS NULL THEN TRUE ELSE users.last_name ~* ANY($2) END) OR
-     (CASE WHEN ARRAY_LENGTH($2::text[], 1) IS NULL THEN TRUE ELSE users.email ~* ANY($2) END) OR
-     (CASE WHEN ARRAY_LENGTH($2::text[], 1) IS NULL THEN TRUE ELSE users.phone_number ~* ANY($2) END)
-    ) AND
+WITH u AS
+(
+  SELECT id,
+  (
+    COALESCE(SIMILARITY(users.email, $1), 0) +
+    COALESCE(SIMILARITY(users.phone_number, $1), 0) +
+    COALESCE(SIMILARITY(users.first_name, $1), 0) +
+    COALESCE(SIMILARITY(users.last_name, $1), 0)
+  ) / 4.0 AS sim
+  FROM users
+  WHERE
     users.deleted_at IS NULL
-ORDER BY users.first_name
+  ORDER BY sim DESC
+)
+SELECT id,
+       (COUNT(*) OVER())::INT AS total
+FROM u
+WHERE sim >= $3
+LIMIT $2
