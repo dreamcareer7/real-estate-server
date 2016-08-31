cb83b281aed3cf9285e0e4ce705aa541ff64a708
dreamcareer7 <dreamcareer070@gmail.com>
2016-08-31 18:34:12 +0400

    Fixed summary endpoint to reflect latest proposal. -r3

--- lib/sql/notification/ack_alert.sql
+++ lib/sql/notification/ack_alert.sql
@@ -9,21 +9,26 @@ WHERE
     SELECT id FROM notifications
     WHERE
       (
-        subject_class     = 'User'
-        AND action       = 'Created'
-        AND object_class = 'Alert'
-        AND object = $2
+        subject_class = 'User' AND
+        action = 'Created' AND
+        object_class = 'Alert' AND
+        object = $2
       ) OR
       (
-        subject_class     = 'User'
-        AND action       = 'Edited'
-        AND object_class = 'Alert'
-        AND object = $2
+        subject_class = 'User' AND
+        action = 'Edited' AND
+        object_class = 'Alert' AND
+        object = $2
       ) OR
       (
-        subject_class     = 'Listing'
-        AND action       = 'BecameAvailable'
-        AND auxiliary_subject_class = 'Alert'
-        AND auxiliary_subject = $2
+        subject_class = 'Listing' AND
+        action = 'BecameAvailable' AND
+        auxiliary_subject_class = 'Alert' AND
+        auxiliary_subject = $2
+      ) OR
+      (
+        subject_class = 'Listing'
+
+
       )
   )
