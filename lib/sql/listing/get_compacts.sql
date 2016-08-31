42f9b24f31f778e72887d18b018942ff4dab0dfa
dreamcareer7 <dreamcareer070@gmail.com>
2016-09-01 00:22:08 +0400

    Added id to CompactListing.open_houses.

--- lib/sql/listing/get_compacts.sql
+++ lib/sql/listing/get_compacts.sql
@@ -65,13 +65,14 @@ SELECT 'compact_listing' AS TYPE,
        (
          SELECT json_agg(a) FROM (
            SELECT
-            type as open_house_type,
-            'open_house' as type,
-            EXTRACT(EPOCH FROM created_at) as created_at,
-            EXTRACT(EPOCH FROM updated_at) as updated_at,
-            EXTRACT(EPOCH FROM start_time) as start_time,
-            EXTRACT(EPOCH FROM end_time)   as end_time
-            ,description
+             id,
+             type as open_house_type,
+             'open_house' as type,
+             EXTRACT(EPOCH FROM created_at) as created_at,
+             EXTRACT(EPOCH FROM updated_at) as updated_at,
+             EXTRACT(EPOCH FROM start_time) as start_time,
+             EXTRACT(EPOCH FROM end_time)   as end_time,
+             description
            FROM open_houses WHERE listing_mui = listings.matrix_unique_id AND start_time > NOW()
          ) AS a
        ) AS open_houses,
