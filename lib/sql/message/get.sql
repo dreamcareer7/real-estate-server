706d0e4aad1d3f6fcc34ef47dcf50270e47784b0
dreamcareer7 <dreamcareer070@gmail.com>
2016-09-30 18:06:16 +0200

    Show the number of people who have acked a message

--- lib/sql/message/get.sql
+++ lib/sql/message/get.sql
@@ -5,6 +5,13 @@ SELECT 'message' AS type,
          FROM attachments_eav
          WHERE object = $1
        ) AS attachments,
+
+       (
+        SELECT count(*) FROM notifications_users WHERE acked_at IS NOT NULL AND notification IN(
+          SELECT id FROM notifications WHERE object_class = 'Message' AND object = $1
+        )
+       ) as ack_count,
+
        EXTRACT(EPOCH FROM created_at) AS created_at,
        EXTRACT(EPOCH FROM updated_at) AS updated_at
 FROM messages
