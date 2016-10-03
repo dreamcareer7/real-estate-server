37ab4b13499a5f2089a630825aabb99fe45b8ef6
dreamcareer7 <dreamcareer070@gmail.com>
2016-10-03 21:10:11 +0400

    Fixed an issue causing personal rooms to show up on GET /rooms/search

--- lib/sql/room/search_users.sql
+++ lib/sql/room/search_users.sql
@@ -11,7 +11,7 @@ WITH p AS (
     SELECT room
     FROM rooms_users
     WHERE "user" = $1
-  )
+  ) AND rooms.room_type <> 'Personal'
   GROUP BY rooms_users.room,
            rooms.room_type,
            rooms.updated_at
