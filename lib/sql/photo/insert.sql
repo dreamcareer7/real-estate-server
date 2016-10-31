3ad742d1afba13a0940847d4a51733dd843fc001
dreamcareer7 <dreamcareer070@gmail.com>
2016-10-31 13:28:20 +0100

    Revisions for photos

--- lib/sql/photo/insert.sql
+++ lib/sql/photo/insert.sql
@@ -13,6 +13,7 @@ ON CONFLICT (matrix_unique_id) DO UPDATE SET
   description = $3,
   "order" = $5,
   url = null,
-  last_processed = null
+  last_processed = null,
+  revision = photos.revision + 1
   WHERE photos.matrix_unique_id = $1
 RETURNING id
