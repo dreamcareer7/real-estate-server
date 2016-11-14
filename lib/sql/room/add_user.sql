f3e31b1e8f006adfe806707b464b75a9edbfbb92
dreamcareer7 <dreamcareer070@gmail.com>
2016-11-14 21:58:31 +0400

    Assigning phone numbers on Room.resolvePhoneForSeamless() instead of Room.addUser().

--- lib/sql/room/add_user.sql
+++ lib/sql/room/add_user.sql
@@ -1,29 +1,6 @@
 
-INSERT INTO rooms_users ("user", room, phone_handler)
+INSERT INTO rooms_users ("user", room)
 VALUES (
   $1,
-  $2,
-  (
-    SELECT id
-    FROM seamless_phone_pool
-    WHERE phone_number NOT IN (
-      SELECT seamless_phone_pool.phone_number
-      FROM rooms_users
-      INNER JOIN seamless_phone_pool
-      ON seamless_phone_pool.id = rooms_users.phone_handler
-      WHERE "user" = $1
-    )
-    UNION (
-      SELECT seamless_phone_pool.id
-      FROM seamless_phone_pool
-      INNER JOIN rooms_users
-      ON seamless_phone_pool.id = rooms_users.phone_handler
-      INNER JOIN rooms
-      ON rooms.id = rooms_users.room
-      WHERE rooms_users."user" = $1
-      ORDER BY rooms.updated_at
-      LIMIT 1
-    )
-    LIMIT 1
-  )
+  $2
 )
--- lib/sql/room/seamless_resolve_phone.sql
+++ lib/sql/room/seamless_resolve_phone.sql
@@ -1,7 +1,41 @@
+WITH u AS
+(
+  UPDATE rooms_users
+  SET phone_handler =
+  (
+    SELECT id
+    FROM seamless_phone_pool
+    WHERE phone_number NOT IN
+    (
+      SELECT seamless_phone_pool.phone_number
+      FROM rooms_users
+      INNER JOIN seamless_phone_pool
+      ON seamless_phone_pool.id = rooms_users.phone_handler
+      WHERE "user" = $1
+    )
+    UNION
+    (
+      SELECT seamless_phone_pool.id
+      FROM seamless_phone_pool
+      INNER JOIN rooms_users
+      ON seamless_phone_pool.id = rooms_users.phone_handler
+      INNER JOIN rooms
+      ON rooms.id = rooms_users.room
+      WHERE rooms_users."user" = $1
+      ORDER BY rooms.updated_at
+      LIMIT 1
+    )
+    LIMIT 1
+  )
+  WHERE "user" = $1 AND
+        room = $2 AND
+        phone_handler IS NULL
+  RETURNING phone_handler
+)
 SELECT seamless_phone_pool.phone_number AS phone_number
 FROM rooms_users
 INNER JOIN seamless_phone_pool
-ON rooms_users.phone_handler = seamless_phone_pool.id
+ON (rooms_users.phone_handler = seamless_phone_pool.id OR u.phone_handler = seamless_phone_pool.id)
 WHERE rooms_users."user" = $1 AND
       rooms_users.room = $2
 LIMIT 1
