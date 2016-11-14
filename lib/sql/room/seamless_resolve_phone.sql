0bd3d3cb5e10764fcdd6f4847f7e9a2039795d64
dreamcareer7 <dreamcareer070@gmail.com>
2016-11-14 22:10:36 +0400

    Fixed an issue with seamless_resolve_phone SQL.

--- lib/sql/room/seamless_resolve_phone.sql
+++ lib/sql/room/seamless_resolve_phone.sql
@@ -35,7 +35,7 @@ WITH u AS
 SELECT seamless_phone_pool.phone_number AS phone_number
 FROM rooms_users
 INNER JOIN seamless_phone_pool
-ON (rooms_users.phone_handler = seamless_phone_pool.id OR u.phone_handler = seamless_phone_pool.id)
+ON (rooms_users.phone_handler = seamless_phone_pool.id OR seamless_phone_pool.id = (SELECT phone_handler FROM u))
 WHERE rooms_users."user" = $1 AND
       rooms_users.room = $2
 LIMIT 1
