f85914d76e3bd0cc9b976fb61f1d2a8d572c5f6d
dreamcareer7 <dreamcareer070@gmail.com>
2016-12-25 14:38:45 +0100

    Fixed form search Enevelope Recipients as a model

--- lib/sql/envelope/get.sql
+++ lib/sql/envelope/get.sql
@@ -1,6 +1,10 @@
 SELECT *,
   'envelope' AS type,
    EXTRACT(EPOCH FROM created_at) AS created_at,
-   EXTRACT(EPOCH FROM updated_at) AS updated_at
+   EXTRACT(EPOCH FROM updated_at) AS updated_at,
+
+   (
+    SELECT ARRAY_AGG(id) FROM envelopes_recipients WHERE envelopes_recipients.envelope = envelopes.id
+   ) as recipients
 
 FROM envelopes WHERE id = $1
