321fd725ae4dfe4069a3e7a8ce06112c1f598942
dreamcareer7 <dreamcareer070@gmail.com>
2016-12-26 20:19:32 +0100

    Expose form name

--- lib/sql/form/get.sql
+++ lib/sql/form/get.sql
@@ -2,5 +2,6 @@ SELECT *,
   'form' AS type,
     EXTRACT(EPOCH FROM created_at) AS created_at,
     EXTRACT(EPOCH FROM updated_at) AS updated_at,
-    EXTRACT(EPOCH FROM deleted_at) AS deleted_at
+    EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
+    name
 FROM forms WHERE id = $1
