7216afa3b336d1c0c560616d8f20da4dd1cd1b20
dreamcareer7 <dreamcareer070@gmail.com>
2017-02-08 01:08:59 +0400

    Fixed an issue with migration script. -r1

--- migrations/1486498345593_removing_tasks_and_transactions.js
+++ migrations/1486498345593_removing_tasks_and_transactions.js
@@ -11,8 +11,44 @@ const up = [
   'DROP TABLE IF EXISTS transaction_contacts',
   'DROP TABLE IF EXISTS transactions',
   'DROP TABLE IF EXISTS notes',
-  'DELETE FROM notifications WHERE subject_class = \'Task\' OR object_class = \'Task\' OR auxiliary_object_class = \'Task\' OR auxiliary_subject_class = \'Task\'',
-  'DELETE FROM notifications WHERE subject_class = \'Transaction\' OR object_class = \'Transaction\' OR auxiliary_object_class = \'Transaction\' OR auxiliary_subject_class = \'Transaction\''
+  'WITH tan AS (\
+    SELECT id FROM notifications WHERE subject_class = \'Task\' OR object_class = \'Task\' OR auxiliary_object_class = \'Task\' OR auxiliary_subject_class = \'Task\'\
+  ),\
+  trn AS (\
+    SELECT id FROM notifications WHERE subject_class = \'Transaction\' OR object_class = \'Transaction\' OR auxiliary_object_class = \'Transaction\' OR auxiliary_subject_class = \'Transaction\'\
+  ),\
+  r1 AS (\
+    DELETE FROM messages WHERE notification IN (SELECT id FROM tan)\
+  ),\
+  r2 AS (\
+    DELETE FROM messages WHERE notification IN (SELECT id FROM trn)\
+  ),\
+  r3 AS (\
+    DELETE FROM notifications_users WHERE notification IN (SELECT id FROM tan)\
+  ),\
+  r4 AS (\
+    DELETE FROM notifications_users WHERE notification IN (SELECT id FROM trn)\
+  ),\
+  r5 AS (\
+    DELETE FROM notifications_deliveries WHERE notification IN (SELECT id FROM tan)\
+  ),\
+  r6 AS (\
+    DELETE FROM notifications_deliveries WHERE notification IN (SELECT id FROM trn)\
+  ),\
+  r7 AS (\
+    DELETE FROM notifications_acks WHERE notification IN (SELECT id FROM tan)\
+  ),\
+  r8 AS (\
+    DELETE FROM notifications_acks WHERE notification IN (SELECT id FROM trn)\
+  ),\
+  r9 AS (\
+    DELETE FROM notifications WHERE id IN (SELECT id FROM tan)\
+  ),\
+  r10 AS (\
+    DELETE FROM notifications WHERE id IN (SELECT id FROM trn)\
+  )\
+  SELECT now()\
+  '
 ]
 
 const down = [
