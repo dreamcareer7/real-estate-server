47ab20ef00977712fbf54a444047dcfa3cbd0656
dreamcareer7 <dreamcareer070@gmail.com>
2017-02-08 00:36:10 +0400

    Removed useless Transaction, Task and Note models, unit tests and endpoint logic. RIP. -r1

---
+++ migrations/1486498345593_removing_tasks_and_transactions.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'DROP TABLE important_dates',
+  'DROP TABLE task_contacts',
+  'DROP TABLE tasks',
+  'DROP TABLE transaction_contact_roles',
+  'DROP TABLE transaction_contacts',
+  'DROP TABLE transactions',
+  'DELETE FROM notifications WHERE subject_class = \'Task\' OR object_class = \'Task\' OR auxiliary_object_class = \'Task\' OR auxiliary_subject_class = \'Task\'',
+  'DELETE FROM notifications WHERE subject_class = \'Transaction\' OR object_class = \'Transaction\' OR auxiliary_object_class = \'Transaction\' OR auxiliary_subject_class = \'Transaction\''
+]
+
+const down = [
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
