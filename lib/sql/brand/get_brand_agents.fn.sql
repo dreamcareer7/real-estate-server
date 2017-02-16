6b5548d31991ae0fc1d93556a37c0eca741d2060
dreamcareer7 <dreamcareer070@gmail.com>
2017-02-16 03:12:57 +0100

    Proper function + better error message

--- lib/sql/brand/get_brand_agents.fn.sql
+++ lib/sql/brand/get_brand_agents.fn.sql
@@ -2,8 +2,8 @@ CREATE OR REPLACE FUNCTION get_brand_agents(id uuid) RETURNS
    setof uuid
 AS
 $$
-  SELECT DISTINCT agents.id AS id FROM users
-    FULL JOIN agents         ON users.agent = agents.id
+  SELECT DISTINCT agents.id AS id FROM agents
+    FULL JOIN users          ON agents.id = users.agent
     FULL JOIN offices        ON agents.office_mui = offices.matrix_unique_id
     FULL JOIN brands_offices ON offices.id = brands_offices.office
     FULL JOIN brands_agents  ON agents.id = brands_agents.agent
