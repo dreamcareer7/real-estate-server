a9d373e738079133ad23ba20274744128434119b
dreamcareer7 <dreamcareer070@gmail.com>
2017-02-16 02:58:33 +0100

    Fixing brand/get

---
+++ lib/sql/brand/get_brand_agents.fn.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION get_brand_agents(id uuid) RETURNS
+   setof uuid
+AS
+$$
+  SELECT DISTINCT agents.id AS id FROM users
+    FULL JOIN agents         ON users.agent = agents.id
+    FULL JOIN offices        ON agents.office_mui = offices.matrix_unique_id
+    FULL JOIN brands_offices ON offices.id = brands_offices.office
+    FULL JOIN brands_agents  ON agents.id = brands_agents.agent
+    FULL JOIN brands_users   ON users.id = brands_users.user
+  WHERE (
+    (
+      brands_offices.brand = $1
+    )
+    OR
+    (
+      brands_agents.brand = $1
+    )
+    OR
+    (
+      brands_users.brand = $1
+    )
+  )
+$$
+LANGUAGE sql;
