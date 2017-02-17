1fdd3963141729b1bd8c27cedbb6ddce94a62e3a
dreamcareer7 <dreamcareer070@gmail.com>
2017-02-17 18:33:03 +0100

    Better ACL implementation for deals based on brands.

---
+++ migrations/1487300126893_deal-brand.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+const fn = fs.readFileSync('./lib/sql/brand/brand_children.fn.sql').toString()
+
+const up = [
+  'BEGIN',
+  'ALTER TABLE deals ADD brand uuid REFERENCES brands(id)',
+  fn,
+  'COMMIT'
+]
+
+const down = [
+  'BEGIN',
+  'ALTER TABLE deals DROP brand',
+  'DROP FUNCTION brand_children(id uuid)',
+  'COMMIT'
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
