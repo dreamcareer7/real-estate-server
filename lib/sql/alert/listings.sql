be91cce93e8c8d92e2c45400ed01f2414ac2448e
dreamcareer7 <dreamcareer070@gmail.com>
2017-03-07 19:21:35 +0100

    alert.listings association only returns the last 4 listings not all.

--- lib/sql/alert/listings.sql
+++ lib/sql/alert/listings.sql
@@ -1,8 +1,9 @@
 SELECT
-  listing
+  DISTINCT listing
 FROM recommendations
 WHERE
   deleted_at IS NULL AND
   hidden = FALSE AND
   ARRAY[$1::uuid] IN (referring_objects)
 ORDER BY created_at DESC
+LIMIT 4
