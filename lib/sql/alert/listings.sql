dea41b35549dff5d4c057e5a0ea780151fce7581
dreamcareer7 <dreamcareer070@gmail.com>
2017-03-07 20:18:02 +0100

    fix query

--- lib/sql/alert/listings.sql
+++ lib/sql/alert/listings.sql
@@ -1,9 +1,9 @@
 SELECT
-  DISTINCT listing
+  listing
 FROM recommendations
 WHERE
   deleted_at IS NULL AND
   hidden = FALSE AND
   ARRAY[$1::uuid] IN (referring_objects)
 ORDER BY created_at DESC
-LIMIT 4 -- FIXME
+LIMIT 4
