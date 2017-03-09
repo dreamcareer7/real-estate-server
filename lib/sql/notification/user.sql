4e21c45c4d1f020da0cc7332feed0e9c17852854
dreamcareer7 <dreamcareer070@gmail.com>
2017-03-09 09:52:25 +0400

    Fixed notification collection issue.

--- lib/sql/notification/user.sql
+++ lib/sql/notification/user.sql
@@ -1,8 +1,8 @@
 WITH c AS (
-  SELECT id,
+  SELECT notifications.id AS id,
          (COUNT(*) OVER())::INT AS total,
-         created_at,
-         updated_at
+         notifications.created_at,
+         notifications.updated_at
   FROM notifications
   FULL JOIN notifications_users
     ON notifications.id = notifications_users.notification
@@ -27,23 +27,23 @@ SELECT id,
        total
 FROM c
 WHERE CASE
-    WHEN $2 = 'Since_C' THEN notifications.created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-    WHEN $2 = 'Max_C' THEN notifications.created_at <= TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-    WHEN $2 = 'Since_U' THEN notifications.updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-    WHEN $2 = 'Max_U' THEN notifications.updated_at <= TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
-    WHEN $2 = 'Init_C' THEN notifications.created_at <= CLOCK_TIMESTAMP()
-    WHEN $2 = 'Init_U' THEN notifications.updated_at <= CLOCK_TIMESTAMP()
+    WHEN $2 = 'Since_C' THEN created_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Max_C' THEN created_at <= TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Since_U' THEN updated_at > TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Max_U' THEN updated_at <= TIMESTAMP WITH TIME ZONE 'EPOCH' + $3 * INTERVAL '1 MICROSECOND'
+    WHEN $2 = 'Init_C' THEN created_at <= NOW()
+    WHEN $2 = 'Init_U' THEN updated_at <= NOW()
     ELSE TRUE
     END
 ORDER BY
     CASE $2
-        WHEN 'Since_C' THEN notifications.created_at
-        WHEN 'Since_U' THEN notifications.updated_at
+        WHEN 'Since_C' THEN created_at
+        WHEN 'Since_U' THEN updated_at
     END,
     CASE $2
-        WHEN 'Max_C' THEN notifications.created_at
-        WHEN 'Max_U' THEN notifications.updated_at
-        WHEN 'Init_C' THEN notifications.created_at
-        WHEN 'Init_U' THEN notifications.updated_at
+        WHEN 'Max_C' THEN created_at
+        WHEN 'Max_U' THEN updated_at
+        WHEN 'Init_C' THEN created_at
+        WHEN 'Init_U' THEN updated_at
     END DESC
 LIMIT $4;
