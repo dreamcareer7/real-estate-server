28dec9e4e6ec52dd7331ef5513f2b4cc13972a8f
dreamcareer7 <dreamcareer070@gmail.com>
2017-03-28 05:34:26 +0400

    Fixed Coming Soon sort order.

---
+++ migrations/1490664697229_fix_coming_soon_sort_order.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+const fs = require('fs')
+const fn = fs.readFileSync('./lib/sql/listing/order_listings.fn.sql').toString()
+
+const up = [
+  'BEGIN',
+  'DROP FUNCTION order_listings(listing_status) CASCADE',
+  fn,
+  'CREATE INDEX listings_filters_status_order ON listings_filters(order_listings(status))',
+  'COMMIT'
+]
+
+const down = [
+  'DROP FUNCTION order_listings(listing_status) CASCADE;'
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
