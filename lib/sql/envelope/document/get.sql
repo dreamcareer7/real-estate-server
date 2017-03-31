15a2889537c7a204e6eeaf01794393ab205f136f
dreamcareer7 <dreamcareer070@gmail.com>
2017-03-31 17:51:08 +0200

    fix EnvelopeDocument

--- lib/sql/envelope/document/get.sql
+++ lib/sql/envelope/document/get.sql
@@ -1,6 +1,4 @@
 SELECT *,
-  'envelope_document' AS type,
-   EXTRACT(EPOCH FROM created_at) AS created_at,
-   EXTRACT(EPOCH FROM updated_at) AS updated_at
+  'envelope_document' AS type
 
 FROM envelopes_documents WHERE id = $1
