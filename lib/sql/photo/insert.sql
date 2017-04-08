38fff6087708d783a15fe2a7582c35232a9e7abb
dreamcareer7 <dreamcareer070@gmail.com>
2017-04-08 22:39:22 +0200

    syntax error in sql fixed. sue me.

--- lib/sql/photo/insert.sql
+++ lib/sql/photo/insert.sql
@@ -24,7 +24,7 @@ ON CONFLICT (matrix_unique_id) DO UPDATE SET
    -- So we have to delay actual download of the image for like 15 minutes.
    -- Otherwise, the image we get will be the old one we already had.
 
-  to_be_processed_at = CLOCK_TIMESTAMP() + INTERVAL '15 minute'
+  to_be_processed_at = CLOCK_TIMESTAMP() + INTERVAL '15 minute',
   revision = photos.revision + 1
 
 WHERE photos.matrix_unique_id = $1
