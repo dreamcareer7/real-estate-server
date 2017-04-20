addd8a9573f67444901655fbf1cbbd1eaf871ad3
dreamcareer7 <dreamcareer070@gmail.com>
2017-04-20 15:24:28 +0400

    Removing old notifications. closes #701

---
+++ migrations/1492663103456_removing_old_notifications.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'DROP TABLE IF EXISTS notifications_acks',
+  `WITH p AS
+   (
+     SELECT id
+     FROM notifications
+     WHERE
+     (
+       (subject_class = 'Contact' AND action = 'CreatedFor' AND object_class = 'User') OR
+       (subject_class = 'Listing' AND action = 'PriceDropped' AND object_class = 'Room') OR
+       (subject_class = 'Listing' AND action = 'StatusChanged' AND object_class = 'Room') OR
+       (subject_class = 'OpenHouse' AND action = 'Available' AND object_class = 'Listing')
+     )
+   ),
+   r1 AS
+   (
+     DELETE FROM notifications_deliveries
+     WHERE notification IN (SELECT id FROM p)
+   ),
+   r2 AS
+   (
+     DELETE FROM notifications_users
+     WHERE notification IN (SELECT id FROM p)
+   ),
+   r3 AS
+   (
+     DELETE FROM messages
+     WHERE notification IN (SELECT id FROM p)
+   ),
+   r4 AS
+   (
+     DELETE FROM notifications WHERE id IN (SELECT id FROM p)
+   )
+   SELECT NOW()`
+]
+
+const down = [
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
