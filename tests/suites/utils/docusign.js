2f52525e6cf22069ab36b1690068a3e96e6283cd
dreamcareer7 <dreamcareer070@gmail.com>
2017-04-23 04:14:24 +0200

    docusign script logs into docusign using a provided username and password and login url using a selenium server

---
+++ tests/suites/utils/docusign.js
@@ -0,0 +1,25 @@
+const webdriverio = require('webdriverio')
+const deasync = require('deasync')
+const config = require('../../../lib/config')
+
+const run = ({url, username, password}, cb) => {
+  const client = webdriverio.remote(config.selenium).init()
+
+  return client
+    .url(url)
+    .setValue('input#username', username)
+    .click('form button')
+    .waitForExist('input#password')
+    .setValue('input#password', password)
+    .click('form button')
+    .waitForExist('form', 15000, true)
+    .getUrl()
+    .then(url => {
+      cb(null, url)
+    })
+    .catch( cb )
+    .end()
+}
+
+
+module.exports = deasync(run)
