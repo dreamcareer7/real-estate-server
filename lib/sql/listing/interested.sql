362304b0a3381adf7461449a15f37e31630204fd
dreamcareer7 <dreamcareer070@gmail.com>
2017-04-25 22:14:01 +0400

    Fixed an issue with interested logic. -r1

--- lib/sql/listing/interested.sql
+++ lib/sql/listing/interested.sql
@@ -4,4 +4,4 @@ FULL JOIN rooms_users
   ON rooms_users.room = recommendations.room
 WHERE recommendations.listing = $1 AND
       recommendations.deleted_at IS NULL AND
-      COALESCE(ARRAY_LENGTH(referring_objects, 1), 0) > 0
+      COALESCE(ARRAY_LENGTH(recommendations.referring_objects, 1), 0) > 0
