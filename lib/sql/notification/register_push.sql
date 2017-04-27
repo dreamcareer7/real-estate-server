81c867dc790517f5a5b6f7cd0e85dec2a052630b
dreamcareer7 <dreamcareer070@gmail.com>
2017-04-27 13:57:06 +0400

    Removing old device_id and device_tokens from registered_tokens. closes #708 -r1

--- lib/sql/notification/register_push.sql
+++ lib/sql/notification/register_push.sql
@@ -1,7 +1,16 @@
-WITH deleted AS (
+WITH
+rm_null_ids AS
+(
   DELETE FROM notifications_tokens WHERE device_id IS NULL AND "user" = $1
+),
+rm_dup_ids AS
+(
+  DELETE FROM notifications_tokens WHERE device_id = $2
+),
+rm_dup_tokens AS
+(
+  DELETE FROM notifications_tokens WHERE device_token = $3
 )
-
 INSERT INTO notifications_tokens
 (
   "user",
