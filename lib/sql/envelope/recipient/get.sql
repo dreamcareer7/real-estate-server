a1e3ae8c56dc705a980717756e383c0fdf7bf88b
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-07 20:10:16 +0400

    Fixed an issue with envelope_recipient get sql.

--- lib/sql/envelope/recipient/get.sql
+++ lib/sql/envelope/recipient/get.sql
@@ -1,7 +1,7 @@
-SELECT *,
+SELECT envelopes_recipients.*,
        'envelope_recipient' AS type,
        EXTRACT(EPOCH FROM created_at) AS created_at,
        EXTRACT(EPOCH FROM updated_at) AS updated_at
 FROM envelopes_recipients
-JOIN unnest($1::uuid[]) WITH ORDINALITY t(rid, ord) ON envelope_recipients.id = rid
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(rid, ord) ON envelopes_recipients.id = rid
 ORDER BY t.ord
