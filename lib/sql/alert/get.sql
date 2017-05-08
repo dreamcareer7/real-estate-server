ab010573105d4325ccaf9fc04709cbf0bdb206d3
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-08 17:35:46 +0400

    Alert sql fix for .getAll() model.

--- lib/sql/alert/get.sql
+++ lib/sql/alert/get.sql
@@ -1,28 +1,27 @@
-WITH recs AS
-(
-  SELECT recommendations.id AS id,
-         matrix_unique_id AS mui
-  FROM recommendations
-  LEFT JOIN recommendations_eav
-  ON recommendations.id = recommendations_eav.recommendation AND
-     (CASE WHEN $2::uuid IS NOT NULL THEN recommendations_eav."user" = $2 ELSE FALSE END) AND
-     recommendations_eav.action = 'Read'
-  WHERE room = (SELECT room FROM alerts WHERE id = $1 LIMIT 1) AND
-        recommendations_eav.id IS NULL AND
-        recommendations.deleted_at IS NULL AND
-        recommendations.hidden IS FALSE AND
-        COALESCE(ARRAY_LENGTH(recommendations.referring_objects, 1), 0) > 0 AND
-        ARRAY[$1] <@ recommendations.referring_objects
-  ORDER BY recommendations.updated_at
-)
 SELECT *,
        'alert' AS type,
-       CASE WHEN $2::uuid IS NULL THEN 0 ELSE (SELECT COUNT(*)::INT FROM recs) END AS new_recommendations,
+       (
+         SELECT COUNT(recommendations.id)
+         FROM recommendations
+         LEFT JOIN recommendations_eav
+           ON recommendations.id = recommendations_eav.recommendation AND
+           (
+             CASE WHEN $2::uuid IS NOT NULL THEN recommendations_eav."user" = $2 ELSE FALSE END
+           ) AND
+           recommendations_eav.action = 'Read'
+         WHERE recommendations_eav.id IS NULL AND
+               recommendations.deleted_at IS NULL AND
+               recommendations.hidden IS FALSE AND
+               COALESCE(ARRAY_LENGTH(recommendations.referring_objects, 1), 0) > 0 AND
+               ARRAY[alerts.id] <@ recommendations.referring_objects
+       ) AS new_recommendations,
        (
          SELECT url FROM photos
-         WHERE listing_mui = (SELECT mui FROM recs LIMIT 1) AND
-               photos.url IS NOT NULL AND photos.deleted_at IS NULL
-         ORDER BY "order" LIMIT 1
+         INNER JOIN recommendations
+           ON photos.listing_mui = recommendations.matrix_unique_id
+         WHERE ARRAY[alerts.id] <@ recommendations.referring_objects
+         ORDER BY recommendations.updated_at DESC
+         LIMIT 1
        ) as cover_image_url,
        ST_AsGeoJSON(points) AS points,
        EXTRACT(EPOCH FROM created_at) AS created_at,
@@ -34,5 +33,5 @@ SELECT *,
        listing_statuses::text[] AS listing_statuses,
        excluded_listing_ids::uuid[] AS excluded_listing_ids
 FROM alerts
-WHERE id = $1
-LIMIT 1
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(aid, ord) ON alerts.id = aid
+ORDER BY t.ord
