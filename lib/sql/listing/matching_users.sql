7f694ff3c1bc94e15f8fb414666aa26a7e931d77
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-08 18:47:37 +0200

    This issue was actually found and fixed new `features/remove-mls` branch. Hotfixing it here.

--- lib/sql/listing/matching_users.sql
+++ lib/sql/listing/matching_users.sql
@@ -3,73 +3,75 @@ FROM alerts
 INNER JOIN rooms_users
   ON alerts.room = rooms_users.room
 WHERE
-  alerts.deleted_at IS NULL AND
-  (alerts.minimum_price IS NULL OR alerts.minimum_price <= $1) AND
-  (alerts.maximum_price IS NULL OR alerts.maximum_price >= $1) AND
+  deleted_at IS NULL AND
+  (minimum_price IS NULL OR minimum_price <= $1) AND
+  (maximum_price IS NULL OR maximum_price >= $1) AND
 
-  (alerts.minimum_square_meters IS NULL OR alerts.minimum_square_meters <= $2) AND
-  (alerts.maximum_square_meters IS NULL OR alerts.maximum_square_meters >= $2) AND
+  (minimum_square_meters IS NULL OR minimum_square_meters <= $2) AND
+  (maximum_square_meters IS NULL OR maximum_square_meters >= $2) AND
 
-  (alerts.minimum_lot_square_meters IS NULL OR alerts.minimum_lot_square_meters <= $11) AND
-  (alerts.maximum_lot_square_meters IS NULL OR alerts.maximum_lot_square_meters >= $11) AND
+  (minimum_lot_square_meters IS NULL OR minimum_lot_square_meters <= $11) AND
+  (maximum_lot_square_meters IS NULL OR maximum_lot_square_meters >= $11) AND
 
-  (alerts.minimum_bedrooms  IS NULL OR alerts.minimum_bedrooms  <=  $3) AND
+  (minimum_bedrooms  IS NULL OR minimum_bedrooms  <=  $3) AND
 
-  (alerts.minimum_bathrooms IS NULL OR alerts.minimum_bathrooms <= $4) AND
+  (minimum_bathrooms IS NULL OR minimum_bathrooms <= $4) AND
 
-  (alerts.minimum_year_built IS NULL OR alerts.minimum_year_built <= $9) AND
-  (alerts.maximum_year_built IS NULL OR alerts.maximum_year_built >= $9) AND
+  (minimum_year_built IS NULL OR minimum_year_built <= $9) AND
+  (maximum_year_built IS NULL OR maximum_year_built >= $9) AND
 
-  (alerts.listing_statuses IS NULL OR alerts.listing_statuses @> ARRAY[$12::listing_status]) AND
+  (listing_statuses IS NULL OR listing_statuses @> ARRAY[$12::listing_status]) AND
 
-  (alerts.pool IS NULL OR alerts.pool = FALSE OR $10 = TRUE) AND
+  (pool IS NULL OR pool = $10) AND
 
-  (alerts.property_types    IS NULL OR alerts.property_types @> ARRAY[$5::property_type])       AND
-  (alerts.property_subtypes IS NULL OR alerts.property_subtypes @> ARRAY[$6::property_subtype]) AND
+  (pets IS NULL OR pets = $31) AND
 
-  (alerts.points IS NULL OR ST_Within(ST_SetSRID(ST_MakePoint($7, $8), 4326), alerts.points)) AND
+  (property_types    IS NULL OR property_types @> ARRAY[$5::property_type])       AND
+  (property_subtypes IS NULL OR property_subtypes @> ARRAY[$6::property_subtype]) AND
 
-  (alerts.minimum_parking_spaces IS NULL OR alerts.minimum_parking_spaces <= $13) AND
+  (points IS NULL OR ST_Within(ST_SetSRID(ST_MakePoint($7, $8), 4326), points)) AND
 
-  (alerts.list_offices IS NULL OR alerts.list_offices @> ARRAY[$14]) AND
-  (alerts.list_agents  IS NULL OR alerts.list_agents @> ARRAY[$15])  AND
+  (minimum_parking_spaces IS NULL OR minimum_parking_spaces <= $13) AND
 
-  (alerts.selling_offices IS NULL OR alerts.selling_offices @> ARRAY[$16]) AND
-  (alerts.selling_agents  IS NULL OR alerts.selling_agents  @> ARRAY[$17])  AND
+  (list_offices IS NULL OR list_offices @> ARRAY[$14]) AND
+  (list_agents  IS NULL OR list_agents @> ARRAY[$15])  AND
 
-  (alerts.architectural_styles IS NULL OR alerts.architectural_styles @> $18) AND
+  (selling_offices IS NULL OR selling_offices @> ARRAY[$16]) AND
+  (selling_agents  IS NULL OR selling_agents  @> ARRAY[$17])  AND
 
-  (alerts.counties IS NULL OR alerts.counties @> ARRAY[$19]) AND
+  (architectural_styles IS NULL OR architectural_styles @> $18) AND
 
-  (alerts.postal_codes IS NULL OR alerts.postal_codes @> ARRAY[$30]) AND
+  (counties IS NULL OR counties @> ARRAY[$19]) AND
 
-  (alerts.subdivisions IS NULL OR alerts.subdivisions @> ARRAY[$20]) AND
+  (postal_codes IS NULL OR postal_codes @> ARRAY[$30]) AND
 
-  (alerts.school_districts     IS NULL OR alerts.school_districts @> ARRAY[$21])    AND
+  (subdivisions IS NULL OR subdivisions @> ARRAY[$20]) AND
 
-  (alerts.primary_schools      IS NULL OR alerts.primary_schools @> ARRAY[$22])     AND
+  (school_districts     IS NULL OR school_districts @> ARRAY[$21])    AND
 
-  (alerts.middle_schools       IS NULL OR alerts.primary_schools @> ARRAY[$23])     AND
+  (primary_schools      IS NULL OR primary_schools @> ARRAY[$22])     AND
 
-  (alerts.elementary_schools   IS NULL OR alerts.elementary_schools @> ARRAY[$24])  AND
+  (middle_schools       IS NULL OR primary_schools @> ARRAY[$23])     AND
 
-  (alerts.senior_high_schools  IS NULL OR alerts.senior_high_schools @> ARRAY[$25]) AND
+  (elementary_schools   IS NULL OR elementary_schools @> ARRAY[$24])  AND
 
-  (alerts.junior_high_schools  IS NULL OR alerts.junior_high_schools @> ARRAY[$26]) AND
+  (senior_high_schools  IS NULL OR senior_high_schools @> ARRAY[$25]) AND
 
-  (alerts.intermediate_schools IS NULL OR alerts.junior_high_schools @> ARRAY[$27]) AND
+  (junior_high_schools  IS NULL OR junior_high_schools @> ARRAY[$26]) AND
 
-  (alerts.mls_areas IS NULL OR
+  (intermediate_schools IS NULL OR junior_high_schools @> ARRAY[$27]) AND
+
+  (mls_areas IS NULL OR
     (
       to_jsonb(ARRAY[
         substring($28 FROM E'[0-9]+')::int,
         substring($29 FROM E'[0-9]+')::int
-      ]) IN (SELECT jsonb_array_elements(alerts.mls_areas))
+      ]) IN (SELECT jsonb_array_elements(mls_areas))
     ) OR
     (
       to_jsonb(ARRAY[
         substring($28 FROM E'[0-9]+')::int,
         0
-      ]) IN (SELECT jsonb_array_elements(alerts.mls_areas))
+      ]) IN (SELECT jsonb_array_elements(mls_areas))
     )
   )
