62bf4e1601d7d69890215222475afa00f8d0f0e8
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-10 18:37:30 +0400

    Fixed an issue causing User.getAll() to malfunction.

--- lib/sql/client/get.sql
+++ lib/sql/client/get.sql
@@ -1,12 +1,11 @@
 WITH c AS (
   SELECT
   DISTINCT ON(id)
-  *
+  clients.*
   FROM clients
   WHERE id = ANY($1::uuid[])
   ORDER BY id DESC
 )
-
-SELECT * FROM c
+SELECT c.* FROM c
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(cid, ord) ON c.id = cid
 ORDER BY t.ord
