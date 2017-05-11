2b430069fc80981fc774774f405026b039d2bc72
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-11 22:46:34 +0400

    Fixed an issue causing individual User.get()s to be called on Room.getAll().

--- lib/sql/room/get.sql
+++ lib/sql/room/get.sql
@@ -24,7 +24,24 @@ SELECT 'room' AS TYPE,
        ) AS notification_settings,
        (
          SELECT ARRAY_AGG(DISTINCT("user")) FROM rooms_users WHERE room = rooms.id
-       ) AS users
+       ) AS users,
+       (
+         SELECT JSON_AGG(JSON_BUILD_OBJECT
+           (
+             'id', users.id,
+             'first_name', users.first_name,
+             'last_name', users.last_name,
+             'email', users.email,
+             'phone_number', users.phone_number,
+             'fake_email', users.fake_email
+           )
+         )
+         FROM rooms_users
+         INNER JOIN users
+           ON rooms_users."user" = users.id
+         WHERE rooms_users.room = rooms.id
+         GROUP BY rooms_users.room
+       ) AS users_info
 FROM rooms
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(rid, ord) ON rooms.id = rid
 ORDER BY t.ord
