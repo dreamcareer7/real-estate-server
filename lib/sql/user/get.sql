5305b1a578537dc4a1f487e6bbb85eb781614019
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-16 12:45:47 +0200

    last_seen_at as Dan-preferred format

--- lib/sql/user/get.sql
+++ lib/sql/user/get.sql
@@ -3,9 +3,10 @@ SELECT 'user' AS type,
        to_char(NOW() AT TIME ZONE users.timezone, 'FMHH:MI AM - FMDay Mon DD, YYYY') AS current_time,
        (((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time > '08:00:00'::time) AND
        ((CURRENT_TIME(0) AT TIME ZONE users.timezone)::time < '23:59:59'::time)) AS push_allowed,
-       EXTRACT(EPOCH FROM users.created_at) AS created_at,
-       EXTRACT(EPOCH FROM users.updated_at) AS updated_at,
-       EXTRACT(EPOCH FROM users.deleted_at) AS deleted_at,
+       EXTRACT(EPOCH FROM users.created_at)   AS created_at,
+       EXTRACT(EPOCH FROM users.updated_at)   AS updated_at,
+       EXTRACT(EPOCH FROM users.deleted_at)   AS deleted_at,
+       EXTRACT(EPOCH FROM users.last_seen_at) AS last_seen_at,
        COALESCE
        (
          profile_image_url,
