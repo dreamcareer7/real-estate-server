9f2f03a5bfe5a885c2212ad3cd2108a510c5b7df
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-16 18:06:12 +0200

    First attempt at making contact/user fast.

--- lib/sql/contact/user.sql
+++ lib/sql/contact/user.sql
@@ -1,42 +1,50 @@
-WITH c AS (
+with crefs AS (
+  SELECT DISTINCT(UNNEST(refs)) AS id
+  FROM contacts
+  WHERE "user" = $1 AND
+        contacts.deleted_at IS NULL AND
+        contacts.merged IS FALSE
+),
+p AS (
+  SELECT crefs.id AS contact,
+         contacts_emails.email AS email,
+         contacts_phone_numbers.phone_number AS phone_number,
+         users.id AS "user"
+  FROM crefs
+  LEFT JOIN contacts_emails ON
+    crefs.id = contacts_emails.contact
+  LEFT JOIN contacts_phone_numbers ON
+    crefs.id = contacts_phone_numbers.contact
+  LEFT JOIN users ON
+    users.phone_number = contacts_phone_numbers.phone_number OR
+    users.email = contacts_emails.email
+  WHERE contacts_emails.deleted_at IS NULL AND
+        contacts_phone_numbers.deleted_at IS NULL
+),
+a AS (
+  SELECT activities.reference,
+         MAX(activities.created_at) as updated_at
+  FROM p
+  INNER JOIN activities ON (
+    activities.reference = p.contact OR
+    activities.reference = p.user
+  )
+  GROUP BY activities.reference
+),
+c AS (
   SELECT id,
          refs,
          (COUNT(*) OVER())::INT AS total,
          created_at,
-         COALESCE
-         (
-           (
-             SELECT MAX(updated_at)
-             FROM activities
-             WHERE reference = id OR
-                   reference IN
-                   (
-                     SELECT id
-                     FROM users
-                     WHERE email IN
-                     (
-                       SELECT DISTINCT(email)
-                       FROM contacts_emails
-                       WHERE contact = ANY(refs) AND
-                             deleted_at IS NULL
-                     )
-                     UNION
-                     SELECT id
-                     FROM users
-                     WHERE phone_number IN
-                     (
-                       SELECT DISTINCT(phone_number)
-                       FROM contacts_phone_numbers
-                       WHERE contact = ANY(refs) AND
-                             deleted_at IS NULL
-                     )
-                   )
-           ), updated_at
-         ) AS updated_at
+         COALESCE(
+           (SELECT a.updated_at FROM a WHERE a.reference = contacts.id),
+           contacts.updated_at
+         ) as updated_at
   FROM contacts
   WHERE "user" = $1 AND
-        deleted_at IS NULL
+  deleted_at IS NULL
 )
+
 SELECT id,
        total
 FROM c
@@ -58,4 +66,4 @@ ORDER BY
         WHEN 'Init_C' THEN created_at
         WHEN 'Init_U' THEN updated_at
     END DESC
-LIMIT $4;
+LIMIT $4
