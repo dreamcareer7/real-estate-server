fae4bba9450b7e4f5fc53de94321e70eba5c1a72
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-19 15:42:13 +0200

    All script now take a --query <query> argument. We can use it to call the scripts with manual DQML's.

--- scripts/mls/program.js
+++ scripts/mls/program.js
@@ -3,3 +3,4 @@ const program = require('commander')
 module.exports = program
 .option('-l, --limit <limit>', 'Limit RETS server response manually (default: 100)')
 .option('--start-from <hours_ago>', 'Fetches all the updates since <hours_ago>')
+.option('--query <query>', 'Use specified DQML <query>')
