c6b430612154507d616f4db4628a3a0bcff7c322
dreamcareer7 <dreamcareer070@gmail.com>
2017-05-22 10:55:33 +0200

    index it.

---
+++ migrations/1495412583088_indexing-notifications-users.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'CREATE INDEX ON notifications_users(notification)'
+]
+
+const down = []
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
