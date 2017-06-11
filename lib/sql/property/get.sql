814f2d82f5d8d462281e7a8e77c0e46a420d67a5
dreamcareer7 <dreamcareer070@gmail.com>
2017-06-11 03:28:53 +0200

    Apparently Property.get and Property.getAll where unused. Removed them.

--- lib/sql/property/get.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'property' AS TYPE,
-       properties.*,
-       EXTRACT(EPOCH FROM properties.created_at) AS created_at,
-       EXTRACT(EPOCH FROM properties.updated_at) AS updated_at
-FROM properties
-JOIN unnest($1::uuid[]) WITH ORDINALITY t(pid, ord) ON properties.id = pid
-ORDER BY t.ord
