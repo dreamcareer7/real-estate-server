78f7503c8dfe0bfd88460fa99ab80de216480e33
dreamcareer7 <dreamcareer070@gmail.com>
2017-06-12 15:38:29 +0200

    MLS suite can do open houses, price drops and status changes

--- tests/suites/mls.js
+++ tests/suites/mls.js
@@ -1,5 +1,24 @@
 const listing = require('./mls/listing.js')
 const office = require('./mls/office.js')
+const openhouse = require('./mls/openhouse.js')
+
+registerSuite('room', ['create'])
+
+const saveAlert = cb => {
+  const criteria = {
+    minimum_price: listing.listing.price-1,
+    maximum_price: listing.listing.price+1,
+  }
+
+  return frisby.create('create alert')
+    .post('/rooms/' + results.room.create.data.id + '/alerts', criteria)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: criteria
+    })
+}
 
 const addListing = (cb) => {
   return frisby.create('add a listing')
@@ -9,6 +28,19 @@ const addListing = (cb) => {
     })
     .after(cb)
     .expectStatus(200)
+}
+
+const getFeed = cb => {
+  return frisby.create('get feed')
+    .get('/rooms/' + results.room.create.data.id + '/recs/feed')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      info: {
+        count: 1
+      }
+    })
 }
 
 const addOffice = (cb) => {
@@ -21,7 +53,46 @@ const addOffice = (cb) => {
     .expectStatus(200)
 }
 
+const addOpenHouse = (cb) => {
+  return frisby.create('add an open house')
+    .post('/jobs', {
+      name: 'MLS.OpenHouse',
+      data: {processed:openhouse}
+    })
+    .after(cb)
+    .expectStatus(200)
+}
+
+const priceDrop = (cb) => {
+  listing.listing.price -= 3000
+
+  return frisby.create('change price')
+    .post('/jobs', {
+      name: 'MLS.Listing',
+      data: {processed:listing}
+    })
+    .after(cb)
+    .expectStatus(200)
+}
+
+const statusChange = (cb) => {
+  listing.listing.status = 'Sold'
+
+  return frisby.create('change status')
+    .post('/jobs', {
+      name: 'MLS.Listing',
+      data: {processed:listing}
+    })
+    .after(cb)
+    .expectStatus(200)
+}
+
 module.exports = {
+  saveAlert,
   addListing,
-  addOffice
+  getFeed,
+  addOffice,
+  addOpenHouse,
+  priceDrop,
+  statusChange
 }
---
+++ tests/suites/mls/openhouse.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"start_time": new Date((new Date).getTime()+1000*60*30),
+	"end_time": new Date((new Date).getTime()+1000*60*60),
+	"listing_mui": 2418967,
+	"type": "Public",
+	"matrix_unique_id": 49323840
+}
