ecaed23515c902f73306f7895c70f49123496c45
dreamcareer7 <dreamcareer070@gmail.com>
2017-07-05 12:39:23 +0200

    Couple of more tests

--- tests/suites/notification.js
+++ tests/suites/notification.js
@@ -3,6 +3,7 @@ const config = require('../../lib/config.js')
 const user = require('./data/user.js')
 const room = require('./data/room.js')
 
+registerSuite('room', ['create'])
 registerSuite('mls', ['saveAlert', 'addListing', 'refreshListings'])
 
 // const notification_response = require('./expected_objects/notification.js')
@@ -19,21 +20,35 @@ const getUsersNotification = (cb) => {
 
 const getNotification = (cb) => {
   return frisby.create('get a notification by id')
-    .get('/notifications/' + results.notification.getUsersNotification.data[0].id)
+    .get(`/notifications/${results.notification.getUsersNotification.data[0].id}`)
     .after(cb)
     .expectStatus(200)
 }
 
 const getNotification404 = (cb) => {
   return frisby.create('expect 404 with invalid notification id')
-    .get('/notifications/' + uuid.v1())
+    .get(`/notifications/${uuid.v1()}`)
     .after(cb)
     .expectStatus(404)
+}
+
+const seenNotification = (cb) => {
+  return frisby.create('mark a notification as seen')
+    .patch(`/notifications/${results.notification.getUsersNotification.data[0].id}/seen`)
+    .after(cb)
+    .expectStatus(204)
 }
 
 const acknowledgeNotification = (cb) => {
   return frisby.create('acknowledge notification')
     .delete('/notifications/')
+    .after(cb)
+    .expectStatus(204)
+}
+
+const acknowledgeRoomNotification = (cb) => {
+  return frisby.create('acknowledge room notifications')
+    .delete(`/rooms/${results.room.create.data.id}/notifications`)
     .after(cb)
     .expectStatus(204)
 }
@@ -107,7 +122,9 @@ module.exports = {
   getUsersNotification,
   getNotification,
   getNotification404,
+  seenNotification,
   acknowledgeNotification,
+  acknowledgeRoomNotification,
   pushNotification,
   cancelPushNotification,
   createRoom,
