9782447e96bd17f457df3c520b7f304a9cec6f55
dreamcareer7 <dreamcareer070@gmail.com>
2017-07-18 03:17:55 +0200

    syntax error

--- migrations/1500333373413_task-activities.js
+++ migrations/1500333373413_task-activities.js
@@ -6,14 +6,14 @@ const db = require('../lib/utils/db')
 const up = [
   'BEGIN',
 
-  'ALTER TYPE activity_type ADD VALUE \'UserUpdatedSubmission\''
-  'ALTER TYPE activity_type ADD VALUE \'UserCompletedSubmission\''
-  'ALTER TYPE notification_object_class ADD VALUE \'Submission\''
+  'ALTER TYPE activity_type ADD VALUE \'UserUpdatedSubmission\'',
+  'ALTER TYPE activity_type ADD VALUE \'UserCompletedSubmission\'',
+  'ALTER TYPE notification_object_class ADD VALUE \'Submission\'',
 
-  'ALTER TYPE activity_type ADD VALUE \'UserSubmittedReview\''
-  'ALTER TYPE activity_type ADD VALUE \'UserRejectedReview\''
-  'ALTER TYPE activity_type ADD VALUE \'UserApprovedReview\''
-  'ALTER TYPE notification_object_class ADD VALUE \'Review\''
+  'ALTER TYPE activity_type ADD VALUE \'UserSubmittedReview\'',
+  'ALTER TYPE activity_type ADD VALUE \'UserRejectedReview\'',
+  'ALTER TYPE activity_type ADD VALUE \'UserApprovedReview\'',
+  'ALTER TYPE notification_object_class ADD VALUE \'Review\'',
 
   'ALTER TABLE reviews_history ALTER status DROP DEFAULT',
 
