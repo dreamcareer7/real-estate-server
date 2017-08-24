4feb25355f8bb3b21fbaca99f588184beafaa452
dreamcareer7 <dreamcareer070@gmail.com>
2017-08-24 11:57:38 +0200

    Update the by_brand query

--- lib/sql/deal/brand.sql
+++ lib/sql/deal/brand.sql
@@ -1,8 +1,7 @@
 SELECT
-  DISTINCT tasks.deal AS id
-FROM tasks
+  DISTINCT id
+FROM deals
 WHERE
   deleted_at IS NULL
   AND brand IN(SELECT brand_children($1))
-  AND needs_attention = true
 ORDER BY updated_at DESC
