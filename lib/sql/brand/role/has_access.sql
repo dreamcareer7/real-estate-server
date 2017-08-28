ccf9077d254099aae8ac634869e7f0f795a287a4
dreamcareer7 <dreamcareer070@gmail.com>
2017-08-28 16:54:17 +0200

    Revised access management for brands.

---
+++ lib/sql/brand/role/has_access.sql
@@ -0,0 +1,9 @@
+SELECT
+  count(*) > 0 AS has
+FROM
+  brands_roles
+JOIN
+  brands_users ON brands_users.role = brands_roles.id
+WHERE
+  brands_users.user = $1
+  AND brand IN(SELECT * FROM brand_children($2))
