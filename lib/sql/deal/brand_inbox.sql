02d7fa0a3793cd9dc6b8bd0c33b0a402f0e04044
dreamcareer7 <dreamcareer070@gmail.com>
2017-09-10 23:31:08 +0200

    According to Shayan, backups _can_ show up on inbox. For now. We're not 100% sure yet.

--- lib/sql/deal/brand_inbox.sql
+++ lib/sql/deal/brand_inbox.sql
@@ -4,7 +4,6 @@ FROM deals
   JOIN deals_checklists ON deals.id = deals_checklists.deal
   JOIN tasks ON deals_checklists.id = tasks.checklist
 WHERE
-      deals_checklists.terminated_at  IS NULL
-  AND deals_checklists.deactivated_at IS NULL
+  deals_checklists.terminated_at  IS NULL
   AND tasks.needs_attention = TRUE
   AND deals.brand IN (SELECT brand_children($1::uuid))
