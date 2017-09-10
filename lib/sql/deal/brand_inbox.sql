55660b36a9248e70a020bdb0e6330084ed4edcab
dreamcareer7 <dreamcareer070@gmail.com>
2017-09-10 23:21:03 +0200

    If a checklist is terminated or deactivated, don't show it on inbox.

--- lib/sql/deal/brand_inbox.sql
+++ lib/sql/deal/brand_inbox.sql
@@ -4,6 +4,7 @@ FROM deals
   JOIN deals_checklists ON deals.id = deals_checklists.deal
   JOIN tasks ON deals_checklists.id = tasks.checklist
 WHERE
-  tasks.needs_attention = TRUE
-  AND
-  deals.brand IN (SELECT brand_children($1::uuid))
+      deals_checklists.terminated_at  IS NULL
+  AND deals_checklists.deactivated_at IS NULL
+  AND tasks.needs_attention = TRUE
+  AND deals.brand IN (SELECT brand_children($1::uuid))
