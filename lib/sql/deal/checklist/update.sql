8f41ba44c6f6b1d6be19656375841a419503fb2f
dreamcareer7 <dreamcareer070@gmail.com>
2017-09-15 21:15:37 +0200

    Dont touch the deactivated_at or terminated_at if none of the criteria matches.

--- lib/sql/deal/checklist/update.sql
+++ lib/sql/deal/checklist/update.sql
@@ -7,6 +7,7 @@ UPDATE deals_checklists SET
       WHEN $4::boolean = false THEN NULL
       WHEN $4 IS NULL  THEN NULL
       WHEN deactivated_at IS NULL AND $4 IS NOT NULL THEN CLOCK_TIMESTAMP()
+      ELSE terminated_at
     END
   ),
 
@@ -15,6 +16,7 @@ UPDATE deals_checklists SET
       WHEN $5::boolean = false THEN NULL
       WHEN $5 IS NULL  THEN NULL
       WHEN terminated_at IS NULL AND $5 IS NOT NULL THEN CLOCK_TIMESTAMP()
+      ELSE terminated_at
     END
   )
 
