dd16e21e463b9cd50a841cbb2743c1c914e4e5c9
dreamcareer7 <dreamcareer070@gmail.com>
2017-11-08 17:27:37 -0800

    We need more information from office to stamp

--- lib/sql/office/get.sql
+++ lib/sql/office/get.sql
@@ -1,7 +1,11 @@
 SELECT id,
        'office' as type,
+       mls_id,
        name,
-       mls_id
+       long_name,
+       address,
+       phone,
+       fax
 FROM offices
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(oid, ord) ON offices.id = oid
 ORDER BY t.ord
