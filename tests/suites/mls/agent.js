a0f202d3a0263a70ae39557be00cb3f60a491175
dreamcareer7 <dreamcareer070@gmail.com>
2017-12-12 22:31:24 +0330

    Changed office and agent mls test data to not exist in real db dumps

--- tests/suites/mls/agent.js
+++ tests/suites/mls/agent.js
@@ -1,16 +1,16 @@
 module.exports = {
   'email': 'jewellsweet@sbcglobal.net',
-  'mlsid': '0092013',
+  'mlsid': '00920130',
   'fax': '(972) 264-4703',
   'phone_number': '(972) 264-4703',
-  'first_name': 'Jewell',
+  'first_name': 'Gholi',
   'last_name': 'Sweet',
-  'full_name': 'Jewell Sweet',
+  'full_name': 'Gholi Sweet',
   'nar_number': '797500044',
   'office_mui': 15512742,
   'status': 'Active',
-  'office_mlsid': 'RCHT01',
+  'office_mlsid': 'RCHT01X',
   'work_phone': '(469) 358-8080',
-  'matrix_unique_id': 15515553,
+  'matrix_unique_id': 155155530,
   'matrix_modified_dt': '2016-04-26T14:00:18.090'
 }
