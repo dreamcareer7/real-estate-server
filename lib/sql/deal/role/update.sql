38bb7f50509b400d3b9fb627f083794833160405
dreamcareer7 <dreamcareer070@gmail.com>
2017-12-20 15:12:47 -0800

    Also update company_title fixed #915

--- lib/sql/deal/role/update.sql
+++ lib/sql/deal/role/update.sql
@@ -3,9 +3,10 @@ UPDATE deals_roles SET
   legal_first_name = $3,
   legal_middle_name = $4,
   legal_last_name = $5,
-  commission_dollar = $6,
-  commission_percentage = $7,
-  brokerwolf_id = $8,
-  brokerwolf_row_version = $9
+  company_title = $6,
+  commission_dollar = $7,
+  commission_percentage = $8,
+  brokerwolf_id = $9,
+  brokerwolf_row_version = $10
 
 WHERE id = $1
