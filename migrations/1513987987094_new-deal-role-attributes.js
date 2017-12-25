952d570e68b930e89bbdd5992d1d226bcc679401
dreamcareer7 <dreamcareer070@gmail.com>
2017-12-25 03:12:24 -0800

    First migrate the data then drop the column which was used in migration

--- migrations/1513987987094_new-deal-role-attributes.js
+++ migrations/1513987987094_new-deal-role-attributes.js
@@ -12,7 +12,6 @@ const up = [
   'ALTER TABLE deals_roles ADD phone_number TEXT',
   'ALTER TABLE deals_roles DROP CONSTRAINT deals_roles_unique',
   'ALTER TABLE deals_roles ALTER "user" DROP NOT NULL',
-  'ALTER TABLE envelopes_recipients DROP "user"',
   'ALTER TABLE envelopes_recipients DROP role',
   'ALTER TABLE envelopes_recipients ADD role uuid REFERENCES deals_roles(id)',
   `UPDATE envelopes_recipients SET role = (
@@ -23,6 +22,7 @@ const up = [
       )
   )`,
   'ALTER TABLE envelopes_recipients ALTER role SET NOT NULL',
+  'ALTER TABLE envelopes_recipients DROP "user"',
   'COMMIT'
 ]
 
