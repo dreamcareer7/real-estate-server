35edea81afdea1c909022d92f04622d9358ad031
dreamcareer7 <dreamcareer070@gmail.com>
2017-12-25 02:45:17 -0800

    Finalizing the refactor on envelope recipients fixes #920

--- lib/sql/deal/role/update.sql
+++ lib/sql/deal/role/update.sql
@@ -3,12 +3,15 @@ UPDATE deals_roles SET
   legal_first_name = $3,
   legal_middle_name = $4,
   legal_last_name = $5,
-  email = $6,
-  phone_number = $7,
-  company_title = $8,
-  commission_dollar = $9,
-  commission_percentage = $10,
-  brokerwolf_id = $11,
-  brokerwolf_row_version = $12
+  "user" = COALESCE($6, (
+    SELECT id FROM users WHERE LOWER(email) = LOWER($7)
+  )),
+  email = $7,
+  phone_number = $8,
+  company_title = $9,
+  commission_dollar = $10,
+  commission_percentage = $11,
+  brokerwolf_id = $12,
+  brokerwolf_row_version = $13
 
 WHERE id = $1
