ff14393cae9271a71efd9cdac313845fd4a5f6af
dreamcareer7 <dreamcareer070@gmail.com>
2018-01-05 12:27:38 -0800

    property types are no more.

--- tests/suites/schemas/deal/getContexts.js
+++ tests/suites/schemas/deal/getContexts.js
@@ -3,9 +3,6 @@ const H = require('../jsonSchemaHelper')
 module.exports = H.schemaArray({
   items: H.Object({
     name: H.Required(H.String),
-    type: H.Required(H.String),
-    property_type: H.Array({
-      minItems: 1
-    })
+    type: H.Required(H.String)
   })
 })
