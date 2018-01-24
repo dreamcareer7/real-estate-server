9b1b5489037574c2c979b88f99a3e86e20127ac9
dreamcareer7 <dreamcareer070@gmail.com>
2018-01-24 13:43:19 +0330

    [TS/Contact] Updated Contact type defs

--- types/models/Contact.d.ts
+++ types/models/Contact.d.ts
@@ -10,12 +10,12 @@ declare interface IParentContact extends IContactBase {
   deals?: IDeal[];
   sub_contacts: IContact[];
 
-  type: 'contact';
+  type: "contact";
 }
 
 declare interface IContact extends IContactBase {
   deleted_at?: number | null;
-  type: 'sub_contact';
+  type: "sub_contact";
   user: UUID;
 
   attributes: StringMap<IContactAttribute[]>;
@@ -24,23 +24,25 @@ declare interface IContact extends IContactBase {
   refs: UUID[];
 }
 
-declare type EAttributeTypes = 
-  "phone_number" |
-  "email" |
-  "name" |
-  "birthday" |
-  "tag" |
-  "profile_image_url" |
-  "cover_image_url" |
-  "company" |
-  "job_title" |
-  "stage" |
-  "address" |
-  "website" |
-  "source_type" |
-  "brand" |
-  "note" |
-  "relation";
+declare type EAttributeTypes =
+  | "phone_number"
+  | "email"
+  | "name"
+  | "birthday"
+  | "tag"
+  | "profile_image_url"
+  | "cover_image_url"
+  | "company"
+  | "job_title"
+  | "stage"
+  | "address"
+  | "website"
+  | "source_type"
+  | "source_id"
+  | "last_modified_on_source"
+  | "brand"
+  | "note"
+  | "relation";
 
 declare interface IContactAttribute {
   id: UUID;
@@ -75,12 +77,12 @@ declare interface IContactAddressAttribute extends IContactAttribute {
 }
 
 declare interface IContactEmailAttribute extends IContactAttribute {
-  type: 'email';
+  type: "email";
   email: String;
 }
 
 declare interface IContactPhoneAttribute extends IContactAttribute {
-  type: 'phone_number';
+  type: "phone_number";
   phone_number: String;
 }
 
@@ -94,10 +96,18 @@ declare namespace Contact {
   function getDisplayName(contact: IParentContact): String;
   function getAbbreviatedDisplayName(contact: IParentContact): String;
 
-  function getForUser(user_id: UUID, paging: any, cb: Callback<IParentContact[]>): void;
+  function getForUser(
+    user_id: UUID,
+    paging: any,
+    cb: Callback<IParentContact[]>
+  ): void;
   function get(contact_id: UUID, cb: Callback<IParentContact>): void;
   function getAll(contact_ids: UUID[], cb: Callback<IParentContact[]>): void;
-  function add(user_id: UUID, contact: IContact, cb: Callback<IParentContact>): void;
+  function add(
+    user_id: UUID,
+    contact: IContact,
+    cb: Callback<IParentContact>
+  ): void;
   function remove(contact_id: UUID, cb: Callback<void>): void;
   function patch(
     contact_id: UUID,
@@ -109,7 +119,10 @@ declare namespace Contact {
     user_id: UUID,
     attribute_id: UUID,
     attribute_type: EAttributeTypes,
-    attribute: IContactAttribute | IContactEmailAttribute | IContactPhoneAttribute,
+    attribute:
+      | IContactAttribute
+      | IContactEmailAttribute
+      | IContactPhoneAttribute,
     cb: Callback<IParentContact>
   ): void;
   function addAttribute(
@@ -131,7 +144,16 @@ declare namespace Contact {
     attribute_id: UUID,
     cb: Callback<IContactAttribute>
   ): void;
-  function getByTags(user_id: UUID, tags: String[], cb: Callback<IParentContact>): void;
+  function getByTags(
+    user_id: UUID,
+    tags: String[],
+    cb: Callback<IParentContact>
+  ): void;
+  function getByAttribute(
+    user_id: UUID,
+    attribute: String,
+    values: any[]
+  ): Promise<IParentContact[]>;
   function stringSearch(
     user_id: UUID,
     terms: String[],
@@ -141,11 +163,25 @@ declare namespace Contact {
   function getAllTags(user_id: UUID, cb: Callback<String[]>): void;
   function setRefs(contact_id: UUID, refs: UUID[], cb: Callback<void>): void;
 
-  type TOverride = Record<'source_type' | 'brand', String>;
+  type TOverride = Record<"source_type" | "brand", String>;
 
-  function isConnected(user_id: UUID, peer_id: UUID, cb: Callback<boolean>): void;
-  function connect(user_id: UUID, peer_id: UUID, override: TOverride, cb: Callback<void>): void;
-  function join(user_id: UUID, peer_id: UUID, override: TOverride, cb: Callback<IParentContact>): void;
+  function isConnected(
+    user_id: UUID,
+    peer_id: UUID,
+    cb: Callback<boolean>
+  ): void;
+  function connect(
+    user_id: UUID,
+    peer_id: UUID,
+    override: TOverride,
+    cb: Callback<void>
+  ): void;
+  function join(
+    user_id: UUID,
+    peer_id: UUID,
+    override: TOverride,
+    cb: Callback<IParentContact>
+  ): void;
   function convertUser(user: IUser, override: TOverride): IContact;
 
   function publicize(model: IContact): IContact;
