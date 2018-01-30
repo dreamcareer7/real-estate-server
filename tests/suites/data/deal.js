3c4135e33af316894bb1c64806e1c6046a350e9a
dreamcareer7 <dreamcareer070@gmail.com>
2018-01-29 22:25:20 -0800

    ESLINT fixes

--- tests/suites/data/deal.js
+++ tests/suites/data/deal.js
@@ -11,25 +11,25 @@ const full_address = '3030 Bryan Street Unit 308, 75204, Dallas, TX'
 
 const address = {
   postal_code: {
-    value:'75204',
+    value: '75204',
   },
   street_name: {
-    value:'Bryan'
+    value: 'Bryan'
   },
   unit_number: {
-    value:'308'
+    value: '308'
   },
   full_address: {
-    value:full_address
+    value: full_address
   },
   street_number: {
-    value:'3030'
+    value: '3030'
   },
   street_suffix: {
-    value:'Street'
+    value: 'Street'
   },
   street_address: {
-    value:'3030 Bryan Street'
+    value: '3030 Bryan Street'
   }
 }
 
