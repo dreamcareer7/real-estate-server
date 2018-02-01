8b692607757f4089d225074d82eefd7f1539f673
dreamcareer7 <dreamcareer070@gmail.com>
2018-02-01 17:42:14 +0330

    [CRM/Task] Enabled task update & filter options on get
    #945

--- tests/suites/task.js
+++ tests/suites/task.js
@@ -1,12 +1,11 @@
-registerSuite('contact', ['create'])
-
+const uuid = require('node-uuid')
 const { task } = require('./data/task')
+
+registerSuite('contact', ['create'])
 
 function create(cb) {
   return frisby.create('create a task')
-    .post('/crm/tasks', Object.assign({
-      contact: results.contact.create.data[0].id
-    }, task))
+    .post('/crm/tasks', task)
     .after(cb)
     .expectStatus(200)
     .expectJSON({
@@ -17,6 +16,38 @@ function create(cb) {
 function getForUser(cb) {
   return frisby.create('get list of assigned tasks')
     .get('/crm/tasks')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSONLength('data', 1)
+}
+
+function update(cb) {
+  return frisby.create('update task contact')
+    .put(`/crm/tasks/${results.task.create.data.id}/?associations[]=crm_task.contact`, Object.assign({
+      contact: results.contact.create.data[0].id
+    }, task))
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      data: {
+        contact: {
+          id: results.contact.create.data[0].id
+        }
+      }
+    })
+}
+
+function getAllDoesntIgnoreFilters(cb) {
+  return frisby.create('make sure filters are not ignored')
+    .get(`/crm/tasks/?contact=${uuid.v4()}`)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSONLength('data', 0)
+}
+
+function filterByContact(cb) {
+  return frisby.create('get tasks related to a contact')
+    .get(`/crm/tasks/?contact=${results.contact.create.data[0].id}`)
     .after(cb)
     .expectStatus(200)
     .expectJSONLength('data', 1)
@@ -40,6 +71,9 @@ function makeSureTaskIsDeleted(cb) {
 module.exports = {
   create,
   getForUser,
+  update,
+  getAllDoesntIgnoreFilters,
+  filterByContact,
   remove,
   makeSureTaskIsDeleted,
 }
