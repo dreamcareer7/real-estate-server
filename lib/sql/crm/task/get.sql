4cd7642f91a543c3aee95354baabd3de83e7c385
dreamcareer7 <dreamcareer070@gmail.com>
2018-02-05 23:41:29 +0330

    [CRM/Task] Added deal and contact access check in tasks/get query

--- lib/sql/crm/task/get.sql
+++ lib/sql/crm/task/get.sql
@@ -1,5 +1,11 @@
 SELECT
     crm_tasks.*,
+    (
+        SELECT id FROM deals WHERE id = crm_tasks.deal AND brand IN (SELECT user_brands($2))
+    ) as deal,
+    (
+        SELECT id FROM contacts WHERE id = crm_tasks.contact AND "user" = $2
+    ) as contact,
     'crm_task' as type,
     (
         SELECT
@@ -12,7 +18,8 @@ SELECT
     ) as reminders
 FROM
     crm_tasks
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(did, ord) ON crm_tasks.id = did
 WHERE
     deleted_at IS NULL
     AND assignee = $2
-    AND id = ANY($1)
+ORDER BY t.ord
