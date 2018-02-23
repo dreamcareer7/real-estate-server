7b2df978c7c8633042223585a9d17373b787520b
dreamcareer7 <dreamcareer070@gmail.com>
2018-02-23 11:09:27 -0800

    Make things more concurrent.
    Helps Applause-iOS#293

--- lib/sql/listing/county/counties.mv.sql
+++ lib/sql/listing/county/counties.mv.sql
@@ -3,4 +3,5 @@ SELECT
   DISTINCT(county_or_parish) AS title
 FROM addresses ORDER BY title;
 
+CREATE UNIQUE INDEX counties_title ON counties(title);
 CREATE INDEX county_title_gim ON counties USING gin (title gin_trgm_ops);
