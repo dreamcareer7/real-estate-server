ecf32e8070791480f0eb065603f78fc967f67a42
dreamcareer7 <dreamcareer070@gmail.com>
2018-02-26 22:47:05 +0330

    [CRM/Task] Set updated_at to now() by default

---
+++ migrations/1519672240677_Set crm_task.updated_at to now by default.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'ALTER TABLE crm_tasks ALTER COLUMN updated_at SET NOT NULL',
+  'ALTER TABLE crm_tasks ALTER COLUMN updated_at SET DEFAULT now()',
+]
+
+const down = [
+  'ALTER TABLE crm_tasks ALTER COLUMN updated_at DROP DEFAULT',
+  'ALTER TABLE crm_tasks ALTER COLUMN updated_at DROP NOT NULL',
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), next)
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
