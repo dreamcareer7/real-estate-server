172b29e03153071492d8f8577efab2855dfc567a
dreamcareer7 <dreamcareer070@gmail.com>
2018-02-26 18:17:28 +0330

    [Docs] Don't ignore additional doc generator data in tests folder

---
+++ tests/docs/task.js
@@ -0,0 +1,56 @@
+module.exports = {
+  'GET /crm/tasks': {
+    params: {
+      assignee: {
+        type: 'string',
+        required: false,
+        uuid: true
+      },
+      contact: {
+        type: 'string',
+        required: false,
+        uuid: true
+      },
+      deal: {
+        type: 'string',
+        required: false,
+        uuid: true
+      },
+      listing: {
+        type: 'string',
+        required: false,
+        uuid: true
+      },
+      due_gte: {
+        type: 'number',
+        required: false,
+        description: 'Due date greater than or equal to'
+      },
+      due_lte: {
+        type: 'number',
+        required: false,
+        description: 'Due date less than or equal to'
+      },
+      status: {
+        type: 'string',
+        required: false,
+        enum: ['PENDING', 'DONE']
+      },
+      task_type: {
+        type: 'string',
+        required: false,
+        enum: ['Call', 'Message', 'Todo']
+      },
+      start: {
+        type: 'number',
+        required: false,
+        default: 0
+      },
+      limit: {
+        type: 'number',
+        required: false,
+        default: 10
+      },
+    }
+  }
+}
