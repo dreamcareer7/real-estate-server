f2c2b1d01ba80090d9848cba148e4f451a67a377
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-12 00:12:05 +0330

    [CRM/Task] Oops! Forgot to remove an additional function from the migration

--- migrations/1520789331155_functions-for-task-access-control.js
+++ migrations/1520789331155_functions-for-task-access-control.js
@@ -6,19 +6,16 @@ const fs = require('fs')
 
 const read_access = fs.readFileSync(__dirname + '/../lib/sql/crm/task/read_access.fn.sql', 'utf-8')
 const write_access = fs.readFileSync(__dirname + '/../lib/sql/crm/task/write_access.fn.sql', 'utf-8')
-const has_access = fs.readFileSync(__dirname + '/../lib/sql/crm/task/has_access.fn.sql', 'utf-8')
 
 const up = [
   'BEGIN',
   read_access,
   write_access,
-  has_access,
   'COMMIT'
 ]
 
 const down = [
   'BEGIN',
-  'DROP FUNCTION crm_tasks_has_access(uuid[], uuid)',
   'DROP FUNCTION check_task_read_access(crm_tasks, uuid)',
   'DROP FUNCTION check_task_write_access(crm_tasks, uuid)',
   'COMMIT'
