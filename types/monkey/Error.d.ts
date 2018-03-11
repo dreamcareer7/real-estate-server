6b82520b904ae5f9ece2e7eb313fd4b83c0429fa
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-11 19:12:27 +0330

    [Workers] A little bit of house keeping and global removing in workers
    * Added Job, Metric and Slack as module exports
    * Added monkey patching for Error and Domain types
    * Added Sentry to sendNotification db errors

---
+++ types/monkey/Error.d.ts
@@ -0,0 +1,23 @@
+type TCreateError = (spec: Error, details?: object | string) => Error;
+
+declare interface ErrorConstructor {
+  create: TCreateError;
+
+  Validation: TCreateError;
+  Database: TCreateError;
+  Unauthorized: TCreateError;
+  ResourceNotFound: TCreateError;
+  Forbidden: TCreateError;
+  Conflict: TCreateError;
+  FileSystem: TCreateError;
+  Amazon: TCreateError;
+  Branch: TCreateError;
+  BadRequest: TCreateError;
+  Generic: TCreateError;
+  MethodNotAllowed: TCreateError;
+  PreconditionFailed: TCreateError;
+  NotImplemented: TCreateError;
+  NotAcceptable: TCreateError;
+
+  autoReport: boolean;
+}
