1de6fe995da6d296e19682de4c663403bb6c25c6
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-15 00:04:14 +0330

    [Activity] Uh, total requires type casting.

--- lib/sql/contact/timeline.sql
+++ lib/sql/contact/timeline.sql
@@ -8,7 +8,7 @@ ta as (
   FROM crm_associations
   WHERE contact = ANY($1) AND crm_task IS NOT NULL AND deleted_at IS NULL
 )
-SELECT *, count(*) over() as total FROM (
+SELECT *, (count(*) over())::INT as total FROM (
   (
     SELECT id, due_date as "timestamp", 'crm_task' as "type"
     FROM crm_tasks
