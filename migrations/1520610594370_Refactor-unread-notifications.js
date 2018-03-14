d6009e3f84212f41f7b15d5d803bac5c2ac880a3
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-14 14:25:41 +0330

    [CRM/Task] moved unread notifications view and fn to separate files
    Read those files in the migration

--- migrations/1520610594370_Refactor-unread-notifications.js
+++ migrations/1520610594370_Refactor-unread-notifications.js
@@ -1,51 +1,16 @@
 'use strict'
 
+const fs = require('fs')
 const async = require('async')
 const db = require('../lib/utils/db')
 
+const unread_notifications_view = fs.readFileSync(__dirname + '/../lib/sql/crm/task/unread_notifications.view.sql', 'utf-8')
+const unread_room_notifications_fn = fs.readFileSync(__dirname + '/../lib/sql/crm/task/unread_room_notifications.fn.sql', 'utf-8')
+
 const up = [
   'BEGIN',
-  `CREATE OR REPLACE VIEW unread_notifications AS
-    SELECT
-      notifications.*,
-      notifications_users.user
-    FROM notifications
-    JOIN notifications_users      ON notifications.id = notifications_users.notification
-    FULL JOIN
-      notifications_deliveries ON
-        notifications.id = notifications_deliveries.notification AND
-        notifications_users.user = notifications_deliveries.user
-    WHERE
-      notifications_users.acked_at IS NULL
-      AND notifications_deliveries.id IS NULL`,
-
-  `CREATE OR REPLACE FUNCTION
-      unread_room_notifications(interval) RETURNS TABLE (
-        room uuid,
-        "user" uuid,
-        first_unread double precision,
-        last_unread timestamptz
-      )
-    AS $$
-    SELECT
-      room,
-      "user",
-      EXTRACT( -- Time of the first unread notification for this user on this room.
-        EPOCH FROM ((array_agg(created_at ORDER BY created_at  ASC))[1]::timestamptz)
-      ) as first_unread,
-      (
-        (array_agg(created_at ORDER BY created_at DESC))[1]
-      ) as last_unread
-    FROM
-      unread_notifications
-    WHERE
-      created_at >= (NOW() - $1)
-      AND room IS NOT NULL
-    GROUP BY
-      "user",
-      room
-    $$
-    LANGUAGE SQL STABLE`,
+  unread_notifications_view,
+  unread_room_notifications_fn,
   'COMMIT'
 ]
 
