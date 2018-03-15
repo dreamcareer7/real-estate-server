05c69a6dc6aa9e66e0488216564ef4acb9f22c63
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-16 01:19:56 +0330

    [Migrations] Fixed file path for unread notification related view and fn

--- migrations/1520610594370_Refactor-unread-notifications.js
+++ migrations/1520610594370_Refactor-unread-notifications.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const async = require('async')
 const db = require('../lib/utils/db')
 
-const unread_notifications_view = fs.readFileSync(__dirname + '/../lib/sql/crm/task/unread_notifications.view.sql', 'utf-8')
-const unread_room_notifications_fn = fs.readFileSync(__dirname + '/../lib/sql/crm/task/unread_room_notifications.fn.sql', 'utf-8')
+const unread_notifications_view = fs.readFileSync(__dirname + '/../lib/sql/crm/notification/unread_notifications.view.sql', 'utf-8')
+const unread_room_notifications_fn = fs.readFileSync(__dirname + '/../lib/sql/crm/message/unread_room_notifications.fn.sql', 'utf-8')
 
 const up = [
   'BEGIN',
