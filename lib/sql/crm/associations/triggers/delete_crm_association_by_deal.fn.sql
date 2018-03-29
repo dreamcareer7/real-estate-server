121da8fa4a244241eb210a04b10f99c624e1994b
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-29 16:44:41 +0430

    [CRM/Association] Delete associations after deleting deal or contact

---
+++ lib/sql/crm/associations/triggers/delete_crm_association_by_deal.fn.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION delete_crm_association_by_deal ()
+  RETURNS trigger
+  LANGUAGE plpgsql
+AS $$
+  BEGIN
+    UPDATE
+      crm_associations
+    SET
+      deleted_at = NOW()
+    WHERE
+      deal = NEW.id;
+  END;
+$$
