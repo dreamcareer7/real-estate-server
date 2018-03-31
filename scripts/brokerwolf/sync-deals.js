6c2d2d6f87782c8eb9d028cd0d9a09a5172043eb
dreamcareer7 <dreamcareer070@gmail.com>
2018-03-31 14:40:43 -0700

    Script to sync a bunch of deals

---
+++ scripts/brokerwolf/sync-deals.js
@@ -0,0 +1,52 @@
+require('../connection.js')
+require('../../lib/models/index.js')
+const promisify = require('../../lib/utils/promisify')
+const fs = require('fs')
+
+const file = fs.readFileSync(process.argv[2]).toString()
+
+const deal_ids = file.trim().split('\n').map(id => id.trim())
+
+const run = async () => {
+  const deals = await promisify(Deal.getAll)(deal_ids)
+
+  let i = 0
+
+  const success = []
+  const errors = []
+
+  for(const deal of deals) {
+    i++
+    console.log(`${i}/${deals.length}`)
+
+    try {
+      await Deal.BrokerWolf.sync(deal)
+      success.push(deal)
+    } catch(error) {
+      errors.push({
+        deal,
+        error
+      })
+    }
+  }
+
+
+  console.log('\n\n\n\n\n\nSuccessful')
+
+  for(const deal of success) {
+    console.log(Deal.getContext(deal, 'full_address'))
+  }
+
+  console.log('Error')
+
+  for(const pair of errors) {
+    const { deal, error } = pair
+
+    console.log(deal.id, Deal.getContext(deal, 'full_address'))
+    console.log(error.message)
+    console.log('----------------------------')
+  }
+}
+
+
+run()
