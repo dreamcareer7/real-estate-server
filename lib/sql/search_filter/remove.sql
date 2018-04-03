f8b26867e1eb9c4e1908c592032cdf32933e6d81
dreamcareer7 <dreamcareer070@gmail.com>
2018-04-03 13:07:12 +0430

    Implement access control for search filters

--- lib/sql/search_filter/remove.sql
+++ lib/sql/search_filter/remove.sql
@@ -1,4 +1,4 @@
 UPDATE search_filters
 SET
   deleted_at = clock_timestamp()
-WHERE id = $1 AND user_id = $2
+WHERE id = $1
--- lib/sql/search_filter/update.sql
+++ lib/sql/search_filter/update.sql
@@ -1,6 +1,6 @@
 UPDATE search_filters SET
-    filters = $3,
-    name = $4,
-    is_pinned = $5,
+    filters = $2,
+    name = $3,
+    is_pinned = $4,
     updated_at = clock_timestamp()
-WHERE id = $1 AND user_id = $2
+WHERE id = $1
