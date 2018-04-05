cf4e75a02c20e052e3e89fe1d3f5755b39631197
dreamcareer7 <dreamcareer070@gmail.com>
2018-04-06 01:22:26 +0430

    [Contact] Restored /contacts/tags endpoint

--- lib/sql/tag/get_all.sql
+++ lib/sql/tag/get_all.sql
@@ -1,18 +1,14 @@
-WITH c AS
-(
-  SELECT id
-  FROM contacts
-  WHERE "user" = $1 AND
-        deleted_at IS NULL
-)
-SELECT COALESCE(attribute, '{}'::jsonb) || JSONB_BUILD_OBJECT
-  (
-    'id', id,
-    'type', attribute_type,
-    'created_at', EXTRACT(EPOCH FROM created_at),
-    'updated_at', EXTRACT(EPOCH FROM updated_at)
-  ) AS tag
-FROM contacts_attributes
-WHERE contact IN (SELECT id FROM c) AND
-      attribute_type = 'tag' AND
-      deleted_at IS NULL
+SELECT
+  cattrs.id as id,
+  'tag' as "type",
+  cattrs.text AS tag,
+  EXTRACT(EPOCH FROM cattrs.created_at) AS created_at,
+  EXTRACT(EPOCH FROM cattrs.updated_at) AS updated_at
+FROM
+  contacts
+  INNER JOIN contacts_attributes_with_name AS cattrs
+    ON contacts.id = cattrs.contact
+WHERE
+  contacts.deleted_at IS NULL
+  AND check_contact_read_access(contacts, $1)
+  AND cattrs.name = 'tag';
