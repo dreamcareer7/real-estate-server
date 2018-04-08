875a98a86301bebee5140ca6d228df7ae33f859e
dreamcareer7 <dreamcareer070@gmail.com>
2018-04-08 13:32:56 -0700

    ESLINT

--- migrations/1522277740923_remove-bad-styles.js
+++ migrations/1522277740923_remove-bad-styles.js
@@ -4,7 +4,7 @@ const async = require('async')
 const db = require('../lib/utils/db')
 
 const up = [
-  `UPDATE alerts SET architectural_styles = NULL WHERE architectural_styles = '{Southwestern,Ranch,Spanish,A-Frame,"Mid-Centry Modern",Prairie,"Studio Apartment",Contemporary/Modern,"Split Level",Victorian,Traditional,Mediterranean,Colonial,Oriental,Loft,French,Tudor}'`
+  'UPDATE alerts SET architectural_styles = NULL WHERE architectural_styles = \'{Southwestern,Ranch,Spanish,A-Frame,"Mid-Centry Modern",Prairie,"Studio Apartment",Contemporary/Modern,"Split Level",Victorian,Traditional,Mediterranean,Colonial,Oriental,Loft,French,Tudor}\''
 ]
 
 const down = []
