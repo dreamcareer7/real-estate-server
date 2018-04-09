9973980872b8f9f767585877e5b21f56e8fa14ce
dreamcareer7 <dreamcareer070@gmail.com>
2018-04-10 01:58:05 +0430

    [Contact] Refactor and simplify contact/get query

--- lib/sql/contact/get.sql
+++ lib/sql/contact/get.sql
@@ -1,26 +1,23 @@
-WITH sub_contacts AS (
-  SELECT
-    c1_id AS id
-  FROM
-    joined_contacts
+SELECT
+  c1.id,
+  'sub_contact' AS type,
+  EXTRACT(EPOCH FROM c1.created_at) AS created_at,
+  EXTRACT(EPOCH FROM c1.updated_at) AS updated_at,
+  EXTRACT(EPOCH FROM c1.deleted_at) AS deleted_at,
+  c1.created_by,
+  c1."user",
+  c1.brand,
+  coalesce(c1.parent, c1.id) as parent,
+  get_contact_users(c1.id) as users,
+  get_deals_with_contact($2::uuid, c1.id) as deals
+FROM
+  contacts c1,
+  contacts c2
   JOIN
     unnest($1::uuid[])
     WITH ORDINALITY t(cid, ord)
-    ON c2_id = cid
-)
-SELECT
-  id,
-  'sub_contact' AS type,
-  EXTRACT(EPOCH FROM contacts.created_at) AS created_at,
-  EXTRACT(EPOCH FROM contacts.updated_at) AS updated_at,
-  EXTRACT(EPOCH FROM contacts.deleted_at) AS deleted_at,
-  created_by,
-  "user",
-  brand,
-  coalesce(parent, id) as parent,
-  get_contact_users(id) as users,
-  get_deals_with_contact($2::uuid, id) as deals
-FROM
-  contacts
-  JOIN
-    sub_contacts USING (id)
+    ON c2.id = cid
+WHERE
+  coalesce(c2.parent, c2.id) = coalesce(c1.parent, c1.id)
+  AND c1.deleted_at IS NULL
+  AND c2.deleted_at IS NULL
