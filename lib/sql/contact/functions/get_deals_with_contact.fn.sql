30864dc71ff9b06b6a37b52edc3305755491f703
dreamcareer7 <dreamcareer070@gmail.com>
2018-04-09 21:23:26 +0430

    [Contact] Refactor get_deals_with_contact function to speed up

--- lib/sql/contact/functions/get_deals_with_contact.fn.sql
+++ lib/sql/contact/functions/get_deals_with_contact.fn.sql
@@ -1,31 +1,42 @@
 CREATE OR REPLACE FUNCTION get_deals_with_contact (user_id uuid, contact_id uuid)
   RETURNS uuid[]
-  LANGUAGE SQL
+  LANGUAGE plpgsql
   STABLE
 AS $$
-  SELECT 
-    ARRAY_AGG(deal)
-  FROM 
-    deals_roles
-    JOIN deals ON deals_roles.deal = deals.id 
-  WHERE
-    deals.brand = ANY(SELECT brand FROM user_brands(user_id))
-    AND deals_roles.deleted_at IS NULL 
-    AND (
-      deals_roles.user = ANY(get_contact_users(contact_id))
-      OR LOWER(deals_roles.email) IN (
-        SELECT "text"
-        FROM contacts_attributes_with_name
-        WHERE
-          contact = contact_id
-          AND "name" = 'email'
-      )
-      OR deals_roles.phone_number IN (
-        SELECT "text"
-        FROM contacts_attributes_with_name
-        WHERE
-          contact = contact_id
-          AND "name" = 'phone_number'
-      )
-    )
+  DECLARE
+    cu uuid[];
+    ub uuid[];
+    res uuid[];
+  BEGIN
+    SELECT array_agg(brand) INTO ub FROM user_brands(user_id);
+    SELECT get_contact_users(contact_id) INTO cu;
+    SELECT 
+      ARRAY_AGG(deal)
+    INTO
+      res
+    FROM 
+      deals_roles
+      JOIN deals ON deals_roles.deal = deals.id 
+    WHERE
+      deals.brand = ANY(ub)
+      AND deals_roles.deleted_at IS NULL 
+      AND (
+        deals_roles.user = ANY(cu)
+        OR LOWER(deals_roles.email) IN (
+          SELECT "text"
+          FROM contacts_attributes_with_name
+          WHERE
+            contact = contact_id
+            AND "name" = 'email'
+        )
+        OR deals_roles.phone_number IN (
+          SELECT "text"
+          FROM contacts_attributes_with_name
+          WHERE
+            contact = contact_id
+            AND "name" = 'phone_number'
+        )
+      );
+    RETURN res;
+  END;
 $$
