46b80bb0ac93b5ac775213688723aeb55dcfaefa
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-01 20:28:27 +0430

    [Contact] Add timestamps

--- lib/sql/contact/list/get.sql
+++ lib/sql/contact/list/get.sql
@@ -1,5 +1,8 @@
 SELECT
     id,
+    EXTRACT(EPOCH FROM created_at) AS created_at,
+    EXTRACT(EPOCH FROM updated_at) AS updated_at,
+    EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
     filters,
     name,
     is_pinned,
