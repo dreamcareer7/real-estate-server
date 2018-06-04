b3ea7242374338e26793502d7ce07bb41f564874
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-04 19:13:38 +0430

    [ContactList] Put pinned lists at the top and sort by creation time

--- lib/sql/contact/list/list_for_user.sql
+++ lib/sql/contact/list/list_for_user.sql
@@ -1,4 +1,10 @@
 SELECT
-    id
-FROM contact_search_lists
-WHERE "user" = $1 AND deleted_at IS NULL
+  id
+FROM
+  contact_search_lists
+WHERE
+  "user" = $1
+  AND deleted_at IS NULL
+ORDER BY
+  is_pinned DESC,
+  created_at DESC
