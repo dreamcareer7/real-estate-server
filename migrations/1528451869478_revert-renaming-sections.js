bc0f20f18bfa05a008c9866352b009da706e3acb
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-08 14:31:14 +0430

    [Contact] Revert changing profile section names

---
+++ migrations/1528451869478_revert-renaming-sections.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'BEGIN',
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Names'
+  WHERE
+    section = 'Details'`,
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Details'
+  WHERE
+    section = 'Contact Info'`,
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Details'
+  WHERE
+    name = 'job_title' OR name = 'company'`,
+
+  'COMMIT'
+]
+
+const down = [
+  'BEGIN',
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Names'
+  WHERE
+    name = 'job_title' OR name = 'company'`,
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Contact Info'
+  WHERE
+    section = 'Details'`,
+
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Details'
+  WHERE
+    section = 'Names'`,
+
+  'COMMIT'
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client, release) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), err => {
+      release()
+      next(err)
+    })
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
