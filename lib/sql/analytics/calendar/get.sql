e1f14ff718c8c7fe69c12c8274b908847fb7340b
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-12 17:57:00 +0430

    [Dashboard] Fixed checking date range bug.

--- lib/sql/analytics/calendar/get.sql
+++ lib/sql/analytics/calendar/get.sql
@@ -20,11 +20,19 @@ WHERE
   AND (CASE
     WHEN "recurring" IS True THEN (
       (
-        DATE_PART('month', "timestamp") >= date_part('month', to_timestamp($3))
-        OR DATE_PART('day', "timestamp") >= date_part('day', to_timestamp($3))
+        DATE_PART('month', "timestamp") > date_part('month', to_timestamp($3))
+        OR (
+          DATE_PART('month', "timestamp") = date_part('month', to_timestamp($3))
+          AND
+          DATE_PART('day', "timestamp") >= date_part('day', to_timestamp($3))
+        )
       ) AND (
-        DATE_PART('month', "timestamp") <= date_part('month', to_timestamp($4))
-        OR DATE_PART('day', "timestamp") <= date_part('day', to_timestamp($4))
+        DATE_PART('month', "timestamp") < date_part('month', to_timestamp($4))
+        OR (
+          DATE_PART('month', "timestamp") = date_part('month', to_timestamp($4))
+          AND
+          DATE_PART('day', "timestamp") <= date_part('day', to_timestamp($4))
+        )
       )
     )
     ELSE "timestamp" BETWEEN to_timestamp($3) AND to_timestamp($4)
