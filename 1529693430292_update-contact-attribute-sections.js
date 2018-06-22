704bd8dbc67230772f4d5dbfa8f0af7b74d2a233
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-22 23:24:08 +0430

    [Contact] Move job_title and company attrs to another section

---
+++ 1529693430292_update-contact-attribute-sections.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const async = require('async')
+const db = require('../lib/utils/db')
+
+const up = [
+  'BEGIN',
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Contact Info'
+  WHERE
+    section = 'Details'
+  `,
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Details'
+  WHERE
+    section = 'Names'
+  `,
+  `UPDATE
+    contacts_attribute_defs
+  SET
+    section = 'Details'
+  WHERE
+    name = 'company' OR name = 'job_title'
+  `,
+  'COMMIT'
+]
+
+const down = [
+]
+
+const runAll = (sqls, next) => {
+  db.conn((err, client, release) => {
+    if (err)
+      return next(err)
+
+    async.eachSeries(sqls, client.query.bind(client), err => {
+      release()
+      next(err)
+    })
+  })
+}
+
+const run = (queries) => {
+  return (next) => {
+    runAll(queries, next)
+  }
+}
+
+exports.up = run(up)
+exports.down = run(down)
