e06c35918b5ef33a20f05ac345ef05908dbf38f7
dreamcareer7 <dreamcareer070@gmail.com>
2018-06-25 08:11:10 -0700

    Make it possible to acknolwedge only a specific set of notifications by providing {notifications}

--- lib/sql/notification/ack_personal.sql
+++ lib/sql/notification/ack_personal.sql
@@ -2,12 +2,24 @@ UPDATE notifications_users
 SET
   acked_at = CLOCK_TIMESTAMP()
 WHERE
-  "user" = $1 AND
-  notification IN
-  (
-    SELECT id FROM notifications
-    WHERE room IS NULL AND
-    specific = $1 AND
-    COALESCE(NOT ($1 = ANY(exclude)), TRUE)
-  ) AND
   acked_at IS NULL
+  AND "user" = $1
+  -- If a list of specific notifications is provided, clear them away
+  -- Otherwise, clear all notifications belonging to this user
+  AND (
+    (
+      $2 IS NULL
+      AND notification IN
+      (
+        SELECT id FROM notifications
+        WHERE room IS NULL AND
+        specific = $1 AND
+        COALESCE(NOT ($1 = ANY(exclude)), TRUE)
+      )
+    )
+    OR
+    (
+      $2 IS NOT NULL
+      AND notification = ANY($2::uuid[])
+    )
+  )
