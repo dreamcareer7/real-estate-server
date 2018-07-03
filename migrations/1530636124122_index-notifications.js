59665205331a0b91ffd5e9e2f9607b07b81f5d29
dreamcareer7 <dreamcareer070@gmail.com>
2018-07-03 09:48:20 -0700

    lint

--- migrations/1530636124122_index-notifications.js
+++ migrations/1530636124122_index-notifications.js
@@ -5,12 +5,12 @@ const db = require('../lib/utils/db')
 
 const up = [
   'BEGIN',
-  `CREATE INDEX notifications_subject_class ON notifications(subject_class)`,
-  `CREATE INDEX notifications_object_class ON notifications(object_class)`,
-  `CREATE INDEX notifications_auxiliary_object_class ON notifications(auxiliary_object_class)`,
-  `CREATE INDEX notifications_auxiliary_subject_class ON notifications(auxiliary_subject_class)`,
-  `CREATE INDEX notifications_extra_object_class ON notifications(extra_object_class)`,
-  `CREATE INDEX notifications_extra_subject_class ON notifications(extra_subject_class)`,
+  'CREATE INDEX notifications_subject_class ON notifications(subject_class)',
+  'CREATE INDEX notifications_object_class ON notifications(object_class)',
+  'CREATE INDEX notifications_auxiliary_object_class ON notifications(auxiliary_object_class)',
+  'CREATE INDEX notifications_auxiliary_subject_class ON notifications(auxiliary_subject_class)',
+  'CREATE INDEX notifications_extra_object_class ON notifications(extra_object_class)',
+  'CREATE INDEX notifications_extra_subject_class ON notifications(extra_subject_class)',
   'COMMIT'
 ]
 
