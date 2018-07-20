778f669b987ec1d41ec90da26f8df7ebc957b285
dreamcareer7 <dreamcareer070@gmail.com>
2018-07-21 01:22:05 +0430

    Revert changes for notification to prevent any break in system Now this is ready to be merged into testing

--- lib/sql/alert/triggers/delete_alert_setting.fn.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION delete_alert_setting() RETURNS TRIGGER AS $$
-	BEGIN
-		DELETE FROM user_alert_settings
-		WHERE "user" = OLD."user" AND
-		alert = (select id from alerts where room = OLD.room);
-
-		RETURN OLD;
-	END;
-$$ LANGUAGE plpgsql;
--- lib/sql/listing/matching_users.sql
+++ lib/sql/listing/matching_users.sql
@@ -1,4 +1,4 @@
-SELECT rooms_users."user" AS id, alerts.id as alert_id
+SELECT DISTINCT(rooms_users."user") AS id
 FROM alerts
 INNER JOIN rooms_users
   ON alerts.room = rooms_users.room
