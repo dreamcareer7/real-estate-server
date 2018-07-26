2d9a56f515039bff61c88f3c4faeb263a6950cd2
dreamcareer7 <dreamcareer070@gmail.com>
2018-07-26 19:37:52 +0430

    [Touch] Fix tests

--- tests/suites/data/touch.js
+++ tests/suites/data/touch.js
@@ -3,7 +3,7 @@ const timestamp = new Date()
 const touch = {
   description: 'Hello, Activity World!',
   timestamp: timestamp.getTime() / 1000,
-  activity_type: 'Todo'
+  activity_type: 'Call'
 }
 
 module.exports = {
