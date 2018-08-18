cd8d8d15e17c3fe45a4559fedf918f217cdb2b90
dreamcareer7 <dreamcareer070@gmail.com>
2018-08-18 22:33:05 +0430

    [HTTP] Allow X-Auth-Mode header in CORS

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ db.enableLogging(true)
 // For dev only
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Rechat-Brand')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Rechat-Brand, X-Auth-Mode')
   next()
 })
 
