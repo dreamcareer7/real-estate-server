e16cdbcb78bdb9136fdb53d08377e104702b1689
dreamcareer7 <dreamcareer070@gmail.com>
2018-08-26 18:26:56 +0430

    [TeamContact] Fix crm_associations/get

--- lib/sql/crm/associations/get.sql
+++ lib/sql/crm/associations/get.sql
@@ -1,43 +1,5 @@
 WITH ub AS (
   SELECT brand FROM user_brands($2)
-),
-a_ids AS (
-  (
-    SELECT
-      crm_associations.id,
-      deals.brand,
-      NULL::uuid AS "user"
-    FROM
-      crm_associations
-    JOIN deals
-      ON crm_associations.deal = deals.id
-    JOIN ub USING (brand)
-    WHERE 
-      association_type = 'deal'
-  )
-  UNION
-  (
-    SELECT
-      crm_associations.id,
-      contacts.brand,
-      NULL::uuid AS "user"
-    FROM
-      crm_associations
-    JOIN contacts
-      ON crm_associations.contact = contacts.id
-    JOIN ub USING (brand)
-    WHERE 
-      association_type = 'contact'
-  )
-  UNION
-  (
-    SELECT
-      crm_associations.*
-    FROM
-      crm_associations
-    WHERE
-      association_type = 'listing'
-  )
 )
 SELECT
   id,
@@ -53,6 +15,48 @@ SELECT
   listing,
   'crm_association' as "type"
 FROM
-  a_ids
+  (
+    (
+      SELECT
+        crm_associations.id,
+        deals.brand,
+        NULL::uuid AS "user"
+      FROM
+        crm_associations
+      JOIN deals
+        ON crm_associations.deal = deals.id
+      JOIN ub USING (brand)
+      WHERE 
+        crm_associations.association_type = 'deal'
+    )
+    UNION
+    (
+      SELECT
+        crm_associations.id,
+        contacts.brand,
+        NULL::uuid AS "user"
+      FROM
+        crm_associations
+      JOIN contacts
+        ON crm_associations.contact = contacts.id
+      JOIN ub USING (brand)
+      WHERE 
+        crm_associations.association_type = 'contact'
+    )
+    UNION
+    (
+      SELECT
+        crm_associations.id,
+        NULL::uuid AS brand,
+        $2::uuid AS "user"
+      FROM
+        crm_associations
+        JOIN listings
+          ON listings.id = crm_associations.listing
+      WHERE
+        crm_associations.association_type = 'listing'
+    )
+  ) AS a_ids
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(did, ord) ON a_ids.id = did
+JOIN crm_associations USING (id)
 ORDER BY t.ord
