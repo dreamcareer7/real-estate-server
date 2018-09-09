0f7ffe80ee510444563c9749209d76cbca924ec9
dreamcareer7 <dreamcareer070@gmail.com>
2018-09-09 12:12:36 -0700

    Getting rid of formstack based forms

--- lib/sql/form/get_formstack.sql
+++
@@ -1,1 +0,0 @@
-SELECT id FROM forms WHERE formstack_id = $1 AND deleted_at IS NULL
--- tests/suites/expected_objects/form.js
+++ tests/suites/expected_objects/form.js
@@ -2,9 +2,6 @@ const v = require('./validation.js')
 
 module.exports = {
   id: String,
-  formstack_id: Number,
-  fields: Object,
   name: String,
   type: String,
-  roles: v.optionalArray
 }
