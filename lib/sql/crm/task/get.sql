13070ebd1733bd534851702da69f7c5e2e614280
dreamcareer7 <dreamcareer070@gmail.com>
2018-09-10 17:42:14 +0430

    [CRM/Task] Don't allow fetching a deleted task

--- lib/sql/crm/task/get.sql
+++ lib/sql/crm/task/get.sql
@@ -1,39 +1,41 @@
 SELECT
-    id,
-    EXTRACT(EPOCH FROM created_at) AS created_at,
-    EXTRACT(EPOCH FROM updated_at) AS updated_at,
-    EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
-    title,
-    "description",
-    EXTRACT(EPOCH FROM due_date) AS due_date,
-    "status",
-    task_type,
-    assignee,
-    (
-        SELECT
-            ARRAY_AGG(id ORDER BY "created_at")
-        FROM
-            crm_associations
-        WHERE
-            crm_task = crm_tasks.id
-            AND deleted_at IS NULL
-    ) as associations,
-    (
-        get_files_by_role('CrmTask', crm_tasks.id)
-    ) as files,
-    (
-        SELECT
-            ARRAY_AGG(id ORDER BY "created_at")
-        FROM
-            reminders
-        WHERE
-            task = crm_tasks.id
-            AND deleted_at IS NULL
-    ) as reminders,
-    brand,
-    created_by,
-    'crm_task' as "type"
+  id,
+  EXTRACT(EPOCH FROM created_at) AS created_at,
+  EXTRACT(EPOCH FROM updated_at) AS updated_at,
+  EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
+  title,
+  "description",
+  EXTRACT(EPOCH FROM due_date) AS due_date,
+  "status",
+  task_type,
+  assignee,
+  (
+    SELECT
+      ARRAY_AGG(id ORDER BY "created_at")
+    FROM
+      crm_associations
+    WHERE
+      crm_task = crm_tasks.id
+      AND deleted_at IS NULL
+  ) as associations,
+  (
+    get_files_by_role('CrmTask', crm_tasks.id)
+  ) as files,
+  (
+    SELECT
+      ARRAY_AGG(id ORDER BY "created_at")
+    FROM
+      reminders
+    WHERE
+      task = crm_tasks.id
+      AND deleted_at IS NULL
+  ) as reminders,
+  brand,
+  created_by,
+  'crm_task' as "type"
 FROM
-    crm_tasks
-JOIN unnest($1::uuid[]) WITH ORDINALITY t(did, ord) ON crm_tasks.id = did
-ORDER BY t.ord
+  crm_tasks
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(did, ord)
+  ON crm_tasks.id = did
+ORDER BY
+  t.ord
