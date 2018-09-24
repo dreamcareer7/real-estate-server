4a1020486bbfe211505fb4534512f8fddf13bd52
dreamcareer7 <dreamcareer070@gmail.com>
2018-09-24 14:35:48 +0330

    ESLINT

--- scripts/support/create-solo-brands.js
+++ scripts/support/create-solo-brands.js
@@ -1,43 +1,42 @@
-var brand_data = {
-    "palette": {
-        "primary": "red"
-    },
-    "assets": {},
-    "messages": {},
-    "role": "Agent"
+const brand_data = {
+  palette: {
+    primary: 'red'
+  },
+  assets: {},
+  messages: {},
+  role: 'Agent'
 }
 
 async function createSoloBrand(user) {
-    let brand = await Brand.create({
-        ...brand_data,
-        name: /^\s*$/.test(user.name) ? 'Personal Team' : `${user.name}'s Team`
-    })
+  const brand = await Brand.create({
+    ...brand_data,
+    name: /^\s*$/.test(user.name) ? 'Personal Team' : `${user.name}'s Team`
+  })
 
-    const role = await BrandRole.create({
-        brand: brand.id,
-        role: 'Agent',
-        acl: [
-            'CRM'
-        ]
-    })
+  const role = await BrandRole.create({
+    brand: brand.id,
+    role: 'Agent',
+    acl: ['CRM']
+  })
 
-    await BrandRole.addMember({
-        user: user.id,
-        role: role.id
-    })
+  await BrandRole.addMember({
+    user: user.id,
+    role: role.id
+  })
 
-    await sql.update('INSERT INTO users_solo_brands VALUES ($1::uuid, $2::uuid)', [
-        user.id,
-        brand.id
-    ])
+  await sql.update(
+    'INSERT INTO users_solo_brands VALUES ($1::uuid, $2::uuid)',
+    [user.id, brand.id]
+  )
 }
 
 async function createAllSoloBrands() {
-    for (const user of users) {
-        Context.log('Creating for ' + user.name);
-        await createSoloBrand(user);
-    }
+  for (const user of users) {
+    Context.log('Creating for ' + user.name)
+    await createSoloBrand(user)
+  }
 }
 
-/* eslint-disable-line */
-await createAllSoloBrands()
+async function main() {
+  await createAllSoloBrands()
+}
