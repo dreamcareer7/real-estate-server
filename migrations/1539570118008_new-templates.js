b1cabf1b266ef2f32e1fb995152c124d3ca4f660
dreamcareer7 <dreamcareer070@gmail.com>
2018-10-15 13:12:15 -0700

    Give it a default so it can be applied to db's with existing templates

--- migrations/1539570118008_new-templates.js
+++ migrations/1539570118008_new-templates.js
@@ -8,7 +8,8 @@ const up = [
   'ALTER TABLE brands_forms_templates DROP CONSTRAINT brands_forms_templates_brand_form',
   'ALTER TABLE brands_forms_templates ADD deal_types deal_type[]',
   'ALTER TABLE brands_forms_templates ADD property_types deal_property_type[]',
-  'ALTER TABLE brands_forms_templates ADD name TEXT NOT NULL',
+  `ALTER TABLE brands_forms_templates ADD name
+    TEXT NOT NULL DEFAULT 'Untitled Templated'`,
   'COMMIT'
 ]
 
