abd63eb5ac28e95b1e7939312c1e865a4f90692f
dreamcareer7 <dreamcareer070@gmail.com>
2018-11-06 13:50:54 -0800

    It really passes the tests now.

--- tests/suites/data/template.js
+++ tests/suites/data/template.js
@@ -1,12 +1,8 @@
-const template = 'TEMPLATE BODY'
-
 module.exports = {
   name: 'Test Template',
   width: 600,
   height: 300,
-  thumbnail: 'thumbnail-url',
   brand: null,
   medium: 'Email',
   template_type: 'Listing',
-  template
 }
