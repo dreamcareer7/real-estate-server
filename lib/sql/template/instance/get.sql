64a75aba6b88be7fd8c8e69f867d56c939d5cca2
dreamcareer7 <dreamcareer070@gmail.com>
2018-11-20 09:30:24 -0800

    Dan needs it

--- lib/sql/template/instance/get.sql
+++ lib/sql/template/instance/get.sql
@@ -1,6 +1,7 @@
 SELECT templates_instances.*,
   'template_instance' AS type,
-  EXTRACT(EPOCH FROM created_at) AS created_at
+  EXTRACT(EPOCH FROM created_at) AS created_at,
+  EXTRACT(EPOCH FROM created_at) AS updated_at -- They are not updated at all for now but Dan needs this field.
 FROM templates_instances
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(iid, ord) ON templates_instances.id = iid
 ORDER BY t.ord
