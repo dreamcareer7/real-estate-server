745d62b77e6923938373e5e9640d78031c6e2288
dreamcareer7 <dreamcareer070@gmail.com>
2018-11-19 19:46:06 -0800

    [Marketing] Test coverage

--- tests/suites/template.js
+++ tests/suites/template.js
@@ -1,6 +1,7 @@
 const template = require('./data/template.js')
 
 registerSuite('brand', ['createParent'])
+registerSuite('deal', ['create'])
 
 const create = cb => {
   template.brand = results.brand.createParent.data.id
@@ -26,7 +27,64 @@ const getForUser = cb => {
     })
 }
 
+const instantiate = cb => {
+  const id = results.template.create.data.id
+  const html = '<html />'
+
+  const data = {
+    html,
+    deals: [
+      results.deal.create.data.id
+    ],
+    contacts: []
+  }
+
+  return frisby.create('create an instance of a template')
+    .post(`/templates/${id}/instances`, data)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: {
+        html
+      }
+    })
+}
+
+const share = cb => {
+  const text = 'Please share this'
+
+  const data = {
+    text,
+    recipients: [
+      '+14243828604'
+    ]
+  }
+
+  const id = results.template.instantiate.data.id
+
+  return frisby.create('share an instance')
+    .post(`/templates/instances/${id}/share`, data)
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON(results.template.instantiate)
+}
+
+const getMine = cb => {
+  return frisby.create('get my marketing instances')
+    .get('/templates/instances')
+    .after(cb)
+    .expectStatus(200)
+    .expectJSON({
+      code: 'OK',
+      data: [ results.template.instantiate.data ]
+    })
+}
+
 module.exports = {
   create,
-  getForUser
+  getForUser,
+  instantiate,
+  share,
+  getMine
 }
