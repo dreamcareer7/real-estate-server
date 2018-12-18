70eb6cbcbb73f298fd2a8110a32e4253f3ecad64
dreamcareer7 <dreamcareer070@gmail.com>
2018-12-18 17:00:14 +0330

    [User] Use users.brand as default value for active brand

--- lib/sql/user/get.sql
+++ lib/sql/user/get.sql
@@ -33,7 +33,7 @@ SELECT 'user' AS type,
        (
         SELECT count(*) > 0 FROM docusign_users WHERE "user" = users.id
        ) as has_docusign,
-       COALESCE(users.brand, (SELECT brand FROM users_settings WHERE "user" = users.id ORDER BY updated_at DESC LIMIT 1)) AS active_brand
+       COALESCE((SELECT brand FROM users_settings WHERE "user" = users.id ORDER BY updated_at DESC LIMIT 1), users.brand) AS active_brand
 FROM users
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(uid, ord) ON users.id = uid
 ORDER BY t.ord
