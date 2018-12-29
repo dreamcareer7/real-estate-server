f3c62bec0a37d15c20bd52f320bfba8bbb597b87
dreamcareer7 <dreamcareer070@gmail.com>
2018-12-29 22:11:35 +0330

    [Contact/List] Add user filter support

--- lib/sql/contact/list/list_for_brands.sql
+++ lib/sql/contact/list/list_for_brands.sql
@@ -5,6 +5,12 @@ FROM
 WHERE
   deleted_at IS NULL
   AND brand = ANY($1::uuid[])
+  AND (CASE
+    WHEN $2::uuid[] IS NOT NULL THEN
+      (created_by = ANY($2::uuid[])) OR (created_by IS NULL)
+    ELSE
+      TRUE
+  END)
 ORDER BY
   is_pinned DESC,
   created_at
