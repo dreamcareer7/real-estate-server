cc2e07c8fe2c9b30c6132a0d85595891b071c932
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-03 01:52:19 +0330

    [Contact/Tag] Fix timeout on getting all tags

--- lib/sql/contact/tag/get.sql
+++ lib/sql/contact/tag/get.sql
@@ -1,22 +1,29 @@
-SELECT DISTINCT ON (cattrs.text)
+WITH uc AS (
+  SELECT
+    id
+  FROM
+    contacts
+  WHERE
+    check_contact_read_access(contacts, $1)
+    AND (CASE
+      WHEN $2::uuid[] IS NOT NULL THEN
+        contacts."user" = ANY($2::uuid[])
+      ELSE
+        TRUE
+    END)
+    AND deleted_at IS NULL
+)
+SELECT DISTINCT ON (text)
   cattrs.id,
   'contact_attribute' as "type",
   'tag' as "attribute_type",
-  cattrs.text,
-  EXTRACT(EPOCH FROM cattrs.created_at) AS created_at,
-  EXTRACT(EPOCH FROM cattrs.updated_at) AS updated_at
+  text,
+  EXTRACT(EPOCH FROM created_at) AS created_at,
+  EXTRACT(EPOCH FROM updated_at) AS updated_at
 FROM
-  contacts
-  JOIN contacts_attributes AS cattrs
-    ON contacts.id = cattrs.contact
+  contacts_attributes AS cattrs
+  JOIN uc ON cattrs.contact = uc.id
 WHERE
-  check_contact_read_access(contacts, $1)
-  AND (CASE
-    WHEN $2::uuid[] IS NOT NULL THEN
-      contacts."user" = ANY($2::uuid[])
-    ELSE
-      TRUE
-  END)
-  AND cattrs.attribute_type = 'tag'
+  attribute_type = 'tag'
 ORDER BY
-  cattrs.text
+  text;
