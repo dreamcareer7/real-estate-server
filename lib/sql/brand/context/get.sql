d452c3970fc123225a903041c07a9bd08a2c7321
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-03 04:05:04 -0800

    Revert "Export the default value with the same data type as it should have"
    This reverts commit 0176a9aa335c34cf877ecbfa3600efccf9c722c3.

--- lib/sql/brand/context/get.sql
+++ lib/sql/brand/context/get.sql
@@ -1,16 +1,7 @@
 SELECT brands_contexts.*,
   'brand_context' AS type,
   required::text[] as required,
-  optional::text[] as optional,
-
-  (
-    CASE
-        WHEN data_type = 'Text'   THEN default_value::text
-        WHEN data_type = 'Number' THEN default_value::float
-        WHEN data_type = 'Date'   THEN default_value::timestamp with time zone
-    END
-  ) as default_value
-
+  optional::text[] as optional
 FROM brands_contexts
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(bid, ord) ON brands_contexts.id = bid
 ORDER BY t.ord
