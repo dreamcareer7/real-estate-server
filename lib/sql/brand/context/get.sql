90780851952d271893c4dd8fb2dde2777dcea17c
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-02 16:24:52 -0800

    Export the default value with the same data type as it should have

--- lib/sql/brand/context/get.sql
+++ lib/sql/brand/context/get.sql
@@ -1,7 +1,16 @@
 SELECT brands_contexts.*,
   'brand_context' AS type,
   required::text[] as required,
-  optional::text[] as optional
+  optional::text[] as optional,
+
+  (
+    CASE
+        WHEN data_type = 'Text'   THEN default_value::text
+        WHEN data_type = 'Number' THEN default_value::float
+        WHEN data_type = 'Date'   THEN default_value::timestamp with time zone
+    END
+  ) as default_value
+
 FROM brands_contexts
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(bid, ord) ON brands_contexts.id = bid
 ORDER BY t.ord
