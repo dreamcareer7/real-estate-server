42704238c6f4db3f17d65e36f1744665bec196fa
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-17 23:38:46 -0800

    ESLINT

--- tests/unit/helper.js
+++ tests/unit/helper.js
@@ -2,7 +2,7 @@ const db = require('../../lib/utils/db')
 const promisify = require('../../lib/utils/promisify')
 require('../../lib/models/index')()
 
-const _createContext =  async () => {
+const _createContext = async () => {
   const conn = await promisify(db.conn)()
 
   const context = Context.create({
@@ -21,9 +21,9 @@ const _createContext =  async () => {
 
 const createContext = done => {
   _createContext()
-  .then(context => {
-    context.run(done)
-  })
+    .then(context => {
+      context.run(done)
+    })
 }
 
 module.exports = { createContext }
