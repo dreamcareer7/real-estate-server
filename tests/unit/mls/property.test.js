51429a81b78ecf1b27b0a829e06215b4e7db5117
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-31 22:15:01 -0800

    Loving LINT

--- tests/unit/mls/property.test.js
+++ tests/unit/mls/property.test.js
@@ -8,23 +8,18 @@ const json = require('./json/property')
 const save = async () => {
   const address = await AddressHelper.create()
 
-  const saved = await Property.create(json)
+  const saved = await Property.create({
+    ...json,
+    address_id: address.id
+  })
+
   expect(saved).to.be.a('string')
 
   return saved
 }
 
-const get = async () => {
-  const saved = await save()
-  const fetched = await Address.get(saved)
-
-  expect(fetched.id).to.equal(saved)
-}
-
-
-describe('MLS Address', () => {
+describe('MLS Property', () => {
   createContext()
 
-  it('should save an address', save)
-//   it('should get an address', get)
+  it('should save a property', save)
 })
