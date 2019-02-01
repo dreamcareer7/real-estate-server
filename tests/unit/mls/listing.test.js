e2b2c3efa08a499b339310ea5e9f6ecf5bd450f4
dreamcareer7 <dreamcareer070@gmail.com>
2019-01-31 22:45:20 -0800

    Tests for Listing model. Obviously not complete it's a huge model. Just the basics.

---
+++ tests/unit/mls/listing.test.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai')
+
+const promisify = require('../../../lib/utils/promisify')
+
+const { createContext } = require('../helper')
+
+const json = require('./json/listing')
+const property = require('./json/property')
+const address = require('./json/address')
+
+const save = async () => {
+  const saved = await promisify(Listing.create)({
+    listing: json,
+    property,
+    address,
+    revision: 1
+  })
+
+  return saved.listing.id
+}
+
+const get = async () => {
+  const id = await save()
+  const listing = await promisify(Listing.get)(id)
+
+  expect(listing.id).to.equal(id)
+}
+
+const getAll = async () => {
+  const id = await save()
+  const listings = await promisify(Listing.getAll)([id])
+
+  const ids = listings.map(l => l.id)
+
+  expect(ids).to.include(id)
+}
+
+const getCompacts = async () => {
+  const id = await save()
+  const listings = await promisify(Listing.getCompacts)([id])
+
+  const ids = listings.map(l => l.id)
+
+  expect(ids).to.include(id)
+}
+
+const getByMLSNumber = async () => {
+  const id = await save()
+  const listing = await promisify(Listing.getByMLSNumber)(json.mls_number)
+
+  expect(listing.id).to.equal(id)
+}
+
+const getByMUI = async () => {
+  const id = await save()
+  const listing = await promisify(Listing.getByMUI)(json.matrix_unique_id)
+
+  expect(listing.id).to.equal(id)
+}
+
+describe('MLS Listing', () => {
+  createContext()
+
+  it('should save a listing', save)
+  it('should get a listing', get)
+  it('should get a bunch of listings', getAll)
+  it('should get a bunch of compact listings', getCompacts)
+  it('should get a listing by mls number', getByMLSNumber)
+  it('should get a listing by matrix unique id', getByMUI)
+})
