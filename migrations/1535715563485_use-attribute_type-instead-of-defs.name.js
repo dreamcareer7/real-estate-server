ee35c8c6009db329f8ecd39336d7e08f43d0f5c4
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-10 23:51:24 -0800

    It'll be applied again later with newer migrations. Right now it causes a conflict

--- migrations/1535715563485_use-attribute_type-instead-of-defs.name.js
+++ migrations/1535715563485_use-attribute_type-instead-of-defs.name.js
@@ -5,7 +5,6 @@ const db = require('../lib/utils/db')
 const fs = require('fs')
 
 const get_contact_summaries = fs.readFileSync(__dirname + '/../lib/sql/contact/functions/get_contact_summaries.fn.sql', 'utf-8')
-const get_deals_with_contacts = fs.readFileSync(__dirname + '/../lib/sql/contact/functions/get_deals_with_contacts.fn.sql', 'utf-8')
 const get_users_for_contacts = fs.readFileSync(__dirname + '/../lib/sql/contact/functions/get_users_for_contacts.fn.sql', 'utf-8')
 const user_has_contact_with_another = fs.readFileSync(__dirname + '/../lib/sql/contact/functions/user_has_contact_with_another.fn.sql', 'utf-8')
 
@@ -17,7 +16,6 @@ const up = [
   'DROP FUNCTION IF EXISTS user_has_contact_with_another(uuid, uuid)',
   get_contact_summaries,
   get_users_for_contacts,
-  get_deals_with_contacts,
   user_has_contact_with_another,
   'COMMIT'
 ]
