196f26e85c028e1ca3d391d065598b41ab540344
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-14 00:25:12 +0330

    [Contact/Tag] Fixed migration. Had forgotten to add the triggers.

--- migrations/1550068168659_crm_tags-table.js
+++ migrations/1550068168659_crm_tags-table.js
@@ -43,6 +43,79 @@ const migrations = [
     c.brand,
     ca.text,
     ca.created_at`,
+  `CREATE OR REPLACE FUNCTION add_new_tags_from_attributes() RETURNS TRIGGER
+  LANGUAGE plpgsql
+  AS $$
+    BEGIN
+      WITH new_tag_attrs AS (
+        SELECT DISTINCT ON (ca.text)
+          c.brand,
+          ca.created_at,
+          ca.created_by,
+          ca.text AS tag
+        FROM
+          new_attrs AS ca
+          JOIN contacts c
+            ON ca.contact = c.id
+        WHERE
+          ca.deleted_at IS NULL
+          AND c.deleted_at IS NULL
+          AND ca.attribute_type = 'tag'
+        ORDER BY
+          ca.text,
+          ca.created_at
+      ), current_tags AS (
+        SELECT
+          tag
+        FROM
+          crm_tags
+        WHERE
+          brand = (SELECT brand FROM new_tag_attrs LIMIT 1)
+      ), new_tags AS (
+        SELECT
+          tag
+        FROM
+          new_tag_attrs
+  
+        EXCEPT
+  
+        SELECT
+          tag
+        FROM
+          current_tags
+      )
+      INSERT INTO
+        crm_tags (
+          brand,
+          created_at,
+          created_by,
+          tag
+        )
+      SELECT
+        brand,
+        created_at,
+        created_by,
+        tag
+      FROM
+        new_tag_attrs
+      WHERE
+        tag IN (SELECT tag FROM new_tags);
+  
+      RETURN NULL;
+    END;
+  $$`,
+  `CREATE TRIGGER update_tags_after_attr_insert
+    AFTER INSERT ON contacts_attributes
+    REFERENCING NEW TABLE AS new_attrs
+    FOR EACH STATEMENT
+    EXECUTE PROCEDURE add_new_tags_from_attributes()
+  `,
+  `CREATE TRIGGER update_tags_after_attr_update
+    AFTER UPDATE ON contacts_attributes
+    REFERENCING NEW TABLE AS new_attrs
+    FOR EACH STATEMENT
+    EXECUTE PROCEDURE add_new_tags_from_attributes()
+  `,
   'COMMIT'
 ]
 
