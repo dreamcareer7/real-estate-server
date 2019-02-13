7d667d84bbc9b332ef0aa55531a3c11cade61944
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-14 00:11:19 +0330

    [Contact/Tag] Fix tag migration

--- migrations/1550068168659_crm_tags-table.js
+++ migrations/1550068168659_crm_tags-table.js
@@ -1,4 +1,4 @@
-const db = require('./lib/utils/db')
+const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
@@ -47,13 +47,13 @@ const migrations = [
 ]
 
 const run = async () => {
-  const conn = await db.conn.promise()
+  const { conn, done } = await db.conn.promise()
 
-  for(const sql in migrations) {
+  for(const sql of migrations) {
     await conn.query(sql)
   }
 
-  conn.done()
+  done()
 }
 
 exports.up = cb => {
