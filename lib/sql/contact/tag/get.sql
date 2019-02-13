52d55129f46e63370dfe30dd6d3a0496f5e71bee
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-14 01:02:42 +0330

    [Contact/Tag] Add type field to tags, and fix timestamps

--- lib/sql/contact/tag/get.sql
+++ lib/sql/contact/tag/get.sql
@@ -1,42 +1,13 @@
--- WITH uc AS (
---   SELECT
---     id
---   FROM
---     contacts
---   WHERE
---     check_contact_read_access(contacts, $1)
---     AND (CASE
---       WHEN $2::uuid[] IS NOT NULL THEN
---         contacts."user" = ANY($2::uuid[])
---       ELSE
---         TRUE
---     END)
---     AND deleted_at IS NULL
--- )
--- SELECT DISTINCT ON (text)
---   cattrs.id,
---   'contact_attribute' as "type",
---   'tag' as "attribute_type",
---   text,
---   EXTRACT(EPOCH FROM created_at) AS created_at,
---   EXTRACT(EPOCH FROM updated_at) AS updated_at
--- FROM
---   contacts_attributes AS cattrs
---   JOIN uc ON cattrs.contact = uc.id
--- WHERE
---   attribute_type = 'tag'
--- ORDER BY
---   text;
-
 SELECT
   id,
   brand,
-  created_at,
-  updated_at,
+  EXTRACT(EPOCH FROM created_at) AS created_at,
+  EXTRACT(EPOCH FROM updated_at) AS updated_at
   created_by,
   updated_by,
   tag,
-  tag AS text
+  tag AS text,
+  'crm_tag' AS type
 FROM
   crm_tags
 WHERE
