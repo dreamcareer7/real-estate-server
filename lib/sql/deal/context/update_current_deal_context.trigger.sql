6f07aaecf65d81f00381e041f2ecda9086f8bdf9
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-23 18:54:27 -0800

    Fixes web#2325 Wrote tests for it on prev commit

--- lib/sql/deal/context/update_current_deal_context.trigger.sql
+++ lib/sql/deal/context/update_current_deal_context.trigger.sql
@@ -1,6 +1,7 @@
 DROP TRIGGER IF EXISTS update_current_deal_context ON deal_context;
 DROP TRIGGER IF EXISTS update_current_deal_context ON deals_checklists;
 DROP TRIGGER IF EXISTS update_current_deal_context ON addresses;
+DROP TRIGGER IF EXISTS update_current_deal_context ON addresses;
 
 CREATE OR REPLACE FUNCTION update_current_deal_context_from_mls_trigger()
 RETURNS trigger AS
@@ -25,6 +26,17 @@ $$
 $$
 LANGUAGE PLPGSQL;
 
+CREATE OR REPLACE FUNCTION deals_updated_current_deal_context_trigger()
+RETURNS trigger AS
+$$
+  BEGIN
+    PERFORM update_current_deal_context(NEW.id);
+
+    RETURN NEW;
+  END;
+$$
+LANGUAGE PLPGSQL;
+
 CREATE TRIGGER update_current_deal_context AFTER INSERT OR UPDATE ON deal_context
   FOR EACH ROW EXECUTE PROCEDURE update_current_deal_context_trigger();
 
@@ -33,3 +45,6 @@ CREATE TRIGGER update_current_deal_context AFTER INSERT OR UPDATE ON deals_check
 
 CREATE TRIGGER update_current_deal_context AFTER INSERT OR UPDATE on addresses
   FOR EACH ROW EXECUTE PROCEDURE update_current_deal_context_from_mls_trigger();
+
+CREATE TRIGGER update_current_deal_context AFTER INSERT OR UPDATE on deals
+  FOR EACH ROW EXECUTE PROCEDURE deals_updated_current_deal_context_trigger();
---
+++ migrations/1550882760981_trigger-on-deals.js
@@ -0,0 +1,37 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  `CREATE OR REPLACE FUNCTION deals_updated_current_deal_context_trigger()
+    RETURNS trigger AS
+    $$
+      BEGIN
+        PERFORM update_current_deal_context(NEW.id);
+
+        RETURN NEW;
+      END;
+    $$
+    LANGUAGE PLPGSQL`,
+
+  `CREATE TRIGGER update_current_deal_context AFTER INSERT OR UPDATE on deals
+    FOR EACH ROW EXECUTE PROCEDURE deals_updated_current_deal_context_trigger()`,
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const conn = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
