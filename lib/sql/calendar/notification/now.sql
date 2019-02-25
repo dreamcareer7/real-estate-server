f45ba673e7eb8ab38672209b2a1661d017a180a8
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-26 01:27:30 +0330

    [Calendar] Fix performance issue with notification/now query
    It reduces the execution time by a full 50%, but it's still slow and
    only materialization can fix that slowness.

--- lib/sql/calendar/notification/now.sql
+++ lib/sql/calendar/notification/now.sql
@@ -3,7 +3,7 @@ SELECT
   c."user",
   c.title,
   c.type_label,
-  c.timestamp,
+  c.date AS "timestamp",
   extract(epoch from cns.reminder) as reminder,
   cns.object_type,
   cns.event_type,
@@ -17,6 +17,13 @@ FROM
     FROM
       analytics.calendar,
       unnest(analytics.calendar.users) cu("user")
+    WHERE
+      CASE
+        WHEN recurring IS TRUE THEN
+          range_contains_birthday(now(), now() + interval '1 month', "timestamp")
+        ELSE
+          "timestamp" BETWEEN NOW() AND NOW() + interval '1 month'
+      END
   ) AS c
   JOIN calendar_notification_settings AS cns
     ON c."user" = cns."user"
@@ -25,9 +32,9 @@ FROM
 WHERE
   CASE
     WHEN c.recurring IS TRUE THEN
-      range_contains_birthday(now() - interval '44 hours', now(), c.timestamp - cns.reminder)
+      range_contains_birthday(now() - interval '44 hours', now(), c.date - cns.reminder)
     ELSE
-      (c.timestamp - cns.reminder) between now() - interval '44 hours' and now()
+      (c.date - cns.reminder) between now() - interval '44 hours' and now()
   END
   AND cns.deleted_at IS NULL
   AND c.object_type::calendar_object_type = cns.object_type
@@ -40,7 +47,7 @@ WHERE
       calendar_notification_logs cnl
     WHERE
       cnl.id = c.id
-      AND cnl.timestamp = c.timestamp
+      AND cnl.timestamp = c.date
       AND cnl."user" = c."user"
   )
   AND CASE WHEN $1::int IS NULL THEN TRUE ELSE extract('hour' from now() at time zone u.timezone) = $1 END;
