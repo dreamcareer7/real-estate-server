4f0bda6e3f86ec110ad6b14aa9808e2821a617ba
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-26 09:39:18 +0330

    [Scripts/Notification] Fix async issue.

--- scripts/mls/calendar/notification.js
+++ scripts/mls/calendar/notification.js
@@ -3,14 +3,16 @@
 const MLSJob = require('../../../lib/models/MLSJob')
 const CalendarWorker = require('../../../lib/models/Calendar/worker')
 const runInContext = require('../../../lib/models/Context/util')
+const promisify = require('../../../lib/utils/promisify')
 
 runInContext(`calendar-notification-${new Date().toLocaleTimeString('en-us')}`, async () => {
   await CalendarWorker.sendNotifications()
 
-  MLSJob.insert(
+  await promisify(MLSJob.insert)(
     {
       name: 'calendar_notification'
-    },
-    process.exit
+    }
   )
+
+  process.exit()
 })
