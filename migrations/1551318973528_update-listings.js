6844b6bff1b5fcb594ee34f25cfd394dc1f5cb44
dreamcareer7 <dreamcareer070@gmail.com>
2019-02-27 23:42:21 -0800

    This will be faster. Hopefully.

--- migrations/1551318973528_update-listings.js
+++ migrations/1551318973528_update-listings.js
@@ -2,7 +2,87 @@ const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
-  'UPDATE  listings SET updated_at = updated_at', // Trigger update for listings_filters
+  'TRUNCATE TABLE listings_filters',
+  `INSERT INTO listings_filters
+    SELECT
+      listings.id AS id,
+      listings.status AS status,
+      listings.price AS price,
+      listings.matrix_unique_id AS matrix_unique_id,
+      listings.close_date AS close_date,
+      listings.list_office_mls_id,
+      listings.co_list_office_mls_id,
+      listings.list_agent_mls_id,
+      listings.co_list_agent_mls_id,
+      listings.selling_office_mls_id,
+      listings.co_selling_office_mls_id,
+      listings.selling_agent_mls_id,
+      listings.co_selling_agent_mls_id,
+      listings.close_price AS close_price,
+      listings.created_at AS created_at,
+      listings.mls_number,
+      listings.application_fee_yn,
+      -- Areas are stored as something like this: MCKINNEY AREA (53)
+      -- When filteting, we only want the number (53). So we extract it.
+      (regexp_matches(listings.mls_area_major, E'[0-9]+'))[1]::int as mls_area_major,
+      (regexp_matches(listings.mls_area_minor, E'[0-9]+'))[1]::int as mls_area_minor,
+      properties.square_meters,
+      properties.bedroom_count,
+      properties.half_bathroom_count,
+      properties.full_bathroom_count,
+      properties.property_type,
+      properties.property_subtype,
+      properties.year_built,
+      properties.pool_yn,
+      properties.pets_yn,
+      properties.lot_square_meters,
+      properties.parking_spaces_covered_total,
+      properties.architectural_style,
+      properties.subdivision_name,
+      properties.school_district,
+      properties.elementary_school_name,
+      properties.intermediate_school_name,
+      properties.junior_high_school_name,
+      properties.middle_school_name,
+      properties.primary_school_name,
+      properties.high_school_name,
+      properties.senior_high_school_name,
+      properties.appliances_yn,
+      properties.furnished_yn,
+      properties.fenced_yard_yn,
+      properties.number_of_pets_allowed,
+      addresses.location,
+      addresses.county_or_parish,
+      addresses.postal_code,
+      ARRAY_TO_STRING(
+        ARRAY[
+          addresses.title,
+          addresses.subtitle,
+          addresses.street_number,
+          addresses.street_dir_prefix,
+          addresses.street_name,
+          addresses.street_suffix,
+          addresses.street_dir_suffix,
+          addresses.city,
+          addresses.state,
+          addresses.state_code,
+          addresses.postal_code,
+          addresses.country::text,
+          addresses.country_code::text,
+          (
+            CASE WHEN addresses.unit_number = '' THEN NULL
+            ELSE
+            'Unit ' || addresses.unit_number
+            END
+          ),
+          listings.mls_number
+        ], ' ', NULL
+      ) as address
+    FROM listings
+    JOIN
+      properties  ON listings.matrix_unique_id = properties.matrix_unique_id
+    JOIN
+      addresses   ON listings.matrix_unique_id = addresses.matrix_unique_id`,
   'COMMIT'
 ]
 
