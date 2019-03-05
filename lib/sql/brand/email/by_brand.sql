0c03bc4b6a553a40f848bf76c3a51e3ada43666e
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-05 11:59:17 -0800

    Thanks to Abbas for noticing

--- lib/sql/brand/email/by_brand.sql
+++ lib/sql/brand/email/by_brand.sql
@@ -1,4 +1,7 @@
 SELECT id FROM brands_emails
-WHERE brand IS NULL
-OR brand IN(SELECT brand_parents($1))
+WHERE
+(
+  brand IS NULL
+  OR brand IN(SELECT brand_parents($1))
+)
 AND deleted_at IS NULL
