6ef7a0c517413e4f4bc555ac7d1a8831e01e3250
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-05 21:44:25 -0800

    Initial implementation of email campaigns

---
+++ lib/sql/email/campaign/get.sql
@@ -0,0 +1,8 @@
+SELECT email_campaigns.*,
+  'email_campaign' AS TYPE,
+  EXTRACT(EPOCH FROM created_at) AS created_at,
+  EXTRACT(EPOCH FROM updated_at) AS updated_at,
+  EXTRACT(EPOCH FROM deleted_at) AS deleted_at
+FROM email_campaigns
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(eid, ord) ON email_campaigns.id = eid
+ORDER BY t.ord
