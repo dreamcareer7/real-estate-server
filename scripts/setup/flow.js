c4c04ae597470f46a2e25b562ae3c71b7d1cb627
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-08 00:11:06 -0800

    ESLINT

--- scripts/setup/flow.js
+++ scripts/setup/flow.js
@@ -1,28 +1,28 @@
-const BrandFlow = require("./lib/models/Brand/flow");
+const BrandFlow = require('./lib/models/Brand/flow')
 
-await BrandFlow.create("8cb4a358-8973-11e7-9089-0242ac110003", "a700ba96-c003-11e7-83d6-0242ac11000d", {
-  name: "Rechat Team Onboarding",
-  description: "The process of on-boarding a new team member",
+BrandFlow.create('8cb4a358-8973-11e7-9089-0242ac110003', 'a700ba96-c003-11e7-83d6-0242ac11000d', {
+  name: 'Rechat Team Onboarding',
+  description: 'The process of on-boarding a new team member',
   steps: [
     {
-      title: "Create Rechat email",
+      title: 'Create Rechat email',
       description:
-        "Create a Rechat email address for the new guy to use in other services",
+        'Create a Rechat email address for the new guy to use in other services',
       due_in: 0,
       event: {
-        title: "Create Rechat email",
-        task_type: "Other"
+        title: 'Create Rechat email',
+        task_type: 'Other'
       }
     },
     {
-      title: "Demo of Rechat",
+      title: 'Demo of Rechat',
       description:
-        "Dan gives a quick demo of the Rechat system and explains how it works",
+        'Dan gives a quick demo of the Rechat system and explains how it works',
       due_in: 3,
       event: {
-        title: "Demo of Rechat",
-        task_type: "Call"
+        title: 'Demo of Rechat',
+        task_type: 'Call'
       }
     }
   ]
-});
+}).then(process.exit)
