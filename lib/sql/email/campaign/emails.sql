1aeb0ec9d2cf8319a098ffdd275c8de5c70752b6
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-17 11:50:40 -0700

    Fixes server#1259

--- lib/sql/email/campaign/emails.sql
+++ lib/sql/email/campaign/emails.sql
@@ -1,5 +1,5 @@
 WITH list_contacts AS (
-  SELECT contacts_summaries.email[1], contacts_summaries.id
+  SELECT contacts_summaries.email[1], contacts_summaries.id, email_campaigns_recipients.recipient_type
   FROM   email_campaigns
   JOIN   email_campaigns_recipients ON email_campaigns.id              = email_campaigns_recipients.campaign
   JOIN   crm_lists_members          ON email_campaigns_recipients.list = crm_lists_members.list
@@ -8,14 +8,14 @@ WITH list_contacts AS (
 ),
 
 tag_contacts AS (
-  SELECT contacts_summaries.email[1], contacts_summaries.id
+  SELECT contacts_summaries.email[1], contacts_summaries.id, email_campaigns_recipients.recipient_type
   FROM   email_campaigns
   JOIN   email_campaigns_recipients ON email_campaigns.id                    =  email_campaigns_recipients.campaign
   JOIN   contacts_summaries         ON ARRAY[email_campaigns_recipients.tag] <@ contacts_summaries.tag
   WHERE email_campaigns.id = $1
 )
 
-SELECT email, contact FROM email_campaigns_recipients
+SELECT email, contact, recipient_type FROM email_campaigns_recipients
 WHERE email_campaigns_recipients.campaign = $1
 UNION
 SELECT * FROM list_contacts
--- lib/sql/email/campaign/insert-recipients.sql
+++ lib/sql/email/campaign/insert-recipients.sql
@@ -1,10 +1,11 @@
 INSERT INTO email_campaigns_recipients
-(campaign, tag, list, contact, email)
+(campaign, tag, list, contact, email, recipient_type)
 
 SELECT
   campaign,
   tag,
   list,
   contact,
-  email
+  email,
+  recipient_type
 FROM json_populate_recordset(NULL::email_campaigns_recipients, $1::json)
