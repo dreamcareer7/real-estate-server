9621ec288bf093e80372ac6b85f1acb5a4945fc4
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-21 00:37:40 +0330

    [Contact] Improve full-text search

--- lib/sql/contact/update_searchable_field.sql
+++ lib/sql/contact/update_searchable_field.sql
@@ -1,8 +1,8 @@
 UPDATE
   contacts
 SET
-  searchable_field = csf.searchable_field
+  search_field = csf.search_field
 FROM
-  get_searchable_field_for_contacts($1::uuid[]) csf
+  get_search_field_for_contacts($1::uuid[]) csf
 WHERE
   id = csf.contact
