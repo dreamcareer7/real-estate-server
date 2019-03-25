60b29a9604cdb6ad86acf649e0f29f714d7696b8
dreamcareer7 <dreamcareer070@gmail.com>
2019-03-25 15:56:56 +0000

    [Contact] Update default labels for Phone and Address

---
+++ migrations/1553509510754_update-contacts-label.js
@@ -0,0 +1,31 @@
+const db = require('../lib/utils/db')
+
+
+// issue: https://gitlab.com/rechat/server/issues/1249
+const migrations = [
+  'BEGIN',
+
+  `UPDATE contacts_attribute_defs
+    SET labels = '{Home,Mobile,Work,Fax,WhatsApp,Other}'::text[] WHERE name = 'phone_number'`,
+
+  `UPDATE contacts_attribute_defs
+    SET labels = '{Home,Work,"Investment Property",Other}'::text[] WHERE section = 'Addresses'`,
+
+  'COMMIT'
+]
+
+const run = async () => {
+  const conn = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
