1a447d5ee96e0370a51ec5173fdb65ae8882529a
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-05 15:58:28 -0700

    Optional auto notify for web#2518

--- lib/sql/envelope/insert.sql
+++ lib/sql/envelope/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO envelopes (created_by, deal, status) VALUES ($1, $2, 'Sent')
+INSERT INTO envelopes (created_by, deal, auto_notify, status) VALUES ($1, $2, $3, 'Sent')
 RETURNING id, webhook_token
