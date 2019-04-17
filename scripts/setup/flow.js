26b6a27b6c2bf1132eebf7c476244578e663d6ea
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-17 17:59:46 +0430

    [Flow] Updated the sample data script

--- scripts/setup/flow.js
+++ scripts/setup/flow.js
@@ -1,12 +1,27 @@
 /* eslint-disable no-unused-vars */
 const BrandFlow = require('../../lib/models/Brand/flow')
+const BrandEmail = require('../../lib/models/Brand/email')
+
 const HOUR = 3600
 const DAY = 24 * HOUR
 
+const USER = 'a700ba96-c003-11e7-83d6-0242ac11000d'
+const BRAND = '8cb4a358-8973-11e7-9089-0242ac110003'
+
 async function setupFlows() {
+  const email = await BrandEmail.create({
+    created_by: USER,
+    brand: BRAND,
+    body: 'Hey, {first_name}! Welcome to Rechat!',
+    goal: 'Welcoming the new team member',
+    include_signature: false,
+    subject: 'Welcome!',
+    name: 'Rechat Welcome'
+  })
+
   await BrandFlow.create(
-    '8cb4a358-8973-11e7-9089-0242ac110003',
-    'a700ba96-c003-11e7-83d6-0242ac11000d',
+    BRAND,
+    USER,
     {
       name: 'Rechat Team Onboarding',
       description: 'The process of on-boarding a new team member',
@@ -21,6 +36,13 @@ async function setupFlows() {
             task_type: 'Other'
           },
           is_automated: false
+        },
+        {
+          title: 'Send a welcome email',
+          description: 'Let\'s be friendly!',
+          due_in: DAY,
+          email: email.id,
+          is_automated: true
         },
         {
           title: 'Demo of Rechat',
