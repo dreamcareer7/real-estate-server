6296a763bdfd6ed180232c070b9f41740a761f9b
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-18 11:32:34 +0430

    [Flow] Add missing type field to get queries

--- lib/sql/flow/step/get.sql
+++ lib/sql/flow/step/get.sql
@@ -9,7 +9,9 @@ SELECT
   flow,
   origin,
   (SELECT flows_emails.email FROM flows_emails WHERE flows_emails.id = flows_steps.email LIMIT 1) AS email,
-  (SELECT flows_events.crm_task FROM flows_events WHERE flows_events.id = flows_steps.event LIMIT 1) AS crm_task
+  (SELECT flows_events.crm_task FROM flows_events WHERE flows_events.id = flows_steps.event LIMIT 1) AS crm_task,
+
+  'flow_step' AS type
 FROM
   flows_steps
   JOIN unnest($1::uuid[]) WITH ORDINALITY t(cid, ord) ON flows_steps.id = t.cid
