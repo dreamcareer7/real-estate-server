88ff57dd9f040acdd0165fa6356be199edcca8b6
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-18 21:55:28 +0430

    [Support] Update encrypt-request-id script to support multiple request ids

--- scripts/support/encrypt-request-id.js
+++ scripts/support/encrypt-request-id.js
@@ -1,17 +1,16 @@
 const config = require('../../lib/config')
 require('../../lib/models/Crypto')
 
-const req_id = process.argv[2]
-// console.log(encodeURIComponent(Crypto.encrypt(req_id))
-
-let enc_rechat_id
-
-try {
-  enc_rechat_id = encodeURIComponent(Crypto.encrypt(req_id))
+for (const req_id of (process.argv[2] || '').split('\n').map(arg => arg.trim()).filter(x => x && x.length > 0)) {
+  let enc_rechat_id
+  
+  try {
+    enc_rechat_id = encodeURIComponent(Crypto.encrypt(req_id))
+  }
+  catch (ex) {
+    console.error(ex)
+  }
+  
+  const baseUrl = config.url.protocol + '://' + config.url.hostname + '/_'
+  console.log(`${baseUrl}/requests/${enc_rechat_id}`)
 }
-catch (ex) {
-  console.error(ex)
-}
-
-const baseUrl = config.url.protocol + '://' + config.url.hostname + '/_'
-console.log(`${baseUrl}/requests/${enc_rechat_id}`)
