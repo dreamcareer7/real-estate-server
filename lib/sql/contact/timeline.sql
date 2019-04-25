337d82636bcbc2543e43cf4de8e78149d3b21eac
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-25 16:24:52 +0430

    [Contact] Add scheduled emails to timeline

--- lib/sql/contact/timeline.sql
+++ lib/sql/contact/timeline.sql
@@ -1,4 +1,34 @@
-WITH timeline_content AS (
+WITH list_contacts AS (
+  SELECT email_campaigns_recipients.campaign
+  FROM   email_campaigns_recipients
+  JOIN   crm_lists_members
+    ON   email_campaigns_recipients.list = crm_lists_members.list
+  WHERE  crm_lists_members.contact = $1::uuid
+),
+tag_contacts AS (
+  SELECT email_campaigns_recipients.campaign
+  FROM   email_campaigns_recipients
+  JOIN   contacts_summaries
+    ON   ARRAY[email_campaigns_recipients.tag] <@ contacts_summaries.tag
+  WHERE  contacts_summaries.id = $1::uuid
+),
+auto_emails AS (
+  (
+    SELECT
+      campaign
+    FROM
+      email_campaigns_recipients
+    WHERE
+      contact = $1::uuid
+  )
+  UNION (
+    SELECT campaign FROM list_contacts
+  )
+  UNION (
+    SELECT campaign FROM tag_contacts
+  )
+),
+timeline_content AS (
   (
     SELECT crm_tasks.id, due_date as "timestamp", 'crm_task' as "type"
     FROM crm_tasks
@@ -21,6 +51,20 @@ WITH timeline_content AS (
       deleted_at IS NULL
       AND contact = $1::uuid
       AND attribute_type = 'note'
+  )
+  UNION ALL
+  (
+    SELECT
+      id,
+      due_at AS "timestamp",
+      'email_campaign'
+    FROM
+      email_campaigns
+      JOIN auto_emails
+        ON auto_emails.campaign = email_campaigns.id
+    WHERE
+      due_at IS NOT NULL
+      AND executed_at IS NULL
   )
 ),
 with_total AS (
