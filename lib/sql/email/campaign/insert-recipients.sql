cede5c284d2a4ffc49edd45967b64001d5eba5d6
dreamcareer7 <dreamcareer070@gmail.com>
2019-04-25 19:56:38 -0700

    This will be reused for updating campaigns

--- lib/sql/email/campaign/insert-recipients.sql
+++ lib/sql/email/campaign/insert-recipients.sql
@@ -1,11 +1,21 @@
+WITH to_insert AS (
+  SELECT
+    campaign,
+    tag,
+    list,
+    contact,
+    email,
+    recipient_type
+  FROM json_populate_recordset(NULL::email_campaigns_recipients, $1::json)
+),
+
+clear AS (
+  DELETE FROM email_campaigns_recipients WHERE campaign IN (
+    SELECT campaign FROM to_insert
+  )
+)
+
 INSERT INTO email_campaigns_recipients
 (campaign, tag, list, contact, email, recipient_type)
 
-SELECT
-  campaign,
-  tag,
-  list,
-  contact,
-  email,
-  recipient_type
-FROM json_populate_recordset(NULL::email_campaigns_recipients, $1::json)
+SELECT * FROM to_insert
