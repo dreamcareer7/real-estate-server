eff127001464d9b443a59e869b99e65dae5de5e2
dreamcareer7 <dreamcareer070@gmail.com>
2019-05-30 13:17:07 -0700

    We need to return flat brands for agents to match the new UI. Therefore, make the `q` optional.
    Also, reapply the migration it was missing from boer for some reason.

--- lib/sql/brand/agents.sql
+++ lib/sql/brand/agents.sql
@@ -3,10 +3,12 @@ SELECT
 FROM    get_brand_agents($1) ba
 JOIN    users                 ON ba.user = users.id
 WHERE
-to_tsvector('english',
-  COALESCE(users.first_name,   '') || ' ' ||
-  COALESCE(users.last_name,    '') || ' ' ||
-  COALESCE(users.email,        '') || ' ' ||
-  COALESCE(users.phone_number, '') || ' '
+$2::text IS NULL OR (
+  to_tsvector('english',
+    COALESCE(users.first_name,   '') || ' ' ||
+    COALESCE(users.last_name,    '') || ' ' ||
+    COALESCE(users.email,        '') || ' ' ||
+    COALESCE(users.phone_number, '') || ' '
+  )
+  @@ to_tsquery('english', $2)
 )
-@@ to_tsquery('english', $2)
