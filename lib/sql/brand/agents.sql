b5d3e2bef373eed4d9dc1260a60e06f691f50c7a
dreamcareer7 <dreamcareer070@gmail.com>
2019-06-06 08:50:24 -0700

    Drop server-side search. Clients will get a list of them initially anyways and have all the info.

--- lib/sql/brand/agents.sql
+++ lib/sql/brand/agents.sql
@@ -2,14 +2,4 @@ SELECT
   ba.*
 FROM    get_brand_agents($1) ba
 JOIN    users                 ON ba.user = users.id
-WHERE
-$2::text IS NULL OR (
-  to_tsvector('english',
-    COALESCE(users.first_name,   '') || ' ' ||
-    COALESCE(users.last_name,    '') || ' ' ||
-    COALESCE(users.email,        '') || ' ' ||
-    COALESCE(users.phone_number, '') || ' '
-  )
-  @@ to_tsquery('english', $2)
-)
 ORDER BY users.is_shadow DESC, users.first_name, users.last_name
