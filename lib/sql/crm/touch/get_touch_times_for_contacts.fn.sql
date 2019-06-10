8eaa0239fbbe3bb233c1f874f4ee32409321a322
dreamcareer7 <dreamcareer070@gmail.com>
2019-06-10 15:30:23 +0430

    [Contact] Fix next touch logic.
    Sometimes postgres does not update the same row twice in the same
    transaction. Had to rewrite (and simplify) touch times update logic.
    
    Unblocks Mamal on web#2680 (Sales Dashboard)

---
+++ lib/sql/crm/touch/get_touch_times_for_contacts.fn.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION get_touch_times_for_contacts(uuid[])
+RETURNS TABLE (
+  contact uuid,
+  last_touch timestamptz,
+  next_touch timestamptz
+)
+LANGUAGE SQL
+AS $$
+  WITH last_touches AS (
+    SELECT DISTINCT ON (ca.contact)
+      ca.contact,
+      crm_tasks.due_date AS last_touch
+    FROM
+      crm_tasks
+      JOIN crm_associations AS ca
+        ON ca.crm_task = crm_tasks.id
+    WHERE
+      ca.contact = ANY($1)
+      AND crm_tasks.status = 'DONE'
+      AND crm_tasks.task_type <> ALL(ARRAY['Note', 'Other'])
+      AND crm_tasks.deleted_at IS NULL
+      AND ca.deleted_at IS NULL
+    ORDER BY
+      ca.contact, crm_tasks.due_date desc
+  ),
+  next_touches AS (
+    SELECT
+      last_touches.contact,
+      MIN(COALESCE(last_touch, NOW()) + (touch_freq || ' days')::interval) AS next_touch
+    FROM
+      last_touches
+      JOIN unnest($1::uuid[]) AS cids(id)
+        ON last_touches.contact = cids.id
+      JOIN crm_lists_members AS clm
+        ON last_touches.contact = clm.contact
+      JOIN crm_lists AS csl
+        ON csl.id = clm.list
+    WHERE
+      clm.deleted_at IS NULL
+      AND csl.deleted_at IS NULL
+      AND touch_freq IS NOT NULL
+    GROUP BY
+      last_touches.contact
+  )
+  SELECT
+    lt.contact,
+    lt.last_touch,
+    nt.next_touch
+  FROM
+    last_touches AS lt
+    JOIN next_touches AS nt USING (contact)
+$$
