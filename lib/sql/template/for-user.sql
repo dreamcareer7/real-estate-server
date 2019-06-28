a0de0b2dbbe250829252e73c790cf9aa819a3103
dreamcareer7 <dreamcareer070@gmail.com>
2019-06-28 10:27:55 -0700

    Proper query

--- lib/sql/template/for-user.sql
+++ lib/sql/template/for-user.sql
@@ -6,11 +6,11 @@ WITH user_brands AS (
   AND brands_users.deleted_at IS NULL
 )
 
-SELECT templates.id FROM templates
+SELECT DISTINCT ON (templates.id) templates.id FROM templates
 FULL JOIN brands_allowed_templates bat ON templates.id = bat.template
 WHERE
   (bat.id IS NULL OR bat.brand IN(SELECT brand FROM user_brands))
   AND ($2::template_type[]   IS NULL OR $2 @> ARRAY[template_type])
   AND ($3::template_medium[] IS NULL OR $3 @> ARRAY[medium])
   AND deleted_at IS NULL
-ORDER BY created_at DESC, name ASC
+ORDER BY templates.id, created_at DESC, name ASC
