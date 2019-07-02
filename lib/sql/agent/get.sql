493f5a660824523975aa604491840a0cd1de4292
dreamcareer7 <dreamcareer070@gmail.com>
2019-07-02 12:59:15 -0700

    Drop the concept of agents_images. It was added when we started to onboard claystapp. We wanted the ability to have photos for their agents before they sign up.
    Now that we are adding multi MLS support we would've had to update all this crap.
    Which is not worth the effort.
    
    product#708 has been created so we create user accounts and upload photos for them in the meantime.

--- lib/sql/agent/get.sql
+++ lib/sql/agent/get.sql
@@ -5,14 +5,8 @@ SELECT agents.*,
        EXTRACT(EPOCH FROM deleted_at) AS deleted_at,
        (SELECT id FROM users WHERE agent = agents.id LIMIT 1) as user_id,
        (SELECT id FROM offices WHERE matrix_unique_id = agents.office_mui LIMIT 1) as office_id,
-       COALESCE(
-         (SELECT profile_image_url FROM users WHERE agent = agents.id LIMIT 1),
-         (SELECT url FROM agents_images WHERE mui = agents.matrix_unique_id AND image_type = 'Profile' ORDER BY date DESC LIMIT 1)
-       ) as profile_image_url,
-       COALESCE(
-         (SELECT cover_image_url FROM users WHERE agent = agents.id LIMIT 1),
-         (SELECT url FROM agents_images WHERE mui = agents.matrix_unique_id AND image_type = 'Cover' ORDER BY date DESC LIMIT 1)
-       ) as cover_image_url,
+       (SELECT profile_image_url FROM users WHERE agent = agents.id LIMIT 1) as profile_image_url,
+       (SELECT cover_image_url FROM users WHERE agent = agents.id LIMIT 1) as cover_image_url,
        (
          SELECT ARRAY_AGG(DISTINCT phone)
          FROM agents_phones
