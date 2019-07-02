856bf789a697588453f5e6b1fb73f76a8975c412
dreamcareer7 <dreamcareer070@gmail.com>
2019-07-02 17:31:38 +0430

    [br-microsoft-grapgh] handle expired tokens and revoked account

--- lib/sql/microsoft/credential/revoked.sql
+++ lib/sql/microsoft/credential/revoked.sql
@@ -4,8 +4,6 @@ SET
   revoked = TRUE,
   sync_status = NULL,
   last_sync_at = NULL,
-  contacts_last_sync_at = NULL,
-  messages_sync_history_id = NULL,
-  threads_sync_history_id = NULL
+  contacts_last_sync_at = NULL
 WHERE
   id = $1
