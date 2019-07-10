2aa30833db87418bb4bcfd5eaf7bbe0c6ef1f923
dreamcareer7 <dreamcareer070@gmail.com>
2019-07-10 14:26:54 +0430

    [Peanar] Define MLS queues

--- scripts/workers/peanar.js
+++ scripts/workers/peanar.js
@@ -1,6 +1,7 @@
 const { peanar } = require('../../lib/utils/peanar')
 
 require('../../lib/models/Contact/worker')
+require('../../lib/models/MLS/workers')
 
 async function main() {
   await peanar.worker({ queues: [
@@ -11,6 +12,17 @@ async function main() {
   await peanar.worker({ queues: [
     'contact_import',
   ], n: 2 })
+
+  await peanar.worker({ queues: [
+    'MLS.Unit',
+    'MLS.Room',
+    'MLS.OpenHouse',
+    'MLS.Agent',
+    'MLS.Office',
+    'MLS.Photo',
+    'MLS.Listing',
+    'MLS.Listing.Photos.Validate',
+  ], n: 10})
 
   process.on('SIGINT', () => peanar.shutdown())
   process.on('SIGTERM', () => peanar.shutdown())
