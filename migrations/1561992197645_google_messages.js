eb2e221c34a76d48b0f209fd743cb219e2bfc553
dreamcareer7 <dreamcareer070@gmail.com>
2019-07-26 22:20:13 +0430

    [br-gmail] Handle initial messages sync

---
+++ migrations/1561992197645_google_messages.js
@@ -0,0 +1,45 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+
+  `CREATE TABLE IF NOT EXISTS google_messages(
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    google_credential uuid NOT NULL REFERENCES google_credentials(id),
+
+    message_id TEXT NOT NULL,
+    thread_id TEXT NOT NULL,
+    history_id TEXT NOT NULL,
+    recipients TEXT [],
+    in_bound BOOLEAN NOT NULL,
+    message_created_at NOT NULL DEFAULT clock_timestamp(),
+
+    data JSONB,
+
+    created_at timestamptz NOT NULL DEFAULT clock_timestamp(),
+    updated_at timestamptz NOT NULL DEFAULT clock_timestamp(),
+    deleted_at timestamptz,
+
+    UNIQUE (google_credential, message_id)
+  )`,
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
