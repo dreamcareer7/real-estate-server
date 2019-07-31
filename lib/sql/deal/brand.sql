3a1988c2cfeddb43093144d64948ecf76b5dc1d9
dreamcareer7 <dreamcareer070@gmail.com>
2019-07-31 12:53:24 -0700

    for web#3025

--- lib/sql/deal/brand.sql
+++ lib/sql/deal/brand.sql
@@ -1,12 +1,15 @@
 SELECT      DISTINCT deals.id, deals.updated_at
 FROM        deals
-LEFT JOIN   deals_roles ON deals.id = deals_roles.deal
+LEFT JOIN   deals_roles      ON deals.id = deals_roles.deal
+LEFT JOIN   deals_checklists ON deals_roles.checklist = deals_checklists.id
 WHERE
   (
     deals.brand              IN(SELECT brand_children($1))
     OR (
           deals_roles.brand      IN(SELECT brand_children($1))
-      AND deals_roles.deleted_at IS NULL
+      AND deals_roles.deleted_at          IS NULL
+      AND deals_checklists.deactivated_at IS NULL
+      AND deals_checklists.terminated_at  IS NULL
     )
   )
   AND deals.deleted_at       IS NULL
