aa099e3b294485c2963205caf8679f7478fb9500
dreamcareer7 <dreamcareer070@gmail.com>
2019-08-06 19:12:22 +0430

    [br-gmail] Add google_messages_index migration

---
+++ migrations/1561992197647_google_messages_index.js
@@ -0,0 +1,27 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+
+  `CREATE INDEX google_messages_recipients_idx
+    ON "google_messages" USING GIN ("recipients")`,
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
