07c6b2b1e7fa91cf3ab127c5a2a5af7bfd0b3731
dreamcareer7 <dreamcareer070@gmail.com>
2019-08-08 12:03:26 -0700

    Also allow templates belonging to parent brands

--- lib/sql/template/for-brand.sql
+++ lib/sql/template/for-brand.sql
@@ -5,7 +5,7 @@ SELECT templates.id FROM templates
 FULL JOIN brands_allowed_templates bat ON templates.id = bat.template
 WHERE
   (
-    bat.brand  = $1 OR
+    bat.brand  IN (SELECT * FROM brand_parents($1)) OR
     templates.id NOT IN(SELECT template FROM limited_templates)
   )
   AND ($2::template_type[]   IS NULL OR $2 @> ARRAY[template_type])
