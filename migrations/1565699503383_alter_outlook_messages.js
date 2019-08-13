ae8e8dd487a086b7fe646ca3ecc71ae1a69a46a2
dreamcareer7 <dreamcareer070@gmail.com>
2019-08-13 17:58:39 +0430

    [br-microsoft-graph] Add new migration

---
+++ migrations/1565699503383_alter_outlook_messages.js
@@ -0,0 +1,27 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+
+  'ALTER TABLE microsoft_messages DROP COLUMN IF EXISTS message_created_at',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS message_created_at TIMESTAMPTZ',
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
