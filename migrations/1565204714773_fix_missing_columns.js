3987abbac1c2b786f62fffa17d696b258952ced3
dreamcareer7 <dreamcareer070@gmail.com>
2019-08-13 21:12:22 +0430

    [br-microsoft-graph] Fix migrations-4

--- migrations/1565204714773_fix_missing_columns.js
+++ migrations/1565204714773_fix_missing_columns.js
@@ -9,6 +9,33 @@ const migrations = [
   'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS thread_id TEXT NOT NULL',
   'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS thread_id TEXT NOT NULL',
 
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS internet_message_id TEXT',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS internet_message_id TEXT',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS in_bound BOOLEAN NOT NULL',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS in_bound BOOLEAN NOT NULL',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS recipients TEXT []',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS recipients TEXT []',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS has_attachments Boolean',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS has_attachments Boolean',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS attachments JSONB',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS attachments JSONB',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS "from" JSONB',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS "from" JSONB',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS "to" JSONB',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS "to" JSONB',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS cc JSONB',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS cc JSONB',
+
+  'ALTER TABLE google_messages ADD COLUMN IF NOT EXISTS bcc JSONB',
+  'ALTER TABLE microsoft_messages ADD COLUMN IF NOT EXISTS bcc JSONB',
+
   'COMMIT'
 ]
 
