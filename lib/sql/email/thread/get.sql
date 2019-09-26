48448a85ac456228bfbb87372e0dfc3ba1fd965d
dreamcareer7 <dreamcareer070@gmail.com>
2019-09-26 19:51:34 +0330

    [br-microsoft-graph] Update get-thread and full-thread queries

--- lib/sql/email/thread/get.sql
+++ lib/sql/email/thread/get.sql
@@ -1,94 +1,43 @@
-SELECT 
-  'thread' AS type,
-
-  thread_keys.id AS id,
-
-  (SELECT count(*) FROM (
-    (
-      SELECT
-        id
-      FROM
-        emails
-      WHERE
-        mailgun_id = (
-          SELECT
-            in_reply_to
-          FROM
-            google_messages
-          WHERE
-            google_messages.thread_key = thread_keys.id
-          ORDER BY
-            message_created_at ASC
-          LIMIT 1
-
-        ) OR
-
-        mailgun_id = (
-          SELECT
-            in_reply_to
-          FROM
-            microsoft_messages
-          WHERE
-            microsoft_messages.thread_key = thread_keys.id
-          ORDER BY
-            message_created_at ASC
-          LIMIT 1
-        )
-    )
-    UNION ALL
-    (
-      SELECT
-        id
-      FROM
-        google_messages
-      WHERE
-        google_messages.thread_key = thread_keys.id
-    )
-    UNION ALL
-    (
-      SELECT
-        id
-      FROM
-        microsoft_messages
-      WHERE
-        microsoft_messages.thread_key = thread_keys.id
-    )
-  ) AS thread_emails)::INT AS email_count,
-
-    COALESCE (
-      (
-        SELECT "from" FROM emails WHERE
-          mailgun_id = (
-            SELECT in_reply_to FROM google_messages WHERE google_messages.thread_key = thread_keys.id
-            ORDER BY message_created_at ASC LIMIT 1
-          ) OR
-          mailgun_id = (
-            SELECT in_reply_to FROM microsoft_messages WHERE microsoft_messages.thread_key = thread_keys.id
-            ORDER BY message_created_at ASC LIMIT 1
-          )
-      ),
-
-      (SELECT "from" FROM google_messages    WHERE google_messages.thread_key    = thread_keys.id ORDER BY message_created_at ASC LIMIT 1),
-      (SELECT "from" FROM microsoft_messages WHERE microsoft_messages.thread_key = thread_keys.id ORDER BY message_created_at ASC LIMIT 1)
-    ) AS "head.from",
-
-    COALESCE (
-      (
-        SELECT "to" FROM emails WHERE
-          mailgun_id = (
-            SELECT in_reply_to FROM google_messages WHERE google_messages.thread_key = thread_keys.id
-            ORDER BY message_created_at ASC LIMIT 1
-          ) OR
-          mailgun_id = (
-            SELECT in_reply_to FROM microsoft_messages WHERE microsoft_messages.thread_key = thread_keys.id
-            ORDER BY message_created_at ASC LIMIT 1
-          )
-      ),
-
-      (SELECT "to" FROM google_messages    WHERE google_messages.thread_key    = thread_keys.id ORDER BY message_created_at ASC LIMIT 1),
-      (SELECT "to" FROM microsoft_messages WHERE microsoft_messages.thread_key = thread_keys.id ORDER BY message_created_at ASC LIMIT 1)
-    ) AS "head.to"
-
-FROM (
-  SELECT unnest($1::TEXT[]) AS id
-) AS thread_keys
+WITH rechat_emails AS (
+  (
+    SELECT
+      e.id,
+      g.thread_key,
+      e.created_at,
+      e."from",
+      e."to" || e."cc" || e."bcc" AS "to"
+    FROM
+      google_messages AS g
+      JOIN emails AS e
+        ON g.in_reply_to = e.mailgun_id
+  ) UNION ALL (
+    SELECT
+      e.id,
+      m.thread_key,
+      e.created_at,
+      e."from",
+      e."to" || e."cc" || e."bcc" AS "to"
+    FROM
+      microsoft_messages AS m
+      JOIN emails AS e
+        ON m.in_reply_to = e.mailgun_id
+  )
+)
+SELECT DISTINCT ON (tids.id)
+  tids.id,
+  COALESCE(r.created_at, g.message_date, m.message_date) AS created_at,
+  COALESCE(r."from", g."from", m."from") AS "from",
+  COALESCE(r."to", g."to", m."to") AS "to",
+  array_agg(COALESCE(r.id, g.id, m.id)) OVER (PARTITION BY tids.id) AS emails,
+  (count(*) OVER (PARTITION BY tids.id)) + (r.thread_key IS NOT NULL)::int AS email_count,
+  'thread' AS type
+FROM
+  unnest($1::text[]) AS tids (id)
+  LEFT JOIN google_messages AS g
+    ON tids.id = g.thread_key
+  LEFT JOIN microsoft_messages AS m
+    ON tids.id = m.thread_key
+  LEFT JOIN rechat_emails AS r
+    ON tids.id = r.thread_key
+ORDER BY
+  tids.id, COALESCE(r.created_at, g.message_date, m.message_date)
