04758926199880b779853794da34ea737ff52021
dreamcareer7 <dreamcareer070@gmail.com>
2019-09-30 13:43:08 +0330

    [br-gmail] Refactor google-messages create plugin, Respond g-message as send-email response

---
+++ lib/sql/google/message/get_as_thread_member.sql
@@ -0,0 +1,25 @@
+SELECT
+  'gmail' AS origin,
+  id,
+  message_id,
+  thread_id,
+  thread_key,
+  internet_message_id,
+  has_attachments,
+  attachments,
+  in_bound,
+  subject,
+  NULL AS "snippet",
+  NULL AS "unique_body",
+  NULL AS "html_body",
+  NULL AS "text_body",
+  "from",
+  "to",
+  cc,
+  bcc,
+  message_date
+FROM
+  google_messages
+WHERE
+  google_credential = $1
+  AND message_id = $2
