9635584bdedfaf4f89650a6d1a06ce47a5e3ce6b
dreamcareer7 <dreamcareer070@gmail.com>
2019-10-03 11:59:47 -0700

    Half the work on agent#3336

--- lib/sql/email/campaign/emails.sql
+++ lib/sql/email/campaign/emails.sql
@@ -71,19 +71,31 @@ brand_recipients AS (
 ),
 
 
+agent_recipients AS (
+  SELECT agents.email, email_campaigns_recipients.agent, email_campaigns_recipients.send_type
+  FROM   email_campaigns
+  JOIN   email_campaigns_recipients ON email_campaigns.id = email_campaigns_recipients.campaign
+  JOIN   agents                     ON email_campaigns_recipients.agent= agents.id
+
+  WHERE email_campaigns_recipients.recipient_type = 'Agent'
+    AND email_campaigns.id = $1
+),
+
 all_emails AS (
-  SELECT email, contact, send_type FROM email_campaigns_recipients
+  SELECT email, contact, null as agent, send_type FROM email_campaigns_recipients
   WHERE email_campaigns_recipients.campaign = $1 AND email IS NOT NULL AND contact IS NULL
   UNION
-  SELECT * FROM contact_recipients
+  SELECT *, null as agent FROM contact_recipients
   UNION
-  SELECT * FROM list_contacts
+  SELECT *, null as agent FROM list_contacts
   UNION
-  SELECT * FROM tag_contacts
+  SELECT *, null as agent FROM tag_contacts
   UNION
-  SELECT * FROM all_contacts_recipients
+  SELECT *, null as agent FROM all_contacts_recipients
   UNION
-  SELECT * FROM brand_recipients
+  SELECT *, null as agent FROM brand_recipients
+  UNION
+  SELECT *, null as contact FROM agent_recipients
 )
 
 -- We used to DISTINCT ON(email).
