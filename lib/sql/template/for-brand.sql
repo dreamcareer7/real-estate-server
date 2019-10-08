8f1b9ae01b8baacc6066bd49d96151d220a1979d
dreamcareer7 <dreamcareer070@gmail.com>
2019-10-07 17:42:07 -0700

    quick

--- lib/sql/template/for-brand.sql
+++ lib/sql/template/for-brand.sql
@@ -1,8 +1,8 @@
-SELECT DISTINCT templates.id FROM templates
+SELECT DISTINCT templates.id, templates.created_at FROM templates
 JOIN brands_allowed_templates bat ON templates.id = bat.template
 WHERE
   bat.brand IN (SELECT * FROM brand_parents($1))
   AND ($2::template_type[]   IS NULL OR $2 @> ARRAY[template_type])
   AND ($3::template_medium[] IS NULL OR $3 @> ARRAY[medium])
   AND deleted_at IS NULL
-ORDER BY id, created_at DESC
+ORDER BY templates.id, templates.created_at DESC
