9c5008c7fd7df2fbafd6b3b6c364040c0b28f4af
dreamcareer7 <dreamcareer070@gmail.com>
2019-10-14 18:38:53 +0330

    [br-gmail] Initiate Google-Calendar models

--- migrations/1571054682285_google_calendar.js
+++ migrations/1571054682285_google_calendar.js
@@ -3,40 +3,39 @@ const db = require('../lib/utils/db')
 const migrations = [
   'BEGIN',
 
+  'DROP TABLE IF EXISTS google_calendars',
+
   `CREATE TABLE IF NOT EXISTS google_calendars(
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  
-    "user" uuid NOT NULL REFERENCES users(id),
-    brand uuid NOT NULL REFERENCES brands(id),
 
     google_credential uuid NOT NULL REFERENCES google_credentials(id),
-  
     calendar_id TEXT NOT NULL,
-    etag TEXT,
 
-    accessRole TEXT,
+    access_role TEXT,
     description TEXT,
     summary TEXT,
-    summaryOverride TEXT,
+    summary_override TEXT,
     location TEXT,
-    timeZone TEXT,
+    time_zone TEXT,
 
-    backgroundColor TEXT,
-    foregroundColor TEXT,
-    colorId TEXT,    
+    background_color TEXT,
+    foreground_color TEXT,
+    color_id TEXT,    
 
-    primary BOOLEAN,
+    "primary" BOOLEAN,
     hidden BOOLEAN,
     selected BOOLEAN,
     deleted BOOLEAN,
 
-    defaultReminders JSONB,
-    conferenceProperties JSONB,
-    notificationSettings JSONB,
+    default_reminders JSONB,
+    conference_properties JSONB,
+    notification_settings JSONB,
 
     created_at timestamptz NOT NULL DEFAULT clock_timestamp(),
     updated_at timestamptz NOT NULL DEFAULT clock_timestamp(),
-    deleted_at timestamptz  
+    deleted_at timestamptz,
+
+    UNIQUE (google_credential, calendar_id)
   )`,
 
   'COMMIT'
--- migrations/1571054682286_google_calendar_event.js
+++ migrations/1571054682286_google_calendar_event.js
@@ -3,63 +3,62 @@ const db = require('../lib/utils/db')
 const migrations = [
   'BEGIN',
 
+  'DROP TABLE IF EXISTS google_calendar_events',
+
   `CREATE TABLE IF NOT EXISTS google_calendar_events(
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  
-    "user" uuid NOT NULL REFERENCES users(id),
-    brand uuid NOT NULL REFERENCES brands(id),
 
     google_credential uuid NOT NULL REFERENCES google_credentials(id),
     google_calendar uuid NOT NULL REFERENCES google_calendars(id),
-  
-      event_id TEXT NOT NULL,
-      etag TEXT,
+    event_id TEXT NOT NULL,
 
-      description TEXT,
-      summary TEXT,
-      location TEXT,
-      colorId TEXT,
-      iCalUID TEXT,
-      transparency TEXT,
-      visibility TEXT,
-      hangoutLink TEXT,
-      htmlLink TEXT,
+    description TEXT,
+    summary TEXT,
+    location TEXT,
+    color_id TEXT,
+    ical_uid TEXT,
+    transparency TEXT,
+    visibility TEXT,
+    hangout_link TEXT,
+    html_link TEXT,
 
-      status TEXT,
-      
-      anyoneCanAddSelf BOOLEAN,
-      guestsCanInviteOthers BOOLEAN,
-      guestsCanModify BOOLEAN,
-      guestsCanSeeOtherGuests BOOLEAN,
-      attendeesOmitted BOOLEAN,
-      locked BOOLEAN,
-      privateCopy BOOLEAN,
+    status TEXT,
+    
+    anyone_can_add_self BOOLEAN,
+    guests_can_invite_others BOOLEAN,
+    guests_can_modify BOOLEAN,
+    guests_can_see_other_guests BOOLEAN,
+    attendees_omitted BOOLEAN,
+    locked BOOLEAN,
+    private_copy BOOLEAN,
 
-      sequence NUMBER,
+    sequence BIGINT,
 
-      creator JSONB,
-      organizer JSONB,
-      attendees JSONB,
-      attachments JSONB,
-      conferenceData JSONB,
-      extendedProperties JSONB,
-      gadget JSONB,
-      reminders JSONB,
-      source JSONB,
-      
-      created timestamptz NOT NULL,
-      updated timestamptz NOT NULL,
-      
-      start JSONB,
-      end JSONB,
-      endTimeUnspecified BOOLEAN,
-      recurrence JSONB,
-      recurringEventId TEXT,
-      originalStartTime JSONB,
+    creator JSONB,
+    organizer JSONB,
+    attendees JSONB,
+    attachments JSONB,
+    conference_data JSONB,
+    extended_properties JSONB,
+    gadget JSONB,
+    reminders JSONB,
+    source JSONB,
+    
+    created timestamptz NOT NULL,
+    updated timestamptz NOT NULL,
+    
+    "start" JSONB,
+    "end" JSONB,
+    end_time_unspecified BOOLEAN,
+    recurrence JSONB,
+    recurring_eventId TEXT,
+    original_start_time JSONB,
 
     created_at timestamptz NOT NULL DEFAULT clock_timestamp(),
     updated_at timestamptz NOT NULL DEFAULT clock_timestamp(),
-    deleted_at timestamptz  
+    deleted_at timestamptz,
+
+    UNIQUE (google_credential, google_calendar, event_id)
   )`,
 
   'COMMIT'
