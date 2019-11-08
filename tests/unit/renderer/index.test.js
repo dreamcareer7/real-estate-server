48bcb0d57f00605180cfd5cc917385a4eb3a002c
dreamcareer7 <dreamcareer070@gmail.com>
2019-11-07 23:00:04 -0800

    A test to make sure everything is correctly rendered

--- tests/unit/renderer/index.test.js
+++ tests/unit/renderer/index.test.js
@@ -5,7 +5,6 @@ const renderer = require('../../../lib/utils/render')
 const ListingHelper = require('../mls/helpers/listing')
 const config = require('../../../lib/config')
 
-
 const worker = async function() {
   this.timeout(30000)
 
@@ -24,12 +23,54 @@ const worker = async function() {
     promises.push(promise)
   }
 
-  const all = await Promise.all(promises)
-  console.log('Done')
+  await Promise.all(promises)
+}
+
+const timeout = async function() {
+  this.timeout(40000)
+
+  const max = 10000
+  const template = (await readFile(`${__dirname}/template.html`)).toString()
+  const listing = await ListingHelper.create()
+  const user = await User.getByEmail(config.tests.username)
+
+  const promises = []
+
+  for(let i = 0; i<=max; i++) {
+    const promise = renderer.isolated.htmlString(template, {
+      listing,
+      user
+    })
+    promises.push(promise)
+  }
+
+  await Promise.all(promises)
+}
+
+const accuracy = async function() {
+  const max = 100
+  const template = `Item {{i}}`
+
+  const promises = []
+
+  for(let i = 0; i<=max; i++) {
+    const promise = renderer.isolated.htmlString(template, {
+      i
+    })
+    promises.push(promise)
+  }
+
+  const results = await Promise.all(promises)
+
+  for(let i = 0; i<=max; i++) {
+    const result = results[i]
+    expect(result).to.equal(`Item ${i}`)
+  }
 }
 
 describe('Renderer', () => {
   createContext()
 
-  it('should render a template in a worker', worker)
+  it('should render a big template many times quickly', timeout)
+  it('should make sure everything is rendered properly', accuracy)
 })
