85fce79d0913f5f0a65e57d92afdc72348b9123e
dreamcareer7 <dreamcareer070@gmail.com>
2019-11-21 12:58:58 -0800

    A complete refactor. The previous versions used to get an array of all contexts and render them all together. That mean there was an array that would have to contain all render requests. This array would grow large and large until the Nodejs' memory heap size was reached and nodejs would crash.
    This new mechanism is far more scale-able. It creates a resource
    handler which can be used to generate as many renders as possible
    but they don't need to happen in 1 function call:
    each render request is handled independantly so the GC can remove them.
    
    Also did some cleanups. Also we don't need a pool anymore.
    Each render request will spawn a process and then let it go when render is
    completed.

--- tests/unit/renderer/index.test.js
+++ tests/unit/renderer/index.test.js
@@ -6,41 +6,51 @@ const ListingHelper = require('../mls/helpers/listing')
 const config = require('../../../lib/config')
 
 const timeout = async function() {
-  this.timeout(60000)
+  this.timeout(30000)
 
   const max = 10000
   const template = (await readFile(`${__dirname}/template.html`)).toString()
   const listing = await ListingHelper.create()
   const user = await User.getByEmail(config.tests.username)
 
-  const contexts = []
+  const { render, release } = await renderer.compileHtml(template)
 
   for(let i = 0; i <= max; i++) {
-    contexts.push({
+    await render({
       listing,
       user
     })
   }
 
-  await renderer.htmlStrings(template, contexts)
+  release()
 }
 
 const accuracy = async function() {
   const max = 100
   const template = 'Item {{i}}'
 
-  const contexts = []
+  const { render, release } = await renderer.compileText(template)
 
   for(let i = 0; i <= max; i++) {
-    contexts.push({i})
-  }
-
-  const results = await renderer.htmlStrings(template, contexts)
-
-  for(let i = 0; i <= max; i++) {
-    const result = results[i]
+    const result = await render({i})
     expect(result).to.equal(`Item ${i}`)
   }
+
+  release()
+}
+
+const validate = async function() {
+  const template = '{{i}'
+
+  let error
+
+  try {
+    await renderer.compileText(template)
+  } catch(err) {
+    error = err
+  }
+
+  expect(error.http).to.equal(400)
 }
 
 describe('Renderer', () => {
@@ -48,4 +58,5 @@ describe('Renderer', () => {
 
   it('should render a big template many times quickly', timeout)
   it('should make sure everything is rendered properly', accuracy)
+  it('should error during compilation of an invalid template', validate)
 })
