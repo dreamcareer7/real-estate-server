7da0fe290f083b5bdfeae4df9dac4e181ff0ed84
dreamcareer7 <dreamcareer070@gmail.com>
2019-11-29 19:09:57 +0330

    Update workers, Roolback temporary changes in sync query

--- lib/sql/microsoft/credential/sync_due.sql
+++ lib/sql/microsoft/credential/sync_due.sql
@@ -3,7 +3,6 @@ SELECT
 FROM 
   microsoft_credentials
 WHERE
-  ( id = 'bb38a5c5-a03c-4259-97f8-eafcb3a01865' AND sync_status is null ) OR
   (
     (
       (last_sync_at <= (NOW() - $1::interval) OR last_sync_at IS NULL)
