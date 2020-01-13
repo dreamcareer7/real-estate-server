fe26ce6295fa308fd18d674fbbbeb60822f70473
dreamcareer7 <dreamcareer070@gmail.com>
2020-01-13 11:50:05 +0330

    Fix eslint

--- tests/unit/microsoft/calendar.test.js
+++ tests/unit/microsoft/calendar.test.js
@@ -1,13 +1,13 @@
-const uuid       = require('uuid')
-const { expect } = require('chai')
-const { createContext } = require('../helper')
+// const uuid       = require('uuid')
+// const { expect } = require('chai')
+// const { createContext } = require('../helper')
 
-const Context             = require('../../../lib/models/Context')
-const User                = require('../../../lib/models/User')
-const BrandHelper         = require('../brand/helper')
-const MicrosoftCredential = require('../../../lib/models/Microsoft/credential')
-const MicrosoftCalendar   = require('../../../lib/models/Microsoft/calendar')
+// const Context             = require('../../../lib/models/Context')
+// const User                = require('../../../lib/models/User')
+// const BrandHelper         = require('../brand/helper')
+// const MicrosoftCredential = require('../../../lib/models/Microsoft/credential')
+// const MicrosoftCalendar   = require('../../../lib/models/Microsoft/calendar')
 
-const { createMicrosoftMessages } = require('./helper')
+// const { createMicrosoftMessages } = require('./helper')
 
-let user, brand, googleCredential
+// let user, brand, googleCredential
