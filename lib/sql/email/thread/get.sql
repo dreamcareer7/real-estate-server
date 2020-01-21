c66d2fdace839bf4b45a2007ed91f2fa92fdb6f0
dreamcareer7 <dreamcareer070@gmail.com>
2020-01-21 16:03:41 +0330

    thread: fix typo

--- lib/sql/email/thread/get.sql
+++ lib/sql/email/thread/get.sql
@@ -38,8 +38,8 @@ SELECT
                 google_messages
               WHERE
                 thread_key = email_threads.id
-                AND google_messages.google_credential = email_threads.google_credential)
-                AND google_messages.deleted_at IS NULL
+                AND google_messages.google_credential = email_threads.google_credential
+                AND google_messages.deleted_at IS NULL)
             WHEN microsoft_credential IS NOT NULL THEN
               (SELECT
                 jsonb_agg(jsonb_build_object('id', microsoft_messages.id, 'type', 'microsoft_message') ORDER BY message_date)
@@ -47,8 +47,8 @@ SELECT
                 microsoft_messages
               WHERE
                 thread_key = email_threads.id
-                AND microsoft_messages.microsoft_credential = email_threads.microsoft_credential)
-                AND microsoft_messages.deleted_at IS NULL
+                AND microsoft_messages.microsoft_credential = email_threads.microsoft_credential
+                AND microsoft_messages.deleted_at IS NULL)
             ELSE
               '[]'::jsonb
           END
