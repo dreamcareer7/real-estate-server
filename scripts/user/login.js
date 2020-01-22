16725121aa4627f83fea20b15c5cba3059e04f9a
dreamcareer7 <dreamcareer070@gmail.com>
2020-01-21 21:31:29 -0800

    Script to help login as a user

---
+++ scripts/user/login.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+require('../connection.js')
+require('../../lib/models/index.js')
+
+const promisify = require('../../lib/utils/promisify')
+
+const update = async () => {
+  const user = await User.getByEmail(process.argv[2])
+  const client = await Client.get(process.argv[3])
+
+  const link = await User.getLoginLink(user, client)
+  console.log(link)
+
+  process.exit()
+}
+
+update()
+  .catch(e => {
+    console.log(e)
+    process.exit()
+  })
