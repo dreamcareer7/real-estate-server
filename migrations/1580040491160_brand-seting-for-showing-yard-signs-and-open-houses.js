c002c175bf54077aaef90b9d20040ede69e26507
dreamcareer7 <dreamcareer070@gmail.com>
2020-01-26 04:13:11 -0800

    Fix the migration

--- migrations/1580040491160_brand-seting-for-showing-yard-signs-and-open-houses.js
+++ migrations/1580040491160_brand-seting-for-showing-yard-signs-and-open-houses.js
@@ -1,12 +1,10 @@
 const db = require('../lib/utils/db')
 
 const migrations = [
-  'BEGIN',
   `ALTER TYPE brand_setting
-    ADD VALUE 'EnableOpenHouseRequest'`,
+    ADD VALUE 'EnableOpenHouseRequests'`,
   `ALTER TYPE brand_setting
-    ADD VALUE 'EnableYardSignRequest'`,
-  'COMMIT'
+    ADD VALUE 'EnableYardSignRequests'`,
 ]
 
 
