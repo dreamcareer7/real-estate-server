2b4d63fc888d4697b4e8871fed1d65f134f3c13d
dreamcareer7 <dreamcareer070@gmail.com>
2020-01-29 16:09:40 -0800

    Check the errors

--- tests/functional/suites/email.js
+++ tests/functional/suites/email.js
@@ -132,6 +132,9 @@ const getByBrand = cb => {
           subject: invalid.subject,
           html: invalid.html,
           sent: 0,
+          error: String,
+          errored_at: Date,
+          errored_within: String
         },
 
         {
