840066c88bc015a7c7f8a15506074eb3fa9e05ec
dreamcareer7 <dreamcareer070@gmail.com>
2020-02-17 16:33:14 +0330

    In progress

--- lib/sql/calendar/integration/microsoft_sync_due.sql
+++ lib/sql/calendar/integration/microsoft_sync_due.sql
@@ -1,19 +1,12 @@
 SELECT
   id
 FROM 
-  google_credentials
+  microsoft_credentials
 WHERE
   (
-    (
-      (calendars_last_sync_at <= (NOW() - $1::interval) OR calendars_last_sync_at IS NULL)
-      AND (sync_status = 'success' OR sync_status IS NULL)
-    )
-    OR
-    (
-      (calendars_last_sync_at <= (NOW() - '3600 seconds'::interval))
-      AND (sync_status = 'pending' OR sync_status = 'failed')
-    )
+    (calendars_last_sync_at <= (NOW() - $1::interval) OR calendars_last_sync_at IS NULL)
+    AND (sync_status = 'success' OR sync_status IS NULL)
   )
   AND revoked IS FALSE
   AND deleted_at IS NULL
-  AND google_calendar IS NOT NULL
+  AND microsoft_calendar IS NOT NULL
