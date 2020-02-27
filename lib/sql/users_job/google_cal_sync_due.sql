54c2a0e3954668b49ad599bec2f486e55f924b8f
dreamcareer7 <dreamcareer070@gmail.com>
2020-02-27 18:14:27 +0330

    Update syncCalendar

--- lib/sql/users_job/google_cal_sync_due.sql
+++ lib/sql/users_job/google_cal_sync_due.sql
@@ -5,6 +5,6 @@ FROM
 WHERE
   (executed_at <= (NOW() - $1::interval) OR executed_at IS NULL)
   AND google_credential IS NOT NULL
-  AND job_name IS 'calendar'
+  AND job_name = 'calendar'
   AND status <> 'pending'
   AND deleted_at IS NULL
