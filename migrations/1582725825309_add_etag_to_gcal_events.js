3e9917b95e42e619093d81dbc3ebeeb50a2931a8
dreamcareer7 <dreamcareer070@gmail.com>
2020-02-28 18:23:51 +0330

    Update syncCalendar, Fix bugs

--- migrations/1582725825309_add_etag_to_gcal_events.js
+++ migrations/1582725825309_add_etag_to_gcal_events.js
@@ -6,7 +6,7 @@ const migrations = [
   'ALTER TABLE google_calendar_events ADD COLUMN IF NOT EXISTS etag TEXT NOT NULL',
 
   'ALTER TABLE calendar_integration ADD COLUMN IF NOT EXISTS etag TEXT NOT NULL',
-  'ALTER TABLE calendar_integration ADD COLUMN IF NOT EXISTS local_etag TEXT NOT NULL',
+  'ALTER TABLE calendar_integration ADD COLUMN IF NOT EXISTS local_etag TEXT NULL',
 
   'COMMIT'
 ]
