c171f7982a4a342969ba2c3e8ac8213554f65dfc
dreamcareer7 <dreamcareer070@gmail.com>
2020-02-29 14:25:09 +0330

    Update sync_due queries

--- lib/sql/google/credential/gmail_sync_due.sql
+++ lib/sql/google/credential/gmail_sync_due.sql
@@ -6,12 +6,12 @@ WHERE
   (
     (
       (last_sync_at <= (NOW() - $1::interval) OR last_sync_at IS NULL)
-      AND (sync_status = 'success' OR sync_status IS NULL)
+      AND (sync_status = 'success' OR sync_status = 'failed' OR sync_status IS NULL)
     )
     OR
     (
       (last_sync_at <= (NOW() - '24 hours'::interval))
-      AND (sync_status = 'pending' OR sync_status = 'failed')
+      AND sync_status = 'pending'
     )
   )
   AND revoked IS FALSE
