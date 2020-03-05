e83345b23a91bbd0f4967eed38f8fdb95dcca292
dreamcareer7 <dreamcareer070@gmail.com>
2020-03-06 00:11:08 +0330

    Handle deletion of remote primary and other google calendars

--- lib/sql/calendar_integration/get_by_deal_contexts.sql
+++ lib/sql/calendar_integration/get_by_deal_contexts.sql
@@ -1,1 +1,1 @@
-SELECT id FROM calendar_integration WHERE deal_context = ANY($1::uuid[]) AND contact IS NULL
+SELECT id FROM calendar_integration WHERE deal_context = ANY($1::uuid[]) AND contact IS NULL AND deleted_at IS NULL
