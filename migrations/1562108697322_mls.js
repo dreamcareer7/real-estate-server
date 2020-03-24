1fe1e938b636eeb86c3ab39986ff70a98b610932
dreamcareer7 <dreamcareer070@gmail.com>
2020-03-24 17:26:18 +0430

    mls: fix the old multi mls migration

--- migrations/1562108697322_mls.js
+++ migrations/1562108697322_mls.js
@@ -6,7 +6,40 @@ const ap = fs.readFileSync(__dirname + '/../lib/sql/agent/agents_phones.mv.sql',
 const ut = fs.readFileSync(__dirname + '/../lib/sql/deal/context/update_current_deal_context.trigger.sql', 'utf-8')
 const mc = fs.readFileSync(__dirname + '/../lib/sql/deal/context/get_mls_context.fn.sql', 'utf-8')
 const lf = fs.readFileSync(__dirname + '/../lib/sql/alert/update_listings_filters.fn.sql', 'utf-8')
-const ba = fs.readFileSync(__dirname + '/../lib/sql/brand/get_brand_agents.fn.sql', 'utf-8')
+const ba = `CREATE OR REPLACE FUNCTION get_brand_agents(id uuid) RETURNS TABLE (
+  "user"     uuid,
+  agent      uuid,
+  mui        integer,
+  mls        mls,
+  brand_user uuid,
+  brand_role uuid,
+  brand      uuid,
+  enabled    boolean
+) AS
+$$
+ SELECT
+   users.id                as "user",
+   agents.id               as agent,
+   agents.matrix_unique_id as mui,
+   agents.mls              as mls,
+   brands_users.id         as brand_user,
+   brands_roles.id         as brand_role,
+   brands_roles.brand      as brand,
+   (brands_users.deleted_at IS NULL AND brands_roles.deleted_at IS NULL) as enabled
+
+ FROM users
+ LEFT JOIN agents  ON users.agent = agents.id
+ JOIN brands_users ON brands_users.user = users.id
+ JOIN brands_roles ON brands_users.role = brands_roles.id
+ WHERE
+   users.user_type = 'Agent'
+   AND
+   brands_roles.brand IN(
+     SELECT brand_children($1)
+   )
+$$
+LANGUAGE sql`
+
 const bu = fs.readFileSync(__dirname + '/../lib/sql/brand/get_brand_users.fn.sql', 'utf-8')
 const pa = fs.readFileSync(__dirname + '/../lib/sql/brand/propose_brand_agents.fn.sql', 'utf-8')
 
