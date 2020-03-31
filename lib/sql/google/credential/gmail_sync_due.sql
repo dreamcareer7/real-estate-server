3d82b480184d745a406ae49320d0f9405aeb7350
dreamcareer7 <dreamcareer070@gmail.com>
2020-03-31 19:55:13 +0430

    [Google] Rollback gmail_sync_due query

--- lib/sql/google/credential/gmail_sync_due.sql
+++ lib/sql/google/credential/gmail_sync_due.sql
@@ -5,7 +5,7 @@ FROM
 WHERE
   (
     (
-      (last_sync_at <= (NOW() - '24 hours'::interval) OR last_sync_at IS NULL)
+      (last_sync_at <= (NOW() - $1::interval) OR last_sync_at IS NULL)
       AND (sync_status = 'success' OR sync_status = 'failed' OR sync_status IS NULL)
     )
     OR
