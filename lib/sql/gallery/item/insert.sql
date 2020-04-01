eb447d12ff229b3c71375b3035cb1407870d8599
dreamcareer7 <dreamcareer070@gmail.com>
2020-04-01 03:10:17 -0700

    Updates to gallery item model based on latest conversations with Mohsen and Shayan

--- lib/sql/gallery/item/insert.sql
+++ lib/sql/gallery/item/insert.sql
@@ -1,10 +1,9 @@
 INSERT INTO gallery_items
-(gallery, name, description, "order", file)
+(gallery, name, "order", file)
 
 SELECT
   gallery,
   name,
-  description,
   "order",
   file
 FROM json_populate_recordset(NULL::gallery_items, $1::json)
