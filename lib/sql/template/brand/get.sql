d0bf01f5638eb443568eb4f7cfde45055b273708
dreamcareer7 <dreamcareer070@gmail.com>
2020-04-30 17:10:46 -0700

    Better logic

--- lib/sql/template/brand/get.sql
+++ lib/sql/template/brand/get.sql
@@ -4,19 +4,9 @@ SELECT brands_allowed_templates.*,
   EXTRACT(EPOCH FROM templates.created_at) AS updated_at,
   'brand_template' AS TYPE,
 
-  -- When thumbnail_requested_at IS NOT NULL, that means the current thumbnails are invalid
-  -- and outdated. In those cases, we return null, so the clients can show a placeholder
-  -- or something, as the current thumbnail/preview are outdated/invalid/misleading.
-
-  -- Once new thumbnails are generated, thumbnail_requested_at becomes null, and we'll use
-  -- new thumbnail/preview.
-
   (
-    CASE WHEN thumbnail_requested_at IS NULL THEN thumbnail ELSE NULL END
-  ) as thumbnail,
-  (
-    CASE WHEN thumbnail_requested_at IS NULL THEN preview   ELSE NULL END
-  ) as preview
+    thumbnail IS NOT NULL AND thumbnail_rendered_at > thumbnail_requested_at
+  ) as is_thumbnail_ready
 
 FROM brands_allowed_templates
 JOIN templates on brands_allowed_templates.template = templates.id
--- lib/sql/template/brand/update-thumbnails.sql
+++ lib/sql/template/brand/update-thumbnails.sql
@@ -1,4 +1,5 @@
 UPDATE brands_allowed_templates SET
   thumbnail = $2,
-  preview = $3
+  preview = $3,
+  thumbnail_rendered_at = NOW()
 WHERE id = $1
