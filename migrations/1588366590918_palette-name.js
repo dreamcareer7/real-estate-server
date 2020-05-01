7ac4fbb9f757b1e5b9ee6da8958a7a54d57435ca
dreamcareer7 <dreamcareer070@gmail.com>
2020-05-01 13:59:46 -0700

    Mamal & Sanaz khefted me on this.

--- migrations/1588366590918_palette-name.js
+++ migrations/1588366590918_palette-name.js
@@ -1,4 +1,9 @@
-CREATE OR REPLACE FUNCTION PALETTE_TO_JSON(input palette)
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  'ALTER TYPE palette ADD name TEXT',
+  `CREATE OR REPLACE FUNCTION PALETTE_TO_JSON(input palette)
 RETURNS JSON AS $$
 
   WITH palette AS (
@@ -75,4 +80,86 @@ RETURNS JSON AS $$
       )
     END
 $$
-LANGUAGE SQL;
+LANGUAGE SQL;`,
+  `CREATE OR REPLACE FUNCTION JSON_TO_PALETTE(input JSONB)
+RETURNS palette AS $$
+  SELECT
+    ROW(
+      $1->>'body-bg-color',
+      $1->>'body-text-color',
+      $1->>'body-font-family',
+      $1->>'body-font-size',
+      $1->>'body-font-weight',
+      $1->>'body-logo-wide',
+      $1->>'body-logo-square',
+
+      $1->>'container-bg-color',
+      $1->>'container-text-color',
+      $1->>'container-font-family',
+      $1->>'container-font-size',
+      $1->>'container-font-weight',
+      $1->>'container-logo-wide',
+      $1->>'container-logo-square',
+
+      $1->>'button-bg-color',
+      $1->>'button-text-color',
+      $1->>'button-font-family',
+      $1->>'button-font-size',
+      $1->>'button-font-weight',
+      $1->>'button-border',
+
+      $1->>'light-text-color',
+      $1->>'light-font-family',
+      $1->>'light-font-size',
+      $1->>'light-font-weight',
+
+      $1->>'h1-text-color',
+      $1->>'h1-font-family',
+      $1->>'h1-font-size',
+      $1->>'h1-font-weight',
+
+      $1->>'h2-text-color',
+      $1->>'h2-font-family',
+      $1->>'h2-font-size',
+      $1->>'h2-font-weight',
+
+      $1->>'h3-text-color',
+      $1->>'h3-font-family',
+      $1->>'h3-font-size',
+      $1->>'h3-font-weight',
+
+      $1->>'inverted-button-bg-color',
+      $1->>'inverted-button-text-color',
+      $1->>'inverted-light-text-color',
+      $1->>'inverted-h1-text-color',
+      $1->>'inverted-h2-text-color',
+      $1->>'inverted-h3-text-color',
+      $1->>'inverted-logo-wide',
+      $1->>'inverted-logo-square',
+      $1->>'inverted-container-bg-color',
+      $1->>'inverted-container-text-color',
+
+      $1->>'website',
+      $1->>'name'
+    )::palette
+$$
+LANGUAGE SQL`,
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
