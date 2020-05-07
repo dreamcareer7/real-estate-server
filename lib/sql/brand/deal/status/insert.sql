031f3799c003b448a6b5f864f449adcc5b2d9e67
dreamcareer7 <dreamcareer070@gmail.com>
2020-05-06 17:50:07 -0700

    Forgot we had this thing called JS code.

--- lib/sql/brand/deal/status/insert.sql
+++ lib/sql/brand/deal/status/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO brands_deal_statuses
-(brand, label, deal_types, property_types, color, admin_only, archived)
+(brand, label, deal_types, property_types, color, admin_only, is_archived, is_active, is_pending)
 VALUES
-($1, $2, $3, $4, $5, $6, $7)
+($1, $2, $3, $4, $5, $6, $7, $8, $9)
 RETURNING id
