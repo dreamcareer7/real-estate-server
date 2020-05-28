ed385ebe74ea9f34555fee03f13530df91a1ac29
dreamcareer7 <dreamcareer070@gmail.com>
2020-05-28 22:36:13 +0430

    [Google-Contacts] Update queries

--- lib/sql/users_job/google/gcontacts_sync_due.sql
+++ lib/sql/users_job/google/gcontacts_sync_due.sql
@@ -4,7 +4,7 @@ FROM
   users_jobs 
 WHERE
   google_credential IS NOT NULL
-  AND job_name = 'calendar'
-  AND (start_at <= (NOW() - $1::interval) OR start_at IS NULL)
+  AND job_name = 'contacts'
+  AND (start_at <= (NOW() - '6 hours'::interval) OR start_at IS NULL)
   AND deleted_at IS NULL
 FOR UPDATE SKIP LOCKED
