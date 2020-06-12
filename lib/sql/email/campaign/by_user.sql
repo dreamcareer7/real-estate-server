2c229f98ffa937f107ca3c9f9065a8be7a3904bb
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-12 00:46:22 -0700

    Limit Ceiling.

--- lib/sql/email/campaign/by_user.sql
+++ lib/sql/email/campaign/by_user.sql
@@ -1,2 +1,5 @@
 SELECT id FROM email_campaigns
-WHERE created_by = $1 AND created_at > $2 AND deleted_at IS NULL
+WHERE created_by = $1
+AND   (due_at >= $2 OR $2 IS NULL)
+AND   (due_at <= $3 OR $3 IS NULL)
+AND   deleted_at IS NULL
