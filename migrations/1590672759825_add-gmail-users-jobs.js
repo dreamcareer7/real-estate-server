4afb0a5e7b72733ce4eabbcc6c67849026915c50
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-13 09:08:07 +0430

    [Users_jobs] Update old migration to have not multiple impact on Boer

--- migrations/1590672759825_add-gmail-users-jobs.js
+++ migrations/1590672759825_add-gmail-users-jobs.js
@@ -15,7 +15,7 @@ const run = async () => {
   const g_result = await conn.query('SELECT * FROM google_credentials WHERE revoked IS FALSE AND deleted_at IS NULL')
 
   for(const row of g_result.rows) {
-    const record = [row.user, row.brand, row.id, null, 'contacts', null, null]
+    const record = [row.user, row.brand, row.id, null, 'contacts', null, new Date()]
 
     await conn.query('INSERT INTO users_jobs("user", brand, google_credential, microsoft_credential, job_name, status, start_at) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (google_credential, job_name) DO UPDATE SET status = null, start_at = null, updated_at = null', record)
   }
