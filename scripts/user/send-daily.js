9f658d07b535aee9da67d8f985cebfdb71a51d74
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-17 00:48:11 -0700

    email addresses are more convenient

--- scripts/user/send-daily.js
+++ scripts/user/send-daily.js
@@ -7,7 +7,11 @@ const Daily = require('../../lib/models/Daily')
 const Job = require('../../lib/models/Job')
 
 const send = async () => {
-  const { rows } = await Daily.sendForUser(process.argv[2])
+  const user = await User.getByEmail(process.argv[2])
+  if (!user)
+    throw Error.ResourceNotFound()
+
+  const { rows } = await Daily.sendForUser(user.id)
   await Job.handleContextJobs()
   const email = await Email.get(rows[0].email)
   fs.writeFileSync('/tmp/1.html', email.html)
