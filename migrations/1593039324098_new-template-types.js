b3b0d0c2b38f43cab300b4b3116920aefcf2fa86
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-24 16:20:42 -0700

    come on

--- migrations/1593039324098_new-template-types.js
+++ migrations/1593039324098_new-template-types.js
@@ -1,24 +1,24 @@
 const db = require('../lib/utils/db')
 
 const migrations = [
-  'ALTER TYPE template_type ADD VALUE \'FathersDay\'         IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'MothersDay\'         IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'MemorialDay\'        IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Passover\'           IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'ChineseNewYear\'     IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'LaborDay\'           IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Hannukkah\'          IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'FourthOfJuly\'       IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'VeteransDay\'        IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Thanksgiving\'       IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Halloween\'          IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'MLKDay\'             IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'IndependenceDay\'    IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Diwaly\'             IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Rosh Hashanah\'      IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'Kwanzaa\'            IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'WeddingAnniversary\' IF NOT EXISTS',
-  'ALTER TYPE template_type ADD VALUE \'HomeAnniversary\'    IF NOT EXISTS'
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'FathersDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'MothersDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'MemorialDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Passover\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'ChineseNewYear\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'LaborDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Hannukkah\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'FourthOfJuly\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'VeteransDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Thanksgiving\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Halloween\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'MLKDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'IndependenceDay\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Diwaly\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Rosh Hashanah\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'Kwanzaa\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'WeddingAnniversary\'',
+  'ALTER TYPE template_type ADD VALUE IF NOT EXISTS \'HomeAnniversary\''
 ]
 
 
