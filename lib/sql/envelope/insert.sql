69d4050572e8e7bad6e7cc004da71a5453c21c4c
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-24 04:05:01 -0700

    Update the sql

--- lib/sql/envelope/insert.sql
+++ lib/sql/envelope/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO envelopes (created_by, deal, auto_notify, status) VALUES ($1, $2, $3, 'Sent')
+INSERT INTO envelopes (created_by, owner, deal, auto_notify, status) VALUES ($1, $2, $3, $4, 'Sent')
 RETURNING id, webhook_token
