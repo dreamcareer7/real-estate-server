668fe34fd18e772491e6c1b1dea6e01965c78585
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-30 00:20:19 +0430

    [Emails] Upgrade emails_events job, In progress

---
+++ migrations/1593448792350_upgrade_emails_event.js
@@ -0,0 +1,31 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+
+  'ALTER TABLE emails_events ADD COLUMN url         TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN ip          TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN client_os   TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN client_type TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN device      TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN location    JSON NULL',
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
