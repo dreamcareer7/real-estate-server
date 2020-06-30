ac6c82fce94b1cf74b4a65c43c2ab3ccddf1beaf
dreamcareer7 <dreamcareer070@gmail.com>
2020-06-30 18:40:49 +0430

    Upgrade emails_event table

--- migrations/1593448792350_upgrade_emails_event.js
+++ migrations/1593448792350_upgrade_emails_event.js
@@ -3,11 +3,20 @@ const db = require('../lib/utils/db')
 const migrations = [
   'BEGIN',
 
+  `CREATE TYPE public.emails_events_device AS ENUM(
+    'console',
+    'mobile',
+    'tablet',
+    'smarttv',
+    'wearable',
+    'embedded'
+  )`,
+
   'ALTER TABLE emails_events ADD COLUMN url         TEXT NULL',
   'ALTER TABLE emails_events ADD COLUMN ip          TEXT NULL',
   'ALTER TABLE emails_events ADD COLUMN client_os   TEXT NULL',
   'ALTER TABLE emails_events ADD COLUMN client_type TEXT NULL',
-  'ALTER TABLE emails_events ADD COLUMN device      TEXT NULL',
+  'ALTER TABLE emails_events ADD COLUMN device_type public.emails_events_device NULL',
   'ALTER TABLE emails_events ADD COLUMN location    JSON NULL',
 
   'COMMIT'
