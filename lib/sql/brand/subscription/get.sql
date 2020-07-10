4ae50989223ad46f2598dfe81335818835e653f2
dreamcareer7 <dreamcareer070@gmail.com>
2020-07-10 07:21:11 -0700

    Expose more.

--- lib/sql/brand/subscription/get.sql
+++ lib/sql/brand/subscription/get.sql
@@ -1,11 +1,11 @@
 SELECT brands_subscriptions.*,
   chargebee_subscriptions.chargebee_id,
   chargebee_subscriptions.status,
-  chargebee_subscriptions.plan,
   chargebee_subscriptions.chargebee_object->'trial_start' as trial_start,
   chargebee_subscriptions.chargebee_object->'trial_end' as trial_end,
   chargebee_subscriptions.chargebee_object->'billing_period_unit' as billing_period_unit,
   chargebee_subscriptions.chargebee_object->'billing_period' as billing_period,
+  chargebee_subscriptions.plan,
   'brand_subscription' AS TYPE,
   EXTRACT(EPOCH FROM brands_subscriptions.created_at) AS created_at,
   EXTRACT(EPOCH FROM brands_subscriptions.updated_at) AS updated_at
