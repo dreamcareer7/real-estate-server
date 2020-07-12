26811e3418be398afd979dbca31d66e735dee016
dreamcareer7 <dreamcareer070@gmail.com>
2020-07-12 02:19:17 -0700

    Fix the updates and make the code simpler and more robust

--- lib/sql/billing_plan/insert.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO billing_plans(acl, chargebee_id, chargebee_object) VALUES ($1, $2, $3) RETURNING id
--- lib/sql/billing_plan/update.sql
+++
@@ -1,5 +0,0 @@
-UPDATE billing_plans SET
-  acl = $2,
-  chargebee_id = $3,
-  chargebee_object = $4
-WHERE id = $1
