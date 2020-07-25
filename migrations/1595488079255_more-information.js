2da553d92afc781ae39cf234ecde8ae250ec209a
dreamcareer7 <dreamcareer070@gmail.com>
2020-07-24 20:17:22 -0700

    Fix the migration

--- migrations/1595488079255_more-information.js
+++ migrations/1595488079255_more-information.js
@@ -1,7 +1,7 @@
 const db = require('../lib/utils/db')
 
 const Envelope = require('../lib/models/Envelope')
-const Content = require('../lib/models/Context')
+const Context = require('../lib/models/Context')
 
 const createContext = (conn, cb) => {
   const context = Context.create()
