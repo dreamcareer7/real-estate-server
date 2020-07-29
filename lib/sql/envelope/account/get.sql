c2b5ff3d6114bbadd18477322ee0eae9c45351da
dreamcareer7 <dreamcareer070@gmail.com>
2020-07-29 13:04:10 -0700

    for web#4407

---
+++ lib/sql/envelope/account/get.sql
@@ -0,0 +1,10 @@
+SELECT 'docusign_account' AS type,
+  EXTRACT(EPOCH FROM created_at)   AS created_at,
+  EXTRACT(EPOCH FROM updated_at)   AS updated_at,
+
+  first_name,
+  last_name,
+  email
+FROM docusign_users
+JOIN unnest($1::uuid[]) WITH ORDINALITY t(uid, ord) ON docusign_users.id = uid
+ORDER BY t.ord
