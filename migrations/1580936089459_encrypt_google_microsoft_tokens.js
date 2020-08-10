3e26856d6aa7b3c717ff0d725333f73bdc1403f5
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-10 12:28:50 -0700

    new Buffer is now deprecated.

--- migrations/1580936089459_encrypt_google_microsoft_tokens.js
+++ migrations/1580936089459_encrypt_google_microsoft_tokens.js
@@ -11,8 +11,8 @@ const encryptTokens = async (tokens) => {
   const decrypted_aToken = Crypto.decrypt(tokens.access_token)
   const decrypted_rToken = Crypto.decrypt(tokens.refresh_token)
 
-  promises.push(KMS.encrypt(new Buffer(decrypted_aToken, 'utf-8')))
-  promises.push(KMS.encrypt(new Buffer(decrypted_rToken, 'utf-8')))
+  promises.push(KMS.encrypt(Buffer.from(decrypted_aToken, 'utf-8')))
+  promises.push(KMS.encrypt(Buffer.from(decrypted_rToken, 'utf-8')))
 
   const result = await Promise.all(promises)
 
