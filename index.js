a02d230822434e08cc97ea19f9ba2bd931b80c06
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-16 20:55:02 -0700

    Use pm2 to cluster.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const throng = require('throng')
 const migrate = require('./lib/utils/migrate.js')
 const config = require('./lib/config.js')
 
@@ -7,22 +6,10 @@ const {app, start} = require('./lib/bootstrap.js')
 require('./lib/utils/logger.js')(app)
 require('./lib/utils/atomic.js')(app)
 
-const cluster = () => {
-  throng({
-    workers: config.cluster.workers,
 
-    start: id => {
-      start(config.http.port)
-      console.log(`Listening (${id}) on http://localhost:${config.http.port}`)
-    }
-  })
-}
-
-const normal = () => {
+const start = () => {
   start(config.http.port)
   console.log(`Listening on http://localhost:${config.http.port}`)
 }
-
-const starter = process.argv.includes('cluster') ? cluster : normal
 
 migrate(starter)
