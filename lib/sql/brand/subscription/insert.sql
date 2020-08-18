caa9d8171d72f0089cc03398dde7caaa52ea14af
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-18 11:50:43 -0700

    God Bless Tests.

--- lib/sql/brand/subscription/insert.sql
+++ lib/sql/brand/subscription/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO brands_subscriptions
-(created_within, brand, plan, customer, status, chargebee_id, chargebee_object)
+(created_within, brand, plan, customer, status, plan_quantity, chargebee_id, chargebee_object)
 VALUES
-($1, $2, $3, $4, $5, $6, $7)
+($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING id
