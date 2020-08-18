3d8ff8090465c7949b692fe6df334ea2f7acf3ea
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-18 04:06:39 -0700

    Proper entity name

--- lib/sql/brand/subscription/get.sql
+++ lib/sql/brand/subscription/get.sql
@@ -1,5 +1,5 @@
 SELECT brands_subscriptions.*,
-  'chargebee_subscription' AS TYPE,
+  'brand_subscription' AS TYPE,
   EXTRACT(EPOCH FROM created_at) AS created_at,
   EXTRACT(EPOCH FROM updated_at) AS updated_at
 
