a8179278f4ccc2b9d7c7faf4d27b5aebf9df9811
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-18 05:10:39 -0700

    Correct the query.

--- lib/sql/brand/subscription/by-brand.sql
+++ lib/sql/brand/subscription/by-brand.sql
@@ -1,2 +1,3 @@
 SELECT * FROM brands_subscriptions
 WHERE brand = $1 AND deleted_at IS NULL
+AND   status IN('active', 'in_trial')
--- lib/sql/brand/subscription/find.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM brands_subscriptions
-WHERE customer = $1
-AND   plan     = $2
-AND   status IN('active', 'in_trial')
