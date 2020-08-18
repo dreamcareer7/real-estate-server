612466f134cb8e06b988f854beefd03b9307d462
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-18 11:12:32 -0700

    Expose plan_quantity

--- lib/sql/brand/subscription/get.sql
+++ lib/sql/brand/subscription/get.sql
@@ -1,7 +1,12 @@
 SELECT brands_subscriptions.*,
   'brand_subscription' AS TYPE,
   EXTRACT(EPOCH FROM created_at) AS created_at,
-  EXTRACT(EPOCH FROM updated_at) AS updated_at
+  EXTRACT(EPOCH FROM updated_at) AS updated_at,
+  plan,
+  customer,
+  status,
+  plan_quantity,
+  chargebee_id
 
 FROM brands_subscriptions
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(bsid, ord) ON brands_subscriptions.id = bsid
