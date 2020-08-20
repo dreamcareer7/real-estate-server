d50b749ed8124bf09ee5d7abbcd0fa75d335f5ba
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-20 12:04:32 -0700

    Expose end date

--- lib/sql/brand/subscription/get.sql
+++ lib/sql/brand/subscription/get.sql
@@ -6,7 +6,8 @@ SELECT brands_subscriptions.*,
   customer,
   status,
   plan_quantity,
-  chargebee_id
+  chargebee_id,
+  (chargebee_obect->'trial_end')::int as trial_ends_at
 
 FROM brands_subscriptions
 JOIN unnest($1::uuid[]) WITH ORDINALITY t(bsid, ord) ON brands_subscriptions.id = bsid
