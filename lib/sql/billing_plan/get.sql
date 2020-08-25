61fc1a4aa38407a1a8931cc036f426963f816eb8
dreamcareer7 <dreamcareer070@gmail.com>
2020-08-25 02:24:50 -0700

    pf

--- lib/sql/billing_plan/get.sql
+++ lib/sql/billing_plan/get.sql
@@ -5,8 +5,8 @@ SELECT
   chargebee_object->>'name' as name,
   chargebee_object->>'description' as description,
   chargebee_object->>'currency_code' as currency_code,
-  chargebee_object->>'price'::int as price,
-  chargebee_object->>'period'::int as period,
+  (chargebee_object->>'price')::int as price,
+  (chargebee_object->>'period')::int as period,
   chargebee_object->>'period_unit' as period_unit,
   EXTRACT(EPOCH FROM created_at) AS created_at,
   EXTRACT(EPOCH FROM updated_at) AS updated_at
