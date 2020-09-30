6c1e602361fe2f63bc0084a2c266487fc920eeb7
dreamcareer7 <dreamcareer070@gmail.com>
2020-09-30 10:54:21 -0700

    Scripts to generate links.

---
+++ scripts/template/sms/listing.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const Context = require('../../../lib/models/Context')
+const db = require('../../../lib/utils/db')
+const User = require('../../../lib/models/User')
+const Listing = require('../../../lib/models/Listing/get')
+
+const getDb = async () => {
+  return new Promise((resolve, reject) => {
+    db.conn((err, client) => {
+      if (err)
+        return reject(err)
+
+      resolve(client)
+    })
+  })
+}
+
+
+const run = async () => {
+  const context = Context.create()
+  context.set({
+    db: await getDb()
+  })
+  context.enter()
+
+  const user = await User.getByEmail(process.argv[2])
+  const listing = await Listing.getByMLSNumber(process.argv[3])
+
+  const loginUrl = await User.getLoginLink({
+    user,
+    options: {
+      action: 'OpenMarketingWizard',
+      listing: listing.id,
+    }
+  })
+
+  console.log(loginUrl)
+
+}
+
+run()
+  .then(process.exit)
+  .catch(e => {
+    console.log(e)
+    process.exit()
+  })
