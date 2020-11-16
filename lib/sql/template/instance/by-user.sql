ff7ee0e8295740fd3139b97e79ded88728df36ea
dreamcareer7 <dreamcareer070@gmail.com>
2020-11-16 12:08:44 -0800

    Limit 0 means zero. Limit NULL means all.

--- lib/sql/template/instance/by-user.sql
+++ lib/sql/template/instance/by-user.sql
@@ -1,6 +1,6 @@
 SELECT * FROM templates_instances
-WHERE created_by = $1
+WHERE created_by = 'ca2933ea-9d82-11e5-b8da-f23c91c841bd'
 AND deleted_at IS NULL
 ORDER BY created_at DESC
-LIMIT COALESCE($2, 0)
+LIMIT $2
 OFFSET COALESCE($3, 0)
