b133e97cdeccb8a0dcbe50e8d592aa8a6878eec3
dreamcareer7 <dreamcareer070@gmail.com>
2020-12-08 12:44:07 -0800

    phew! sorry!

--- lib/sql/template/instance/by-user.sql
+++ lib/sql/template/instance/by-user.sql
@@ -5,8 +5,8 @@ FROM templates_instances
 JOIN templates ON templates.id = templates_instances.template
 WHERE templates_instances.created_by = $1
 AND templates_instances.deleted_at IS NULL
-AND $2::template_type[] IS NULL OR templates.template_type = ANY($2)
-AND $3::template_medium[] IS NULL OR templates.medium = ANY($3)
+AND ($2::template_type[] IS NULL OR templates.template_type = ANY($2))
+AND ($3::template_medium[] IS NULL OR templates.medium = ANY($3))
 ORDER BY templates_instances.created_at DESC
 LIMIT $4
 OFFSET COALESCE($5, 0)
