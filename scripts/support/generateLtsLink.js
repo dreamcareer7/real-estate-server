edbec1f6d51def96f94eb3a697c141bb8c015fc6
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-20 16:44:19 +0330

    lts: generate link support script

---
+++ scripts/support/generateLtsLink.js
@@ -0,0 +1,17 @@
+const b64 = require('base64url').default
+const Crypto = require('../../lib/models/Crypto')
+const Url = require('../../lib/models/Url')
+
+// eslint-disable-next-line no-unused-vars
+function generateLtsLink({
+  brand,
+  user,
+  protocol = 'LeadTransmissionProtocol',
+  mls = 'NTREIS',
+  source,
+}) {
+  const payload = JSON.stringify({ brand, user, protocol, mls, source })
+  const encrypted = Crypto.encrypt(payload)
+
+  return Url.api({ uri: b64.encode(encrypted) })
+}
