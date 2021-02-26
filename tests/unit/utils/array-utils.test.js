26dedf3418668a4be266fce1cb8d63ef0fd362a5
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-26 16:14:15 +0330

    feat(test): add test cases for array-utils

---
+++ tests/unit/utils/array-utils.test.js
@@ -0,0 +1,7 @@
+describe('utils/array-utils', () => {
+  describe('.haveSameMembers()', () => {
+    it('needs two required arguments')
+    it('returns true when the arguments have same members')
+    it('return false when there\'s some difference(s) between the arrays')
+  })
+})
