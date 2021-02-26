52ea5ba711d50f1f8e3190726801423bd9329273
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-26 16:35:09 +0330

    feat(test): impl. test cases for `haveSameMembers`

--- tests/unit/utils/array-utils.test.js
+++ tests/unit/utils/array-utils.test.js
@@ -1,7 +1,26 @@
+const { expect } = require('chai')
+const { haveSameMembers } = require('../../../lib/utils/array-utils')
+
 describe('utils/array-utils', () => {
   describe('.haveSameMembers()', () => {
-    it('needs two required arguments')
-    it('returns true when the arguments have same members')
-    it('return false when there\'s some difference(s) between the arrays')
+    it('needs two required arguments', () => {
+      expect(haveSameMembers).to.have.lengthOf(2)
+    })
+    
+    it('returns true when the arguments have same members', () => {
+      const obj1 = { baz: ['qux', 'nux'], qux: { bar: 'foo' }, foo: 'bar' }
+      const obj2 = { foo: 'bar', baz: ['qux', 'nux'], qux: { bar: 'foo' } }
+      
+      const arr1 = [1, '3', 'foo', null, undefined, true, 4n, obj1]
+      const arr2 = [undefined, true, 'foo', obj2, 4n, 1, '3', null]
+
+      expect(haveSameMembers(arr1, arr2)).to.be.true
+    })
+    
+    it('return false when there\'s some difference(s) between the arrays', () => {
+      expect(haveSameMembers(['1'], [1])).to.be.false
+      expect(haveSameMembers([1n], [1])).to.be.false
+      expect(haveSameMembers([null], [undefined])).to.be.false
+    })
   })
 })
