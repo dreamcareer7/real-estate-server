743eadc74c26dbe7ca4558a077a1f18619dc2f59
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-26 00:26:01 -0800

    Form Libraries

--- lib/sql/form/by-brand.sql
+++ lib/sql/form/by-brand.sql
@@ -1,10 +1,18 @@
-SELECT id
+SELECT forms.id, form_libraries.name as library
 FROM forms
-WHERE deleted_at IS NULL
+JOIN form_libraries ON forms.library = form_libraries.id
+LEFT JOIN form_libraries_brands flb ON form_libraries.id = flb.library AND flb.brand = $1
+WHERE forms.deleted_at IS NULL
 AND (
-  brand IS NULL
-  OR brand IN(
-    SELECT brand_parents($1::uuid)
+  (
+    forms.brand IS NULL
+    OR forms.brand IN (
+      SELECT brand_parents($1::uuid)
+    )
+  )
+  OR
+  (
+    flb.id IS NOT NULL
   )
 )
-ORDER BY name ASC
+ORDER BY forms.name ASC
