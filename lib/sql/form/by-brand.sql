4d89f27e262dd0ee4879c00f5d7f7e471a13c95f
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-26 00:54:30 -0800

    Fix Da Query

--- lib/sql/form/by-brand.sql
+++ lib/sql/form/by-brand.sql
@@ -1,18 +1,24 @@
-SELECT forms.id, form_libraries.name as library
-FROM forms
-JOIN form_libraries ON forms.library = form_libraries.id
-LEFT JOIN form_libraries_brands flb ON form_libraries.id = flb.library AND flb.brand = $1
-WHERE forms.deleted_at IS NULL
-AND (
-  (
-    forms.brand IS NULL
-    OR forms.brand IN (
-      SELECT brand_parents($1::uuid)
+WITH f AS (
+  SELECT DISTINCT forms.id, forms.name
+  FROM forms
+  LEFT JOIN form_libraries ON forms.library = form_libraries.id
+  LEFT JOIN form_libraries_brands flb ON form_libraries.id = flb.library
+  WHERE forms.deleted_at IS NULL
+  AND (
+    (
+      forms.brand IS NULL
+      OR forms.brand IN (
+        SELECT brand_parents($1::uuid)
+      )
+    )
+    OR
+    (
+      flb.id IS NOT NULL AND (
+        flb.brand IN (
+          SELECT brand_parents($1::uuid)
+        )
+      )
     )
   )
-  OR
-  (
-    flb.id IS NOT NULL
-  )
 )
-ORDER BY forms.name ASC
+SELECT * FROM f ORDER BY name ASC
