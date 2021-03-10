0daab38a9d1a506d1ea969dcd127edc239a47157
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-10 20:48:34 +0330

    test(microsoft): add test-cases related to `chooseAddressAmongAttributes`

---
+++ tests/unit/microsoft/generate-m-contacts.test.js
@@ -0,0 +1,17 @@
+const { expect, assert } = require('chai')
+
+const { generateMContacts } = require('../../../lib/models/Microsoft/workers/contacts/people/helpers/attributes')
+
+describe('Microsoft', () => {
+  describe('Contact Attributes', () => {
+    describe('.normzlizeAttributes()')
+    
+    describe('.chooseAddressAmongAttributes()', () => {
+      it('returns null when attributes are empty')
+      it('maps rechat-address props. to microsoft address props.')
+      it('prefers to choose a primary address with highest index...')
+    })
+
+    describe('.generateMContacts()')
+  })
+})
