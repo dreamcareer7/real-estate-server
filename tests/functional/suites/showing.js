a9205f86c7389b7385bf48c845f4bce3cd05181b
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-18 23:30:02 +0330

    lint

--- tests/functional/suites/showing.js
+++ tests/functional/suites/showing.js
@@ -1,13 +1,6 @@
 const merge = require('deepmerge')
 
-registerSuite('brand', [
-  'createParent',
-  'attributeDefs',
-  'createBrandLists',
-  'create',
-  'addRole',
-  'addMember'
-])
+registerSuite('brand', ['createParent', 'attributeDefs', 'createBrandLists', 'create', 'addRole', 'addMember'])
 
 registerSuite('user', ['create', 'upgradeToAgentWithEmail', 'markAsNonShadow'])
 registerSuite('listing', ['getListing'])
@@ -16,26 +9,30 @@ function _create(description, override, cb) {
   /** @type {import("../../../lib/models/Showing/showing/types").ShowingInput} */
   const showing = {
     approval_type: 'All',
-    availabilities: [{
-      weekday: 'Monday',
-      availability: [7 * 60, 10 * 60]
-    }],
+    availabilities: [
+      {
+        weekday: 'Monday',
+        availability: [7 * 60, 10 * 60],
+      },
+    ],
     duration: 15 * 60,
-    roles: [{
-      brand: results.brand.create.data.id,
-      can_approve: true,
-      role: 'SellerAgent',
-      cancel_notification_type: ['email'],
-      confirm_notification_type: ['push', 'email'],
-      first_name: 'John',
-      last_name: 'Doe',
-      email: 'john@doe.com',
-      phone_number: '+15551523652',
-      user: results.authorize.token.data.id
-    }],
+    roles: [
+      {
+        brand: results.brand.create.data.id,
+        can_approve: true,
+        role: 'SellerAgent',
+        cancel_notification_type: ['email'],
+        confirm_notification_type: ['push', 'email'],
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john@doe.com',
+        phone_number: '+15551523652',
+        user: results.authorize.token.data.id,
+      },
+    ],
     start_date: new Date().toISOString(),
     listing: results.listing.getListing.data.id,
-    notice_period: 3 * 3600
+    notice_period: 3 * 3600,
   }
 
   return frisby
@@ -47,33 +44,39 @@ function _create(description, override, cb) {
 }
 
 function create(cb) {
-  return _create('create a showing', {}, function(err, res, json) {
-      const setup = frisby.globalSetup()
+  return _create('create a showing', {}, function (err, res, json) {
+    const setup = frisby.globalSetup()
 
-      setup.request.headers['X-RECHAT-BRAND'] = results.brand.create.data.id
-      setup.request.headers['x-handle-jobs'] = 'yes'
+    setup.request.headers['X-RECHAT-BRAND'] = results.brand.create.data.id
+    setup.request.headers['x-handle-jobs'] = 'yes'
 
-      frisby.globalSetup(setup)
+    frisby.globalSetup(setup)
 
-      cb(err, res, json)
-    })
+    cb(err, res, json)
+  })
     .expectStatus(200)
     .expectJSON({
-      data: {}
+      data: {},
     })
 }
 
 function createWithValidationError(cb) {
-  return _create('create a showing with invalid data', {
-    availabilities: [{
-      weekday: 'Monday',
-      availability: [7 * 60, 10 * 60]
-    }, {
-      weekday: 'Monday',
-      availability: [7 * 60, 10 * 60]
-    }],
-  }, cb)
-  .expectStatus(400)
+  return _create(
+    'create a showing with invalid data',
+    {
+      availabilities: [
+        {
+          weekday: 'Monday',
+          availability: [7 * 60, 10 * 60],
+        },
+        {
+          weekday: 'Monday',
+          availability: [7 * 60, 10 * 60],
+        },
+      ],
+    },
+    cb
+  ).expectStatus(400)
 }
 
 module.exports = {
