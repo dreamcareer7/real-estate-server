9611b082e6bad8fbc56de218ee4ae52ee0c68b52
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-21 01:17:44 -0700

    Cloudflare is my best friend now.

--- lib/sql/godaddy/domain/save.sql
+++ lib/sql/godaddy/domain/save.sql
@@ -1,3 +1,3 @@
 INSERT INTO godaddy_domains
-(name, owner, order_id, hosted_zone) VALUES ($1, $2, $3, $4)
+(name, owner) VALUES ($1, $2)
 RETURNING id
---
+++ lib/sql/godaddy/domain/set-order.sql
@@ -0,0 +1,1 @@
+UPDATE godaddy_domains SET "order" = $2 WHERE id = $1
