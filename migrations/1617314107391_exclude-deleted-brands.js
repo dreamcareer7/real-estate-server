5dd0b39d2abb9b089f2e15d598ab6eebe8c2febc
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-01 17:22:57 -0700

    Prevent access for office double ender deals

--- migrations/1617314107391_exclude-deleted-brands.js
+++ migrations/1617314107391_exclude-deleted-brands.js
@@ -1,4 +1,8 @@
-CREATE OR REPLACE FUNCTION user_brands("user" uuid, roles text[]) RETURNS TABLE(
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  `CREATE OR REPLACE FUNCTION user_brands("user" uuid, roles text[]) RETURNS TABLE(
   brand uuid
 )
 AS
@@ -20,4 +24,23 @@ $$
 
 SELECT brand_children(roles.brand) FROM roles
 $$
-LANGUAGE sql;
+LANGUAGE sql;`,
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
