4521c578a0edfffb52bb27c7e13bda34d8711532
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-01 18:15:59 -0700

    Fix.

--- lib/sql/deal/has-access.sql
+++ lib/sql/deal/has-access.sql
@@ -5,7 +5,17 @@ WITH user_brands AS (
 SELECT
   count(*) > 0 AS has_access
 FROM deals
-LEFT JOIN deals_roles      ON deals.id = deals_roles.deal
+  LEFT JOIN deals_roles ON deals.id = deals_roles.deal AND (
+  (
+    deals.deal_type = 'Selling' AND
+    NOT (deals_roles.role IN('BuyerAgent', 'CoBuyerAgent'))
+  )
+  OR
+  (
+    deals.deal_type = 'Buying'  AND
+    NOT (deals_roles.role IN ('SellerAgent'::deal_role, 'CoSellerAgent'::deal_role))
+  )
+)
 LEFT JOIN deals_checklists ON deals_roles.checklist = deals_checklists.id
 WHERE
   deals.id = $1
