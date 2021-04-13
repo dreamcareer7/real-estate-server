82f871b7042196497d0071c575a95d551803ff9e
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-13 14:07:07 -0700

    Yup

---
+++ migrations/1618348008791_forgotten-migration.js
@@ -0,0 +1,48 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  `CREATE OR REPLACE VIEW brands_relations AS (
+  SELECT id,
+  (
+    SELECT
+      b.name
+    FROM
+      brands as b
+      JOIN brand_parents(brands.id) bp(id) using (id)
+    WHERE
+      b.brand_type = 'Office'
+    LIMIT 1
+  ) AS office,
+
+  (
+    SELECT
+      b.name
+    FROM
+      brands as b
+      JOIN brand_parents(brands.id) bp(id) using (id)
+    WHERE
+      b.brand_type = 'Region'
+    LIMIT 1
+  ) AS region
+
+  FROM
+    brands
+)`
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
