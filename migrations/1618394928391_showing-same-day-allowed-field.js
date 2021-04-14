3844e0396d23a826d8b3a530cc62cc96eeba6f40
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-14 16:03:21 +0430

    showing: fix same_day migration

--- migrations/1618394928391_showing-same-day-allowed-field.js
+++ migrations/1618394928391_showing-same-day-allowed-field.js
@@ -3,8 +3,9 @@ const db = require('../lib/utils/db')
 const migrations = [
   'BEGIN',
   'ALTER TABLE showings ALTER COLUMN notice_period DROP NOT NULL',
-  'ALTER TABLE showings ADD COLUMN same_day_allowed boolean NOT NULL',
+  'ALTER TABLE showings ADD COLUMN same_day_allowed boolean NOT NULL DEFAULT TRUE',
   'ALTER TABLE showings ADD CONSTRAINT s_same_day CHECK ((same_day_allowed IS FALSE AND notice_period IS NULL) OR (same_day_allowed IS TRUE AND notice_period IS NOT NULL))',
+  'ALTER TABLE showings ALTER COLUMN DROP DEFAULT',
   'COMMIT'
 ]
 
