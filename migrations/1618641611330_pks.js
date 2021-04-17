3d11b0231e5d1f804d95065b4c1985e86378f3db
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-17 00:19:28 -0700

    One last Pk

--- migrations/1618641611330_pks.js
+++ migrations/1618641611330_pks.js
@@ -5,7 +5,6 @@ const migrations = [
   'ALTER TABLE password_recovery_records ADD PRIMARY KEY (id)',
   'ALTER TABLE phone_verifications ADD PRIMARY KEY (id)',
   'ALTER TABLE recommendations_eav ADD PRIMARY KEY (id)',
-  'ALTER TABLE state_codes ADD COLUMN id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY',
   'ALTER TABLE websites_hostnames ADD PRIMARY KEY (id)',
   'COMMIT'
 ]
--- migrations/1618643847564_pk.js
+++ migrations/1618643847564_pk.js
@@ -1,4 +1,9 @@
-CREATE OR REPLACE FUNCTION update_current_deal_context(deal_id uuid)
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+
+  `CREATE OR REPLACE FUNCTION update_current_deal_context(deal_id uuid)
 RETURNS void AS
 $$
   BEGIN
@@ -107,4 +112,27 @@ $$
 
   END;
 $$
-LANGUAGE PLPGSQL;
+LANGUAGE PLPGSQL;`,
+
+  'ALTER TABLE current_deal_context ALTER id SET DEFAULT uuid_generate_v4()',
+  'ALTER TABLE current_deal_context ADD PRIMARY KEY (id)',
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
