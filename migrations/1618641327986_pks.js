4337576faed1d7b84bd7ecc715e01dd9399953e2
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-16 23:38:24 -0700

    Another set

---
+++ migrations/1618641327986_pks.js
@@ -0,0 +1,30 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  'ALTER TABLE brands_offices ADD PRIMARY KEY (id)',
+  'ALTER TABLE default_brands ADD PRIMARY KEY (id)',
+  'ALTER TABLE email_verifications ADD PRIMARY KEY (id)',
+  'ALTER TABLE godaddy_domains ADD PRIMARY KEY (id)',
+  'ALTER TABLE migrations ADD COLUMN id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY',
+  'ALTER TABLE mls_jobs ADD PRIMARY KEY (id)',
+  'ALTER TABLE open_house ADD PRIMARY KEY (id)',
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
