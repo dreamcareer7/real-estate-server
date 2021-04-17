2d13efda231b134189a34157966530c47f98df6f
dreamcareer7 <dreamcareer070@gmail.com>
2021-04-16 23:14:52 -0700

    PK

---
+++ migrations/1618640001141_pks.js
@@ -0,0 +1,22 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'ALTER TABLE mls_data SET PRIMARY KEY (id)'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
