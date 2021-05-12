25624a44b7c8e80afe32662d5aa12f7cfaf24ad9
dreamcareer7 <dreamcareer070@gmail.com>
2021-05-11 17:54:03 -0700

    Fix da script

--- scripts/mls/notify.js
+++ scripts/mls/notify.js
@@ -1,19 +1,14 @@
 #!/usr/bin/env node
 
 const { justListed, openHouse, priceImprovement } = require('../../lib/models/Listing/notify-agents')
-const Listing = require('../../lib/models/Listing/get')
-const promisify = require('../../lib/utils/promisify')
 const createContext = require('../workers/create-context')
 
 const send = async () => {
-  const listing = await promisify(Listing.get)(process.argv[2])
+  const id = process.argv[2]
 
-  if (!listing)
-    throw Error.ResourceNotFound()
-
-  await justListed(listing)
-  await priceImprovement(listing)
-  await openHouse(listing)
+  await justListed(id)
+  await priceImprovement(id)
+  await openHouse(id)
 }
 
 async function main() {
