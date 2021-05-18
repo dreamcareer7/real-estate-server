2b47acb5f42f0ab17c498c85480f469ac4449527
dreamcareer7 <dreamcareer070@gmail.com>
2021-05-18 17:22:28 +0430

    showing: fix notifications endpoint

--- lib/sql/showing/showing/notifications.sql
+++ lib/sql/showing/showing/notifications.sql
@@ -1,9 +1,15 @@
 SELECT
   id, (COUNT(*) OVER ())::int AS total
 FROM
-  unread_notifications
+  unread_notifications un
+  JOIN showing_appointments AS a
+    ON un.object = a.id
+  JOIN showings AS s
+    ON a.showing = s.id
 WHERE
   "user" = $1::uuid
+  AND object_class = 'ShowingAppointment'
+  AND s.deleted_at IS NULL
 ORDER BY
   created_at
 LIMIT $2
