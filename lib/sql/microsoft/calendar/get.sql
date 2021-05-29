be4bf3797f7739a7272d5f3d66917872468902b4
dreamcareer7 <dreamcareer070@gmail.com>
2021-05-29 18:43:42 +0200

    [Microsoft] Debug sync process

--- lib/sql/microsoft/calendar/get.sql
+++ lib/sql/microsoft/calendar/get.sql
@@ -3,8 +3,8 @@ SELECT
 FROM
   microsoft_calendars
 JOIN 
-  unnest($1::uuid[]) WITH ORDINALITY t(gcid, ord)
+  unnest($1::uuid[]) WITH ORDINALITY t(mcid, ord)
 ON 
-  microsoft_calendars.id = gcid
+  microsoft_calendars.id = mcid
 ORDER BY 
   microsoft_calendars.created_at DESC
