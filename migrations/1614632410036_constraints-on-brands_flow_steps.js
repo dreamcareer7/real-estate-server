bbfea0eb6786862a1a55a2adc07ec59b791f13ae
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-01 15:03:38 +0430

    flow: calculate flow step orders

--- migrations/1614632410036_constraints-on-brands_flow_steps.js
+++ migrations/1614632410036_constraints-on-brands_flow_steps.js
@@ -2,6 +2,22 @@ const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
+  `WITH orders AS (
+    SELECT
+      id,
+      row_number() OVER (partition by flow order by wait_for) AS "order"
+    FROM
+      brands_flow_steps
+  )
+  UPDATE
+    brands_flow_steps
+  SET
+    "order" = orders.order
+  FROM
+    orders
+  WHERE
+    brands_flow_steps.id = orders.id
+  `,
   'ALTER TABLE brands_flow_steps ADD CONSTRAINT brands_flow_steps_unique_step_order UNIQUE (flow, "order") DEFERRABLE INITIALLY IMMEDIATE',
   'ALTER TABLE brands_flow_steps ALTER "order" SET NOT NULL',
   'COMMIT'
