b44d11314005049d27cc8f01e3951dbdae2258b5
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-10 15:20:16 +0430

    showing: delete the damn availabilities

--- lib/sql/showing/availability/update.sql
+++ lib/sql/showing/availability/update.sql
@@ -1,19 +1,28 @@
-WITH d AS MATERIALIZED (
+WITH d AS (
   DELETE FROM
     showings_availabilities
   WHERE
     showing = $1::uuid
+  RETURNING
+    id
+), i AS (
+  INSERT INTO showings_availabilities (
+    showing,
+    weekday,
+    availability
+  ) SELECT
+    $1::uuid,
+    sr.weekday,
+    sr.availability::int4range
+  FROM
+    json_to_recordset($2::json) AS sr (
+      weekday iso_day_of_week,
+      availability int4range
+    )
+  RETURNING
+    id
 )
-INSERT INTO showings_availabilities (
-  showing,
-  weekday,
-  availability
-) SELECT
-  $1::uuid,
-  sr.weekday,
-  sr.availability::int4range
+SELECT
+  1
 FROM
-  json_to_recordset($2::json) AS sr (
-    weekday iso_day_of_week,
-    availability int4range
-  )
+  d, i
