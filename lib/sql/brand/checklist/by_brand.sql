02e1f801661eca188ce11b4d9b3bd3c47805826d
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-13 11:49:18 -0700

    Sorry

--- lib/sql/brand/checklist/by_brand.sql
+++ lib/sql/brand/checklist/by_brand.sql
@@ -1,5 +1,5 @@
-# This query returns all brand checklist for a brand and all it's parent's.
-# Parents should come last. BrandChecklist.createWithTasks depends on that.
+-- This query returns all brand checklist for a brand and all it's parent's.
+-- Parents should come last. BrandChecklist.createWithTasks depends on that.
 
 SELECT id FROM brands_checklists 
 JOIN brand_parents($1) WITH ORDINALITY AS b(i,n) ON brands_checklists.brand = b.i 
