21a560ae32b942523f3c72110da6368f7648193e
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-15 16:14:58 +0430

    misc: resolve https://gitlab.com/rechat/server/-/merge_requests/166#note_601454319

--- lib/sql/showing/appointment/recently_done.sql
+++ lib/sql/showing/appointment/recently_done.sql
@@ -4,9 +4,5 @@ SELECT
 FROM
   showings_appointments
 WHERE
-  status IN (
-    'Confirmed'::showing_appointment_status,
-    'Requested'::showing_appointment_status,
-    'Rescheduled'::showing_appointment_status
-  ) AND
-  time < now() - interval $1;
+  status = ANY('{Confirmed,Requested,Rescheduled}'::showing_appointment_status[])
+  AND time < now() - interval $1;
