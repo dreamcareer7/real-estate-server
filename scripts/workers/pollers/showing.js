7cc923d84f4bad3871f4ac30e72b5236d92c226c
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-15 16:11:53 +0430

    misc: resolve https://gitlab.com/rechat/server/-/merge_requests/166#note_601454312

--- scripts/workers/pollers/showing.js
+++ scripts/workers/pollers/showing.js
@@ -1,10 +1,10 @@
-const { finalizeRecentlyDone } = require('../../../lib/models/Showing/workers/finalize_recently_done')
+const { finalizeRecentlyDone } = require('../../../lib/models/Showing/appointment/poller')
 const { poll, shutdown } = require('../utils/poll')
 
 function start () {
   poll({
     fn: finalizeRecentlyDone,
-    name: 'Showing.finalizeRecentlyDone',
+    name: 'Showing.appointment.finalizeRecentlyDone',
     wait: 15 * 60000 // 15 min
   })
 }
