739a5c51a8373a7bc5b4f83564cb632f7059b988
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-16 22:26:33 +0430

    feat: Support other combinations of statuses
    Refs #1760

--- lib/sql/showing/appointment/recently_done.sql
+++ lib/sql/showing/appointment/recently_done.sql
@@ -4,5 +4,5 @@ SELECT
 FROM
   showings_appointments
 WHERE
-  status = ANY('{Confirmed,Requested,Rescheduled}'::showing_appointment_status[])
-  AND time < now() - interval $1;
+  status = ANY('{$1}'::showing_appointment_status[])
+  AND time < now() - interval $2;
