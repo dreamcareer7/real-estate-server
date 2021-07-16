c6591b7192b57a9425343f9ffe96e40d8eeedf23
dreamcareer7 <dreamcareer070@gmail.com>
2021-07-16 17:04:52 +0430

    feat(sql): order unread notifications by created_at
    Now, we'll get older notifications first.
    Refs #1785

--- lib/sql/notification/unread_over_time.sql
+++ lib/sql/notification/unread_over_time.sql
@@ -2,4 +2,5 @@ SELECT id FROM unread_notifications
 WHERE
   deleted_at IS NULL AND
   created_at BETWEEN now() - $2::interval AND now() - $3::interval AND
-  object_class = $1;
+  object_class = $1
+ORDER BY created_at ASC, updated_at ASC;
