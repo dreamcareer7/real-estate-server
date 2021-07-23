e3e852a1b7ce720fdb3c2079ae63d94c6737ac1c
dreamcareer7 <dreamcareer070@gmail.com>
2021-07-23 17:00:35 +0430

    feat(tests): functional test for finalizeRecentlyDone

--- tests/functional/pollers.js
+++ tests/functional/pollers.js
@@ -1,7 +1,9 @@
 const EmailCampaign = require('../../lib/models/Email/campaign/due')
+const ShowingAppointment = require('../../lib/models/Showing/appointment/poller')
 
 const pollers = {
-  'EmailCampaign.sendDue': EmailCampaign.sendDue
+  'EmailCampaign.sendDue': EmailCampaign.sendDue,
+  'Showing.appointment.finalizeRecentlyDone': ShowingAppointment.finalizeRecentlyDone,
 }
 
 module.exports = pollers
