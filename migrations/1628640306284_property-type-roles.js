f7c572af9e97aac5b25228194e21b7eb78849116
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-15 15:05:06 -0700

    Migration to implement the same logic

--- migrations/1628640306284_property-type-roles.js
+++ migrations/1628640306284_property-type-roles.js
@@ -1,9 +1,88 @@
 const db = require('../lib/utils/db')
 
+//                                 Lease           Sale
+// Buyer                                            X
+// BuyerAgent                       X               X
+// CoBuyerAgent                     O               O                  
+// BuyerPowerOfAttorney                             O
+// BuyerLawyer                                      O
+// BuyerReferral                                    O
+
+// Seller                                           X
+// SellerAgent                      X               X
+// CoSellerAgent                    O               O
+// SellerPowerOfAttorney                            O
+// SellerLawyer                                     O
+// SellerReferral                                   O
+
+// Title                                            O
+// Lender                                           O
+// Appraiser                                        O
+
+
+// Tenant                           X
+// TenantPowerOfAttorney            O
+
+// Landlord                         X
+// LandlordPowerOfAttorney          O
+
+// Lawyer                           O               O
+// TeamLead                         O               O
+
 const migrations = [
   'BEGIN',
   'ALTER TABLE brands_property_types ADD required_roles deal_role[]',
   'ALTER TABLE brands_property_types ADD optional_roles deal_role[]',
+
+  `UPDATE brands_property_types SET 
+    required_roles = ARRAY[
+      'Buyer', 
+      'BuyerAgent',
+
+      'Seller',
+      'SellerAgent'
+    ]::deal_role[],
+
+    optional_roles = ARRAY[
+      'CoBuyerAgent',
+      'BuyerPowerOfAttorney',
+      'BuyerLawyer',
+      'BuyerReferral',
+
+      'CoSellerAgent',
+      'SellerPowerOfAttorney',
+      'SellerLawyer',
+      'SellerReferral',
+
+      'Title',
+      'Lender',
+      'Appraiser',
+      'Lawyer',
+      'TeamLead'
+    ]::deal_role[]
+  WHERE is_lease = FALSE`,
+
+  `UPDATE brands_property_types SET 
+    required_roles = ARRAY[
+      'Tenant', 
+      'BuyerAgent',
+
+      'Landlord',
+      'SellerAgent'
+    ]::deal_role[],
+
+    optional_roles = ARRAY[
+      'CoBuyerAgent',
+      'CoSellerAgent',
+
+      'TenantPowerOfAttorney',
+      'LandlordPowerOfAttorney',
+
+      'Lawyer',
+      'TeamLead'
+    ]::deal_role[]
+  WHERE is_lease = TRUE`,
+
   'COMMIT'
 ]
 
