94114d5c50c6ccffaaf3fa21cb81f8ebff357ea1
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-17 23:03:20 +0430

    listing: move access control logic to controller

--- lib/sql/listing/get_mls_number.sql
+++ lib/sql/listing/get_mls_number.sql
@@ -1,8 +1,3 @@
 SELECT id
 FROM listings
 WHERE mls_number = $1
-AND (
-  $2::mls IS NULL
-  OR public_display IS TRUE
-  OR mls = $2::mls
-)
--- lib/sql/listing/get_mui.sql
+++ lib/sql/listing/get_mui.sql
@@ -1,10 +1,4 @@
 SELECT id
 FROM listings
 WHERE matrix_unique_id = $1 AND mls = $2
-AND (
-  $3::mls IS NULL
-  OR public_display IS TRUE
-  OR mls = $3::mls
-)
-
 LIMIT 1
