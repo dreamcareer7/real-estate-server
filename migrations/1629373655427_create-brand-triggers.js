e65b41a8f96c7f9bae2483b59088bba31b8c41a5
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-19 22:49:11 +0430

    feat(db): create brand_triggers table
    Refs #1850

---
+++ migrations/1629373655427_create-brand-triggers.js
@@ -0,0 +1,43 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  `CREATE TABLE brand_triggers (
+    id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+
+    created_by uuid NOT NULL REFERENCES users (id),
+    "user" uuid NOT NULL REFERENCES users (id),
+
+    brand uuid NOT NULL REFERENCES brands (id),
+    template uuid REFERENCES brands_allowed_templates (id),
+    template_instance uuid REFERENCES templates_instances (id),
+
+    event_type text NOT NULL,
+    wait_for interval NOT NULL,
+    subject text NOT NULL,
+
+    created_at timestamp NOT NULL DEFAULT now(),
+    updated_at timestamp NOT NULL DEFAULT now(),
+    deleted_at timestamp,
+
+    CONSTRAINT bt_template_xor_template_instance CHECK (num_nonnulls(template, template_instance) = 1)
+  )`,
+  'COMMIT',
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
