b3dfebd10a4b8150746450c6a4b67628eedb12a6
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-24 16:55:06 -0700

    For web#5530

--- lib/sql/brand/deal/status/by-brand.sql
+++ lib/sql/brand/deal/status/by-brand.sql
@@ -1,4 +1,5 @@
 SELECT id FROM brands_deal_statuses
 WHERE brand IN (SELECT brand_parents($1::uuid))
 AND deleted_at IS NULL
-ORDER BY is_active DESC, is_pending DESC, is_archived DESC, label ASC;
+ORDER BY is_active DESC, is_pending DESC, is_archived DESC, label ASC
+LIMIT $2
