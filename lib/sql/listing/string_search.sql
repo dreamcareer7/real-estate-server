ffaf4016a6b40372e5925fa7b48ce16ab7feb2f4
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-29 13:01:24 -0700

    Make it fast again

--- lib/sql/listing/string_search.sql
+++ lib/sql/listing/string_search.sql
@@ -1,8 +1,15 @@
-SELECT id FROM search_listings(to_tsquery('english', $1))
-WHERE
-  CASE
-    WHEN $2::listing_status[] IS NULL THEN TRUE
-    ELSE status = ANY($2::listing_status[])
-  END
-ORDER BY order_listings(status)
-LIMIT COALESCE($3, 50)
+WITH r AS MATERIALIZED (
+  SELECT id FROM search_listings(to_tsquery('english', $1))
+  WHERE
+    CASE
+      WHEN $2::listing_status[] IS NULL THEN TRUE
+      ELSE status = ANY($2::listing_status[])
+    END
+  ORDER BY order_listings(status)
+)
+
+SELECT * FROM r LIMIT COALESCE($3, 50)
+
+-- This used to be a simple query with no CTE.
+-- But the LIMIT was causing a major slowdown.
+-- I could not figure out why and needed to hotfix it.
