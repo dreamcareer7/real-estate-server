fd4d3e75bbcce60bf3ae2f17257434853783ea99
dreamcareer7 <dreamcareer070@gmail.com>
2021-08-31 20:10:27 +0430

    feat(contact): query to find contacts having a date attribute
    Refs #1857

---
+++ lib/sql/contact/having_date_attribute.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT
+  c.id
+FROM contacts_attributes_date AS cad
+JOIN contacts AS c ON c.id = cad.contact
+WHERE
+  cad.deleted_at IS NULL AND
+  c.deleted_at IS NULL AND
+  cad.attribute_type = $1::text
--- types/db.d.ts
+++ types/db.d.ts
@@ -206,6 +206,7 @@ declare type TDbSqlAddress =
   | 'contact/export/max_indices'
   | 'contact/get'
   | 'contact/has_access'
+  | 'contact/having_date_attribute'
   | 'contact/lead/save'
   | 'contact/list/create'
   | 'contact/list/create_default_lists'
