c703f614895378e52ab1fd928898fd5585ad9bfc
dreamcareer7 <dreamcareer070@gmail.com>
2021-09-19 19:51:36 -0700

    Small improvements

--- migrations/1629779816401_multi-agent.js
+++ migrations/1629779816401_multi-agent.js
@@ -10,6 +10,44 @@ const migrations = [
   `INSERT INTO users_agents ("user", agent)
    SELECT id, agent FROM users WHERE agent IS NOT NULL`,
   'ALTER TABLE users DROP agent',
+
+  `CREATE OR REPLACE FUNCTION get_brand_agents(id uuid) RETURNS TABLE (
+     "user"     uuid,
+     agent      uuid,
+     mui        bigint,
+     mls        mls,
+     brand_user uuid,
+     brand_role uuid,
+     brand      uuid,
+     enabled    boolean
+  ) AS
+  $$
+    SELECT
+      users.id                as "user",
+      agents.id               as agent,
+      agents.matrix_unique_id as mui,
+      agents.mls              as mls,
+      brands_users.id         as brand_user,
+      brands_roles.id         as brand_role,
+      brands_roles.brand      as brand,
+      (brands_users.deleted_at IS NULL AND brands_roles.deleted_at IS NULL) as enabled
+
+    FROM users
+    LEFT JOIN users_agents ON users_agents.user = users.id
+    LEFT JOIN agents       ON users_agents.agent = agents.id
+    JOIN brands_users      ON brands_users.user = users.id
+    JOIN brands_roles      ON brands_users.role = brands_roles.id
+    WHERE
+      users.user_type = 'Agent'
+      AND
+      brands_roles.brand IN(
+        SELECT brand_children($1)
+      )
+  $$
+  STABLE
+  PARALLEL SAFE
+  LANGUAGE sql`,
+
   'COMMIT'
 ]
 
