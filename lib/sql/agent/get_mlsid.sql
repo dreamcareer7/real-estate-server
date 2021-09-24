5dc774a80781861b610f9543dea8e142695ed343
dreamcareer7 <dreamcareer070@gmail.com>
2021-09-24 03:39:14 -0700

    Our search now works on license numbers as well. Requested by Abbas.

--- lib/sql/agent/get_mlsid.sql
+++ lib/sql/agent/get_mlsid.sql
@@ -1,3 +1,6 @@
 SELECT id
 FROM agents
-WHERE regexp_replace(upper(mlsid), '^0*', '', 'g') = regexp_replace(upper($1), '^0*', '', 'g')
+WHERE 
+regexp_replace(upper(mlsid), '^0*', '', 'g') = regexp_replace(upper($1), '^0*', '', 'g')
+OR
+regexp_replace(upper(license_number), '^0*', '', 'g') = regexp_replace(upper($1), '^0*', '', 'g')
