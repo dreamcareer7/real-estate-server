f06c6884b7afc29ac294081df76fc2cc9059186c
dreamcareer7 <dreamcareer070@gmail.com>
2021-10-07 17:39:55 +0330

    ignore migration

--- migrations/1633528144580_update_new_filter_fields.js
+++ migrations/1633528144580_update_new_filter_fields.js
@@ -2,8 +2,6 @@ const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
-  'UPDATE listings_filters SET bathroom_count=properties.bathroom_count FROM properties WHERE listings_filters.matrix_unique_id=properties.matrix_unique_id',
-  'UPDATE listings_filters SET lot_size_area=properties.lot_size_area FROM properties WHERE listings_filters.matrix_unique_id=properties.matrix_unique_id',
   'COMMIT'
 ]
 
