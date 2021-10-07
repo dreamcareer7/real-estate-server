e4487a7c4a528f91f9e4b3f8c6b0d0ee587c149b
dreamcareer7 <dreamcareer070@gmail.com>
2021-10-07 08:16:36 +0330

    ref: adapt test cases w/ recent changes of status_fsm

--- tests/unit/showing/appointment-status-fsm.test.js
+++ tests/unit/showing/appointment-status-fsm.test.js
@@ -1,6 +1,6 @@
 
 describe('Showing/Appointment/status-fsm', () => {
-  context('everyoneApproved()', () => {
+  context('utils.everyoneApproved()', () => {
     it('throws when roles are empty')
     
     it('returns true when all roles confirmed')
@@ -11,53 +11,53 @@ describe('Showing/Appointment/status-fsm', () => {
     })    
   })
 
-  context('findLatestApproval()', () => {
+  context('utils.findLatestApproval()', () => {
     it('can find latest approved approval')
     it('can find latest rejected approval')
     it('returns nil when nothing found')
   })
 
-  context('send()', () => {
+  context('utils.send()', () => {
     it('does nothing when mailer is nil')
     it('calls send method after resolving non-nil mailer')
   })
 
-  context('patchStatus()', () => {
+  context('utils.patchStatus()', () => {
     // noop
   })
 
-  context('predicate()', () => {
+  context('utils.predicate()', () => {
     context('returns a function that...', () => {
       it('returns true when its args include actual value')
       it('returns false when iss args does not include actual value')
     })
   })
 
-  context('finalized()', () => {
+  context('handlers.finalized()', () => {
     it('populate all showing roles')
     it('clears notifications of populated showing roles')
   })
 
-  context('completed()', () => {
+  context('handlers.completed()', () => {
     it('sends get-feedback sms & email to buyer')
   })
 
-  context('confirmed()', () => {
+  context('handlers.confirmed()', () => {
     it('sends appointment-confirmed sms & email to buyer')
     it('sends confirmation notif to other roles')
   })
 
-  context('rejected()', () => {
+  context('handlers.rejected()', () => {
     it('sends appointment-rejected sms & email to buyer')
     it('sends rejection notif to other roles')
   })
 
-  context('canceledAfterConfirm()', () => {
+  context('handlers.canceledAfterConfirm()', () => {
     it('sends appointment-canceled sms & email to buyer')
     it('sends cancelation notif to other roles')
   })
 
-  context('guessNewStatus()', () => {
+  context('utils.guessNewStatus()', () => {
     it('maps simple actions to their related appointment status')
     it('returns Completed/Canceled for Finished based on old status')
 
@@ -71,20 +71,20 @@ describe('Showing/Appointment/status-fsm', () => {
     })
   })
 
-  context('handleAction()', () => {
+  context('handlers.handleAction()', () => {
     it('does nothing if status is not changed')
-    it('calls completed if new status is Completed')
-    it('calls finalized if new status is Completed or Canceled')
-    it('calls patchStatus w/ appointment ID and newStatus')
+    it('calls handlers.completed if new status is Completed')
+    it('calls handlers.finalized if new status is Completed or Canceled')
+    it('calls utils.patchStatus w/ appointment ID and newStatus')
 
     context('when an approval is performed...', () => {
-      it('calls confirmed if new status is confirmed')
-      it('calls rejected if it was a rejection')
-      it('calls canceledAfterConfirm if it was a cancelation')
+      it('calls handlers.confirmed if new status is confirmed')
+      it('calls handlers.rejected if it was a rejection')
+      it('calls handlers.canceledAfterConfirm if it was a cancelation')
     })
   })
 
   context('.dispatchEvent()', () => {
-    it('calls handleAction w/ populated payload')
+    it('calls handlers.handleAction w/ populated payload')
   })
 })
