862cd90a45a347fd0d95ce46ccfc947f2577fc04
dreamcareer7 <dreamcareer070@gmail.com>
2021-10-22 16:46:13 +0330

    feat(super-campaign): add function to bulk update campaign (ID)

--- types/db.d.ts
+++ types/db.d.ts
@@ -349,6 +349,7 @@ declare type TDbSqlAddress =
   | 'email/super_campaign/enrollment/table'
   | 'email/super_campaign/enrollment/update'
   | 'email/super_campaign/enrollments/delete_by'
+  | 'email/super_campaign/enrollment/update_campaigns'
   | 'email/super_campaign/get'
   | 'email/super_campaign/insert'
   | 'email/super_campaign/recipients'
