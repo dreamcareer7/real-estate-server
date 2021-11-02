e5f8f96dc74c7ce8200f21fc6fcf80a8943e0d1d
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-01 20:36:24 -0700

    Sorry

--- lib/sql/users_job/google/gcal_sync_due.sql
+++ lib/sql/users_job/google/gcal_sync_due.sql
@@ -12,5 +12,5 @@ WHERE
   -- Don't requeue anything that has been queued.
   -- However, there is a chance that we had queued something, but the queue has got purged or anything and it was never processed.
   -- So, if after 3 hours, it's still not processed, queue again
-  (AND status <> 'queued' OR updated_at < (NOW() - '3 hour'::interval)
+  (AND status <> 'queued' OR updated_at > (NOW() - '3 hour'::interval)
 FOR UPDATE SKIP LOCKED
