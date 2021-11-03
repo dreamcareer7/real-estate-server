5bfd5c6398f526e0234cc9dbea5906b6b9003291
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-03 19:22:41 +0330

    feat: make all arguments optional. but at least one of them must be truthy

--- lib/sql/email/super_campaign/enrollment/delete_by.sql
+++ lib/sql/email/super_campaign/enrollment/delete_by.sql
@@ -4,7 +4,16 @@ SET
   deleted_at = now(),
 WHERE
   deleted_at IS NULL AND
-  brand = $1::uuid AND
-  "user" = $2::uuid AND
-  super_campaign = $1::uuid
+  (CASE
+    WHEN $1::uuid IS NULL THEN TRUE
+    ELSE brand = $1::uuid
+  END) AND
+  (CASE
+    WHEN $2::uuid IS NULL THEN TRUE
+    ELSE "user" = $2::uuid
+  END) AND
+  (CASE
+    WHEN $3::uuid IS NULL THEN TRUE
+    ELSE super_campaign = $3::uuid
+  END)
   
