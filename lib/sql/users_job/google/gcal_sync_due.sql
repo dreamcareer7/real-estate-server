a6afe81a7dc021b0a60f30841c57fc3f2104aea8
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-03 14:43:20 -0700

    Oh my god im sorry im sorry

--- lib/sql/users_job/google/gcal_sync_due.sql
+++ lib/sql/users_job/google/gcal_sync_due.sql
@@ -5,7 +5,7 @@ FROM
 WHERE
   google_credential IS NOT NULL
   AND job_name = 'calendar'
-  AND ((start_at <= (NOW() - '10 minute'::interval) OR start_at IS NULL) OR status = 'waiting')
+  AND ((start_at <= (NOW() - $1::interval) OR start_at IS NULL) OR status = 'waiting')
   AND ((resume_at <= NOW()) OR (resume_at IS null))
   AND recurrence IS TRUE
   AND deleted_at IS NULL
