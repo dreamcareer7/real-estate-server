5d565a21342b995a7be366d4e9f71c5e11b53e2a
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-03 14:37:41 -0700

    Sorry lol syntax error

--- lib/sql/users_job/google/gcal_sync_due.sql
+++ lib/sql/users_job/google/gcal_sync_due.sql
@@ -5,12 +5,12 @@ FROM
 WHERE
   google_credential IS NOT NULL
   AND job_name = 'calendar'
-  AND ((start_at <= (NOW() - $1::interval) OR start_at IS NULL) OR status = 'waiting')
+  AND ((start_at <= (NOW() - '10 minute'::interval) OR start_at IS NULL) OR status = 'waiting')
   AND ((resume_at <= NOW()) OR (resume_at IS null))
   AND recurrence IS TRUE
   AND deleted_at IS NULL
   -- Don't requeue anything that has been queued.
   -- However, there is a chance that we had queued something, but the queue has got purged or anything and it was never processed.
   -- So, if after 3 hours, it's still not processed, queue again
-  (AND status <> 'queued' OR updated_at > (NOW() - '3 hour'::interval)
+  AND (status <> 'queued' OR updated_at > (NOW() - '3 hour'::interval))
 FOR UPDATE SKIP LOCKED
