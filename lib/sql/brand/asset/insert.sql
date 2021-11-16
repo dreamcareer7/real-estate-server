77169bdbb9ec226914abfc454cacc6006078e197
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-15 22:18:00 -0800

    for #1948

--- lib/sql/brand/asset/insert.sql
+++ lib/sql/brand/asset/insert.sql
@@ -1,1 +1,3 @@
-INSERT INTO brands_assets (created_by, brand, file, label, template_type, medium) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id
+INSERT INTO brands_assets (created_by, brand, file, label, template_type, medium)
+SELECT $1, UNNEST($2::uuid[]), $3, $4, $5, $6
+RETURNING id
