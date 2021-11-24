6092a6c92343a2c8c0892cba64eb92ab251a0ecd
dreamcareer7 <dreamcareer070@gmail.com>
2021-11-24 17:45:05 +0330

    ref(user-settings): fix validators and improve types...

--- lib/models/User/setting/types.d.ts
+++ lib/models/User/setting/types.d.ts
@@ -1,23 +1,34 @@
-
 type MissingProps = 'deleted_at' | 'created_by' | 'updated_by';
+type JsonObject = Record<string, any>;
+type JsonArray = Array<any>;
+type Nullable<T> = T | null;
 
-export type Json = Record<string, any> | any[] | string | number | boolean | null;
-
-export interface UserSetting extends Omit<IModel, MissingProps> {
-  type: 'user_setting';
-
-  grid_deals_agent_network_sort_field: Json;
-  mls_saved_search_hint_dismissed: Json;
-  import_tooltip_visite: Json;
-  mls_sort_field: Json;
-  grid_deals_sort_field: Json;
-  grid_contacts_sort_field: Json;
-  user_filter: Json;
-  mls_last_browsing_location: Json;
-  onboarding_marketing_center: Json;
-  contact_view_mode_field: Json;
-  grid_deals_sort_field_bo: Json;
-  insight_layout_sort_field: Json;
-  deals_grid_filter_settings: Json;
-  super_campaign_admin_permission: Json;
+export interface UserSettingFields {
+  grid_deals_agent_network_sort_field: Nullable<string>;
+  mls_saved_search_hint_dismissed: Nullable<number>;
+  import_tooltip_visited: Nullable<number>;
+  mls_sort_field: Nullable<string>;
+  grid_deals_sort_field: Nullable<string>;
+  grid_contacts_sort_field: Nullable<string>;
+  user_filter: Nullable<JsonArray>;
+  mls_last_browsing_location: Nullable<JsonObject>;
+  onboarding_marketing_center: Nullable<number>;
+  contact_view_mode_field: Nullable<string>;
+  grid_deals_sort_field_bo: Nullable<string>;
+  insight_layout_sort_field: Nullable<JsonObject>;
+  deals_grid_filter_settings: Nullable<JsonObject>;
+  super_campaign_admin_permission: boolean;
 }
+
+export interface UserSetting extends Omit<IModel, MissingProps>, UserSettingFields {
+  type: 'user_setting';
+  brand: IBrand['id'];
+  user: IUser['id'];
+}
+
+export type SettingKey = keyof UserSettingFields;
+
+export type ClientSettingKey =
+  | SettingKey
+  | 'mls-saved-search-hint-dismissed'
+  | 'onboarding__marketing-center';
