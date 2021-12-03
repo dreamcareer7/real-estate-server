4ec3326316ec61d06e64b67fd60c0d2817035fa3
dreamcareer7 <dreamcareer070@gmail.com>
2021-12-04 02:13:08 +0330

    filter flows by creators + delete user trigger script

--- scripts/support/delete-user-triggers.js
+++ scripts/support/delete-user-triggers.js
@@ -5,11 +5,19 @@ const Trigger = {
   ...require('../../lib/models/Trigger/filter'),
   ...require('../../lib/models/Trigger/delete'),
 }
+const Flow = {
+  ...require('../../lib/models/Flow/filter'),
+  ...require('../../lib/models/Flow/stop')
+}
 
 const brandId = process.argv[2]
 const userId = process.argv[3]
 
 runInContext(`delete-user-triggers-${new Date().toLocaleTimeString('en-us')}`, async () => {
+  const flowIds = await Flow.filter({ brand: brandId, created_by: userId })
+  await Promise.all(
+    flowIds.map(async (flowId) => Flow.stop(userId, flowId))
+  )
   const triggers = await Trigger.filter({
     brand: brandId,
     created_by: userId,
