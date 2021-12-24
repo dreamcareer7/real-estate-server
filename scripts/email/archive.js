d7d79224fe45f9efcf6702945c73743501a42518
dreamcareer7 <dreamcareer070@gmail.com>
2021-12-25 01:18:49 +0330

    email: migration script, attempt 2

--- scripts/email/archive.js
+++ scripts/email/archive.js
@@ -3,21 +3,24 @@ const sql = require('../../lib/utils/sql')
 const { runInContext } = require('../../lib/models/Context/util')
 const { peanar } = require('../../lib/utils/peanar')
 const archive = require('../../lib/models/Email/archive/upload')
+const Context = require('../../lib/models/Context')
 
 const query = `
-  SELECT id FROM emails WHERE html IS NOT NULL OR text IS NOT NULL LIMIT 10000
+  SELECT id FROM emails WHERE html IS NOT NULL OR text IS NOT NULL LIMIT 100000
 `
 
 async function archiveEmails() {
   const ids = await sql.selectIds(query, [])
+  Context.log(`found ${ids.length} emails...`)
 
   for (const id of ids) {
-    await archive(id)
+    await archive.immediate(id)
   }
 }
 
 async function main() {
   await peanar.declareAmqResources()
+  Context.log('connected to peanar instance...')
 
   let counter = 1
   await runInContext(`EmailArchive-${counter}`, archiveEmails, undefined, { exitAfterFinish: false })
@@ -26,4 +29,4 @@ async function main() {
   await peanar.shutdown()
 }
 
-main().catch(ex => console.error(ex)).finally(process.exit())
+main().catch(ex => console.error(ex)).finally(() => process.exit())
