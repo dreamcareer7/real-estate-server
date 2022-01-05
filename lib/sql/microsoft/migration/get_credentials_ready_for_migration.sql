f01aa8b97183893b1e6089621693599c6db8976c
dreamcareer7 <dreamcareer070@gmail.com>
2022-01-05 20:57:07 +0330

    ref(migration): get contacts joined credentials

--- lib/sql/microsoft/migration/get_credentials_ready_for_migration.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    contact_migration_status
-FROM
-    microsoft_credentials
-WHERE
-    revoked = false 
-    AND deleted_at IS NULL 
-    AND contact_migration_status = 'pending' 
--- migrations/1641329800645_microsoft_contact_immutable_id_migration.js
+++ migrations/1641329800645_microsoft_contact_immutable_id_migration.js
@@ -3,7 +3,6 @@ const db = require('../lib/utils/db')
 const migrations = [
   'BEGIN',
   'ALTER TABLE microsoft_contacts ADD COLUMN IF NOT EXISTS new_remote_id text',
-  'ALTER TABLE microsoft_credentials ADD COLUMN IF NOT EXISTS contact_migration_status text DEFAULT \'waiting\'',
   'COMMIT'
 ]
 
