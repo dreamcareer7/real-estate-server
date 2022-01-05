21a78b11b91d6acf2a08b2c870ccec2ed6f8ff16
dreamcareer7 <dreamcareer070@gmail.com>
2022-01-05 20:57:07 +0330

    ref(migration): get contacts joined credentials

--- lib/sql/microsoft/migration/get_contacts_without_new_id.sql
+++ lib/sql/microsoft/migration/get_contacts_without_new_id.sql
@@ -1,13 +1,19 @@
-SELECT
-  id,
-  remote_id
-FROM
-  microsoft_contacts
-WHERE
-  microsoft_credential = $1 AND new_remote_id IS NULL AND source = 'contacts'
-ORDER BY 
-  id
-LIMIT
-  $2
-OFFSET
-  $3
+SELECT 
+	DISTINCT microsoft_credential as cid,
+	microsoft_contacts.id,
+	microsoft_contacts.remote_id
+	
+FROM 
+  microsoft_contacts 
+
+JOIN 
+  microsoft_credentials 
+	ON microsoft_credential = microsoft_credentials."id"
+	
+WHERE 
+  new_remote_id IS NULL AND
+	microsoft_contacts.source = 'contacts' AND
+	microsoft_credentials.deleted_at IS NULL AND
+	microsoft_credentials.revoked = FALSE
+	
+LIMIT 1000
