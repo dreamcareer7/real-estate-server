60953eda1fcc2d17e83093edc0be857e7feedffb
dreamcareer7 <dreamcareer070@gmail.com>
2022-01-11 00:15:44 +0330

    use scheduler for archive-old-emails script

--- scripts/email/archive-old-emails.js
+++ scripts/email/archive-old-emails.js
@@ -7,60 +7,31 @@ const { runInContext } = require('../../lib/models/Context/util')
 const promisify = require('../../lib/utils/promisify')
 const Context = require('../../lib/models/Context')
 
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-
 const defaultTtime = '1970-01-01T00:00:00'
 
-let totalCount = 0
+const query = `SELECT id, created_at
+  FROM emails
+  WHERE
+    (html IS NOT NULL OR text IS NOT NULL)
+    AND (mailgun_id IS NOT NULL OR google_id IS NOT NULL OR microsoft_id IS NOT NULL)
+    AND (created_at::timestamptz > $1::timestamptz)
+  ORDER BY created_at ASC
+  LIMIT 1000
+`
 
-let lastEmailDate = ''
-
-const queueEmails = async () => {
-
-  const [lastJob] = await promisify(MLSJob.getLastRun)('archive_old_emails')
-  lastEmailDate = lastJob?.last_modified_date
-
+runInContext(`archive-old-emails-${new Date().toLocaleTimeString('en-us')}`, async () => {
+  const lastEmailDate = (await promisify(MLSJob.getLastRun)('archive_old_emails'))?.[0]?.last_modified
   const time = lastEmailDate || defaultTtime
-
-  const query = `SELECT id, created_at
-    FROM emails
-    WHERE
-      (html IS NOT NULL OR text IS NOT NULL)
-      AND (mailgun_id IS NOT NULL OR google_id IS NOT NULL OR microsoft_id IS NOT NULL)
-      AND (created_at::timestamptz > $1::timestamptz)
-    ORDER BY created_at ASC
-    LIMIT 1000
-  `
-
   const emails = await sql.select(query, [time])
-  const emailsCount = emails.length
-
-  if (!emailsCount){
-    Context.log(`all emails queued to be archived! total: ${totalCount} emails`)
-    return 0
-  }
-
-  for (let i = 0; i < emailsCount; i++) {
+  if (!emails) { return }
+  for (let i = 0; i < emails.length; i++) {
     await archive(emails[i].id)
   }
-
   await promisify(MLSJob.insert)(
     {
       name: 'archive_old_emails',
-      last_modified_date: emails[emailsCount - 1].created_at,
+      last_modified_date: emails[emails.length - 1].created_at,
     }
   )
-
-  totalCount += emailsCount
-
-  return emailsCount
-}
-
-runInContext(`archive-old-emails-${new Date().toLocaleTimeString('en-us')}`, async () => {
-  let count = 0
-  do {
-    count = await queueEmails()
-    await sleep(1000 * 3600 / 2)
-  } while (count)
+  Context.log(`queued ${emails.length} emails to be archived.`)
 })
