63ef93e61b6c0bcde2278fa7cba4e6d37863219c
dreamcareer7 <dreamcareer070@gmail.com>
2022-01-19 00:56:06 +0330

    fix(test): resolved contacts folder

--- types/db.d.ts
+++ types/db.d.ts
@@ -554,6 +554,7 @@ declare type TDbSqlAddress =
   | 'microsoft/contact/get_by_credential'
   | 'microsoft/contact/get_by_microsoft_credential'
   | 'microsoft/contact/get_by_rechat_contacts'
+  | 'microsoft/contact/get_by_remote_folder_id'
   | 'microsoft/contact/get_by_remote_ids'
   | 'microsoft/contact/get_by_source'
   | 'microsoft/contact/hard_delete'
