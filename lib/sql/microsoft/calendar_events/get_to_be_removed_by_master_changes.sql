d18c9dff2f69d0625af1bb1540bc21b3b1a18e04
dreamcareer7 <dreamcareer070@gmail.com>
2022-02-02 01:16:18 +0330

    fix(calendar): delete events by master event bulk changes

--- lib/sql/microsoft/calendar_events/get_to_be_removed_by_master_changes.sql
+++ lib/sql/microsoft/calendar_events/get_to_be_removed_by_master_changes.sql
@@ -7,9 +7,4 @@ WHERE
 	AND microsoft_calendar = $2
 	AND series_master_id = $3
 	AND deleted_at IS NULL
-	AND (
-	(
-			event_start->>'dateTime')::date  < $4::date
-		OR 
-			(event_start->>'dateTime')::date > $5::date
-	)
+	AND NOT (event_id = ANY($4::TEXT[]))
