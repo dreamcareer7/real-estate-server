18f70a61dddfdc7e98cf126484477341e241f8a2
dreamcareer7 <dreamcareer070@gmail.com>
2022-02-03 14:19:22 +0330

    Revert "Merge remote-tracking branch 'origin/1913-migration-contacts-phase-two'"
    This reverts commit c9d83858acc75057a3b280d644e84ef71b6d3fc3, reversing
    changes made to fe0251b87edba22fd8c1b496afe947c7eb49a114.

--- lib/sql/microsoft/contact/get_by_remote_folder_id.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  remote_id
-FROM
-  microsoft_contacts
-WHERE 
-  microsoft_credential = $1
-  AND deleted_at IS NULL 
-  AND "data" ->> 'parentFolderId' = ANY($2)
--- tests/unit/microsoft/contact.test.js
+++ tests/unit/microsoft/contact.test.js
@@ -7,9 +7,6 @@ const User             = require('../../../lib/models/User/get')
 const BrandHelper      = require('../brand/helper')
 const MicrosoftContact = require('../../../lib/models/Microsoft/contact')
 const ContactIntegration = require('../../../lib/models/ContactIntegration')
-const { 
-  findNewAttributes 
-} = require('../../../lib/models/Microsoft/workers/contacts/people/helpers/attributes')
 
 const { attributes } = require('../contact/helper')
 const { createMicrosoftCredential } = require('./helper')
@@ -53,7 +50,7 @@ async function create() {
       microsoft_credential: credential.id,
       contact: contactIds[0],
       remote_id: mcontact.id,
-      data: (mcontact.data),
+      data: JSON.stringify(mcontact.data),
       source: mcontact.source,
       etag: 'etag',
       parked: false
@@ -79,7 +76,6 @@ async function create() {
     expect(microsoftContact.microsoft_credential).to.be.equal(createdMicrosoftContact.microsoft_credential)
     expect(microsoftContact.remote_id).to.be.equal(createdMicrosoftContact.remote_id)
     expect(microsoftContact.contact).to.not.be.equal(null)
-    expect(microsoftContact.data).to.not.be.equal(microsoft_contacts_offline[0].data)
   }
 
   const result = await ContactIntegration.insert(integration_records)
@@ -99,7 +95,7 @@ async function update() {
     records.push({
       microsoft_credential: credential.id,
       remote_id: mcontact.remote_id,
-      data: (sample),
+      data: JSON.stringify(sample),
       source: mcontact.source,
       etag: 'etag',
       parked: false
@@ -263,221 +259,6 @@ async function resetContactIntegration() {
   expect(after.length).to.be.equal(0)
 }
 
-async function contactAttributeUpdate() {
-  // This method should be updated fot each attribute
-  const data = {
-    'id': 'AAkALgAAAAAAHYQDEapmEc2byACqAC-EWg0A3vdaSQcSnEazMnWI1bbVjAAAJe4Z7wAA',
-    'title': null,
-    'surname': 'Sang3',
-    'birthday': null,
-    'jobTitle': null,
-    'nickName': null,
-    'changeKey': 'EQAAABYAAADe91pJBxKcRrMydYjVttWMAAAl63a3',
-    'givenName': 'Saloom3',
-    'categories': [],
-    'homePhones': [],
-    'middleName': null,
-    'spouseName': null,
-    '@odata.etag': 'W/"EQAAABYAAADe91pJBxKcRrMydYjVttWMAAAl63a3"',
-    '@odata.type': '#microsoft.graph.contact',
-    'companyName': 'compaa',
-    'displayName': 'Saloom3 Sang3',
-    'homeAddress': {},
-    'mobilePhone': null,
-    'otherAddress': {},
-    'personalNotes': '',
-    'businessPhones': [],
-    'emailAddresses': [
-      {
-        'name': 'samlo3@sang3.com',
-        'address': 'samlo3@sang3.com'
-      }
-    ],
-    'parentFolderId': 'AQMkADAwATNiZmYAZC00NAAzNy1iZDkxLTAwAi0wMAoALgAAAzaRB-o7U85BpvwI1n_Wgy4BAN73WkkHEpxGszJ1iNW21YwAAAIBDgAAAA==',
-    'businessAddress': {},
-    'createdDateTime': '2022-01-20T17:18:29Z',
-    'businessHomePage': null,
-    'lastModifiedDateTime': '2022-01-20T18:41:38Z'
-  }
-  const oldData =  {
-    'id': 'AAkALgAAAAAAHYQDEapmEc2byACqAC-EWg0A3vdaSQcSnEazMnWI1bbVjAAAJe4Z7wAA',
-    'title': null,
-    'surname': 'Sang2',
-    'birthday': null,
-    'jobTitle': null,
-    'nickName': null,
-    'changeKey': 'EQAAABYAAADe91pJBxKcRrMydYjVttWMAAAl63Zx',
-    'givenName': 'Saloom2',
-    'categories': [],
-    'homePhones': [],
-    'middleName': null,
-    'spouseName': null,
-    '@odata.etag': 'W/"EQAAABYAAADe91pJBxKcRrMydYjVttWMAAAl63Zx"',
-    '@odata.type': '#microsoft.graph.contact',
-    'companyName': null,
-    'displayName': 'Saloom2 Sang2',
-    'homeAddress': {},
-    'mobilePhone': null,
-    'otherAddress': {},
-    'personalNotes': '',
-    'businessPhones': [],
-    'emailAddresses': [
-      {
-        'name': 'samlo2@sang2.com',
-        'address': 'samlo2@sang2.com'
-      }
-    ],
-    'parentFolderId': 'AQMkADAwATNiZmYAZC00NAAzNy1iZDkxLTAwAi0wMAoALgAAAzaRB-o7U85BpvwI1n_Wgy4BAN73WkkHEpxGszJ1iNW21YwAAAIBDgAAAA==',
-    'businessAddress': {},
-    'createdDateTime': '2022-01-20T17:18:29Z',
-    'businessHomePage': null,
-    'lastModifiedDateTime': '2022-01-20T17:19:17Z'
-  }
-  const refinedAtts = [
-    {
-      'id': 'f0a9c158-b992-45c7-901d-565ab65f86b7',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': '5ca36b57-256c-4c59-ae55-ec2df0cb5c79',
-      'created_at': 1642704004.54976,
-      'updated_at': 1642704004.54976,
-      'deleted_at': null,
-      'label': null,
-      'is_primary': false,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'source_type',
-      'text': 'Microsoft',
-      'date': null,
-      'type': 'contact_attribute'
-    },
-    {
-      'id': 'cfd5f795-9f33-4f9c-807f-456018bc0ed6',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': 'b14d0fad-a96e-4d08-aad7-8a4191886d4d',
-      'created_at': 1642704004.56652,
-      'updated_at': 1642704004.56652,
-      'deleted_at': null,
-      'label': null,
-      'is_primary': false,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'first_name',
-      'text': 'Saloom2',
-      'date': null,
-      'type': 'contact_attribute'
-    },
-    {
-      'id': '7a60eef0-e90e-4ea5-ad20-2b44f215e65e',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': '54c70044-370b-4223-af17-4da2bc7a7f5d',
-      'created_at': 1642704004.56982,
-      'updated_at': 1642704004.56982,
-      'deleted_at': null,
-      'label': null,
-      'is_primary': false,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'last_name',
-      'text': 'Sang2',
-      'date': null,
-      'type': 'contact_attribute'
-    },
-    {
-      'id': '12c44eae-9d89-4a37-a217-f04554002469',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': '0aa03800-e740-4a04-90ea-c43f9581d2f9',
-      'created_at': 1642704004.57257,
-      'updated_at': 1642704004.57257,
-      'deleted_at': null,
-      'label': 'Other',
-      'is_primary': true,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'email',
-      'text': 'samlo2@sang2.com',
-      'date': null,
-      'type': 'contact_attribute'
-    },
-    {
-      'id': '91b872c5-4891-4248-9dd9-70bc6ea6a967',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': '312a3cac-c2f6-4a28-8425-5548cc08c9da',
-      'created_at': 1642704007.50461,
-      'updated_at': 1642704007.50461,
-      'deleted_at': null,
-      'label': null,
-      'is_primary': false,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'profile_image_url',
-      'text': 'https://d2dzyv4cb7po1i.cloudfront.net/afdb83a6-6e6b-11ec-ae4b-42010a960002/avatars/636665c0-7a20-11ec-a415-ffdb8f7c1bef.jpg',
-      'date': null,
-      'type': 'contact_attribute'
-    },
-    {
-      'id': '29ad2137-b48d-4f01-9e14-167179410049',
-      'contact': 'ab1285af-4cef-4970-97b9-fbc338218144',
-      'attribute_def': '802e7a96-ae9f-41bf-9eba-56e210b57643',
-      'created_at': 1642704007.50733,
-      'updated_at': 1642704007.50733,
-      'deleted_at': null,
-      'label': null,
-      'is_primary': false,
-      'is_partner': false,
-      'index': null,
-      'created_by': '26b22f76-6e6b-11ec-849f-42010a960002',
-      'updated_by': null,
-      'attribute_type': 'cover_image_url',
-      'text': 'https://d2dzyv4cb7po1i.cloudfront.net/afdb83a6-6e6b-11ec-ae4b-42010a960002/avatars/636665c0-7a20-11ec-a415-ffdb8f7c1bef.jpg',
-      'date': null,
-      'type': 'contact_attribute'
-    }
-  ]
-  const contactFolders = {
-    'AQMkADAwATNiZmYAZC00NAAzNy1iZDkxLTAwAi0wMAoALgAAAzaRB-o7U85BpvwI1n_Wgy4BAN73WkkHEpxGszJ1iNW21YwAAAAjOv_bAAAA': 'VIP',
-    'AQMkADAwATNiZmYAZC00NAAzNy1iZDkxLTAwAi0wMAoALgAAAzaRB-o7U85BpvwI1n_Wgy4BAN73WkkHEpxGszJ1iNW21YwAAAAjOv_cAAAA': '1 new',
-    'AQMkADAwATNiZmYAZC00NAAzNy1iZDkxLTAwAi0wMAoALgAAAzaRB-o7U85BpvwI1n_Wgy4BAN73WkkHEpxGszJ1iNW21YwAAAAO7164AAAA': 'Importants'
-  }
-
-  const finalAttributes = [
-    {
-      id: 'cfd5f795-9f33-4f9c-807f-456018bc0ed6',
-      attribute_type: 'first_name',
-      text: 'Saloom3'
-    },
-    {
-      id: '7a60eef0-e90e-4ea5-ad20-2b44f215e65e',
-      attribute_type: 'last_name',
-      text: 'Sang3'
-    },
-    { attribute_type: 'company', text: 'compaa' },
-    {
-      attribute_type: 'email',
-      text: 'samlo3@sang3.com',
-      label: 'Other',
-      is_primary: false
-    }
-  ]
-
-  // @ts-ignore
-  const {attributes, deletedAtt} = findNewAttributes(data, oldData, refinedAtts, contactFolders)
-
-  expect(deletedAtt.length).to.be.equal(1)
-  expect(deletedAtt[0]).to.be.equal('12c44eae-9d89-4a37-a217-f04554002469')
-  expect(attributes.length).to.be.equal(4)
-  expect(attributes).to.be.deep.equal(finalAttributes)
-}
-
 
 describe('Microsoft', () => {
   describe('Microsoft Contacts', () => {
@@ -495,6 +276,5 @@ describe('Microsoft', () => {
     it('should remove contact-folders by credential', removeFoldersByCredential)
     it('should permanently delete several Microsoft contact records', hardDelete)
     it('should reset contact integration links', resetContactIntegration)
-    it('should update contact attribute', contactAttributeUpdate)
   })
 })
