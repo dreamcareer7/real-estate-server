3a7e08e8fceced5be2dd5804ac85eb70a01e6265
dreamcareer7 <dreamcareer070@gmail.com>
2022-02-10 21:16:06 +0330

    fix: mistakes in migration

--- scripts/support/fix-email-campaigns-recipients.sql
+++ scripts/support/fix-email-campaigns-recipients.sql
@@ -1,5 +1,5 @@
 WITH problematic_triggers as (
-  SELECT t.id, t.scheduled_after
+  SELECT t.id, t.scheduled_after, t.campaign
     FROM
       triggers t
     JOIN email_campaigns ec
@@ -11,7 +11,7 @@ WITH problematic_triggers as (
 ),
 parent_triggers as (
   SELECT
-    t.brand, t.campaign,
+    t.id, t.brand, t.campaign,
     ec.from, ecr.tag, ecr.list, ecr.contact,
     ecr.email, ecr.send_type, ecr.recipient_type,
     ecr.agent
@@ -28,6 +28,10 @@ INSERT
   INTO email_campaigns_recipients
     (campaign, tag, list, contact, email, brand, send_type, recipient_type, agent)
 SELECT
-  (campaign, tag, list, contact, email, brand, send_type, recipient_type, agent)
+  probt.campaign, pat.tag, pat.list, pat.contact, pat.email, pat.brand, pat.send_type, pat.recipient_type, agent
 FROM
-  parent_triggers
+  parent_triggers pat
+JOIN
+  problematic_triggers probt
+ON
+  pat.id = probt.scheduled_after
