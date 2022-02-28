3026f2f237182e4e2fa8d67c751758b997eca640
dreamcareer7 <dreamcareer070@gmail.com>
2022-02-28 23:02:10 +0330

    feat: add types for social post

---
+++ lib/models/SocialPost/types.d.ts
@@ -0,0 +1,35 @@
+export interface Post {
+  id: UUID
+  brand: UUID
+  user: UUID
+  facebook_page: UUID
+  template?: UUID
+  caption?: string
+  post_link?: string
+  failed_at?: number
+  failure?: string
+  created_at?: number
+  updated_at?: number
+  deleted_at?: number
+  due_at?: number
+  executed_at?: number
+}
+
+export interface Filter {
+  user?: UUID
+  executed?: 'true' | 'false'
+  start?: number
+  limit?: number
+}
+
+export interface FilterResult {
+  ids: UUID[]
+  total: number
+}
+
+export interface SocialPostInsertInput {
+  facebookPage: UUID
+  template: UUID
+  due_at: number
+  caption: string
+}
