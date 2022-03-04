81cef83c790e8191b917abf4c217352428269b26
dreamcareer7 <dreamcareer070@gmail.com>
2022-03-04 15:06:16 +0330

    refactor: add types for get query

--- lib/sql/facebook/facebook_credentials/get.sql
+++ lib/sql/facebook/facebook_credentials/get.sql
@@ -1,4 +1,5 @@
 SELECT   
+    facebook_credentials.id,
     facebook_credentials.user,
     facebook_credentials.brand,
     facebook_credentials.facebook_id,
@@ -6,6 +7,7 @@ SELECT
     facebook_credentials.first_name,
     facebook_credentials.last_name,
     facebook_credentials.scope,
+    facebook_credentials.access_token,
     EXTRACT(EPOCH FROM facebook_credentials.created_at) AS created_at,
     EXTRACT(EPOCH FROM facebook_credentials.updated_at) AS updated_at,    
     'facebook_credential' as "type"
