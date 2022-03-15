f08ef0933133f2dc56047291fd1b553a5ad7310b
dreamcareer7 <dreamcareer070@gmail.com>
2022-03-15 23:15:27 +0330

    test(calendar): set log

--- tests/unit/calendar/calendar.test.js
+++ tests/unit/calendar/calendar.test.js
@@ -280,7 +280,7 @@ async function testExportForEvents() {
   const dateFormat = (dateInString) => moment(dateInString).tz('Asia/Tehran').format('YYYY/MM/DD') // skip the time
 
   const eventFormat = (dateInString) => moment(dateInString).tz('Asia/Tehran').format('YYYY/MM/DD hh:mm') // skip secondes and milliseconds 
-  
+  console.log({feed: feedValues[3]},{now}, {next30Mins}, {next60Mins})
   expect(eventFormat(feedValues[1]['dtstamp'])).to.be.equal(eventFormat(now))
   expect(dateFormat(feedValues[1]['start'])).to.be.equal(dateFormat(now))
   expect(dateFormat(feedValues[1]['end'])).to.be.equal(dateFormat(now))
@@ -526,7 +526,7 @@ describe('Calendar', () => {
   describe('Events', () => {
     beforeEach(setup)
     it('should put events in correct timezones', testCorrectTimezone)
-    it('should export events correctly for allday and none allday events', testExportForEvents)
+    it.only('should export events correctly for allday and none allday events', testExportForEvents)
   })
 
   describe('Contacts', () => {
