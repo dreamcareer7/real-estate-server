597a4d7c9bb25b755c5e58c929a4897a9bc4dee0
dreamcareer7 <dreamcareer070@gmail.com>
2022-03-16 17:00:57 +0330

    or conditions for coverage

--- lib/sql/agent/search.sql
+++ lib/sql/agent/search.sql
@@ -2,9 +2,9 @@ SELECT
 id
 FROM agents
 WHERE
-(first_name || ' ' || middle_name || ' ' || last_name) ILIKE '%' || $1 || '%'
+((first_name || ' ' || middle_name || ' ' || last_name) ILIKE '%' || $1 || '%'
 OR full_name ILIKE '%' || $1 || '%'
-OR license_number ILIKE '%' || $1 || '%'
+OR license_number ILIKE '%' || $1 || '%')
 
 AND status = 'Active'
 
