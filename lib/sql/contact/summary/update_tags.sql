2ee4b25fe1d6d394461e7e9e34b89a15dcd16995
dreamcareer7 <dreamcareer070@gmail.com>
2022-03-25 00:52:04 +0430

    fix(contact): fix for contacts with no tags
    for #1855

--- lib/sql/contact/summary/update_tags.sql
+++ lib/sql/contact/summary/update_tags.sql
@@ -1,17 +1,23 @@
 WITH ctags AS MATERIALIZED (
   SELECT
-    ca.contact AS id,
-    array_agg(ca.text ORDER BY ca.text) AS tag
+    c.id,
+    array_agg(ca.text ORDER BY ca.text) FILTER (WHERE ca.text IS NOT NULL) AS tag
   FROM
     contacts AS c
-    LEFT JOIN contacts_attributes_text AS ca ON c.id = ca.contact
+    LEFT JOIN (
+      SELECT
+        *
+      FROM
+        contacts_attributes_text
+      WHERE
+        deleted_at IS NULL
+        AND attribute_type = 'tag'
+    ) AS ca ON c.id = ca.contact
   WHERE
-    ca.deleted_at IS NULL
-    AND c.deleted_at IS NULL
+    c.deleted_at IS NULL
     AND c.brand = $1::uuid
-    AND ca.attribute_type = 'tag'
   GROUP BY
-    ca.contact
+    c.id
   HAVING
     min(c.tag) IS DISTINCT FROM array_agg(ca.text ORDER BY ca.text)
 )
