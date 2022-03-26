adcd39f8fb1021fd44f78ec4d51275a86a19e861
dreamcareer7 <dreamcareer070@gmail.com>
2022-03-26 02:01:05 -0700

    Big one, for #2148.
    So, basically, we want to add task-level access controls. So task.acl is added. This is easy.
    But then, these acl are based on _roles_. This is where it gets complicated. Current roles are
    `BackOffice` and `Agents`. That means every time we are returning a set of tasks, we need to know
    if the user accessing them is a BackOffice user or an Agent. But this is not as easy as you may
    think. There's levels and levels of access here.
    
    Also, the `BackOffice` and `Agents` are only for now. We are thinking of extending this to client portals,
    offer portals, etc.
    
    So, how do we handlde this complexity in different parts of the app?
    
    The current solution creates a table-returning function: deals_acl(user)
    This would return a table with following columns: deal, brand, acl
    
    Basically, given a user id, it would return a list of all deals the user has access to with their:
    
    * deal id
    * brand id in which they have access to that deal through
    * acl type
    
    This function is then used in 4 places:
    
    `Deal.limitAccess()`, which is used through js to make sure a user wont access a deal he doesn't have access to
    
    Deal.filter() which instead of having a separate logic for deal acl's reused the function and logic (it used to copy the code)
    
    Deal.getBrandDeals through deal/brand.sql which loads a list of deals for a brand
    
    Task.getAll() through task/get.sql, which ensure when we return a set of tasks, only the ones the user has access to
    are returned to him.
    
    Now, up until now, first 3 items had separate implementations of the same logic. They copied code.
    Now, thanks to the new table returning function, all 3 of them share the logic and code. Great.
    
    The bad news? It has a bit of a performance impact.
    The function, although is inlined, uses `brand_children()`, which is somewhat expensive. Not sure how I can eliminate that.
    
    This new function, deals_acl, calls user_brands() 4 times. user_brands() calls brand_children(). So brand_children() ends up being called 4 times.
    The previous code was calling brand_children() twice already. So the impact is not going to be huge.
    Also note that the function takes only $25ms on our biggest brand with ~9000 sub brands so far. So the impact is really minimal.
    
    Last note. This is sensitive stuff. Probably requires some serious testing.

--- lib/sql/deal/brand.sql
+++ lib/sql/deal/brand.sql
@@ -1,30 +1,6 @@
-SELECT      DISTINCT deals.id, deals.updated_at
-FROM        deals
-LEFT JOIN   deals_roles      ON deals.id = deals_roles.deal
-LEFT JOIN   deals_checklists ON deals_roles.checklist = deals_checklists.id
-WHERE
-  (
-    deals.brand              IN(SELECT brand_children($1))
-    OR (
-          deals_roles.brand      IN(SELECT brand_children($1))
-      AND deals_roles.deleted_at          IS NULL
-      AND deals_checklists.deactivated_at IS NULL
-      AND deals_checklists.terminated_at  IS NULL
-      AND ( -- This part prevents double ended deals from seeing each other
-        (
-          deals.deal_type = 'Selling' AND
-          NOT (deals_roles.role IN('BuyerAgent', 'CoBuyerAgent'))
-        )
-        OR
-        (
-          deals.deal_type = 'Buying'  AND
-          NOT (deals_roles.role IN ('SellerAgent'::deal_role, 'CoSellerAgent'::deal_role))
-        )
-      )
-    )
-  )
-  AND deals.deleted_at       IS NULL
-
-
+SELECT   DISTINCT deals.id, deals.updated_at
+FROM     deals
+JOIN     deals_acl($1) acl ON deals.id = acl.deal
+WHERE    deals.deleted_at IS NULL
 ORDER BY deals.updated_at DESC
 LIMIT $2
