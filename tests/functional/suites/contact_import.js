3fb75b230c9e2b11c4e7d852d9026d9bccba947d
dreamcareer7 <dreamcareer070@gmail.com>
2022-04-15 10:13:45 +0300

    test(contacts): add tests for xls as well as xlsx files

--- tests/functional/suites/contact_import.js
+++ tests/functional/suites/contact_import.js
@@ -1,3 +1,4 @@
+const xlsx = require('xlsx')
 const path = require('path')
 const fs = require('fs')
 const mappings = require('./data/csv_mappings')
@@ -6,57 +7,115 @@ registerSuite('contact', [
   'getAttributeDefs'
 ])
 
-function uploadCSV(cb) {
-  const csv = fs.createReadStream(path.resolve(__dirname, 'data/contacts.csv'))
+const CSV_PATH = path.resolve(__dirname, 'data', 'contacts.csv')
+const CSV_ROWS = 192
 
-  return frisby.create('upload a CSV file')
-    .post('/contacts/upload', {
-      file: csv
-    },
-    {
-      json: false,
-      form: true
-    })
-    .addHeader('content-type', 'multipart/form-data')
-    .after((err, res, body) => {
-      cb(err, {...res, body: JSON.parse(body)}, body)
-    })
-    .expectStatus(200)
-    .expectJSON({
-      code: 'OK'
-    })
+const R = () => results
+const F = frisby
+
+const resolve = x => typeof x === 'function' ? x() : x
+const the = {
+  userId: () => R().authorize.token.data.id,
+  csvFileId: () => R().contact_import.uploadCSV.data.id,
+  xlsFileId: () => R().contact_import.uploadXls.data.id,
+  xlsxFileId: () => R().contact_import.uploadXlsx.data.id,
 }
 
-function importCSV(cb) {
-  const data = {
-    file_id: results.contact_import.uploadCSV.data.id,
-    owner: results.authorize.token.data.id,
-    mappings
-  }
-  
-  return frisby.create('import contacts from CSV file')
-    .post('/contacts/import.csv', data)
+/**
+ * @param {xlsx.BookType} bookType
+ * @returns {import('stream').Readable}
+ */
+function createWorkbook (bookType) {
+  const wb = xlsx.readFile(CSV_PATH)
+  const buff = xlsx.write(wb, { type: 'buffer', bookType })
+
+  // XXX Why this is not working?! maybe Frisby does not support Readable Streams
+  // return require('stream').Readable.from(buff)
+
+  const fileName = `rechat-test-contact-import.${bookType}`
+  const filePath = path.resolve(require('os').tmpdir(), fileName)
+  fs.writeFileSync(filePath, buff)
+  return fs.createReadStream(filePath)
+}
+
+/**
+ * @param {import('stream').Readable} readable 
+ * @param {string} name 
+ */
+function upload (readable, name) {
+  return cb => F
+    .create(name)
+    .post('/contacts/upload', { file: readable }, { json: false, form: true })
+    .addHeader('content-type', 'multipart/form-data')
+    .after((err, res, body) => cb(err, {...res, body: JSON.parse(body)}, body))
+    .expectStatus(200)
+    .expectJSON({ code: 'OK' })
+}
+
+/**
+ * @param {object} opts 
+ * @param {UUID | (() => UUID)} opts.fileId
+ * @param {UUID | (() => UUID)} opts.ownerId
+ * @param {string | (() => string)} opts.ext
+ * @param {string} opts.name
+ */
+function importFile ({ fileId, ownerId, ext, name }) {
+  return cb => F
+    .create(name)
+    .post(`/contacts/import.${resolve(ext)}`, {
+      file_id: resolve(fileId),
+      owner: resolve(ownerId),
+      mappings,
+    })
     .after(cb)
     .expectStatus(200)
 }
 
-function getContacts(cb) {
-  return frisby.create('check if contacts all contacts are imported')
+/**
+ * @param {number} expectedTotal
+ * @param {string=} [name]
+ */
+function checkContacts(
+  expectedTotal, 
+  name = `check if ${expectedTotal} contacts are imported`,
+) {
+  return cb => F
+    .create(name)
     .get('/contacts?limit=1')
-    .after((err, res, body) => {
-      
-      cb(err, res, body)
-    })
     .expectJSON({
       info: {
-        total: 192
+        total: expectedTotal
       }
     })
+    .after(cb)
     .expectStatus(200)
 }
 
 module.exports = {
-  uploadCSV,
-  importCSV,
-  getContacts
+  uploadCSV: upload(fs.createReadStream(CSV_PATH), 'upload a CSV file'),
+  importCSV: importFile({
+    name: 'import contacts from CSV file',
+    fileId: the.csvFileId, 
+    ownerId: the.userId, 
+    ext: 'csv', 
+  }),
+  getContacts: checkContacts(CSV_ROWS),
+
+  uploadXls: upload(createWorkbook('xls'), 'upload a XLS file'),
+  importXls: importFile({
+    name: 'import contacts from XLS file',
+    fileId: the.xlsFileId,
+    ownerId: the.userId,
+    ext: 'xls',
+  }),
+  getContactsXls: checkContacts(CSV_ROWS * 2),
+
+  uploadXlsx: upload(createWorkbook('xlsx'), 'upload a XLSX file'),
+  importXlsx: importFile({
+    name: 'import contacts from XLSX file',
+    fileId: the.xlsxFileId,
+    ownerId: the.userId,
+    ext: 'xlsx',
+  }),
+  getContactsXlsx: checkContacts(CSV_ROWS * 3),
 }
