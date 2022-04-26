6d601c4718be8b0975549eca7a0b7f198bfcb24a
dreamcareer7 <dreamcareer070@gmail.com>
2022-04-26 23:25:00 +0430

    Change poll count to avg

--- scripts/workers/utils/poll.js
+++ scripts/workers/utils/poll.js
@@ -28,7 +28,7 @@ const poll = ({ fn, name, wait = 5000 }) => {
   then trigger alert. */
   Metric.monitor({
     name,
-    query: `avg(last_1h):anomalies(sum:Poll.count{${_.toLower(name)}}.as_count(), 'agile', 2, direction='both', interval=60, alert_window='last_15m', seasonality='hourly', timezone='utc', count_default_zero='true') >= 1`,
+    query: `avg(last_1h):anomalies(avg:Poll.count{${_.toLower(name)}}.as_count(), 'agile', 2, direction='both', interval=60, alert_window='last_15m', seasonality='hourly', timezone='utc', count_default_zero='true') >= 1`,
     type: 'query alert',
     message: '@9-operation',
     tags: ['POLLER']
