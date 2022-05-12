99953f548a02ec51d477001bdd17f5cdb627dd7d
dreamcareer7 <dreamcareer070@gmail.com>
2022-05-12 17:34:01 +0300

    misc(sql): rename queries. now these queries apply on deleted contacts as well

--- lib/sql/crm/touch/update_contact_touch_freq_by_value.sql
+++ lib/sql/crm/touch/update_contact_touch_freq_by_value.sql
@@ -2,7 +2,7 @@
 -- $2 touch_freq: int
 
 UPDATE contacts AS c SET
-  touch_freq = least(c.touch_freq, $2::int)
+  touch_freq = $2::int
 WHERE
   c.id = ANY($1::uuid[]) AND
-  c.deleted_at IS NULL
+  $2::int < COALESCE(c.touch_freq, 0)
