8b1801ab7046f11db1571039697d948dd4a11157
dreamcareer7 <dreamcareer070@gmail.com>
2022-05-20 12:04:20 +0300

    misc(crm): we dont need update_contact_touch_freq_by_value anymore

--- lib/sql/crm/touch/update_contact_touch_freq_by_value.sql
+++
@@ -1,8 +0,0 @@
--- $1 contact_ids: uuid[]
--- $2 touch_freq: int
-
-UPDATE contacts AS c SET
-  touch_freq = $2::int
-WHERE
-  c.id = ANY($1::uuid[]) AND
-  (c.touch_freq IS NULL OR c.touch_freq > $2::int)
