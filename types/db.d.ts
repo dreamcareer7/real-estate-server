bf6bd10bec1ad0fc6550e0d287191f7fbb15cd6d
dreamcareer7 <dreamcareer070@gmail.com>
2022-06-09 16:13:25 +0300

    ref(showing): send email to non-delivered (by email) notifications

--- types/db.d.ts
+++ types/db.d.ts
@@ -645,6 +645,7 @@ declare type TDbSqlAddress =
   | 'notification/tokens/delete'
   | 'notification/unread'
   | 'notification/unread_over_time'
+  | 'notification/non_delivered'
   | 'notification/unread_room'
   | 'notification/unregister_push'
   | 'notification/user'
