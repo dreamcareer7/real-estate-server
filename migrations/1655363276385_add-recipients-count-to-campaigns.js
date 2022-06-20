4866e36ee74519b228725f1a42cf6a5b52114e5a
dreamcareer7 <dreamcareer070@gmail.com>
2022-06-20 17:20:29 +0300

    ref: a script to calculate recipients_count
    Refs web#6491

--- migrations/1655363276385_add-recipients-count-to-campaigns.js
+++ migrations/1655363276385_add-recipients-count-to-campaigns.js
@@ -2,26 +2,8 @@ const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
-
   `ALTER TABLE email_campaigns
      ADD COLUMN recipients_count int`,
-
-  `WITH recipients_counts AS (
-     SELECT
-       ecre.campaign,
-       count(DISTINCT lower(trim(ecre.email))) AS recipients_count
-     FROM email_campaigns AS ec
-     JOIN email_campaigns_recipient_emails AS ecre ON ecre.campaign = ec.id
-     WHERE
-       ecre.email IS NOT NULL AND
-       ec.executed_at IS NOT NULL
-     GROUP BY ecre.campaign
-   )
-   UPDATE email_campaigns AS ec SET
-     recipients_count = rc.recipients_count
-   FROM recipients_counts AS rc
-   WHERE rc.campaign = ec.id`,
-
   'COMMIT',
 ]
 
---
+++ scripts/email/calc-recipients-count.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env/node
+
+const sql = require('../../lib/utils/sql')
+
+const query = `
+  WITH
+  chunk AS (
+    SELECT * FROM email_campaigns
+    WHERE executed_at IS NOT NULL
+    LIMIT $1::int
+    FOR UPDATE SKIP LOCKED
+  ),
+  recipients_counts AS (
+    SELECT
+      ecre.campaign,
+      count(DISTINCT lower(trim(ecre.email))) AS recipients_count
+    FROM chunk AS ch
+    JOIN email_campaigns_recipient_emails AS ecre ON ecre.campaign = ch.id
+    WHERE ecre.email IS NOT NULL
+    GROUP BY ecre.campaign
+  )
+  UPDATE email_campaigns AS ec SET
+    recipients_count = rc.recipients_count
+  FROM recipients_counts AS rc
+  WHERE rc.campaign = ec.id
+`
+
+async function calcRecipientsCount ({ chunkSize = 1000 } = {}) {
+  return sql.update(query, [chunkSize])
+}
+
+if (process.env.NODE_ENV !== 'tests') {
+  const { runInContext } = require('../../lib/models/Context/util')
+
+  runInContext(
+    `calc-recipients-count-${new Date().toISOString()}`,
+    calcRecipientsCount
+  ).catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
+}
+
+module.exports = { calcRecipientsCount }
