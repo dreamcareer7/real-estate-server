33dc9c3e0485c6a877fda67f46a1ecb755dbac38
dreamcareer7 <dreamcareer070@gmail.com>
2022-06-28 17:20:25 +0300

    feat: add Zillow interface

--- lib/models/LeadChannel/types.d.ts
+++ lib/models/LeadChannel/types.d.ts
@@ -1,8 +1,4 @@
-
-export type LeadSourceType =
-  | 'Zillow'
-  | 'Realtor'
-  ;
+export type LeadSourceType = 'Zillow' | 'Realtor'
 
 export interface LeadChannelInput {
   sourceType: LeadSourceType
@@ -30,4 +26,81 @@ export interface Filter {
 export interface FilterResult {
   ids: UUID[]
   total: number
+}
+
+export interface Zillow {
+  Source: string
+  ContactMessage: {
+    ContactInfo: {
+      PartnerAgentIdentifier: string
+      ZillowAgentID: string
+      ZillowAgentProfileURL: string
+      ClientGUID: string
+      ContactName: string
+      ContactPhone: string
+      ContactEmail: string
+      ContactMessage: string
+      ContactDateTime: string
+      ContactUrl: string
+      ZillowAgentWebsiteURL: string
+      Brokerage: string
+      CommunicationGUID: string
+      Update: boolean
+      ContactType: string
+      LeadDateTime: string
+      Reassignment: boolean
+      IsConnected: boolean
+      Assignee: {
+        ZillowAssigneeAgentID: string
+        ZillowAssigneeAgentProfileURL: string
+        ZillowAssigneeAgentWebsiteURL: string
+        ReassignmentDateTime: string
+        ZillowAssigneeAgentPhone: string
+        ZillowAssigneeAgentEmail: string
+        ZillowAssigneeAgentName: string
+        ZillowAssigneePartnerAgentIdentifier: string
+      }
+    }
+    SearchCriteria: {
+      City: string
+      State: string
+      ZIP: string
+      MinBeds: string
+      MinBaths: string
+      MinPrice: string
+      MaxPrice: string
+      MinSqFt: string
+      MaxSqFt: string
+      MinLotSize: string
+      MaxLotSize: string
+      MinYearBuilt: string
+      MaxYearBuilt: string
+      PropertyTypes: string
+    }
+    PropertyInfo: {
+      ZillowPropertyURL: string
+      MLSNumber: string
+      StreetAddress: string
+      City: string
+      State: string
+      Zip: string
+      Beds: string
+      Baths: string
+      ListingPrice: string
+      ListingStatus: string
+      PhotoURL: string
+    }
+  }
+}
+
+export interface Zillow_Sns{
+  Message: Zillow
+  Type: string
+  MessageId: string
+  TopicArn: string
+  Timestamp: string
+  SignatureVersion: string
+  Signature: string
+  SigningCertURL: string
+  UnsubscribeURL: string
 }
