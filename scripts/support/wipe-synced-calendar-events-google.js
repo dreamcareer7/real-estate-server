254462a4e498b5ecbdd024ce51838e46cf3a1e54
dreamcareer7 <dreamcareer070@gmail.com>
2022-07-01 17:05:17 -0700

    ESLINT

--- scripts/support/wipe-synced-calendar-events-google.js
+++ scripts/support/wipe-synced-calendar-events-google.js
@@ -1,10 +1,10 @@
 const { runInContext } = require('../../lib/models/Context/util')
-const GoogleCredential = require('../../lib/models/Google/credential/get');
-const GoogleCalendar = require('../../lib/models/Google/calendar/get');
-const { deleteLocalCalendars } = require('../../lib/models/Google/workers/calendars/calendar');
+const GoogleCredential = require('../../lib/models/Google/credential/get')
+const GoogleCalendar = require('../../lib/models/Google/calendar/get')
+const { deleteLocalCalendars } = require('../../lib/models/Google/workers/calendars/calendar')
 
 runInContext('wipe-synced-calendar-events', async function() {
-  const cred = await GoogleCredential.get('6476e1af-2c84-4345-ba14-601644ec0862');
+  const cred = await GoogleCredential.get('6476e1af-2c84-4345-ba14-601644ec0862')
   const calendars = await GoogleCalendar.getAllByGoogleCredential(cred.id)
 
   await deleteLocalCalendars(cred, calendars)
