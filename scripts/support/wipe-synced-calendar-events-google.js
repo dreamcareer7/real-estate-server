76d4cd7958aa50791e48efb59ca5a78f8411c522
dreamcareer7 <dreamcareer070@gmail.com>
2022-07-06 21:32:42 -0400

    a couple of polishes on calendar reset job

--- scripts/support/wipe-synced-calendar-events-google.js
+++ scripts/support/wipe-synced-calendar-events-google.js
@@ -3,9 +3,19 @@ const GoogleCredential = require('../../lib/models/Google/credential/get');
 const GoogleCalendar = require('../../lib/models/Google/calendar/get');
 const { deleteLocalCalendars } = require('../../lib/models/Google/workers/calendars/calendar');
 
-runInContext('wipe-synced-calendar-events', async function() {
-  const cred = await GoogleCredential.get('6476e1af-2c84-4345-ba14-601644ec0862');
+/**
+ * @param {import('commander').Command} program 
+ */
+async function main(program) {
+  const options = program.opts()
+  const cred = await GoogleCredential.get(options.credential);
   const calendars = await GoogleCalendar.getAllByGoogleCredential(cred.id)
+  console.log(calendars.map(c => c.id))
 
   await deleteLocalCalendars(cred, calendars)
+}
+
+runInContext('wipe-synced-calendar-events', main, program => {
+  program
+    .option('-c, --credential <credential>', 'google credential id')
 }).catch(ex => console.error(ex))
