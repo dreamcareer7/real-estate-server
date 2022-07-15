f53e827a17a8a11e60fd7cf02bb32149906f117f
dreamcareer7 <dreamcareer070@gmail.com>
2022-07-16 00:23:27 +0300

    feat(contact): select brands of assigned contacts, too

--- lib/sql/contact/authorized_brands.sql
+++ lib/sql/contact/authorized_brands.sql
@@ -5,3 +5,14 @@ FROM
   JOIN contacts USING (id)
 WHERE
   deleted_at IS NULL
+
+UNION
+
+SELECT DISTINCT
+  brand As id
+FROM
+  contacts_roles
+WHERE
+  deleted_at IS NULL AND
+  role IN ('assignee', 'owner') AND
+  contact = ANY($1::uuid[])
