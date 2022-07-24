ee9488968a54f13aed9f54620ef011903dd6b613
dreamcareer7 <dreamcareer070@gmail.com>
2022-07-24 12:03:46 -0700

    Out agent/get queries now make index-only calls to agents_phones and agents_emails tables which makes them orders of magnitude faster. Getting ~8k agents goes from ~14seconds to .5s

---
+++ migrations/1658689097004_better-indexes.js
@@ -0,0 +1,29 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  'DROP INDEX CONCURRENTLY agents_phones_agent',
+  'DROP INDEX CONCURRENTLY agents_emails_agent',
+
+  'CREATE INDEX CONCURRENTLY agents_phones_agent ON agents_phones(mui, mls, phone)',
+  'CREATE INDEX CONCURRENTLY agents_emails_agent ON agents_emails(mui, mls, email)',
+
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
