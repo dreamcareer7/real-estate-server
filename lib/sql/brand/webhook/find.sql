169d912146a810ff8eb1fc76db3c3c0530bcdc63
dreamcareer7 <dreamcareer070@gmail.com>
2022-07-28 10:48:40 -0400

    contact: fix webhook/find query
    for #2327

--- lib/sql/brand/webhook/find.sql
+++ lib/sql/brand/webhook/find.sql
@@ -2,7 +2,7 @@ SELECT
   id
 FROM
   brands_webhooks
-  JOIN unnest($1::uuid[]) AS brands(brand) USING (brand)
+  JOIN unnest($1::uuid[]) AS b(brand) USING (brand)
 WHERE 
   topic = $2
   deleted_at IS NULL
