5a28f7ea2ce759f99f5fed1621054f1be22d898b
dreamcareer7 <dreamcareer070@gmail.com>
2022-08-01 15:13:58 -0400

    contact: don't update last touch on bulk tag

---
+++ lib/sql/crm/touch/update_next_touch_for_contacts.sql
@@ -0,0 +1,23 @@
+UPDATE
+  contacts
+SET
+  next_touch = nt.next_touch
+FROM
+  (
+    SELECT
+      cids.id AS contact,
+      MIN(COALESCE(last_touch, NOW()) + (touch_freq || ' days')::interval) AS next_touch
+    FROM
+      unnest($1::uuid[]) AS cids(id)
+      JOIN contacts AS c
+        ON cids.id = c.id
+      JOIN get_contact_touch_freqs($1::uuid[]) AS tf
+        ON cids.id = tf.id
+    GROUP BY
+      cids.id
+  ) AS nt
+WHERE
+  contacts.id = nt.contact
+  AND contacts.next_touch IS DISTINCT FROM nt.next_touch
+RETURNING
+  contacts.id
