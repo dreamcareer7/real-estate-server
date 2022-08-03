687728b1509c397e88d1960bdad6887718d2b96a
dreamcareer7 <dreamcareer070@gmail.com>
2022-08-03 22:16:23 +0300

    feat: add constraint for theme #2357

---
+++ migrations/1659550619999_add-color-constraint-to-brand-setting.js
@@ -0,0 +1,91 @@
+const db = require('../lib/utils/db')
+
+const colorKeys = [
+  'primary-main',
+  'primary-light',
+  'primary-dark',
+  'primary-contrast-text',
+  'secondary-main',
+  'secondary-light',
+  'secondary-dark',
+  'secondary-contrast-text',
+  'error-main',
+  'error-light',
+  'error-dark',
+  'error-contrast-text',
+  'warning-main',
+  'warning-light',
+  'warning-dark',
+  'warning-contrast-text',
+  'info-main',
+  'info-light',
+  'info-dark',
+  'info-contrast-text',
+  'success-main',
+  'success-light',
+  'success-dark',
+  'success-contrast-text',
+  'navbar-background',
+  'navbar-contrast-text',
+  'navbar-button-main',
+  'navbar-button-light',
+  'navbar-button-dark',
+  'navbar-button-contrast-text',
+  'common-black',
+  'common-white',
+  'background-level1',
+  'background-level2',
+  'background-default',
+  'background-paper',
+  'divider',
+  'text-hint',
+  'text-disabled',
+  'text-secondary',
+  'text-primary',
+]
+
+const generateMigration = () => {
+  let str = ''
+  let split = ''
+  for (let i = 0; i < colorKeys.length; i++) {
+    const color = colorKeys[i]
+    str += ` ${split} (
+      (theme)."${color}" ~* '^rgba\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)$'
+      or
+      (theme)."${color}" is null
+      or 
+      (theme)."${color}" = ''
+    )`
+    split = 'and'
+  }
+  return str
+}
+
+const constraintQuery = `
+  alter table brand_settings add constraint brand_settings_theme check (
+    ${generateMigration()}
+  )
+`
+console.log(constraintQuery)
+const migrations = [
+  'BEGIN',
+  constraintQuery,
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+  
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
