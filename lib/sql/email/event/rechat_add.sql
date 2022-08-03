4b5f5a4c0d4bb45d666910ef35dc167675c88ad2
dreamcareer7 <dreamcareer070@gmail.com>
2022-08-03 01:31:27 -0700

    Revert "Sorry"
    This reverts commit 3f826f278a85e5477f896a6e14215445eb99b2c2.

--- lib/sql/email/event/rechat_add.sql
+++ lib/sql/email/event/rechat_add.sql
@@ -1,5 +1,5 @@
 WITH inserted AS (
-  INSERT INTO emails_events (email, event, created_at, occured_at, recipient, url, ip, client_os, client_type, device_type, location)
+  INSERT INTO emails_events (email, event, created_at, occured_at, recipient, url, ip, client_os, client_type, device_type, location, campaign)
   VALUES
   (
     (
@@ -14,7 +14,10 @@ WITH inserted AS (
     $7,
     $8,
     $9,
-    $10
+    $10,
+    (
+      SELECT emails.campaign FROM emails WHERE id = $1::uuid
+    )
   )
   RETURNING id, created_at
 ),
