45ab9a4f65bac8946a39cd8d83e1dcfda178edbb
dreamcareer7 <dreamcareer070@gmail.com>
2022-08-30 17:46:58 +0300

    feat: resolve assets category for marketing center

--- lib/sql/template/brand/category.sql
+++ lib/sql/template/brand/category.sql
@@ -1,12 +1,24 @@
 SELECT   template_type,
          medium
-FROM     brands_allowed_templates bat
-JOIN     templates
-ON       bat.template = templates.id
-WHERE    bat.brand IN
-         (
-                SELECT Brand_parents(brand_ids)
-                FROM   Unnest($1::uuid[]) brand_ids)
-AND      deleted_at IS NULL
+FROM     (
+                SELECT template_type,
+                       medium
+                FROM   brands_allowed_templates bat
+                JOIN   templates
+                ON     bat.template = templates.id
+                WHERE  bat.brand IN
+                       (
+                              SELECT Brand_parents(brand_ids)
+                              FROM   unnest($1::uuid[]) brand_ids)
+                AND    deleted_at IS NULL
+                UNION ALL
+                SELECT template_type,
+                       medium
+                FROM   brands_assets
+                WHERE  brand IN
+                       (
+                              SELECT brand_parents(brand_ids)
+                              FROM   unnest($1::uuid[]) brand_ids)
+                AND    deleted_at IS NULL) AS data
 GROUP BY template_type,
-         medium
+         medium         
