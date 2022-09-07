b85b94fb48b1e15a125f866cdbd5d1b51158e063
dreamcareer7 <dreamcareer070@gmail.com>
2022-09-07 22:38:38 +0300

    ref(contact): improve performance of contact filter
    Refs #2280

--- lib/sql/contact/functions/delete_access.fn.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION public.check_contact_delete_access(contact contacts, brand uuid)
-  RETURNS boolean
-  LANGUAGE sql
-  IMMUTABLE AS $$
-    SELECT contact.brand = brand
-  $$
--- lib/sql/contact/has_access.sql
+++ lib/sql/contact/has_access.sql
@@ -1,11 +1,24 @@
+WITH related_contacts_roles AS (
+  SELECT *
+  FROM all_contacts_roles AS cr
+  WHERE
+    cr.role_user = $3::uuid OR (
+      cr.role_brand = $2::uuid AND
+      cr.role_type = 'owner'::contact_role
+    )
+)
 SELECT
-  id,
-  check_contact_read_access(contacts, $2, $3) AS "read",
-  check_contact_write_access(contacts, $2, $3) AS "write",
-  check_contact_delete_access(contacts, $2) AS "delete"
+  c.id,
+  bool_or(cr.role_contact IS NOT NULL) as "read",
+  bool_or(cr.role_contact IS NOT NULL) as "write",
+  bool_or(cr.role_type IS NOT DISTINCT FROM 'owner') AS "delete"
 FROM
-  contacts
+  contacts AS c
 JOIN
-  unnest($1::uuid[]) WITH ORDINALITY t(did, ord) ON contacts.id = did
+  unnest($1::uuid[]) WITH ORDINALITY t(did, ord) ON c.id = did
+LEFT JOIN
+  related_contacts_roles AS cr ON cr.role_contact = c.id
 WHERE
-  deleted_at IS NULL
+  c.deleted_at IS NULL
+GROUP BY
+  c.id
