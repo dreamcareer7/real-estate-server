5154422723f15cb097ca4313309e43aa07afa4af
dreamcareer7 <dreamcareer070@gmail.com>
2022-09-12 21:57:08 +0300

    refactor: improve get template categories performance

--- lib/sql/template/brand/category.sql
+++ lib/sql/template/brand/category.sql
@@ -1,4 +1,5 @@
-SELECT   template_type,
+SELECT   distinct 
+         template_type,
          medium
 FROM     (
                 SELECT template_type,
@@ -6,27 +7,22 @@ FROM     (
                 FROM   brands_allowed_templates bat
                 JOIN   templates
                 ON     bat.template = templates.id
-                WHERE  '1' = $4 
-                AND
-                bat.brand IN
-                       (
-                              SELECT Brand_parents(brand_ids)
-                              FROM   unnest($1::uuid[]) brand_ids)
-                AND ($2::template_type[]   IS NULL OR $2 @> ARRAY[templates.template_type])
-                AND ($3::template_medium[] IS NULL OR $3 @> ARRAY[templates.medium])
-                AND    deleted_at IS NULL
+                JOIN Brand_parents($1::uuid) AS bp(id)
+                ON bat.brand = bp.id
+                WHERE  
+                '1' = $4 
+                AND ($2::template_type[] IS NULL OR templates.template_type = ANY($2::template_type[]))
+                AND ($3::template_medium[] IS NULL OR templates.medium = ANY($3::template_medium[]))
+                AND deleted_at IS NULL
                 UNION ALL
                 SELECT template_type,
                        medium
                 FROM   brands_assets
-                WHERE  '1' = $5
-                AND 
-                brand IN
-                       (
-                              SELECT brand_parents(brand_ids)
-                              FROM   unnest($1::uuid[]) brand_ids)
-                AND ($2::template_type[]   IS NULL OR $2 @> ARRAY[brands_assets.template_type])
-                AND ($3::template_medium[] IS NULL OR $3 @> ARRAY[brands_assets.medium])                              
-                AND    deleted_at IS NULL) AS data
-GROUP BY template_type,
-         medium         
+                JOIN Brand_parents($1::uuid) AS bp(id)
+                ON brands_assets.brand = bp.id
+                WHERE  
+                '1' = $5
+                AND ($2::template_type[] IS NULL OR brands_assets.template_type = ANY($2::template_type[]))
+                AND ($3::template_medium[] IS NULL OR brands_assets.medium = ANY($3::template_medium[]))                         
+                AND  deleted_at IS NULL
+       ) AS data
