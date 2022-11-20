28fcc52c2ea3cc542cb0fd622a72f86b9a1f14f1
dreamcareer7 <dreamcareer070@gmail.com>
2022-11-20 05:49:08 -0800

    Remove duplicated hostnames, make sure it wont happen again

---
+++ migrations/1668951530525_unique-hostnames.js
@@ -0,0 +1,27 @@
+const db = require('../lib/utils/db')
+
+const migrations = [
+  'BEGIN',
+  `WITH w AS (SELECT LOWER(wh.hostname), uuid_timestamp(wh.id), wh.id FROM websites_hostnames wh  WHERE LOWER(wh.hostname) IN(SELECT LOWER(hostname)  FROM websites_hostnames GROUP BY 1 HAVING count(*) > 1) ORDER BY 1,2), w2 AS(SELECT ROW_NUMBER() OVER(), * FROM w) DELETE FROM websites_hostnames WHERE id IN(SELECT id FROM w2 WHERE row_number %2 = 0) RETURNING *, uuid_timestamp(id)`,
+  'DROP INDEX websites_hostnames_hostname',
+  'CREATE UNIQUE INDEX websites_hostnames_hostname_unique ON websites_hostnames (lower(hostname))',
+  'ALTER TABLE websites_hostnames DROP CONSTRAINT websites_hostnames_key',
+  'COMMIT'
+]
+
+
+const run = async () => {
+  const { conn } = await db.conn.promise()
+
+  for(const sql of migrations) {
+    await conn.query(sql)
+  }
+
+  conn.release()
+}
+
+exports.up = cb => {
+  run().then(cb).catch(cb)
+}
+
+exports.down = () => {}
