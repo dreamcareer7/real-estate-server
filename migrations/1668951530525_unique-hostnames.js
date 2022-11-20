124ddd77b9ed873cbc21e4795a33e0209ead023b
dreamcareer7 <dreamcareer070@gmail.com>
2022-11-20 05:55:52 -0800

    ESLINT

--- migrations/1668951530525_unique-hostnames.js
+++ migrations/1668951530525_unique-hostnames.js
@@ -2,7 +2,7 @@ const db = require('../lib/utils/db')
 
 const migrations = [
   'BEGIN',
-  `WITH w AS (SELECT LOWER(wh.hostname), uuid_timestamp(wh.id), wh.id FROM websites_hostnames wh  WHERE LOWER(wh.hostname) IN(SELECT LOWER(hostname)  FROM websites_hostnames GROUP BY 1 HAVING count(*) > 1) ORDER BY 1,2), w2 AS(SELECT ROW_NUMBER() OVER(), * FROM w) DELETE FROM websites_hostnames WHERE id IN(SELECT id FROM w2 WHERE row_number %2 = 0) RETURNING *, uuid_timestamp(id)`,
+  'WITH w AS (SELECT LOWER(wh.hostname), uuid_timestamp(wh.id), wh.id FROM websites_hostnames wh  WHERE LOWER(wh.hostname) IN(SELECT LOWER(hostname)  FROM websites_hostnames GROUP BY 1 HAVING count(*) > 1) ORDER BY 1,2), w2 AS(SELECT ROW_NUMBER() OVER(), * FROM w) DELETE FROM websites_hostnames WHERE id IN(SELECT id FROM w2 WHERE row_number %2 = 0) RETURNING *, uuid_timestamp(id)',
   'DROP INDEX websites_hostnames_hostname',
   'CREATE UNIQUE INDEX websites_hostnames_hostname_unique ON websites_hostnames (lower(hostname))',
   'ALTER TABLE websites_hostnames DROP CONSTRAINT websites_hostnames_key',
