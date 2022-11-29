b3a5dd657910d24f63f566acff7afcd46502a090
dreamcareer7 <dreamcareer070@gmail.com>
2022-11-28 21:57:21 -0800

    Include brand assets in daily email

--- lib/sql/brand/asset/by-brand.sql
+++ lib/sql/brand/asset/by-brand.sql
@@ -1,5 +1,5 @@
 SELECT id FROM brands_assets 
 WHERE deleted_at IS NULL 
-AND brand IN(SELECT brand_parents($1))
+AND brand IN((SELECT brand_parents(brand_ids) FROM UNNEST($1::uuid[]) brand_ids))
 AND ($2::template_type[] IS NULL OR template_type = ANY($2))
 AND ($3::template_medium[] IS NULL OR medium = ANY($3))
